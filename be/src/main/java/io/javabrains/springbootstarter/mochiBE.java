3b87fa29263a8e87bd0d288d091de1e5235ea12e
dmac088 <danielmackie82@gmail.com>
2018-02-02 10:48:29 +0800

    fixed fe build path to src/main/java

--- be/src/main/java/io/javabrains/springbootstarter/mochiBE.java
+++ be/src/main/java/io/javabrains/springbootstarter/mochiBE.java
@@ -1,5 +1,7 @@
 package io.javabrains.springbootstarter;
 
+import io.java.fe.Fe;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class mochiBE {
 
 	public static void main(String[] args) {
-		SpringApplication.run(mochiBE.class, args);
+		SpringApplication.run(Fe.class, args);
 	}
 }
---
+++ fe/src/main/java/io/java/fe/Fe.java
@@ -0,0 +1,9 @@
+package io.java.fe;
+
+public class Fe {
+
+	 public static void main(String[] args) {
+	        // Prints "Hello, World" to the terminal window.
+	        System.out.println("Hello, World");
+	    }
+}
