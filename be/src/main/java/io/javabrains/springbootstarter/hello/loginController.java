46df90df32bfed9486ecf6185959d58bf8fdd6dd
dmac088 <danielmackie82@gmail.com>
2018-02-04 13:41:48 +0800

    templates and static content now owrking in the back end package

--- be/src/main/java/io/javabrains/springbootstarter/hello/loginController.java
+++ be/src/main/java/io/javabrains/springbootstarter/hello/loginController.java
@@ -1,16 +1,24 @@
 package io.javabrains.springbootstarter.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
  
 @Controller
 public class loginController {
    
     
-    @RequestMapping(value={"/index"})
+    /*@RequestMapping(value={"/greeting"})
     public String index(){
-    	System.out.println("called index");
-        return "index";	
+    	System.out.println("called greeting");
+        return "greeting";	
+    }*/
+    
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
     
     @RequestMapping(value={"/signup"})
@@ -31,12 +39,6 @@ public class loginController {
         return "home";
     }
    
-    @RequestMapping(value={"/test"})
-    public String test(){
-    	System.out.println("called test");
-        return "Pois";
-    }
-    
     @RequestMapping(value="/403")
     public String Error403(){
         return "403";
