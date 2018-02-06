481f3d0e415ec53097086ebade1ed985d3819c2c
dmac088 <danielmackie82@gmail.com>
2018-02-06 15:55:53 +0800

    working webpack and react-native

--- fe/node_modules/react-native-webpack-server/hot/entry.js
+++
@@ -1,4 +0,0 @@
-if (module.hot) {
-  //! Terrible hack to marshall WS messages onto React Native's event loop.
-  setInterval(function() {}, 200);
-}
--- fe/src/main/java/io/javabrains/fe/Fe.java
+++
@@ -1,8 +0,0 @@
-package io.javabrains.fe;
-
-public class Fe {
-	 public static void main(String[] args) {
-	        // Prints "Hello, World" to the terminal window.
-	        System.out.println("Hello, World");
-	    }
-}
