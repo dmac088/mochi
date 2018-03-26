8f50b187604499cc5f3c2be32bdfb7f3816b83ef
dmac088 <danielmackie82@gmail.com>
2018-03-26 22:15:22 +0800

    added data_import.sql to allow creation of test data in the source db

---
+++ be/src/main/resources/database/data_import.sql
@@ -0,0 +1,10 @@
+
+
+delete from mochi.product;
+
+insert into mochi.product(prd_id, prd_desc, prd_img_pth, prd_rrp, prd_cat_desc)
+values (1, 'mango', 'images/mango.jpg', 5.99, 'fruit');
+
+insert into mochi.product(prd_id, prd_desc, prd_img_pth, prd_rrp, prd_cat_desc)
+values (2, 'orange', 'images/orange.jpg', 8.99, 'fruit');
+
