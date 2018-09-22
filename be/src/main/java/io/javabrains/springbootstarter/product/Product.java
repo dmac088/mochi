d2d76b9eb41fff0270dbb5e8ec8f5b9026a66181
dmac088 <danielmackie82@gmail.com>
2018-09-22 16:19:26 +0800

    added additional attributes to Product class

--- be/src/main/java/io/javabrains/springbootstarter/product/Product.java
+++ be/src/main/java/io/javabrains/springbootstarter/product/Product.java
@@ -1,19 +1,17 @@
 package io.javabrains.springbootstarter.product;
 
 import io.javabrains.springbootstarter.product_lcl.ProductLcl;
-import io.javabrains.springbootstarter.role.Role;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
@@ -36,8 +34,30 @@ public class Product {
 	@OneToMany(fetch = FetchType.LAZY,
             mappedBy = "product")
 	private List<ProductLcl> lclAttr = new ArrayList<ProductLcl>();
+
+	@Column(name="upc_cd")
+	private String productUPC;
+	
+	@Column(name="prd_crtd_dt")
+	private Date productCreateDt;
 	
 	
+	public String getProductUPC() {
+		return productUPC;
+	}
+
+	public void setProductUPC(String productUPC) {
+		this.productUPC = productUPC;
+	}
+
+	public Date getProductCreateDt() {
+		return productCreateDt;
+	}
+
+	public void setProductCreateDt(Date productCreateDt) {
+		this.productCreateDt = productCreateDt;
+	}
+
 	public List<ProductLcl> getLclAttr() {
 		return lclAttr;
 	}
