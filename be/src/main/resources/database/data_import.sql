c5a905768192225adb3abee9f921dc0c37f30693
dmac088 <danielmackie82@gmail.com>
2018-09-26 20:56:21 +0800

    added organization to the model

--- be/src/main/resources/database/data_import.sql
+++ be/src/main/resources/database/data_import.sql
@@ -1,14 +1,17 @@
-﻿delete from mochi.customer;
+
+delete from mochi.customer;
 
 delete from mochi.role;
 delete from mochi.role_type;
 
+delete from mochi.organisation;
 delete from mochi.person;
 delete from mochi.party;
 delete from mochi.party_type;
 
 delete from mochi.product_attr_lcl;
 delete from mochi.product;
+
 
 
 INSERT INTO mochi.party_type(
@@ -23,11 +26,17 @@ INSERT INTO mochi.party(
 	pty_id, pty_typ_id, pty_usr_nm, pty_pwd)
 	VALUES (1, 1, 'dmac088', 'password');
 
+INSERT INTO mochi.party(
+	pty_id, pty_typ_id, pty_usr_nm, pty_pwd)
+	VALUES (2, 2, 'bigball', 'password');
+
 INSERT INTO mochi.person(
-	pty_id, psn_gvn_nm_en, psn_fml_nm_en, psn_nm_cn, enb)
+	psn_id, psn_gvn_nm_en, psn_fml_nm_en, psn_nm_cn, enb)
 	VALUES (1, 'Daniel', 'Mackie', '丹尼爾麥基', '1');
-
-
+    
+INSERT INTO mochi.organisation(
+	org_id, org_nme, org_reg_no)
+	VALUES (2, 'Big Balloons LTD', '1234567');    
 
 insert into mochi.product(prd_id, upc_cd, prd_crtd_dt)
 values (1, '12345678', now());
@@ -60,6 +69,14 @@ INSERT INTO mochi.role(
 	rle_id, rle_typ_id, rle_start_dttm, pty_id)
 	VALUES (1, 1, now(), 1);
 
+INSERT INTO mochi.role(
+	rle_id, rle_typ_id, rle_start_dttm, pty_id)
+	VALUES (2, 1, now(), 2);
+
 INSERT INTO mochi.customer(
 	rle_id, cst_id)
 	VALUES (1, '0123456789');
+
+INSERT INTO mochi.customer(
+	rle_id, cst_id)
+	VALUES (2, '9876543210');
