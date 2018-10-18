12fb40cdf1825e99f68148113a0c60eb9acee732
Mackie <Daniel.Mackie@dfs.com>
2018-10-18 18:31:24 +0800

    added security on the Customer class as per OAuth tutorial

--- be/src/main/java/io/javabrains/springbootstarter/domain/CustomerService.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/CustomerService.java
@@ -4,6 +4,7 @@ package io.javabrains.springbootstarter.domain;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,20 +13,30 @@ public class CustomerService {
 	@Autowired
 	private CustomCustomerRepository customerRepository; 
 
+	
 	public List<Customer> getAllCustomers() {
 		return customerRepository.findAll();
 	}
 
+	@PreAuthorize("hasAuthority('CUSTOMER_READ')")
 	public Customer getCustomer(Long CustomerId) {
 		return customerRepository.findOne(CustomerId);
 	}
 	
+	@PreAuthorize("hasAuthority('CUSTOMER_CREATE')")
 	public void addCustomer(Customer customer) {
 		customerRepository.save(customer);
 	}
 	
+	@PreAuthorize("hasAuthority('CUSTOMER_UPDATE')")
 	public void updateCustomer(Long id, Customer customer) {
 		customerRepository.save(customer);
 	}
+	
+	@PreAuthorize("hasAuthority('CUSTOMER_DELETE')")
+	public void deleteCustomer(Long id) {
+		customerRepository.delete(id);
+	}
+	
 	
 }
