0ad286db666f63a4ecc90a5a6ac7f9dc2ce127e8
Mackie <Daniel.Mackie@dfs.com>
2018-10-23 20:46:36 +0800

    getting detached ent

--- be/src/main/java/io/javabrains/springbootstarter/test/UnitTestConfig.java
+++ be/src/main/java/io/javabrains/springbootstarter/test/UnitTestConfig.java
@@ -1,6 +1,8 @@
 package io.javabrains.springbootstarter.test;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.web.client.RestTemplate;
 
 public class UnitTestConfig {
@@ -10,5 +12,5 @@ public class UnitTestConfig {
 		RestTemplate template = new RestTemplate();
 		return template;
     }
-	
+
 }
