a94340de01edd091d0cb89ad015e0156083a626b
Mackie <Daniel.Mackie@dfs.com>
2018-10-25 15:45:43 +0800

    added ssl configuration, but you must generate the cert an install on browser

---
+++ ssl/genkeys.bat
@@ -0,0 +1,2 @@
+"%JAVA_HOME%\bin\keytool" -genkeypair -alias mochi -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore ..\be\src\main\resources\mochi.p12 -validity 3650
+"%JAVA_HOME%\bin\keytool" -list -v -storetype pkcs12 -keystore ..\be\src\main\resources\mochi.p12
