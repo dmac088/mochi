162c3354ce1734f88970adabbe40cc998639db19
Mackie <Daniel.Mackie@dfs.com>
2018-10-27 22:34:13 +0800

    added security annotations to most classes

--- be/src/main/java/io/javabrains/springbootstarter/domain/RoleService.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/RoleService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RoleService {
@@ -14,7 +16,8 @@ public class RoleService {
 	@Autowired
 	private RoleRepository RoleRepository; 
 	
-	
+	@PreAuthorize("hasAuthority('ROLE_READER')")
+	@Transactional(readOnly = true)
 	public List<Role> getAllRoles() {
 		List<Role> Roles = new ArrayList<>();
 		Iterator<Role> i = RoleRepository.findAll().iterator();
@@ -24,30 +27,27 @@ public class RoleService {
 		return Roles;
 	}
 
-	/*
-	public List<Role> getAllRoles(Long roleTypeId) {
-		List<Role> Roles = new ArrayList<>();
-		Iterator<Role> i = RoleRepository.findByRoleTypeID(roleTypeId).iterator();
-		while(i.hasNext()) {
-			Roles.add(i.next());
-		}
-		return Roles;
-	}
-	*/
-	
+	@PreAuthorize("hasAuthority('ROLE_READER')")
+	@Transactional(readOnly = true)
 	public Optional<Role> getRole(Long id) {
 		Optional<Role> p = RoleRepository.findById(id);
 		return p;
 	}
 	
+	@PreAuthorize("hasAuthority('ROLE_CREATE')")
+	@Transactional
 	public void addRole(Role Role) {
 		RoleRepository.save(Role);
 	}
 	
+	@PreAuthorize("hasAuthority('ROLE_UPDATE')")
+	@Transactional
 	public void updateRole(String id, Role Role) {
 		RoleRepository.save(Role);
 	}
 	
+	@PreAuthorize("hasAuthority('ROLE_DELETE')")
+	@Transactional
 	public void deleteRole(Long id) {
 		RoleRepository.deleteById(id);
 	}
