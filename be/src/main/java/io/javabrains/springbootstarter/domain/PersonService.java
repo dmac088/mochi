149776b812b90868ad83670b9132253c5c15315c
Mackie <Daniel.Mackie@dfs.com>
2018-10-27 13:02:59 +0800

    added @PreAuthorize annotations to PersonService class as a means to improve security there are extensions to this annotation taht can improve security further, like restricting the data that can be fetched by constraingin on the id of the user

--- be/src/main/java/io/javabrains/springbootstarter/domain/PersonService.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/PersonService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,8 @@ public class PersonService {
 	@Autowired
 	private PersonRepository personRepository; 
 	
+	
+	@PreAuthorize("hasAuthority('PERSON_READER')")
 	@Transactional(readOnly = true)
 	public List<Person> getAllPersons() {
 		List<Person> Persons = new ArrayList<>();
@@ -23,19 +26,28 @@ public class PersonService {
 		return Persons;
 	}
 	
+	@PreAuthorize("hasAuthority('PERSON_READ')")
+	@Transactional(readOnly = true)
 	public Person getPerson(Long id) {
 		Person p = personRepository.findByPartyId(id);
 		return p;
 	}
 
+	@PreAuthorize("hasAuthority('PERSON_CREATE')")
+	@Transactional
 	public void addPerson(Person person) {
 		personRepository.save(person);
 	}
 	
+	@PreAuthorize("hasAuthority('PERSON_UPDATE')")
+	@Transactional
 	public void updatePerson(Long id, Person person) {
 		personRepository.save(person);
 	}
 	
+	
+	@PreAuthorize("hasAuthority('PERSON_DELETE')")
+	@Transactional
 	public void deletePerson(Long id) {
 		personRepository.deleteById(id);
 	}
