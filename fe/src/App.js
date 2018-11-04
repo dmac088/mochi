cfa07fa5548ace3239a7d7214502c5a17a1cd067
dmac088 <danielmackie82@gmail.com>
2018-11-04 19:40:57 +0800

    changed rendering to customerNumber property so taht customer numbers now render on teh screen

--- fe/src/App.js
+++ fe/src/App.js
@@ -11,12 +11,12 @@ class App extends Component {
 
   async componentDidMount() {
     let details = {
-        "client_id": "spring-security-oauth2-read-write-client", 
+        "client_id": "spring-security-oauth2-read-write-client",
 		"username": "admin",
 		"password": "admin1234",
 		"grant_type": "password"
-    };	 
-	
+    };
+
 	let formBody = [];
     for (let property in details) {
         let encodedKey = encodeURIComponent(property);
@@ -24,12 +24,12 @@ class App extends Component {
         formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-	  
-    const response = await 
+
+    const response = await
 	fetch(
 		"https://localhost:8090/oauth/token", {
 		crossDomain: true,
-		method: "POST", 
+		method: "POST",
 		headers: new Headers({
 			"Authorization": "Basic "+btoa('spring-security-oauth2-read-write-client:spring-security-oauth2-read-write-client-password1234'),
 			"Content-Type": "application/x-www-form-urlencoded",
@@ -40,24 +40,24 @@ class App extends Component {
     const body = await response.text();
     console.log(JSON.parse(body).access_token);
     this.setState({ access_token: JSON.parse(body).access_token, isLoading: false });
-	
-	
-	const response2 = await 
+
+
+	const response2 = await
 	fetch(
 		"https://localhost:8090/api/Customer", {
 		crossDomain: true,
-		method: "GET", 
+		method: "GET",
 		headers: new Headers({
 			"Authorization": 	"Bearer "+this.state.access_token,
 			"Content-Type": 	"application/json"
 		})
-	});  
-	
+	});
+
 	const body2 = await response2.text();
 	this.setState({ customers: JSON.parse(body2), isLoading: false })
   }
-  
-  
+
+
 
   render() {
     const {customers, isLoading} = this.state;
@@ -75,8 +75,8 @@ class App extends Component {
         <div className="App-intro">
           <h2>Customer List</h2>
           {customers.map(customer =>
-            <div id={customer.partyId}> 
-              {customer.customerId}
+            <div id={customer.customerNumber} key={customer.roleId}>
+              {customer.customerNumber}
             </div>
           )}
         </div>
