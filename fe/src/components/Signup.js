6dae81f88be444f6b016b3048581e1857379bad3
dmac088 <danielmackie82@gmail.com>
2018-11-13 20:31:53 +0800

    signup form now centering as I want

--- fe/src/components/Signup.js
+++ fe/src/components/Signup.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Signup = (props) => {
       return(
-        <div classname="row">
-        <div className="col-md-6 order-md-1">
+        <div className="row">
+          <div className="col-md-2 order-md-1">
+          </div>
+        <div className="col-md-8 order-md-2">
           <h4 className="mb-3">
 Sign Up
 </h4>
@@ -78,58 +80,9 @@ Submit
 </button>
           </form>
         </div>
+        <div className="col-md-2 order-md-3">
+        </div>
       </div>
       );
   }
 export default Signup;
-
-//
-// <div className="container">
-//  <h1>
-//  Sign Up
-//  </h1>
-//  <p>
-//  Please fill in this form to create an account.
-//  </p>
-//  <hr />
-//  <label htmlFor="email">
-//    <b>Email</b>
-//  </label>
-//  <input
-//    type="text"
-//    placeholder="Enter Email"
-//    name="email"
-//    required />
-//  <label htmlFor="psw">
-// <b>Password</b>
-//  </label>
-//  <input
-//    type="password"
-//    placeholder="Enter Password"
-//    name="psw"
-//    required />
-//  <label htmlFor="psw-repeat">
-//    <b>
-//    Repeat Password
-//    </b>
-//  </label>
-//  <input
-//      type="password"
-//      placeholder="Repeat Password"
-//      name="psw-repeat"
-//      required />
-//  <label>
-//    <input
-//      type="checkbox"
-//      defaultChecked="checked"
-//      name="remember"
-//      style={{marginBottom: 15}} /> Remember me
-//  </label>
-//  <p>By creating an account you agree to our <a href="#" style={{color: 'dodgerblue'}}>Terms &amp; Privacy</a>.</p>
-//  <div className="clearfix">
-//    <button type="button" className="cancelbtn">Cancel</button>
-//    <button type="submit" className="signupbtn">
-//    Sign Up
-//    </button>
-//  </div>
-// </div>
