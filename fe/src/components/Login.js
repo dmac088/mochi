016e999b320fc3b7e2a4b52c8f8930d49c9a3c53
Mackie <Daniel.Mackie@dfs.com>
2018-11-13 15:27:01 +0800

    added login form to the navbar

--- fe/src/components/Login.js
+++ fe/src/components/Login.js
@@ -1,16 +1,85 @@
-import React from 'react';
-import '../scss/style.css';
+import React, {Component} from 'react';
 import Greeting from './Greeting';
 
-const Login = (props) => {
+class Login extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    renderLogoutButton = () => {
+     console.log('render logout button');
+     let button;
+     if(this.props.authenticated) {
+       button = <button
+            onClick={this.props.logoutClick}
+            className="btn btn-outline-success mr-sm-5 my-2 my-sm-0">
+          Logout
+       </button>;
+     }
+     return button;
+    }
+
+    renderLoginButton = () => {
+     console.log('render login button');
+     let button;
+     if(!this.props.authenticated) {
+       button =
+       <button
+           onClick={this.props.loginClick}
+           className="btn btn-outline-success mr-sm-5 my-2 my-sm-0"
+           type="submit">
+          Login
+      </button>;
+     }
+     return button;
+    }
+
+    renderUserNameField = () => {
+     console.log('render username field');
+     let userNameField;
+     if(!this.props.authenticated) {
+       userNameField =  <input
+         className="form-control mr-sm-2"
+         type="input"
+         onChange={this.props.updateUsernameValue}
+         placeholder="Username"
+         aria-label="Username" />;
+     }
+     return userNameField;
+    }
+
+    renderPasswordField = () => {
+     console.log('render password field');
+     let passwordField;
+     if(!this.props.authenticated) {
+       passwordField =
+       <input
+           className="form-control mr-sm-2"
+           type="password"
+           onChange={this.props.updatePasswordValue}
+           placeholder="Password"
+           aria-label="Password" />;
+     }
+     return passwordField;
+    }
+
+  render() {
       return(
         <div>
-          {(!props.authenticated) ? 'Username:' : '' } {props.renderUserNameField()}<br/>
-          {(!props.authenticated) ? 'Password:' : '' } {props.renderPasswordField()}<br/>
-          {props.renderLogoutButton()}
-          {props.renderLoginButton()}
-          <Greeting authenticated={props.authenticated} givenNameEn={props.customer.givenNameEn}/>
+          {this.renderUserNameField()}
+          {this.renderPasswordField()}
+          {this.renderLoginButton()}
+          {this.renderLogoutButton()}
         </div>
       );
-  }
+    }
+}
+
 export default Login;
+
+// {(!props.authenticated) ? 'Username:' : '' } {props.renderUserNameField()}<br/>
+// {(!props.authenticated) ? 'Password:' : '' } {props.renderPasswordField()}<br/>
+// {props.renderLogoutButton()}
+// {props.renderLoginButton()}
+// <Greeting authenticated={props.authenticated} givenNameEn={props.customer.givenNameEn}/>
---
+++ fe/src/components/Search.js
@@ -0,0 +1,18 @@
+
+import React, {Component} from 'react';
+
+const Search = (props) => {
+    return(
+      <input
+        className="form-control mr-sm-2"
+        type="search"
+        placeholder="Search"
+        aria-label="Search" />
+        <button
+          className="btn btn-outline-success mr-sm-5 my-2 my-sm-0"
+          type="submit">Search</button>
+    );
+}
+
+
+export default Search;
