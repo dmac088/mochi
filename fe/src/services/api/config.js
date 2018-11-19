fb10ef9e603a19c67288d02fa07cceaed14d516d
dmac088 <danielmackie82@gmail.com>
2018-11-19 20:56:06 +0800

    fetch clientId from apiConfig

--- fe/src/services/api/config.js
+++ fe/src/services/api/config.js
@@ -1,5 +1,5 @@
 export default {
-	'client_Id': 'c3ByaW5nLXNlY3VyaXR5LW9hdXRoMi1yZWFkLXdyaXRlLWNsaWVudDpzcHJpbmctc2VjdXJpdHktb2F1dGgyLXJlYWQtd3JpdGUtY2xpZW50LXBhc3N3b3JkMTIzNA==',
+	'clientId': 'c3ByaW5nLXNlY3VyaXR5LW9hdXRoMi1yZWFkLXdyaXRlLWNsaWVudDpzcHJpbmctc2VjdXJpdHktb2F1dGgyLXJlYWQtd3JpdGUtY2xpZW50LXBhc3N3b3JkMTIzNA==',
 	'Content-Type': 'application/x-www-form-urlencoded',
 	'url': 'https://localhost:8090',
 };
