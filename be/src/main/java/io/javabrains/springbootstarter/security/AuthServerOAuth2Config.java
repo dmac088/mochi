456eb9d185e3393ab2c9e7df6c0f4ee1fd1c36e3
dmac088 <danielmackie82@gmail.com>
2018-11-20 00:58:03 +0800

    added a customTokenEnhancer to add additional properties to the OAuth reponse JSON payload

--- be/src/main/java/io/javabrains/springbootstarter/security/AuthServerOAuth2Config.java
+++ be/src/main/java/io/javabrains/springbootstarter/security/AuthServerOAuth2Config.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,8 +16,11 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
 import javax.sql.DataSource;
 
 @Configuration
@@ -44,9 +48,20 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
     	return new JdbcTokenStore(dataSource);
     }
 	
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+	    return new CustomTokenEnhancer();
+	}
+	    
+	
     @Bean
     public OAuth2AccessDeniedHandler oauthAccessDeniedHandler() {
         return new OAuth2AccessDeniedHandler();
+    }
+    
+    @Bean
+    public DefaultAccessTokenConverter accessTokenConverter() {
+        return new DefaultAccessTokenConverter();
     }
     
     @Override
@@ -61,8 +76,14 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
     
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager).userDetailsService(userDetailsService);
-        
+        endpoints
+        	.tokenStore(tokenStore())
+        	.tokenEnhancer(tokenEnhancer())
+        	.accessTokenConverter(accessTokenConverter())
+        	.authenticationManager(authenticationManager)
+        	.userDetailsService(userDetailsService);
+        	
     }   
     
+   
 }
---
+++ be/src/main/java/io/javabrains/springbootstarter/security/CustomTokenEnhancer.java
@@ -0,0 +1,29 @@
+package io.javabrains.springbootstarter.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+public class CustomTokenEnhancer implements TokenEnhancer {
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+		// TODO Auto-generated method stub
+		User user = (User) authentication.getPrincipal();
+        final Map<String, Object> additionalInfo = new HashMap<>();
+
+        additionalInfo.put("customInfo", "boob");
+        additionalInfo.put("authorities", user.getAuthorities());
+
+        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+
+        return accessToken;
+    }
+
+
+}
