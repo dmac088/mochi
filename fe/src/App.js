caec9392fec2962a8ba76509c65d1c98420cdc0a
Mackie <Daniel.Mackie@dfs.com>
2018-11-20 13:49:20 +0800

    removed spacing

--- fe/src/App.js
+++ fe/src/App.js
@@ -222,8 +222,6 @@ class App extends Component {
         <div className="App">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
           <Header authenticated={(this.state.authenticated)}
-
-
                   loginClick={this.loginClick.bind(this)}
                   updateCustomerState={this.updateCustomerState.bind(this)}
                   email={this.state.email}
