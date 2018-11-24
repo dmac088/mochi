d0553150d9891dacaad2cd3a7776d8b189d55481
dmac088 <danielmackie82@gmail.com>
2018-11-24 23:42:56 +0800

    table customer column cst_id not auto incrementing when persisting from api Person POST

--- be/src/main/java/io/javabrains/springbootstarter/domain/Customer.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/Customer.java
@@ -3,9 +3,14 @@ package io.javabrains.springbootstarter.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 
 @Entity
@@ -21,7 +26,8 @@ public class Customer extends Role {
 		this.setRoleType(new RoleType(roleTypeId));
 	}
 	
-	@Column(name="cst_id")
+	@Column(name="cst_id", insertable = false)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private String CustomerNumber;
 
 	public String getCustomerNumber() {
