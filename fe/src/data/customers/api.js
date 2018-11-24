d903570931983b966e645c0d29bb2e96fffbc138
dmac088 <danielmackie82@gmail.com>
2018-11-24 14:25:52 +0800

    now getting unsupported media type, however one step further

--- fe/src/data/customers/api.js
+++ fe/src/data/customers/api.js
@@ -1,11 +1,17 @@
 import { fetchApi } from '../../services/api';
 
 const endPoints = {
-	create: '/api/Party',
-	get: '/api/Party/UserName',
+	create: '/api/Person',
+	get: '/api/Person/UserName',
 };
 
-export const create = payload => fetchApi(endPoints.create, payload, {},'POST',{});
+export const create = payload => fetchApi(endPoints.create,
+																					payload, {
+																										'Content-Type': 'application/json'
+																					},
+																					'POST',
+																					{}
+																				);
 
 
 export const get = payload => fetchApi(endPoints.get, payload, {}, 'GET', {});
