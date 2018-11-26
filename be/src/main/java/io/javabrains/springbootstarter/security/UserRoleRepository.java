4cec949702380cbdc722844ffcf04ac21aecac0d
Mackie <Daniel.Mackie@dfs.com>
2018-11-26 12:21:38 +0800

    added UserRoleRepository

--- be/src/main/java/io/javabrains/springbootstarter/security/UserRoleRepository.java
+++ be/src/main/java/io/javabrains/springbootstarter/security/UserRoleRepository.java
@@ -1,12 +1,19 @@
 package io.javabrains.springbootstarter.security;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import io.javabrains.springbootstarter.domain.Party;
+
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
     UserRole findByName(String roleName);
+    
+    Optional<UserRole> findById(Long id);
+    
     
 }
