3265066946e932cd068354bcafbcde4fef4d1e6d
Mackie <Daniel.Mackie@dfs.com>
2018-11-27 13:48:41 +0800

    tidy up and additional comments for clarity

--- fe/src/services/customer/api.js
+++ fe/src/services/customer/api.js
@@ -2,22 +2,22 @@ import { fetchApi } from '../../services/api';
 import apiConfig from '../../services/api/config';
 
 const endPoints = {
-	get: '/api/Person/',
+	get: '/Party/UserName/',
 	other: ''
 };
 
 export const findByUserName = (userName, password) => fetchApi(
-																													endPoints.get + userName,
-																													{},
-																													{
-																														username: userName,
-																														password: password,
-																														grant_type: 'password'
-																													},
-																													'GET',
-																													{
-																															Authorization: 'Basic ' + apiConfig.clientId,
-																															'Content-Type': 'application/x-www-form-urlencoded',
-																															'Cache-Control': 'no-cache'
-																													}
-																								);
+																																endPoints.get + userName,
+																																{},
+																																{
+																																	username: userName,
+																																	password: password,
+																																	grant_type: 'password'
+																																},
+																																'GET',
+																																{
+																																		Authorization: 'Basic ' + apiConfig.clientId,
+																																		'Content-Type': 'application/x-www-form-urlencoded',
+																																		'Cache-Control': 'no-cache'
+																																}
+																															);
