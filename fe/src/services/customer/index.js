d875cf5e6a4371b30e1fdeb4949884d4be876333
Mackie <Daniel.Mackie@dfs.com>
2018-11-27 11:50:30 +0800

    reorganizing the application structure

---
+++ fe/src/services/customer/index.js
@@ -0,0 +1,45 @@
+import store from '../../store';
+import * as api from './api';
+import * as selectors from './selectors';
+import * as actionCreators from './actions';
+import { initialState } from './reducer';
+
+
+export const findByUserName = (userName) =>
+	api.findByUserName(userName)
+	//injection of a function reference means the function behaves
+	//as if it were nested within the parentheses
+	.then(onRequestSuccess)
+	.catch(onRequestFailed);
+
+
+
+
+	const  onRequestSuccess = (response) => {
+		 //we capture our response errors here and act accordingly
+		 console.log('onRequestSuccess');
+		 // if (response.status === 400) {
+			//  console.log('Invalid username or password');
+			//  clearSession();
+			//  return;
+		 // };
+
+		 //response is an object of type promise
+		 //we call the text function to execute the promise
+		 response.text()
+		 .then((responseText) => {
+		 	 return responseText.JSON;
+		 })
+		 .then((responseJSON) => {
+			 store.dispatch(actionCreators.update({ responseJSON, customer: responseJSON }));
+		 })
+		 .catch((e) => {
+	 	 			console.log(e);
+	   });
+	};
+
+
+
+const onRequestFailed = (exception) => {
+	throw exception;
+};
