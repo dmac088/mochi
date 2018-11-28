38f326e3c8546c1050e01d0391ed675baa483bfb
Mackie <Daniel.Mackie@dfs.com>
2018-11-28 18:32:38 +0800

    service class CustomerService now working and will register new customers from registerNewPersonCustomer using the domain model this is much cleaner than placing in a domain controller, I still want a DTO to be used however

--- be/src/main/java/io/javabrains/springbootstarter/domain/PartyPersonController.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/PartyPersonController.java
@@ -1,21 +1,14 @@
 package io.javabrains.springbootstarter.domain;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import io.javabrains.springbootstarter.security.UserRole;
-import io.javabrains.springbootstarter.security.UserRoleService;
 
 @RestController
 @RequestMapping("/api")
@@ -23,9 +16,6 @@ public class PartyPersonController {
 	
 	@Autowired
 	private PartyPersonService personService;	
-	
-	@Autowired
-	private UserRoleService userRoleService;
 	
 	@GetMapping("/Person")
     public List<PartyPerson> getAllPersons() {
@@ -37,34 +27,6 @@ public class PartyPersonController {
 	public PartyPerson getPerson(@PathVariable Long id) {
 		System.out.println("calling getPerson");
 		return personService.getPerson(id);
-	}
-	
-	@PostMapping("/Customer/Signup")
-	public ResponseEntity<PartyPerson> addPerson(@RequestBody PartyPerson person) {
-		//Should this go into an existing service class or a new one?
-				
-		//create the role object
-		person.setPartyRoles(new ArrayList<Role>());
-		RoleCustomer c1 = new RoleCustomer();
-		c1.setRoleStart(new Date());
-		
-		//add the role to person
-		person.addRole(c1);
-		
-		//add the person to role
-		c1.setRoleParty(person);
-		
-		//add the user object
-		person.getPartyUser().setUserRoles(new ArrayList<UserRole>());
-		person.getPartyUser().addUserRole(userRoleService.loadUserRoleByRoleName("CUSTOMER"));
-		
-		person.getPartyUser().setEnabled(true);
-		
-		//persist the parent
-		personService.addPerson(person);
-		
-		//return the response entity
-		return new ResponseEntity<PartyPerson>(person, HttpStatus.OK);
 	}
 	
 	@PutMapping("/Person/{id}")
