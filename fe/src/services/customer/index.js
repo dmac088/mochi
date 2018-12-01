dfd917da0c835f6207ea24b79bbded622a031123
dmac088 <danielmackie82@gmail.com>
2018-12-01 13:35:27 +0800

    post signup authentication now working perfectly!

--- fe/src/services/customer/index.js
+++ fe/src/services/customer/index.js
@@ -1,12 +1,13 @@
 import store from '../../store';
-import * as api from './api';
+import * as customerApi from './api';
+import * as api from '../api';
 import * as selectors from './selectors';
 import * as actionCreators from './actions';
 import { initialState } from './reducer';
 
 
 	export const findByUserName = (userName) =>
-		api.findByUserName(userName)
+		customerApi.findByUserName(userName)
 		//injection of a function reference means the function behaves
 		//as if it were nested within the parentheses
 		.then(onRequestSuccess)
@@ -14,7 +15,44 @@ import { initialState } from './reducer';
 
 
 	export const createNewCustomer = (customer) => {
-		return api.createNewCustomer(customer);
+		return customerApi.createNewCustomer(customer).then((response) => {
+			return response.text();
+			//return session.authenticate(this.state.customer.userName,
+			//we are using local state here since
+			//our input text boxes write to local state and then
+			//persist directly to database to create a new customer, not to redux (yet)
+			//we poplulare reduct by making another call to the api
+			//either using the userName in local state or returned party id in responseText
+			//for this we use the /services/customer/index.js which must be imported
+			//we need the access token from the redux store to make the subsequen API calls
+			//this can be gotten by
+
+			//  console.log(this.props.user);
+			//  console.log(this.props.customer);
+			//  console.log(this.props.tokens);
+
+			//console.log(customerService.findByuserName(this.state.customer.partyUser.userName,
+			//this.state.customer.partyUser.password));
+
+
+
+			//we can reset local state, no impact to global redux state
+			//const routeStack = this.props.navigator.getCurrentRoutes();
+			//this.props.navigator.jumpTo(routeStack[3]);
+
+	}).then((responseText) => {
+			console.log(responseText);
+			return JSON.parse(responseText);
+	})
+	.catch((exception) => {
+		// Displays only the first error message
+		const error = api.exceptionExtractError(exception);
+		const newState = {
+			isLoading: false,
+			...(error ? { error } : {}),
+		};
+			this.setState(newState);
+	});
 	};
 
 
