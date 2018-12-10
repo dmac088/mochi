dfac6077f565a86c62c08cfba5e49d71469617e8
dmac088 <danielmackie82@gmail.com>
2018-12-10 23:36:46 +0800

    stylesheeting seems to be working

--- fe/src/App.js
+++ fe/src/App.js
@@ -14,6 +14,7 @@ import Landing from './components/Landing';
 import Login from './components/Login';
 import Footer from './components/Footer';
 import * as productSelectors from './services/product/selectors';
+import './scss/style.scss';
 
 
 class App extends Component {
