ad794418f3c79f374454d8812db382b4bb07c891
Mackie <Daniel.Mackie@dfs.com>
2018-12-11 11:04:29 +0800

    enables static resource access

--- be/src/main/java/io/javabrains/springbootstarter/security/ResourceServerConfiguration.java
+++ be/src/main/java/io/javabrains/springbootstarter/security/ResourceServerConfiguration.java
@@ -14,13 +14,17 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
     private static final String SECURED_READ_SCOPE = "#oauth2.hasScope('read')";
     private static final String SECURED_WRITE_SCOPE = "#oauth2.hasScope('write')";
     private static final String SECURED_PATTERN = "/api/**";
+    
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
         resources.resourceId(RESOURCE_ID);
     }
+    
+    
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.anonymous().and().authorizeRequests()
+        http	
+        		.anonymous().and().authorizeRequests()
         		.antMatchers(HttpMethod.POST,"/api/Customer/Signup").permitAll()
         		.antMatchers(HttpMethod.GET,"/api/Product/**").permitAll()
         		.antMatchers(HttpMethod.GET,"/images/**").permitAll()
