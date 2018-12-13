0a3d3031f68b607652fe22d8770bd4340ed9951c
Mackie <Daniel.Mackie@dfs.com>
2018-12-13 14:25:49 +0800

    add to cart now working, however quantity increment not working

--- fe/src/components/Landing.js
+++ fe/src/components/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Products from  './Products';
 
 const Landing = (props) =>  {
-  console.log(props);
+  //console.log(props);
     return(
         <div>
           <Products
