e4b71d673a659d16320b327003efa7c85b4f9584
Mackie <Daniel.Mackie@dfs.com>
2018-12-14 11:36:08 +0800

    removal of console.log

--- fe/src/services/cart/reducer.js
+++ fe/src/services/cart/reducer.js
@@ -7,7 +7,6 @@ export const initialState = {
 														};
 
 export const reducer = (state = initialState, action) => {
-	console.log(action);
 	switch (action.type) {
 		case actionTypes.UPDATE:
 			return {
