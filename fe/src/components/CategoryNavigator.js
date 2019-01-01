a1314f508ff2b3e229ccf41c091622adacce0ae6
Mackie <Daniel.Mackie@dfs.com>
2019-01-01 10:50:56 +0800

    added category list component

---
+++ fe/src/components/CategoryNavigator.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const CategoryNavigator = () => {
+  return (
+    <ul>
+      <li>Put a list of categories here!</li>
+    </ul>
+  )
+}
+
+export default CategoryNavigator;
