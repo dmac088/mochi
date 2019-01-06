72e03b536c2f1baa1f2708003a0fb05f1152eaa6
dmac088 <danielmackie82@gmail.com>
2019-01-06 22:47:21 +0800

    back in business

--- be/src/main/java/io/javabrains/springbootstarter/domain/Product.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/Product.java
@@ -2,18 +2,23 @@ package io.javabrains.springbootstarter.domain;
 
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.SecondaryTable;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "product", schema = "mochi")
@@ -34,6 +39,12 @@ public class Product {
 	@ManyToMany(mappedBy = "products")
 	@JsonIgnore
 	private Collection<ProductCategory> categories;
+	
+	@OneToMany(mappedBy="product",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonManagedReference
+	//@Where(clause = " lcl_cd = 'ENG' ")
+	//@Filter(name = "lclCdFilter")
+	private List<ProductAttribute> productAttribute;
 
 	public Long getProductId() {
 		return productId;
