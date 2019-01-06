4d3d45e848d3b122d4af38ae4bb57681c7964fe7
dmac088 <danielmackie82@gmail.com>
2019-01-06 22:05:02 +0800

    added product attribute class

---
+++ be/src/main/java/io/javabrains/springbootstarter/domain/ProductAttribute.java
@@ -0,0 +1,64 @@
+package io.javabrains.springbootstarter.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "product_attr_lcl", schema = "mochi")
+@PrimaryKeyJoinColumn(name = "prd_lcl_id")
+public class ProductAttribute {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="prd_lcl_id")
+	private Long Id;
+	
+	@Column(name="prd_id")
+	private Long productId;
+	
+	@Column(name="prd_desc")
+	private String productDesc;
+	
+	@Column(name="lcl_cd")	
+	private String lclCd;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="prd_id", insertable=false, updatable=false)
+	@JsonBackReference
+	private Product product;
+	
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public String getProductDesc() {
+		return productDesc;
+	}
+
+	public void setProductDesc(String productDesc) {
+		this.productDesc = productDesc;
+	}
+	
+	public String getLclCd() {
+		return lclCd;
+	}
+
+	public void setLclCd(String lclCd) {
+		this.lclCd = lclCd;
+	}
+	
+	
+}
