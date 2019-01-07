fd43baf9cca99169d1de1b91ba6568774ad52461
dmac088 <danielmackie82@gmail.com>
2019-01-07 23:46:50 +0800

    works perfectly

--- be/src/main/java/io/javabrains/springbootstarter/services/ProductCategoryDTOService.java
+++ be/src/main/java/io/javabrains/springbootstarter/services/ProductCategoryDTOService.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import io.javabrains.springbootstarter.domain.ProductAttribute;
 import io.javabrains.springbootstarter.domain.ProductCategory;
 import io.javabrains.springbootstarter.domain.ProductCategoryAttribute;
 import io.javabrains.springbootstarter.domain.ProductCategoryAttributeRepository;
@@ -17,6 +19,9 @@ public class ProductCategoryDTOService implements IProductCategoryDTOService {
     @Autowired
     private ProductCategoryAttributeRepository productCategoryAttributeRepository;
     
+    @Autowired
+    private ProductCategoryRepository productCategoryRepository;
+    
     //API
     //This method should accept a DTO and return a DTO
     //The DTO is coarse grained and contains a flat structure of properties
@@ -26,21 +31,31 @@ public class ProductCategoryDTOService implements IProductCategoryDTOService {
     @Override
 	@Transactional
 	public List<ProductCategoryDTO> getProductCategories(String lcl) {
-		List<ProductCategoryAttribute> lpca = productCategoryAttributeRepository.findByLclCd(lcl);
+    	
+    	List<ProductCategory> lpc = productCategoryRepository.findAll();
+    	
 		final List<ProductCategoryDTO> lpcDTO = new ArrayList<ProductCategoryDTO>();
-		for(ProductCategoryAttribute pca : lpca) {
-        	ProductCategoryDTO pcDTO = new ProductCategoryDTO();
-        	ProductCategory pc = pca.getProductCategory();
-        	pcDTO.setCategoryId(pc.getCategoryId());
-        	pcDTO.setCategoryCode(pc.getCategoryCode());
-        	pcDTO.setCategoryDesc(pca.getCategoryDesc());
-        	//pcDTO.setParent(pc.getParent());
-        	pcDTO.setChildren(pc.getChildren());
-        	pcDTO.setLclCd(pca.getLclCd());
-        	lpcDTO.add(pcDTO);
+		
+		for(ProductCategory pc : lpc) {
+        	lpcDTO.add(convertToProductCategoryDto(pc, lcl));
         }
 		return lpcDTO;
 	}	
+    
+    private ProductCategoryDTO convertToProductCategoryDto(final ProductCategory pc,final String lcl) {
+    	ProductCategoryAttribute pca = productCategoryAttributeRepository.findByLclCdAndCategoryId(lcl, pc.getCategoryId()).get();	
+        final ProductCategoryDTO pcDto = new ProductCategoryDTO();
+        pcDto.setCategoryId(pc.getCategoryId());
+        pcDto.setCategoryCode(pc.getCategoryCode());
+        List<ProductCategoryDTO> pcDTOl = new ArrayList<ProductCategoryDTO>();
+        for(ProductCategory pc1 : pc.getChildren()) {
+        	pcDTOl.add(convertToProductCategoryDto(pc1, lcl));
+        }
+        pcDto.setChildren(pcDTOl);
+        pcDto.setCategoryDesc(pca.getCategoryDesc());
+        pcDto.setLclCd(pca.getLclCd());
+        return pcDto;
+    }
     
     @Override
  	@Transactional
