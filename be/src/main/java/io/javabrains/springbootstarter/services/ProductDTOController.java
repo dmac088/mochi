9b57da560503ccb1808dd2665d4d07c47c995612
Mackie <Daniel.Mackie@dfs.com>
2019-01-07 12:25:44 +0800

    paging now working as desired with correct metadata

--- be/src/main/java/io/javabrains/springbootstarter/services/ProductDTOController.java
+++ be/src/main/java/io/javabrains/springbootstarter/services/ProductDTOController.java
@@ -1,17 +1,13 @@
 package io.javabrains.springbootstarter.services;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/api")
@@ -25,9 +21,9 @@ public class ProductDTOController {
         super();
     }
     
-    @GetMapping("/Product/{lcl}")
-    public List<ProductDTO> getProducts(@PathVariable String lcl) {
-    	return productService.getProducts(lcl);
+    @GetMapping("/Product/{lcl}/page/{page}/size/{size}")
+    public Page<ProductDTO> getProducts(@PathVariable String lcl,@PathVariable int page,@PathVariable int size) {
+    	return productService.getProducts(lcl, page, size);
     }
     
 }
