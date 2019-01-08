9623d3f9c67a08123f4564b0e01514e9931755fc
Mackie <Daniel.Mackie@dfs.com>
2019-01-08 11:49:55 +0800

    cleanup

--- be/src/main/java/io/javabrains/springbootstarter/services/IProductDTOService.java
+++ be/src/main/java/io/javabrains/springbootstarter/services/IProductDTOService.java
@@ -5,11 +5,9 @@ import org.springframework.data.domain.Page;
 
 
 public interface IProductDTOService {
-
-	 //List<ProductDTO> getProducts();
-	 
+	
 	 Page<ProductDTO> getProducts(String lcl, int page, int size);
 	 
 	 Page<ProductDTO> getProducts(String lcl, Long categoryId, int page, int size);
-	// ProductDTO getProduct(Long productId);
+	 
 }
