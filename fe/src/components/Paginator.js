6216132a82d3bfe3caa0b8f86f1a803ad4d5f34e
Mackie <Daniel.Mackie@dfs.com>
2019-01-09 10:24:38 +0800

    changepage function implemented

--- fe/src/components/Paginator.js
+++ fe/src/components/Paginator.js
@@ -2,31 +2,37 @@ import React from 'react';
 
 
 const Paginator = (props) => {
-  return renderPaginator();
+  return (
+    <nav aria-label="Page navigation example">
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">«</span>
+            <span className="sr-only">Previous</span>
+          </a>
+        </li>
+        {renderPaginator(props.page.totalPages, props.changePage)}
+        <li className="page-item">
+          <a className="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">»</span>
+            <span className="sr-only">Next</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+  )
 }
 
-const renderPaginator = () => {
-  return(
-          <nav aria-label="Page navigation example">
-            <ul className="pagination">
-              <li className="page-item">
-                <a className="page-link" href="#" aria-label="Previous">
-                  <span aria-hidden="true">«</span>
-                  <span className="sr-only">Previous</span>
-                </a>
-              </li>
-              <li className="page-item"><a className="page-link" href="#">1</a></li>
-              <li className="page-item"><a className="page-link" href="#">2</a></li>
-              <li className="page-item"><a className="page-link" href="#">3</a></li>
-              <li className="page-item">
-                <a className="page-link" href="#" aria-label="Next">
-                  <span aria-hidden="true">»</span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
+const renderPaginator = (pages, changePage) => {
+  console.log("pages = " +  pages)
+  return Array.apply(null, {length: pages}).map(Number.call,page => {
+    return (
+          <li className="page-item"><a id={page} className="page-link" onClick={changePage} href="#">{page+1}</a></li>
+    )}
   );
 }
+
+
 
 export default Paginator;
