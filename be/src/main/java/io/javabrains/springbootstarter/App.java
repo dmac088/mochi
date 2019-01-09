8a4218888777b3443099066bd29e7f78156a234e
Mackie <Daniel.Mackie@dfs.com>
2019-01-09 19:07:23 +0800

    unused imports removed

--- be/src/main/java/io/javabrains/springbootstarter/App.java
+++ be/src/main/java/io/javabrains/springbootstarter/App.java
@@ -3,7 +3,6 @@ package io.javabrains.springbootstarter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
