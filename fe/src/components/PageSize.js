d39af095683f393c880791765b6866340569cafd
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 14:17:09 +0800

    react-bootstrap now working properly with DropdownButton

--- fe/src/components/PageSize.js
+++ fe/src/components/PageSize.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { Link } from  'react-router-dom';
+import { DropdownButton, MenuItem  } from 'react-bootstrap';
 
 const PageSize = (props) => {
   return (
-      <div className="btn-group">
-        <button type="button" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Page Size ({props.currentPageSize})
-        </button>
-        <div className="dropdown-menu">
-          <Link id="5" onClick={props.changePageSize} className="dropdown-item"  to="/">5</Link>
-          <Link id="10" onClick={props.changePageSize} className="dropdown-item"  to="/">10</Link>
-          <Link id="20" onClick={props.changePageSize} className="dropdown-item"  to="/">20</Link>
-        </div>
-      </div>
-    )
+    <DropdownButton>
+       <MenuItem id="5" onClick={props.changePageSize} eventKey="5">5</MenuItem>
+       <MenuItem id="10" onClick={props.changePageSize} eventKey="10">10</MenuItem>
+       <MenuItem id="20" onClick={props.changePageSize} eventKey="20" active>
+         20
+       </MenuItem>
+    </DropdownButton>
+  )
 };
 
 export default PageSize;
