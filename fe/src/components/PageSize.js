3a6fbb31a010b0dc52ffa95ac0de2f3d51b51806
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 10:57:50 +0800

    components still rendering multiple times

--- fe/src/components/PageSize.js
+++ fe/src/components/PageSize.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Badge, DropdownButton, MenuItem } from 'react-bootstrap';
 
 const PageSize = (props) => {
-    console.log(props.currentPageSize);
-    return (
-      <DropdownButton>
-        <MenuItem id="5" onClick={props.changePageSize}>Action</MenuItem>
-        <MenuItem id="10" onClick={props.changePageSize}>Another action</MenuItem>
-      </DropdownButton>
-    );
-}
+  return (
+      <div className="btn-group">
+        <button type="button" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Page Size ({props.currentPageSize})
+        </button>
+        <div className="dropdown-menu">
+          <a id="5" onClick={props.changePageSize} className="dropdown-item" href="#">5</a>
+          <a id="10" onClick={props.changePageSize} className="dropdown-item" href="#">10</a>
+          <a id="20" onClick={props.changePageSize} className="dropdown-item" href="#">20</a>
+        </div>
+      </div>
+    )
+};
 
 export default PageSize;
