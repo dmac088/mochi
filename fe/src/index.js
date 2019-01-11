c6f50c1872a340bc50e9e02f75479e28ea5ef620
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 10:26:58 +0800

    added popper.js

--- fe/src/index.js
+++ fe/src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 ReactDOM.render(<Provider store={store}>
                 <App />
