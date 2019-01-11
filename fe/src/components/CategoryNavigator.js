1b986e0df8e2df6698d8d1e4cbf60f8c50a12bf9
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 09:22:11 +0800

    added <React.Fragment>

--- fe/src/components/CategoryNavigator.js
+++ fe/src/components/CategoryNavigator.js
@@ -4,7 +4,9 @@ import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
 const renderCategoryListItems = (categoryList, changeCategory) => {
   return categoryList = categoryList.map(category => {
       return(
-        <ListGroupItem key={category.categoryId} id={category.categoryId} onClick={changeCategory}  tag="a" href="#">{category.categoryDesc} <Badge pill>{category.productCount}</Badge></ListGroupItem>
+        <React.Fragment>
+          <ListGroupItem key={category.categoryId} id={category.categoryId} onClick={changeCategory}  tag="a" href="#">{category.categoryDesc} <Badge pill>{category.productCount}</Badge></ListGroupItem>
+        </React.Fragment>
       )
     });
 }
