29605d4420e095ea0165509faf40597388b54cbb
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 12:52:56 +0800

    still some cleanup required but all working well

--- fe/src/components/PageSize.js
+++ fe/src/components/PageSize.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from  'react-router-dom';
 
 const PageSize = (props) => {
   return (
@@ -6,11 +7,11 @@ const PageSize = (props) => {
         <button type="button" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Page Size ({props.currentPageSize})
         </button>
-        <ul className="dropdown-menu">
-          <li id="5" onClick={props.changePageSize} className="dropdown-item">5</li>
-          <li id="10" onClick={props.changePageSize} className="dropdown-item">10</li>
-          <li id="20" onClick={props.changePageSize} className="dropdown-item">20</li>
-        </ul>
+        <div className="dropdown-menu">
+          <Link id="5" onClick={props.changePageSize} className="dropdown-item"  to="/">5</Link>
+          <Link id="10" onClick={props.changePageSize} className="dropdown-item"  to="/">10</Link>
+          <Link id="20" onClick={props.changePageSize} className="dropdown-item"  to="/">20</Link>
+        </div>
       </div>
     )
 };
