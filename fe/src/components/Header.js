5a924ae058600149538a114c9e07b376fbf2f349
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 14:19:47 +0800

    Trying to fix up the NavBar

--- fe/src/components/Header.js
+++ fe/src/components/Header.js
@@ -5,69 +5,72 @@ import Selector from './Selector';
 import Search from './Search';
 import LanguageSelector from './LanguageSelector';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 
 const Header = (props) => {
 
       return(
-      <nav className="navbar navbar-light bg-light justify-content-between">
-        <div className="navbar-brand">
-            <Link to="/">HOME</Link>
-        </div>
-        <div className="form-inline">
-          <Greeting
-            authenticated={props.authenticated}
-            customer={props.customer}
-          />
-        <LanguageSelector
-            changeLang ={props.changeLang}
-          />
-            <Search
-              handleSearch={props.handleSearch}
-              lang={props.lang}
-            />
-                        <div className="cart-info">
-                          <table>
-                            <tbody>
-                              <tr>
-                                <td>No. of items</td>
-                                <td>:</td>
-                                <td>
-                                  <strong>{props.totalItems}</strong>
-                                </td>
-                              </tr>
-                              <tr>
-                                <td>Sub Total</td>
-                                <td>:</td>
-                                <td>
-                                  <strong>{props.total}</strong>
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                        <a className="cart-icon"
-                          href="#">
-                          <img
-                            className={props.cartBounce ? "tada" : " "}
-                            src="https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png"
-                            alt="Cart"
-                          />
-                          {props.totalItems ? (
-                            <span className="cart-count">{props.totalItems}</span>
-                          ) : (
-                            ""
-                          )}
-                        </a>
+        <Navbar>
+          <Nav className="navbar navbar-light bg-light justify-content-between">
+            <div className="navbar-brand">
+                <Link to="/">HOME</Link>
+            </div>
+            <div className="form-inline">
+              <Greeting
+                authenticated={props.authenticated}
+                customer={props.customer}
+              />
+            <LanguageSelector
+                changeLang ={props.changeLang}
+              />
+                <Search
+                  handleSearch={props.handleSearch}
+                  lang={props.lang}
+                />
+                            <div className="cart-info">
+                              <table>
+                                <tbody>
+                                  <tr>
+                                    <td>No. of items</td>
+                                    <td>:</td>
+                                    <td>
+                                      <strong>{props.totalItems}</strong>
+                                    </td>
+                                  </tr>
+                                  <tr>
+                                    <td>Sub Total</td>
+                                    <td>:</td>
+                                    <td>
+                                      <strong>{props.total}</strong>
+                                    </td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </div>
+                            <a className="cart-icon"
+                              href="#">
+                              <img
+                                className={props.cartBounce ? "tada" : " "}
+                                src="https://res.cloudinary.com/sivadass/image/upload/v1493548928/icons/bag.png"
+                                alt="Cart"
+                              />
+                              {props.totalItems ? (
+                                <span className="cart-count">{props.totalItems}</span>
+                              ) : (
+                                ""
+                              )}
+                            </a>
 
 
 
-            <Selector
-              authenticated={props.authenticated}
-              customer={props.customer}
-            />
-        </div>
-      </nav>
+                <Selector
+                  authenticated={props.authenticated}
+                  customer={props.customer}
+                />
+            </div>
+          </Nav>
+        </Navbar>
     );
 }
 
