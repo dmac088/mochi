c8aed6b77ac6f2948fdb7338e1e07db89713d4ca
Mackie <Daniel.Mackie@dfs.com>
2019-01-11 18:08:44 +0800

    Counter is now a function component, but needs styling and event handling methodsin App.js

--- fe/src/components/Counter.js
+++ fe/src/components/Counter.js
@@ -1,30 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Counter extends Component {
-	constructor(props){
-		super(props)
-		this.state = { value: this.props.productQuantity };
-	}
-
-	resetQuantity(){
-		this.setState({
-			value: 1
-		})
-	}
-	render() {
+const Counter = (props) => {
 		return (
 			<div className="stepper-input">
-				<button href="#" className="decrement" onClick={() => {console.log('-')}}>–</button>
-				<input ref="feedQty" type="number" className="quantity" value={this.state.value} onChange={() => {console.log(this.state.value)}} />
+				<button 	className="decrement" onClick={() => {console.log('-')}}>–</button>
+				<input  type="number" className="quantity" value={1} onChange={() => {console.log(this.state.value)}} />
 				<button className="increment" onClick={() => {console.log('+')}}>+</button>
 			</div>
-		)
-	}
+		);
 }
-
-Counter.propTypes = {
-  value: PropTypes.number
-};
 
 export default Counter;
