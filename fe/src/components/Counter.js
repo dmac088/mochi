d52559fda61abdf6715e7a78bfb25e38674a0fc2
dmac088 <danielmackie82@gmail.com>
2019-01-12 18:26:34 +0800

    cart quantity increment now working through redux

--- fe/src/components/Counter.js
+++ fe/src/components/Counter.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Counter = (props) => {
 		return (
 			<div className="stepper-input">
-				<button className="decrement" onClick={props.decrementQuantity}>–</button>
+				<button id={props.productId} className="decrement" onClick={props.decrementQuantity}>–</button>
 				<input  type="number" className="quantity" value={props.productQty} onChange={() => {console.log(props.productQty)}} />
-				<button className="increment" onClick={props.incrementQuantity}>+</button>
+				<button id={props.productId} className="increment" onClick={props.incrementQuantity}>+</button>
 			</div>
 		);
 }
