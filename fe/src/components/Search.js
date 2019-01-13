60b8b4ebe1b5ba8d480f10938c2b5ec825843ae9
dmac088 <danielmackie82@gmail.com>
2019-01-13 22:38:49 +0800

    working better

--- fe/src/components/Search.js
+++ fe/src/components/Search.js
@@ -4,12 +4,12 @@ import { Navbar, Button, FormGroup, FormControl } from 'react-bootstrap';
 
 const Search = (props) => {
     return(
-      <React.Fragment>
+      <Navbar.Form>
           <FormGroup controlId="formInlineName">
             <FormControl type="text" placeholder="Search" />
           </FormGroup>
           <Button onClick={props.handleSearch}>Search</Button>
-      </React.Fragment>
+      </Navbar.Form>
     );
 }
 
