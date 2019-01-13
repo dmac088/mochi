4e6a7aae9a0d00756ec1c4fd79135f65b71a92ab
dmac088 <danielmackie82@gmail.com>
2019-01-13 21:46:43 +0800

    <Navbar.Collapse> added

--- be/src/main/java/io/javabrains/springbootstarter/services/ProductCategoryDTOService.java
+++ be/src/main/java/io/javabrains/springbootstarter/services/ProductCategoryDTOService.java
@@ -2,16 +2,9 @@ package io.javabrains.springbootstarter.services;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-
-import io.javabrains.springbootstarter.domain.ProductAttribute;
 import io.javabrains.springbootstarter.domain.ProductCategory;
 import io.javabrains.springbootstarter.domain.ProductCategoryAttribute;
 import io.javabrains.springbootstarter.domain.ProductCategoryAttributeRepository;
