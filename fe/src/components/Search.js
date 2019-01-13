14ed034781d0ea407d489c2542c2bbcabb5afbd9
dmac088 <danielmackie82@gmail.com>
2019-01-13 21:30:56 +0800

    NavBar is tidied up

--- fe/src/components/Search.js
+++ fe/src/components/Search.js
@@ -1,15 +1,15 @@
 
 import React from 'react';
-import { Button, Form, FormGroup, FormControl } from 'react-bootstrap';
+import { Navbar, Button, FormGroup, FormControl } from 'react-bootstrap';
 
 const Search = (props) => {
     return(
-      <Form inline>
-        <FormGroup controlId="formInlineName">
-          <FormControl type="text" placeholder="Search" />
-        </FormGroup>
-        <Button onClick={props.handleSearch}>Search</Button>
-      </Form>
+      <React.Fragment>
+          <FormGroup controlId="formInlineName">
+            <FormControl type="text" placeholder="Search" />
+          </FormGroup>
+          <Button onClick={props.handleSearch}>Search</Button>
+      </React.Fragment>
     );
 }
 
