d7290e64d2d287d28a94159e9efb00d6ad6ef6c3
Mackie <Daniel.Mackie@dfs.com>
2019-01-14 18:34:58 +0800

    assumption made that all passed action payloads are new values, not reference values to existing objects

--- fe/src/services/cart/reducer.js
+++ fe/src/services/cart/reducer.js
@@ -19,7 +19,6 @@ export const reducer = (state = initialState, action) => {
 	//then passing that new object to the reducer
 	//the reducer then simply takes a copy of the object and passing it back to me
   let cartClone = null;
-	let itemClone = null;
 	switch (action.type) {
 		case actionTypes.UPDATE_CART:
 			console.log("UPDATE_CART");
@@ -27,23 +26,14 @@ export const reducer = (state = initialState, action) => {
 			//if we pass in a whole new cart we simply replace the current by returning a copy
 			return 	_.cloneDeep(action.cart);
 		case actionTypes.ADD_ITEM:
-			console.log("ADD_ITEM");
-			//remember whatever we do, just don't mutate the passed cart object
 			cartClone = _.cloneDeep(action.cart);
-			itemClone = _.cloneDeep(action.item);
-			cartClone.items.push(itemClone);
+			cartClone.items.push(action.item);
 			return cartClone;
 		case actionTypes.UPDATE_ITEM:
-			//remember whatever we do, just don't mutate the passed cart object
-			console.log("UPDATE_ITEM");
 			cartClone = _.cloneDeep(action.cart);
-			//should always be a new item object but we never know
-			itemClone = _.cloneDeep(action.item);
-			cartClone.items[action.index] = itemClone;
+			cartClone.items[action.index] = action.item;
 			return cartClone;
 		case actionTypes.REMOVE_ITEM:
-			//remember whatever we do, just don't mutate the passed cart object
-			console.log("REMOVE_ITEM");
 			cartClone = _.cloneDeep(action.cart);
 			cartClone.items = cartClone.items.filter(function(value, index, arr){
 															return value.productDTO.productId !== action.productId;
