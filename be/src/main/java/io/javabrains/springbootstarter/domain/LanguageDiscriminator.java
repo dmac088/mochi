5027d24a71dcd63f0d3909e0a565db37373fb157
dmac088 <danielmackie82@gmail.com>
2019-01-19 18:40:22 +0800

    chinese categoryDesc now indexed

--- be/src/main/java/io/javabrains/springbootstarter/domain/LanguageDiscriminator.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/LanguageDiscriminator.java
@@ -5,9 +5,10 @@ import org.hibernate.search.analyzer.Discriminator;
 public class LanguageDiscriminator implements Discriminator {
 
     public String getAnalyzerDefinitionName(Object value, Object entity, String field) {
-        if ( value == null || !( entity instanceof ProductAttribute ) ) {
+        if ( value == null || !( entity instanceof ProductAttribute || entity instanceof ProductCategoryAttribute)) {
             return null;
         }
+        //System.out.println(entity.getClass().toString());
         return (String) value;
 
     }
