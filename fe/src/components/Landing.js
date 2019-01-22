430de06dea9c2672fbbdabcccd913a1a4144d986
Mackie <Daniel.Mackie@dfs.com>
2019-01-22 13:40:47 +0800

    further along with query strings

--- fe/src/components/Landing.js
+++ fe/src/components/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Products from './Products';
 import { withRouter } from 'react-router-dom';
 import qs from 'query-string';
+import * as pageService from '../services/page';
 
 class Landing extends Component {
 
@@ -15,25 +16,45 @@ class Landing extends Component {
 
   componentWillMount() {
     const defaultQs = {
-                      currentCategory: "ALL",
-                      currentLang: "ENG",
-                      currentPage: 0,
-                      currentPageSize: 10,
-                      currentPageSort: 2,
-                      currentSearchTerm: "-Z",
+                      category: "ALL",
+                      lang: "ENG",
+                      page: 0,
+                      size: 10,
+                      sort: 2,
+                      term: "",
                     };
     const searchString = qs.stringify(defaultQs);
     this.props.history.push({
       "pathname": '/Search',
       "search": searchString,
     });
+    this.getProducts();
 	}
+
+  getProducts = () => {
+    const values = (qs.parse(this.props.location.search));
+    //console.log(values.category)
+    pageService.findAll(values.lang,
+                        values.category,
+                        (values.term === undefined || values.term === "") ? "-Z" : values.term,
+                        values.page,
+                        values.size,
+                        values.sort)
+    .then((response) => {
+       this.setState((prevState) => ({
+         page: response,
+       }));
+    })
+    .then(() => {
+      console.log(this.state.currentPage);
+    });
+  }
 
   render() {
     console.log(this.props.location.search);
     return(
           <Products
-            productsList={this.props.page.content}
+            productsList={this.state.page.content}
             addToCart={this.props.addToCart}
             openModal={this.props.openModal}
             currentLang={this.props.currentLang}
