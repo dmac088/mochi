e373773675362239b967f7f9abcb7adbbff28a9d
Mackie <Daniel.Mackie@dfs.com>
2019-01-24 12:06:04 +0800

    fixed PageSort component

--- fe/src/components/PageSort.js
+++ fe/src/components/PageSort.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { DropdownButton, MenuItem  } from 'react-bootstrap';
+import { withRouter } from "react-router-dom";
+import qs from 'query-string';
 
 const sortAliases = {
   1:"name (desc)",
   2:"price (desc)",
 }
 
-const PageSort = (props) => {
-  return (
-    <DropdownButton
-        id="pageSort"
-        title={"Sort: " + sortAliases[props.currentPageSort]}
-      >
-       <MenuItem id="1" onClick={props.changePageSort} eventKey="1" active={props.currentPageSort === 1}>{sortAliases[1]}</MenuItem>
-       <MenuItem id="2" onClick={props.changePageSort} eventKey="2" active={props.currentPageSort === 2}>{sortAliases[2]}</MenuItem>
-    </DropdownButton>
-  )
-};
+class PageSort extends Component {
 
-export default PageSort;
+  constructor(props) {
+    super(props);
+  }
+
+  changePageSort = (event) => {
+    //get the query parameters
+    const query =  { sort: event.target.id };
+    const searchString = qs.stringify(query);
+    this.props.history.push({
+      "pathname": '/Search',
+      "search": searchString,
+    });
+  }
+
+
+  render() {
+    return (
+      <DropdownButton
+          id="pageSort"
+          title={"Sort: " + sortAliases[this.props.sort]}>
+         <MenuItem id="1" onClick={this.changePageSort} eventKey="1" active={this.props.sort === 1}>{sortAliases[1]}</MenuItem>
+         <MenuItem id="2" onClick={this.changePageSort} eventKey="2" active={this.props.sort === 2}>{sortAliases[2]}</MenuItem>
+      </DropdownButton>
+    )
+  }
+
+}
+
+export default withRouter(PageSort);
