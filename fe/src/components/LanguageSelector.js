f80213ba4df63f85364130f950e767a948a88653
Mackie <Daniel.Mackie@dfs.com>
2019-01-24 12:11:27 +0800

    fixed language selector

--- fe/src/components/LanguageSelector.js
+++ fe/src/components/LanguageSelector.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavDropdown, MenuItem } from 'react-bootstrap';
+import { withRouter } from "react-router-dom";
+import qs from 'query-string';
 
-const LanguageSelector = (props) => {
-  return (
-    <NavDropdown eventKey={3} title={props.lang} id="basic-nav-dropdown">
-      <MenuItem id="ENG" onClick={props.changeLang}>
-          ENG
-      </MenuItem>
-      <MenuItem id="HKG" onClick={props.changeLang}>
-          HKG
-      </MenuItem>
-    </NavDropdown>
-  );
+class LanguageSelector extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  changeLang = (event) => {
+    //get the query parameters
+    const query =  { lang: event.target.id };
+    const searchString = qs.stringify(query);
+    this.props.history.push({
+      "pathname": '/Search',
+      "search": searchString,
+    });
+  }
+
+  render() {
+    return (
+      <NavDropdown eventKey={3} title={this.props.lang} id="basic-nav-dropdown">
+        <MenuItem id="ENG" onClick={this.changeLang}>
+            ENG
+        </MenuItem>
+        <MenuItem id="HKG" onClick={this.changeLang}>
+            HKG
+        </MenuItem>
+      </NavDropdown>
+    )
+  }
 }
 
-export default LanguageSelector;
+
+export default withRouter(LanguageSelector);
