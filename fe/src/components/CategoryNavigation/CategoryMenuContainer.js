88c750aa3d4fa50097f265df2048a23ddc47f421
Mackie <Daniel.Mackie@dfs.com>
2019-02-13 14:11:05 +0800

    removel of template comments

--- fe/src/components/CategoryNavigation/CategoryMenuContainer.js
+++ fe/src/components/CategoryNavigation/CategoryMenuContainer.js
@@ -36,12 +36,9 @@ class CategoryMenuContainer extends Component {
 render() {
   return (
     <div className="hero-side-category">
-      {/* Category Toggle Wrap */}
       <div className="category-toggle-wrap">
-        {/* Category Toggle */}
         <button onClick={this.toggleVisible} className="category-toggle"> <span className="arrow_carrot-right_alt2 mr-2" /> All Categories</button>
       </div>
-      {/* Category Menu */}
       <ReactTransitionGroup component="nav" className="category-menu">
         {
         ((this.state.menuVisible)
