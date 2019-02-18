c13730d13f34826ce8644e1ba6af14b1c932ea48
Mackie <Daniel.Mackie@dfs.com>
2019-02-18 16:04:55 +0800

    refactoring of pages

---
+++ fe/src/components/Products/Pagination.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+
+class Pagination extends Component {
+
+    render() {
+			return(
+        <div class="pagination-container">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12">
+                <!--=======  pagination-content  =======-->
+
+                <div class="pagination-content text-center">
+                  <ul>
+                    <li><a class="active" href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
+                  </ul>
+                </div>
+
+                <!--=======  End of pagination-content  =======-->
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    }
+}
--- fe/src/components/Products/Products.js
+++ fe/src/components/Products/Products.js
@@ -1,54 +1,50 @@
 import React from 'react';
 import Product from './Product';
+import CategorySidebar from './Sidebars/CategorySidebar';
+import BrandSidebar from './Sidebars/BrandSidebar';
+import PriceSidebar from './Sidebars/PriceSidebar';
+import TopRatedSidebar from './Sidebars/TopRatedSidebar';
+import TagSidebar from './Sidebars/TagSidebar';
+import ShopBanner from './ShopBanner';
+import BreadCrumb from './BreadCrumb';
+import ShopHeader from './ShopHeader';
+import Product from './Product';
+import Pagination from './Pagination';
 import NoResults from "../empty-states/NoResults";
 import ReactCSSTransitionGroup from 'react-addons-transition-group';
 
 
-const Products = (props) => {
+class Products extends Component {
 
-    	let productsData;
-
-			productsData = props.productsList.map(product =>{
-
+  render() {
 				return(
             <div class="shop-page-container mb-50">
               <div class="container">
                 <div class="row">
                   <div class="col-lg-3 order-2 order-lg-1">
-                    <!--=======  sidebar area  =======-->
-
                     <div class="sidebar-area">
-
+                      <CategorySidebar/>
+                      <BrandSidebar/>
+                      <PriceSidebar/>
+                      <TopRatedSidebar/>
+                      <TagSidebar/>
                     </div>
+                  </div>
+                  <div class="col-lg-9 order-1 order-lg-2 mb-sm-35 mb-xs-35">
+                    <ShopBanner/>
+                    <BreadCrumb/>
+                    <ShopHeader/>
+                    <div class="shop-product-wrap list row no-gutters mb-35">
+                      <Product/>
+                    </div>
+                    <Pagination/>
                   </div>
                 </div>
               </div>
             </div>
 					)
 				}
-		);
-
-		// Empty and Loading States
-		let view;
-		if(productsData.length <= 0){
-			view = <NoResults />
-		} else{
-			view =
-			<ReactCSSTransitionGroup
-				transitionName="fadeIn"
-				transitionEnterTimeout={500}
-				transitionLeaveTimeout={300}
-				component="div"
-				className="products">
-					{productsData}
-			</ReactCSSTransitionGroup>
-		}
-		return(
-			<div className="products-wrapper">
-				{view}
-			</div>
-		)
-	}
+}
 
 
 export default Products;
