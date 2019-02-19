d95c133e0126902608c81dbac9b09341f5f91b8e
Mackie <Daniel.Mackie@dfs.com>
2019-02-19 19:09:02 +0800

    breadcrumb now in correct position

--- fe/src/components/Products.js
+++ fe/src/components/Products.js
@@ -19,8 +19,10 @@ class Products extends Component {
 				return(
             <div class="shop-page-container mb-50">
               <div class="container">
+                <BreadCrumb/>
                 <div class="row">
                   <div class="col-lg-3 order-2 order-lg-1">
+
                     <div class="sidebar-area">
                       <CategorySidebar/>
                       <BrandSidebar/>
@@ -29,9 +31,10 @@ class Products extends Component {
                       <TagSidebar/>
                     </div>
                   </div>
+
                   <div class="col-lg-9 order-1 order-lg-2 mb-sm-35 mb-xs-35">
                     <ShopBanner/>
-                    <BreadCrumb/>
+
                     <ShopHeader/>
                     <div class="shop-product-wrap list row no-gutters mb-35">
                       <Product/>
