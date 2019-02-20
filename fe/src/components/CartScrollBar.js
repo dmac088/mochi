ce2a13170d72598069ea954a02b30770d618e0eb
dmac088 <danielmackie82@gmail.com>
2019-02-20 22:52:35 +0800

    asdf

--- fe/src/components/CartScrollBar.js
+++ fe/src/components/CartScrollBar.js
@@ -2,16 +2,20 @@ import React, {Component} from 'react';
 import { Scrollbars } from 'react-custom-scrollbars';
 
 class CartScrollBar extends Component{
+
   constructor(props){
     super(props);
     this.handleScroll = this.handleScroll.bind(this);
   }
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleScroll, { passive: true });
   }
+
   componentWillUnmount(){
     window.removeEventListener('scroll', this.handleScroll, { passive: true });
   }
+
   handleScroll(event) {
     const positions = this.refs.scrollbars.getValues();
     //When the bottom is reached and we're scrolling down, prevent scrolling of the window
@@ -20,6 +24,7 @@ class CartScrollBar extends Component{
       event.stopPropagation();
     }
   }
+  
   render(){
     return(
       <Scrollbars style={{ width: 360, height: 320 }} ref="scrollbars">
