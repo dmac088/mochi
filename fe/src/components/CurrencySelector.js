4fa8a28cca0f6371cd878714b9d8d278f9d9e7a7
Mackie <Daniel.Mackie@dfs.com>
2019-02-22 13:50:38 +0800

    category navigation resolved

---
+++ fe/src/components/CurrencySelector.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import { updateParams } from '../services/helpers/ScreenHelper';
+
+class LanguageSelector extends Component {
+
+  changeCurr = (e) => {
+    const url = this.props.location.pathname;
+    const search = this.props.location.search;
+    const { currency } = this.props.match.params;
+    this.props.history.push(url.replace(currency || '', event.target.id) + search);
+  }
+
+  render() {
+    return (
+
+      <ul>
+        <li><a id="USD" onClick={this.changeCurr}>USD</a></li>
+        <li><a id="HKD" onClick={this.changeCurr}>HKD</a></li>
+      </ul>
+    )
+  }
+}
+
+
+export default withRouter(LanguageSelector);
