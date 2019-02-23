03644f18029d7a9d2e6e2eb995f7ca4b7b28e34e
dmac088 <danielmackie82@gmail.com>
2019-02-23 22:40:02 +0800

    minified slick added to intex.html

--- fe/src/components/Landing/TabSliderContiner.js
+++ fe/src/components/Landing/TabSliderContiner.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Slider from "react-slick";
 const $ = window.$;
 
 class TabSliderContiner extends Component {
