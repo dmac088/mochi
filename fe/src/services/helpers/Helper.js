c588eb576d635fe605bbdf4436316fb77217e376
Mackie <Daniel.Mackie@dfs.com>
2019-02-25 17:48:32 +0800

    changes HeroSlider

--- fe/src/services/helpers/Helper.js
+++ fe/src/services/helpers/Helper.js
@@ -51,3 +51,33 @@ export const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
         <i className="fa fa-caret-right"></i>
       </button>
     );
+
+export const HsSlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+      <button
+        {...props}
+        className={
+          "slick-prev slick-arrow" +
+          (currentSlide === 0 ? " slick-disabled" : "")
+        }
+        aria-label="Previous"
+        aria-disabled={currentSlide === 0 ? true : false}
+        type="button"
+      >
+        <i className="fa fa-chevron-left"></i>
+      </button>
+    );
+
+export const HsSlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+          <button
+            {...props}
+            className={
+              "slick-next slick-arrow" +
+              (currentSlide === slideCount - 1 ? " slick-disabled" : "")
+            }
+            aria-label="Next"
+            aria-disabled={currentSlide === slideCount - 1 ? true : false}
+            type="button"
+          >
+            <i className="fa fa-chevron-right"></i>
+          </button>
+        );
