f0f0d1de4a8221c204858a25dfcb3fa464db2c79
Mackie <Daniel.Mackie@dfs.com>
2019-02-25 15:51:03 +0800

    refactoring

--- fe/src/components/Landing/Tabs/OnSale.js
+++ fe/src/components/Landing/Tabs/OnSale.js
@@ -3,8 +3,7 @@ import { connect } from 'react-redux';
 import Slider from "react-slick";
 const $ = window.$;
 
-class TabSliderContiner extends Component {
-
+class OnSale extends Component {
 
   next = () => {
     this.slider.slickNext();
@@ -27,8 +26,63 @@ class TabSliderContiner extends Component {
           <button onClick={this.previous} type="button" class="slick-arrow slick-prev"><i class="fa fa-caret-left"></i></button>
           <button onClick={this.next} type="button" class="slick-arrow slick-next"><i class="fa fa-caret-right"></i></button>
           <Slider ref={c => (this.slider = c)} {...settings}>
+          <div className="tab-slider-container">
+
             <div className="single-tab-slider-item">
+
               <div className="gf-product tab-slider-sub-product">
+                <div className="image">
+                  <a href="single-product.html">
+                    <span className="onsale">Sale!</span>
+                    <img src="assets/images/products/product09.jpg" className="img-fluid" alt="" />
+                  </a>
+                  <div className="product-hover-icons">
+                    <a className="active" href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
+                    <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
+                    <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
+                    <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+                  </div>
+                </div>
+                <div className="product-content">
+                  <div className="product-categories">
+                    <a href="shop-left-sidebar.html">Fast Foods</a>,
+                    <a href="shop-left-sidebar.html">Vegetables</a>
+                  </div>
+                  <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
+                  <div className="price-box">
+                    <span className="main-price">$89.00</span>
+                    <span className="discounted-price">$80.00</span>
+                  </div>
+                </div>
+              </div>
+              <div className="gf-product tab-slider-sub-product">
+                <div className="image">
+                  <a href="single-product.html">
+                    <span className="onsale">Sale!</span>
+                    <img src="assets/images/products/product10.jpg" className="img-fluid" alt="" />
+                  </a>
+                  <div className="product-hover-icons">
+                    <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
+                    <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
+                    <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
+                    <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+                  </div>
+                </div>
+                <div className="product-content">
+                  <div className="product-categories">
+                    <a href="shop-left-sidebar.html">Fast Foods</a>,
+                    <a href="shop-left-sidebar.html">Vegetables</a>
+                  </div>
+                  <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
+                  <div className="price-box">
+                    <span className="main-price">$89.00</span>
+                    <span className="discounted-price">$80.00</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="single-tab-slider-item">
+              <div className="gf-product gf-product tab-slider-sub-product">
                 <div className="image">
                   <a href="single-product.html">
                     <span className="onsale">Sale!</span>
@@ -229,62 +283,10 @@ class TabSliderContiner extends Component {
                 </div>
               </div>
             </div>
-            <div className="single-tab-slider-item">
-              <div className="gf-product tab-slider-sub-product">
-                <div className="image">
-                  <a href="single-product.html">
-                    <span className="onsale">Sale!</span>
-                    <img src="assets/images/products/product09.jpg" className="img-fluid" alt="" />
-                  </a>
-                  <div className="product-hover-icons">
-                    <a className="active" href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
-                    <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
-                    <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
-                    <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
-                  </div>
-                </div>
-                <div className="product-content">
-                  <div className="product-categories">
-                    <a href="shop-left-sidebar.html">Fast Foods</a>,
-                    <a href="shop-left-sidebar.html">Vegetables</a>
-                  </div>
-                  <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
-                  <div className="price-box">
-                    <span className="main-price">$89.00</span>
-                    <span className="discounted-price">$80.00</span>
-                  </div>
-                </div>
-              </div>
-              <div className="gf-product tab-slider-sub-product">
-                <div className="image">
-                  <a href="single-product.html">
-                    <span className="onsale">Sale!</span>
-                    <img src="assets/images/products/product10.jpg" className="img-fluid" alt="" />
-                  </a>
-                  <div className="product-hover-icons">
-                    <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
-                    <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
-                    <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
-                    <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
-                  </div>
-                </div>
-                <div className="product-content">
-                  <div className="product-categories">
-                    <a href="shop-left-sidebar.html">Fast Foods</a>,
-                    <a href="shop-left-sidebar.html">Vegetables</a>
-                  </div>
-                  <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
-                  <div className="price-box">
-                    <span className="main-price">$89.00</span>
-                    <span className="discounted-price">$80.00</span>
-                  </div>
-                </div>
-              </div>
-            </div>
           </Slider>
         </div>
     )
   }
 }
 
-export default TabSliderContiner;
+export default OnSale;
