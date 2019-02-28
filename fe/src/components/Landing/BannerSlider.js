dbc38a121bf1ea1be315be18743e7ea322a4a99c
Mackie <Daniel.Mackie@dfs.com>
2019-02-28 19:19:29 +0800

    bannerslidercontainer updated

--- fe/src/components/Landing/BannerSlider.js
+++ fe/src/components/Landing/BannerSlider.js
@@ -7,24 +7,24 @@ const $ = window.$;
 
 class BannerSlider extends Component {
 
-componentDidMount(){
-    this.initBannerSlider();
+
+next = () => {
+  this.slider.slickNext();
 }
 
-initBannerSlider = () => {
-  /*--
-  Banner slider active
-  -----------------------------------*/
-  /*
-  let bannerSlider = $('.banner-slider-container');
-  bannerSlider.slick({
+previous = () => {
+  this.slider.slickPrev();
+}
+
+render() {
+  const settings = {
     arrows: true,
     autoplay: false,
     dots: false,
     infinite: true,
     slidesToShow: 4,
-    prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-caret-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fa fa-caret-right"></i></button>',
+    prevArrow: <SlickArrowLeft />,
+    nextArrow: <SlickArrowRight />,
     responsive: [{
       breakpoint: 1499,
       settings: {
@@ -62,123 +62,121 @@ initBannerSlider = () => {
       }
     }
     ]
-  });
-  */
-}
-
-render() {
+  };
   return (
     <div className="banner-slider-container">
-      <div className="gf-product banner-slider-product">
-        <div className="image">
-          <a href="single-product.html">
-            <span className="onsale">Sale!</span>
-            <img src="assets/images/products/product01.jpg" className="img-fluid" alt="" />
-          </a>
-          <div className="product-hover-icons">
-            <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+      <Slider ref={c => (this.slider = c)} {...settings}>
+        <div className="gf-product banner-slider-product">
+          <div className="image">
+            <a href="single-product.html">
+              <span className="onsale">Sale!</span>
+              <img src="assets/images/products/product01.jpg" className="img-fluid" alt="" />
+            </a>
+            <div className="product-hover-icons">
+              <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+            </div>
+          </div>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
+            <div className="price-box">
+              <span className="main-price">$89.00</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
           </div>
         </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
+        <div className="gf-product banner-slider-product">
+          <div className="image">
+            <a href="single-product.html">
+              <span className="onsale">Sale!</span>
+              <img src="assets/images/products/product02.jpg" className="img-fluid" alt="" />
+            </a>
+            <div className="product-hover-icons">
+              <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+            </div>
           </div>
-          <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
-          <div className="price-box">
-            <span className="main-price">$89.00</span>
-            <span className="discounted-price">$80.00</span>
-          </div>
-        </div>
-      </div>
-      <div className="gf-product banner-slider-product">
-        <div className="image">
-          <a href="single-product.html">
-            <span className="onsale">Sale!</span>
-            <img src="assets/images/products/product02.jpg" className="img-fluid" alt="" />
-          </a>
-          <div className="product-hover-icons">
-            <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
-          </div>
-        </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
-          </div>
-          <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
-          <div className="price-box">
-            <span className="main-price">$89.00</span>
-            <span className="discounted-price">$80.00</span>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
+            <div className="price-box">
+              <span className="main-price">$89.00</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="gf-product  banner-slider-product">
-        <div className="image">
-          <a href="single-product.html">
-            <span className="onsale">Sale!</span>
-            <img src="assets/images/products/product03.jpg" className="img-fluid" alt="" />
-          </a>
-          <div className="product-hover-icons">
-            <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+        <div className="gf-product  banner-slider-product">
+          <div className="image">
+            <a href="single-product.html">
+              <span className="onsale">Sale!</span>
+              <img src="assets/images/products/product03.jpg" className="img-fluid" alt="" />
+            </a>
+            <div className="product-hover-icons">
+              <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+            </div>
+          </div>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
+            <div className="price-box">
+              <span className="main-price">$89.00</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
           </div>
         </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
+        <div className="gf-product  banner-slider-product">
+          <div className="image">
+            <a href="single-product.html">
+              <span className="onsale">Sale!</span>
+              <img src="assets/images/products/product04.jpg" className="img-fluid" alt="" />
+            </a>
+            <div className="product-hover-icons">
+              <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+            </div>
           </div>
-          <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
-          <div className="price-box">
-            <span className="main-price">$89.00</span>
-            <span className="discounted-price">$80.00</span>
-          </div>
-        </div>
-      </div>
-      <div className="gf-product  banner-slider-product">
-        <div className="image">
-          <a href="single-product.html">
-            <span className="onsale">Sale!</span>
-            <img src="assets/images/products/product04.jpg" className="img-fluid" alt="" />
-          </a>
-          <div className="product-hover-icons">
-            <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
-          </div>
-        </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
-          </div>
-          <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
-          <div className="price-box">
-            <span className="main-price">$89.00</span>
-            <span className="discounted-price">$80.00</span>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">Sed tempor ehicula non commodo</a></h3>
+            <div className="price-box">
+              <span className="main-price">$89.00</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="gf-product  banner-slider-product">
-        <div className="image">
-          <a href="single-product.html">
-            <span className="onsale">Sale!</span>
-            <img src="assets/images/products/product05.jpg" className="img-fluid" alt="" />
-          </a>
-          <div className="product-hover-icons">
-            <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+        <div className="gf-product  banner-slider-product">
+          <div className="image">
+            <a href="single-product.html">
+              <span className="onsale">Sale!</span>
+              <img src="assets/images/products/product05.jpg" className="img-fluid" alt="" />
+            </a>
+            <div className="product-hover-icons">
+              <a href="#" data-tooltip="Quick view" data-toggle="modal" data-target="#quick-view-modal-container"> <span className="icon_search" /> </a>
+            </div>
+          </div>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
+            <div className="price-box">
+              <span className="main-price">$89.00</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
           </div>
         </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
-          </div>
-          <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
-          <div className="price-box">
-            <span className="main-price">$89.00</span>
-            <span className="discounted-price">$80.00</span>
-          </div>
-        </div>
-      </div>
+      </Slider>
     </div>
   )
 }
