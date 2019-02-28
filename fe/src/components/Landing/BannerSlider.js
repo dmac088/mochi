e9d2022b120881c21c118a532d33f79655d6127f
Mackie <Daniel.Mackie@dfs.com>
2019-02-28 18:56:13 +0800

    removed unecessary css classes

--- fe/src/components/Landing/BannerSlider.js
+++ fe/src/components/Landing/BannerSlider.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Slider from "react-slick";
+import { SlickArrowLeft, SlickArrowRight } from '../../services/helpers/Helper';
 const $ = window.$;
 
 
@@ -13,6 +15,7 @@ initBannerSlider = () => {
   /*--
   Banner slider active
   -----------------------------------*/
+  /*
   let bannerSlider = $('.banner-slider-container');
   bannerSlider.slick({
     arrows: true,
@@ -60,6 +63,7 @@ initBannerSlider = () => {
     }
     ]
   });
+  */
 }
 
 render() {
