20d613b50cf6593ea0d0d9b7fdb3e76c76cac26b
dmac088 <danielmackie82@gmail.com>
2019-03-01 19:56:30 +0800

    added single product page with router

--- fe/src/components/Products/Product.js
+++ fe/src/components/Products/Product.js
@@ -5,61 +5,120 @@ class Product extends Component {
 
   render() {
     return (
-      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
-          <div className="gf-product shop-grid-view-product">
-            <div className="image">
-              <a href="single-product.html">
-                <span className="onsale">Sale!</span>
-                <img src="assets/images/products/product03.jpg" className="img-fluid" alt=""/>
-              </a>
-              <div className="product-hover-icons">
-                <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt"></span></a>
-                <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt"></span> </a>
-                <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt"></span> </a>
-                <a href="#" data-tooltip="Quick view" data-toggle = "modal" data-target="#quick-view-modal-container"> <span className="icon_search"></span> </a>
-              </div>
-            </div>
-            <div className="product-content">
-              <div className="product-categories">
-                <a href="shop-left-sidebar.html">Fast Foods</a>,
-                <a href="shop-left-sidebar.html">Vegetables</a>
-              </div>
-              <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
-              <div className="price-box">
-                <span className="main-price">$89.00</span>
-                <span className="discounted-price">$80.00</span>
-              </div>
-            </div>
-          </div>
-          <div className="gf-product shop-list-view-product">
-            <div className="image">
-              <a href="single-product.html">
-                <span className="onsale">Sale!</span>
-                <img src="assets/images/products/product03.jpg" className="img-fluid" alt=""/>
-              </a>
-              <div className="product-hover-icons">
-                <a href="#" data-tooltip="Quick view" data-toggle = "modal" data-target="#quick-view-modal-container"> <span className="icon_search"></span> </a>
-              </div>
-            </div>
-            <div className="product-content">
-              <div className="product-categories">
-                <a href="shop-left-sidebar.html">Fast Foods</a>,
-                <a href="shop-left-sidebar.html">Vegetables</a>
-              </div>
-              <h3 className="product-title"><a href="single-product.html">Ornare sed consequat nisl eget</a></h3>
-              <div className="price-box mb-20">
-                <span className="main-price">$89.00</span>
-                <span className="discounted-price">$80.00</span>
-              </div>
-              <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
-              <div className="list-product-icons">
-                <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt"></span></a>
-                <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt"></span> </a>
-                <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt"></span> </a>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="single-product-content ">
+        <div class="container">
+            <div class="single-product-content-container mb-35">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 col-xs-12">
+                                    <div class="product-image-slider d-flex flex-custom-xs-wrap flex-sm-nowrap align-items-center mb-sm-35">
+
+                                        <div class="product-small-image-list">
+                                            <div class="nav small-image-slider-single-product" role="tablist">
+                                            <div class="single-small-image img-full">
+                                                <a data-toggle="tab" id="single-slide-tab-1" href="#single-slide1"><img src="assets/images/big-product-image/product04.jpg"
+                                                class="img-fluid" alt="" /></a>
+                                            </div>
+                                            <div class="single-small-image img-full">
+                                                <a data-toggle="tab" id="single-slide-tab-2" href="#single-slide2"><img src="assets/images/big-product-image/product05.jpg"
+                                                    class="img-fluid" alt="" /></a>
+                                                </div>
+                                            <div class="single-small-image img-full">
+                                                <a data-toggle="tab" id="single-slide-tab-3" href="#single-slide3"><img src="assets/images/big-product-image/product06.jpg"
+                                                    class="img-fluid" alt="" /></a>
+                                                </div>
+                                                <div class="single-small-image img-full">
+                                                    <a data-toggle="tab" id="single-slide-tab-4" href="#single-slide4"><img src="assets/images/big-product-image/product07.jpg"
+                                                    class="img-fluid" alt="" /></a>
+                                                </div>
+                                            </div>
+                                           </div>
+                                        <div class="tab-content product-large-image-list">
+                                            <div class="tab-pane fade show active" id="single-slide1" role="tabpanel" aria-labelledby="single-slide-tab-1">
+                                                <div class="single-product-img easyzoom img-full">
+                                                    <img src="assets/images/big-product-image/product04.jpg" class="img-fluid" alt="" />
+                                                    <a href="assets/images/big-product-image/product04.jpg" class="big-image-popup"><i class="fa fa-search-plus"></i></a>
+                                                </div>
+                                            </div>
+                                            <div class="tab-pane fade" id="single-slide2" role="tabpanel" aria-labelledby="single-slide-tab-2">
+                                                <div class="single-product-img easyzoom img-full">
+                                                    <img src="assets/images/big-product-image/product05.jpg" class="img-fluid" alt="" />
+                                                    <a href="assets/images/big-product-image/product05.jpg" class="big-image-popup"><i class="fa fa-search-plus"></i></a>
+                                                </div>
+                                            </div>
+                                            <div class="tab-pane fade" id="single-slide3" role="tabpanel" aria-labelledby="single-slide-tab-3">
+                                                <div class="single-product-img easyzoom img-full">
+                                                    <img src="assets/images/big-product-image/product06.jpg" class="img-fluid" alt="" />
+                                                    <a href="assets/images/big-product-image/product06.jpg" class="big-image-popup"><i class="fa fa-search-plus"></i></a>
+                                                </div>
+                                            </div>
+                                            <div class="tab-pane fade" id="single-slide4" role="tabpanel" aria-labelledby="single-slide-tab-4">
+                                                <div class="single-product-img easyzoom img-full">
+                                                    <img src="assets/images/big-product-image/product07.jpg" class="img-fluid" alt="" />
+                                                    <a href="assets/images/big-product-image/product07.jpg" class="big-image-popup"><i class="fa fa-search-plus"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 col-md-12 col-xs-12">
+                                    <div class="product-feature-details">
+                                        <h2 class="product-title mb-15">Kaoreet lobortis sagittis laoreet</h2>
+
+                                        <p class="product-rating">
+                                            <i class="fa fa-star active"></i>
+                                            <i class="fa fa-star active"></i>
+                                            <i class="fa fa-star active"></i>
+                                            <i class="fa fa-star active"></i>
+                                            <i class="fa fa-star"></i>
+
+                                            <a href="#">(1 customer review)</a>
+                                        </p>
+
+                                        <h2 class="product-price mb-15">
+                                            <span class="main-price">$12.90</span>
+                                            <span class="discounted-price"> $10.00</span>
+                                        </h2>
+
+                                        <p class="product-description mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+
+
+                                        <div class="cart-buttons mb-20">
+                                            <div class="pro-qty mr-20 mb-xs-20">
+                                                <input type="text" value="1" />
+                                            </div>
+                                            <div class="add-to-cart-btn">
+                                                <a href="#"><i class="fa fa-shopping-cart"></i> Add to Cart</a>
+                                            </div>
+                                        </div>
+
+                                        <div class="single-product-action-btn mb-20">
+                                            <a href="#" data-tooltip="Add to wishlist"> <span class="icon_heart_alt"></span> Add to wishlist</a>
+                                            <a href="#" data-tooltip="Add to compare"> <span class="arrow_left-right_alt"></span> Add to compare</a>
+                                        </div>
+
+
+                                        <div class="single-product-category mb-20">
+                                            <h3>Categories: <span><a href="shop-left-sidebar.html">Fast Foods</a>, <a href="shop-left-sidebar.html">Vegetables</a></span></h3>
+                                        </div>
+
+
+                                        <div class="social-share-buttons">
+                                            <h3>share this product</h3>
+                                            <ul>
+                                                <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+                                                <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+                                                <li><a class="google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+                                                <li><a class="pinterest" href="#"><i class="fa fa-pinterest"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
       );
     }
 }
