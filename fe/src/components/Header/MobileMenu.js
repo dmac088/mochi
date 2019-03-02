8791fd6b3d27c67dd928f8e5a44b22a3357ed99f
dmac088 <danielmackie82@gmail.com>
2019-03-02 20:40:06 +0800

    mean menu working nicely without jquery

--- fe/src/components/Header/MobileMenu.js
+++ fe/src/components/Header/MobileMenu.js
@@ -5,7 +5,11 @@ class MobileMenu extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { revealMenu: false };
+    this.state = {
+      revealMenu: false,
+      revealBrandMenu: false,
+      revealBlogMenu: false,
+    };
   }
 
   home = (e) => {
@@ -27,6 +31,20 @@ class MobileMenu extends Component {
     })
   }
 
+  toggleBrandMenu = (e) => {
+    e.preventDefault();
+    this.setState({
+      revealBrandMenu: !this.state.revealBrandMenu,
+    })
+  }
+
+  toggleBlogMenu = (e) => {
+    e.preventDefault();
+    this.setState({
+      revealBlogMenu: !this.state.revealBlogMenu,
+    })
+  }
+
 
   render() {
     return (
@@ -43,11 +61,14 @@ class MobileMenu extends Component {
           <span className={(this.state.revealMenu) ? "menu-close" : "menu-bar"} />
           </a>
           <nav className="mean-nav">
-            <ul style={ (this.state.revealMenu) ? {display: 'block'} : {display: 'none'}}>
-              <li className="active"><a href="#">HOME</a></li>
+            <ul style={(this.state.revealMenu) ? {display: 'block'} : {display: 'none'}}>
+              <li className="active">
+                <a href="#">HOME</a>
+              </li>
               <li className="menu-item-has-children">
-                <a href="#">Brands</a>
-                <ul className="sub-menu mega-menu three-column" style={{display: 'none'}}>
+                <a onClick={this.toggleBrandMenu}
+                   href="#">Brands</a>
+                <ul className="sub-menu mega-menu three-column" style={(this.state.revealBrandMenu) ? {display: 'block'} : {display: 'none'}}>
                   <li><a href="#">Comvita</a></li>
                   <li><a href="#">Airborne</a></li>
                   <li><a href="#">Happy Bee</a></li>
@@ -56,11 +77,18 @@ class MobileMenu extends Component {
                   <li><a href="#">Trilogy</a></li>
                   <li><a href="#">Anchor</a></li>
                 </ul>
-                <a className="mean-expand" href="#" style={{fontSize: 0}}>+</a>
+                <a  onClick={this.toggleBrandMenu}
+                    className="mean-expand"
+                    href="#"
+                    style={{fontSize: 0}}>
+                    {(this.state.revealBrandMenu) ? "-" : "+" }
+                </a>
               </li>
               <li className="menu-item-has-children">
-                <a href="#">BLOG</a>
-                <ul className="sub-menu" style={{display: 'none'}}>
+                <a  onClick={this.toggleBlogMenu}
+                    href="#">BLOG</a>
+                <ul className="sub-menu"
+                    style={(this.state.revealBlogMenu) ? {display: 'block'} : {display: 'none'}}>
                   <li><a href="blog-3-column.html">Blog 3 column</a></li>
                   <li><a href="blog-grid-left-sidebar.html">Blog Grid Left Sidebar</a></li>
                   <li><a href="blog-grid-right-sidebar.html">Blog Grid Right Sidebar</a></li>
@@ -73,7 +101,12 @@ class MobileMenu extends Component {
                   <li><a href="blog-post-audio-format.html">Blog Post Audio Format</a></li>
                   <li><a href="blog-post-video-format.html">Blog Post Video Format</a></li>
                 </ul>
-                <a className="mean-expand" href="#" style={{fontSize: 0}}>+</a>
+                <a  onClick={this.toggleBlogMenu}
+                    className="mean-expand"
+                    href="#"
+                    style={{fontSize: 0}}>
+                    {(this.state.revealBlogMenu) ? "-" : "+" }
+                </a>
               </li>
               <li className="mean-last"><a href="#">CONTACT</a></li>
             </ul>
