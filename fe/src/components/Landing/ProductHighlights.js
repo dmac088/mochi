826beaf152a9d9a53bbaeea7175a369023f219bc
dmac088 <danielmackie82@gmail.com>
2019-03-03 13:18:55 +0800

    syntactic sugar

--- fe/src/components/Landing/ProductHighlights.js
+++ fe/src/components/Landing/ProductHighlights.js
@@ -9,7 +9,9 @@ class ProductHighlights extends Component {
     super(props);
     this.state = {
         theposition: 0,
-        showTab: "featured-tab",
+        showFeatured: true,
+        showArrival: false,
+        showOnSale: false,
     };
   }
 
@@ -17,11 +19,14 @@ class ProductHighlights extends Component {
    showTab = (e) => {
      e.preventDefault();
      this.setState({
-       showTab: e.target.id
+       showFeatured: e.target.id === "featured-tab",
+       showArrival: e.target.id === "new-arrival-tab",
+       showOnSale: e.target.id === "nav-onsale-tab",
      });
    }
 
    render() {
+    const { showFeatured, showArrival,  showOnSale } = this.state;
     return (
       <div className="slider tab-slider mb-35">
         <div className="container">
@@ -30,19 +35,19 @@ class ProductHighlights extends Component {
               <div className="tab-slider-wrapper">
                   <nav>
                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                      <a onClick={this.showTab} className={"nav-item nav-link " + ((this.state.showTab === "featured-tab") ? "active" : "")} id="featured-tab" data-toggle="tab" href="#featured" role="tab" aria-selected="true">Featured</a>
-                      <a onClick={this.showTab} className={"nav-item nav-link " + ((this.state.showTab === "new-arrival-tab") ? "active" : "")} id="new-arrival-tab" data-toggle="tab" href="#new-arrivals" role="tab" aria-selected="false">New Arrival</a>
-                      <a onClick={this.showTab} className={"nav-item nav-link " + ((this.state.showTab === "nav-onsale-tab") ? "active" : "")} id="nav-onsale-tab" data-toggle="tab" href="#on-sale" role="tab" aria-selected="false">On Sale</a>
+                      <a onClick={this.showTab} className={"nav-item nav-link " + ((showFeatured) ? "active" : "")} id="featured-tab" data-toggle="tab" href="#featured" role="tab" aria-selected="true">Featured</a>
+                      <a onClick={this.showTab} className={"nav-item nav-link " + ((showArrival) ? "active" : "")} id="new-arrival-tab" data-toggle="tab" href="#new-arrivals" role="tab" aria-selected="false">New Arrival</a>
+                      <a onClick={this.showTab} className={"nav-item nav-link " + ((showOnSale)? "active" : "")} id="nav-onsale-tab" data-toggle="tab" href="#on-sale" role="tab" aria-selected="false">On Sale</a>
                     </div>
                   </nav>
                   <div className="tab-content" id="nav-tabContent">
-                    <div className={"tab-pane fade " + ((this.state.showTab === "featured-tab") ? "show active" : "")} id="featured" role="tabpanel" aria-labelledby="featured-tab">
+                    <div className={"tab-pane fade " + ((showFeatured) ? "show active" : "")} id="featured" role="tabpanel" aria-labelledby="featured-tab">
                       <Featured />
                     </div>
-                    <div className={"tab-pane fade " + ((this.state.showTab === "new-arrival-tab") ? "show active" : "")} id="new-arrivals" role="tabpanel" aria-labelledby="new-arrival-tab">
+                    <div className={"tab-pane fade " + ((showArrival) ? "show active" : "")} id="new-arrivals" role="tabpanel" aria-labelledby="new-arrival-tab">
                       <NewArrival />
                     </div>
-                    <div className={"tab-pane fade " + ((this.state.showTab === "nav-onsale-tab") ? "show active" : "")} id="on-sale" role="tabpanel" aria-labelledby="nav-onsale-tab">
+                    <div className={"tab-pane fade " + ((showOnSale)? "show active" : "")} id="on-sale" role="tabpanel" aria-labelledby="nav-onsale-tab">
                       <OnSale />
                     </div>
                   </div>
