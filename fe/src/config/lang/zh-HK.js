31d3890b1ed43163f24c5dee85bbe2be428b509a
Mackie <Daniel.Mackie@dfs.com>
2019-03-13 12:02:06 +0800

    dual language for category menu

--- fe/src/config/lang/zh-HK.js
+++ fe/src/config/lang/zh-HK.js
@@ -4,6 +4,9 @@ const lang = {
 	addToCart: "加入购物篮",
   addedToCart: "✔ 添加",
 	searchProduct: "搜索",
+	categoryMenuHeading: "所有類別",
+	moreCategories: "更多類別",
+	lessCategories: "少類別",
 };
 
 export default lang;
