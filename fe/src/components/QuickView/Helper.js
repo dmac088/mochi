53177f928ed2a70bb5b0fa4bb496fbab6ae79d7b
Mackie <Daniel.Mackie@dfs.com>
2019-03-14 13:42:20 +0800

    modal slider now working

--- fe/src/components/QuickView/Helper.js
+++ fe/src/components/QuickView/Helper.js
@@ -2,18 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-export const SlickArrowPrevFullScreen = ({ currentSlide, slideCount, ...props }) => (
-      <i className="fa fa-angle-up"></i>
+export const SlickArrowPrev = ({ currentSlide, slideCount, ...props }) => (
+      <i {...props} className="fa fa-angle-left slick-arrow"></i>
 );
 
-export const SlickArrowNextFullScreen = ({ currentSlide, slideCount, ...props }) => (
-      <i className="fa fa-angle-down slick-next-btn"></i>
-);
-
-export const SlickArrowPrevMobile = ({ currentSlide, slideCount, ...props }) => (
-      <i className="fa fa-angle-left"></i>
-);
-
-export const SlickArrowNextMobile = ({ currentSlide, slideCount, ...props }) => (
-      <i className="fa fa-angle-right slick-next-btn"></i>
+export const SlickArrowNext = ({ currentSlide, slideCount, ...props }) => (
+      <i {...props} className="fa fa-angle-right slick-next-btn slick-arrow"></i>
 );
