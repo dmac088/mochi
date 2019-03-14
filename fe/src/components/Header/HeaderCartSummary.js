aa79638b04d125b94ee0d46f8c4315bd536abf26
Mackie <Daniel.Mackie@dfs.com>
2019-03-14 17:56:35 +0800

    adding to cart working

--- fe/src/components/Header/HeaderCartSummary.js
+++ fe/src/components/Header/HeaderCartSummary.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 import ReactTransitionGroup from 'react-addons-transition-group';
 import Velocity from 'velocity-animate';
 import 'velocity-animate/velocity.ui';
+import * as cartSelector from '../../services/cart/selectors';
+import * as cartService from '../../services/cart';
 const $ = window.$;
 
 class HeaderCartSummary extends Component {
@@ -41,6 +43,7 @@ class HeaderCartSummary extends Component {
   }
 
   render() {
+    const { cart } = this.props;
     return(
       <div onMouseEnter={this.setinContainer} onMouseLeave={this.setNotinContainer} className="shopping-cart" id="shopping-cart">
           <a onClick={this.viewCart} href="#">
@@ -50,16 +53,14 @@ class HeaderCartSummary extends Component {
             <div className="cart-info d-inline-block">
               <p>Shopping Cart
                 <span>
-                  {this.props.cart.totalItems} items - ${this.props.cart.totalAmount}
+                  {cart.totalItems} items - ${cart.totalAmount}
                 </span>
               </p>
             </div>
          </a>
         <ReactTransitionGroup>
           { this.state.visible ? <Accordion
-                                    match={this.props.match}
-                                    history={this.props.history}
-                                    location={this.props.location}
+                                    {...this.props}
                                 /> : null }
         </ReactTransitionGroup>
       </div>
@@ -67,73 +68,81 @@ class HeaderCartSummary extends Component {
   }
 }
 
-  class Accordion extends React.Component {
+class Accordion extends React.Component {
 
-  	componentWillEnter (callback) {
-    	const element = ReactDOM.findDOMNode(this.container);
-      Velocity(element, 'slideDown', { duration: 1000 }).then(callback);
-    }
+  componentWillEnter (callback) {
+    const element = ReactDOM.findDOMNode(this.container);
+    Velocity(element, 'slideDown', { duration: 1000 }).then(callback);
+  }
 
-  	componentWillLeave (callback) {
-      const element = ReactDOM.findDOMNode(this.container);
-      Velocity(element, 'slideUp', { duration: 1000 }).then(callback);
-    }
+  componentWillLeave (callback) {
+    const element = ReactDOM.findDOMNode(this.container);
+    Velocity(element, 'slideUp', { duration: 1000 }).then(callback);
+  }
 
-  	setContainer = (c) => {
-  		this.container = c;
-  	}
+  setContainer = (c) => {
+  	this.container = c;
+  }
 
-    checkout = (e) => {
-      e.preventDefault();
-      const { locale, currency } = this.props.match.params;
-      this.props.history.push('/' + locale + '/' + currency + '/Checkout');
-    }
+  checkout = (e) => {
+    e.preventDefault();
+    const { locale, currency } = this.props.match.params;
+    this.props.history.push('/' + locale + '/' + currency + '/Checkout');
+  }
 
-    viewCart = (e) => {
-      e.preventDefault();
-      const { locale, currency } = this.props.match.params;
-      this.props.history.push('/' + locale + '/' + currency + '/Cart');
-    }
+  viewCart = (e) => {
+    e.preventDefault();
+    const { locale, currency } = this.props.match.params;
+    this.props.history.push('/' + locale + '/' + currency + '/Cart');
+  }
+
+  removeItem = (e) => {
+    e.preventDefault();
+    console.log(e.target.id);
+    cartService.removeFromCart(cartSelector.get(), Number(e.target.id));
+  }
+
+  renderCartItems = (cart) => {
+      return cart.items.map(product => {
+          return(
+            <div key={product.productId} className="cart-float-single-item d-flex">
+              <span className="remove-item">
+                <a onClick={this.removeItem}  href="#">
+                  <i id={product.productId} className="fa fa-times" />
+                </a>
+              </span>
+              <div className="cart-float-single-item-image">
+                <a href="single-product.html"><img src={product.productImage} className="img-fluid" alt="" /></a>
+              </div>
+              <div className="cart-float-single-item-desc">
+                <p className="product-title"> <a href="single-product.html">{product.productDesc} </a></p>
+                <p className="price"><span className="count">1x</span> ${product.productRrp}</p>
+              </div>
+            </div>
+          )
+      });
+  }
 
   	render() {
+      const { cart } = this.props;
   		return (
-      <div className="cart-floating-box" id="cart-floating-box" ref={this.setContainer}>
-        <div className="cart-items">
-          <div className="cart-float-single-item d-flex">
-            <span className="remove-item"><a href="#"><i className="fa fa-times" /></a></span>
-            <div className="cart-float-single-item-image">
-              <a href="single-product.html"><img src="assets/images/products/product01.jpg" className="img-fluid" alt="" /></a>
+        <div className="cart-floating-box" id="cart-floating-box" ref={this.setContainer}>
+          <div className="cart-items">
+            {this.renderCartItems(cart)}
+          <div className="cart-calculation">
+            <div className="calculation-details">
+              <p className="total">Subtotal <span>$22</span></p>
             </div>
-            <div className="cart-float-single-item-desc">
-              <p className="product-title"> <a href="single-product.html">Duis pulvinar obortis eleifend </a></p>
-              <p className="price"><span className="count">1x</span> $20.50</p>
+            <div className="floating-cart-btn text-center">
+              <a onClick={this.checkout} href="#">Checkout</a>
+              <a onClick={this.viewCart} href="#">View Cart</a>
             </div>
-          </div>
-          <div className="cart-float-single-item d-flex">
-            <span className="remove-item"><a href="#"><i className="fa fa-times" /></a></span>
-            <div className="cart-float-single-item-image">
-              <a href="single-product.html"><img src="assets/images/products/product02.jpg" className="img-fluid" alt="" /></a>
-            </div>
-            <div className="cart-float-single-item-desc">
-              <p className="product-title"> <a href="single-product.html">Fusce ultricies dolor vitae</a></p>
-              <p className="price"><span className="count">1x</span> $20.50</p>
-            </div>
-          </div>
-        </div>
-        <div className="cart-calculation">
-          <div className="calculation-details">
-            <p className="total">Subtotal <span>$22</span></p>
-          </div>
-          <div className="floating-cart-btn text-center">
-            <a onClick={this.checkout} href="#">Checkout</a>
-            <a onClick={this.viewCart} href="#">View Cart</a>
           </div>
         </div>
       </div>
       )
   	}
-
-  }
+}
 
 export default connect(state => ({
     cart: state.services.cart,
