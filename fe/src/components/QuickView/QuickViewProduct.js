1d3c179afd5301af0445325970189e09f4d462b0
Mackie <Daniel.Mackie@dfs.com>
2019-03-19 12:30:38 +0800

    centered spinner

--- fe/src/components/QuickView/QuickViewProduct.js
+++ fe/src/components/QuickView/QuickViewProduct.js
@@ -14,9 +14,45 @@ const initialState = {
 	"productId": null,
 	"product": {"productImage": 'assets/images/spinners/default.gif'},
 	"currentImage": "assets/images/spinners/default.gif",
+	"spinnerImage": "assets/images/spinners/default.gif",
 	"quantity": 1,
 	"isShowing": false,
+	"isLoading": false,
 };
+
+const settings = {
+	prevArrow: <SlickArrowPrev />,
+	nextArrow: <SlickArrowNext />,
+	slidesToShow: 3,
+	responsive: [{
+		breakpoint: 1200,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		}
+	},
+	{
+		breakpoint: 991,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+	},
+	{
+		breakpoint: 767,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		}
+	},
+	{
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3
+		}
+	}
+]};
 
 class QuickViewProduct extends Component{
 
@@ -38,6 +74,7 @@ class QuickViewProduct extends Component{
 			"product": {"productImage": 'assets/images/spinners/default.gif'},
 			"currentImage": "assets/images/spinners/default.gif",
 			"isShowing": isShowing,
+			"isLoading": true,
 		},  () => {
 				if(!isShowing) {return;}
 				this.updateData(locale, productId);
@@ -113,7 +150,8 @@ class QuickViewProduct extends Component{
 				setTimeout(() => {
 					this.setState({
 						"product": responseJSON,
-						"currentImage": responseJSON.productImage
+						"currentImage": responseJSON.productImage,
+						"isLoading": false,
 					});
 				}, 1000);
 	    });
@@ -134,42 +172,134 @@ class QuickViewProduct extends Component{
 		});
 	}
 
+	renderSpinner = () => {
+		return (
+			<div className="row justify-content-center">
+				<div className="col-lg-5 col-md-6 col-xs-12">
+					<img src={this.state.spinnerImage}
+							 className="img-fluid"
+							 alt=""
+					/>
+				</div>
+			</div>
+		)
+	}
+
+	renderModal = (currentImage, product) => {
+		return(
+						<div className="row">
+							<div className="col-lg-5 col-md-6 col-xs-12">
+								<div className="product-image-slider">
+									<div className="tab-content product-large-image-list" id="myTabContent">
+										<div className="tab-pane fade show active" id="single-slide1" role="tabpanel" aria-labelledby="single-slide-tab-1">
+											<div className="single-product-img img-full">
+												<img src={currentImage}
+														 className="img-fluid"
+														 alt=""
+												/>
+											</div>
+										</div>
+										<div className="tab-pane fade" id="single-slide2" role="tabpanel" aria-labelledby="single-slide-tab-2">
+											<div className="single-product-img img-full">
+												<img src={product.productImage}
+														 className="img-fluid"
+														 alt=""
+												/>
+											</div>
+										</div>
+										<div className="tab-pane fade" id="single-slide3" role="tabpanel" aria-labelledby="single-slide-tab-3">
+											<div className="single-product-img img-full">
+												<img src="assets/images/products/grapes.jpg"
+														 className="img-fluid"
+														 alt=""
+												/>
+											</div>
+										</div>
+										<div className="tab-pane fade" id="single-slide4" role="tabpanel" aria-labelledby="single-slide-tab-4">
+											<div className="single-product-img img-full">
+												<img 	src="assets/images/products/pistachio.jpg"
+															className="img-fluid"
+															alt=""
+												/>
+											</div>
+										</div>
+									</div>
+									<div className="product-small-image-list">
+										<Slider role="tablist" className="nav small-image-slider" ref={c => (this.slider = c)} {...settings}>
+											<div className="single-small-image img-full">
+												<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-1" href="#single-slide1">
+													<img 	src={product.productImage}
+																className="img-fluid"
+																alt="" />
+												</a>
+											</div>
+											<div className="single-small-image img-full">
+												<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-2" href="#single-slide2">
+													<img 	src="assets/images/products/grapes.jpg"
+																className="img-fluid"
+																alt="" />
+												</a>
+											</div>
+											<div className="single-small-image img-full">
+												<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-3" href="#single-slide3">
+													<img 	src={product.productImage}
+																className="img-fluid"
+																alt="" />
+												</a>
+											</div>
+											<div className="single-small-image img-full">
+												<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-4" href="#single-slide4">
+													<img 	src={product.productImage}
+																alt="" />
+												</a>
+											</div>
+											<div className="single-small-image img-full">
+												<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-2" href="#single-slide2">
+													<img src="assets/images/products/pistachio.jpg"
+															 className="img-fluid"
+															 alt="" />
+												</a>
+											</div>
+										</Slider>
+									</div>
+								</div>
+							</div>
+							<div className="col-lg-7 col-md-6 col-xs-12">
+								<div className="product-feature-details">
+									<h2 className="product-title mb-15">{product.productDesc}</h2>
+									<h2 className="product-price mb-15">
+										<span className="main-price">$ {product.productRrp}</span>
+										<span className="discounted-price"> $10.00</span>
+									</h2>
+									<p className="product-description mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
+									<div className="cart-buttons mb-20">
+										<div className="pro-qty mr-10">
+											<input onChange={this.updateQuantity} type="text" value={this.state.quantity} />
+											<a onClick={this.incrementQuantity} href="#" className="inc qty-btn">+</a>
+											<a onClick={this.decrementQuantity} href="#" className="dec qty-btn">-</a>
+										</div>
+										<div className="add-to-cart-btn">
+											<a onClick={this.addToCart} href="#"><i className="fa fa-shopping-cart"></i> Add to Cart</a>
+										</div>
+									</div>
+									<div className="social-share-buttons">
+										<h3>share this product</h3>
+										<ul>
+											<li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
+											<li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
+											<li><a className="google-plus" href="#"><i className="fa fa-google-plus"></i></a></li>
+											<li><a className="pinterest" href="#"><i className="fa fa-pinterest"></i></a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+		);
+	}
+
   render(){
 		if(this.props.productId === null) {return null;}
-		const { product, currentImage, isShowing} = this.state;
-		const settings = {
-			prevArrow: <SlickArrowPrev />,
-			nextArrow: <SlickArrowNext />,
-			slidesToShow: 3,
-			responsive: [{
-				breakpoint: 1200,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3
-				}
-			},
-			{
-				breakpoint: 991,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2
-				}
-			},
-			{
-				breakpoint: 767,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3
-				}
-			},
-			{
-				breakpoint: 480,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3
-				}
-			}
-		]};
+		const { product, currentImage, isShowing, isLoading} = this.state;
     return(
 			<div className={"modal fade quick-view-modal-container "
 									+ ((isShowing) ? " show" : "")}
@@ -190,113 +320,9 @@ class QuickViewProduct extends Component{
 							</button>
 						</div>
 						<div className="modal-body">
-							<div className="row">
-								<div className="col-lg-5 col-md-6 col-xs-12">
-									<div className="product-image-slider">
-										<div className="tab-content product-large-image-list" id="myTabContent">
-											<div className="tab-pane fade show active" id="single-slide1" role="tabpanel" aria-labelledby="single-slide-tab-1">
-												<div className="single-product-img img-full">
-													<img src={currentImage}
-															 className="img-fluid"
-															 alt=""
-													/>
-												</div>
-											</div>
-											<div className="tab-pane fade" id="single-slide2" role="tabpanel" aria-labelledby="single-slide-tab-2">
-												<div className="single-product-img img-full">
-													<img src={product.productImage}
-															 className="img-fluid"
-															 alt=""
-													/>
-												</div>
-											</div>
-											<div className="tab-pane fade" id="single-slide3" role="tabpanel" aria-labelledby="single-slide-tab-3">
-												<div className="single-product-img img-full">
-													<img src="assets/images/products/grapes.jpg"
-															 className="img-fluid"
-															 alt=""
-													/>
-												</div>
-											</div>
-											<div className="tab-pane fade" id="single-slide4" role="tabpanel" aria-labelledby="single-slide-tab-4">
-												<div className="single-product-img img-full">
-													<img 	src="assets/images/products/pistachio.jpg"
-																className="img-fluid"
-																alt=""
-													/>
-												</div>
-											</div>
-										</div>
-										<div className="product-small-image-list">
-											<Slider role="tablist" className="nav small-image-slider" ref={c => (this.slider = c)} {...settings}>
-												<div className="single-small-image img-full">
-													<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-1" href="#single-slide1">
-														<img 	src={product.productImage}
-																	className="img-fluid"
-																	alt="" />
-													</a>
-												</div>
-												<div className="single-small-image img-full">
-													<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-2" href="#single-slide2">
-														<img 	src="assets/images/products/grapes.jpg"
-																	className="img-fluid"
-																	alt="" />
-													</a>
-												</div>
-												<div className="single-small-image img-full">
-													<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-3" href="#single-slide3">
-														<img 	src={product.productImage}
-																	className="img-fluid"
-																	alt="" />
-													</a>
-												</div>
-												<div className="single-small-image img-full">
-													<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-4" href="#single-slide4">
-														<img 	src={product.productImage}
-																	alt="" />
-													</a>
-												</div>
-												<div className="single-small-image img-full">
-													<a onClick={this.changeImage} data-toggle="tab" id="single-slide-tab-2" href="#single-slide2">
-														<img src="assets/images/products/pistachio.jpg"
-																 className="img-fluid"
-																 alt="" />
-													</a>
-												</div>
-											</Slider>
-										</div>
-									</div>
-								</div>
-								<div className="col-lg-7 col-md-6 col-xs-12">
-									<div className="product-feature-details">
-										<h2 className="product-title mb-15">{product.productDesc}</h2>
-										<h2 className="product-price mb-15">
-											<span className="main-price">$ {product.productRrp}</span>
-											<span className="discounted-price"> $10.00</span>
-										</h2>
-										<p className="product-description mb-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco,Proin lectus ipsum, gravida et mattis vulputate, tristique ut lectus</p>
-										<div className="cart-buttons mb-20">
-											<div className="pro-qty mr-10">
-												<input onChange={this.updateQuantity} type="text" value={this.state.quantity} />
-												<a onClick={this.incrementQuantity} href="#" className="inc qty-btn">+</a>
-												<a onClick={this.decrementQuantity} href="#" className="dec qty-btn">-</a>
-											</div>
-											<div className="add-to-cart-btn">
-												<a onClick={this.addToCart} href="#"><i className="fa fa-shopping-cart"></i> Add to Cart</a>
-											</div>
-										</div>
-										<div className="social-share-buttons">
-											<h3>share this product</h3>
-											<ul>
-												<li><a className="twitter" href="#"><i className="fa fa-twitter"></i></a></li>
-												<li><a className="facebook" href="#"><i className="fa fa-facebook"></i></a></li>
-												<li><a className="google-plus" href="#"><i className="fa fa-google-plus"></i></a></li>
-												<li><a className="pinterest" href="#"><i className="fa fa-pinterest"></i></a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
+								{((!isLoading)
+								? this.renderModal(currentImage, product, settings)
+								: this.renderSpinner(currentImage))}
 						</div>
 					</div>
 				</div>
