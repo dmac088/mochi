62da73c53350051194378782b5921bfe667bdf7f
Mackie <Daniel.Mackie@dfs.com>
2019-03-20 13:34:08 +0800

    removed connect from Register component

--- fe/src/components/Login/Register.js
+++ fe/src/components/Login/Register.js
@@ -67,12 +67,4 @@ class Register extends Component {
   }
 }
 
-export default connect(state => ({
-    tokens: state.services.session.tokens,
-    customer: state.services.session.customer,
-}), dispatch => ({
-	actions: {
-		tokens: bindActionCreators(tokensActionCreators, dispatch),
-    customer: bindActionCreators(customerActionCreators, dispatch),
-	},
-}))(Register);
+export default Register;
