3dedf2b918e8ad520f264329232dd3f5771a5bae
dmac088 <danielmackie82@gmail.com>
2019-03-23 23:13:58 +0800

    fixed scroller first render issue

--- fe/src/components/Scroller.js
+++ fe/src/components/Scroller.js
@@ -6,13 +6,17 @@ class Scroller extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        position: 0,
-        showScroller: false,
+        "position": 0,
+        "showScroller": false,
+        "body": null,
     };
   }
 
   componentDidMount() {
     window.addEventListener('scroll', this.listenToScroll, { passive: true });
+    this.setState({
+      "body": document.querySelector('html,body')
+    });
   }
 
   componentWillUnmount() {
@@ -20,28 +24,24 @@ class Scroller extends Component {
   }
 
   listenToScroll = () => {
-    let scroll = document.documentElement.scrollTop;
+    const scroll = document.documentElement.scrollTop;
     this.setState({
       position: scroll,
     });
   }
 
-  animateScroll = (e) => {
-     e.preventDefault();
-     const body = document.querySelector('html,body');
-     document.querySelector('.scroll-top').onclick = function() {
-       Velocity(body, 'scroll', { duration: 1000 });
-     };
+  animate = (e) => {
+    e.preventDefault();
+    const { body } = this.state;
+    Velocity(body, 'scroll', { duration: 1000 });
   }
 
   render() {
     const { position } = this.state;
     return (
       <React.Fragment>
-        <a
-          onClick={this.animateScroll}
-          href="#"
-          className={"scroll-top " + ((position >= 400) ? "fadeIn" : "fadeOut")}
+        <a onClick={this.animate}
+           className={"scroll-top " + ((position >= 400) ? "fadeIn" : "fadeOut")}
         />
       </React.Fragment>
     )
