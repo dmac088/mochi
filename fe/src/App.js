33007bae1d93a1c1b25ffe33b558295935fcb04b
dmac088 <danielmackie82@gmail.com>
2019-03-24 00:54:32 +0800

    BreadCrumb removal

--- fe/src/App.js
+++ fe/src/App.js
@@ -124,6 +124,7 @@ class App extends Component {
           showQVModal={showQVModal}
           setCurrentProductId={this.setCurrentProductId}
           currentProductId={currentProductId}
+          page="Products"
         />
     );
   }
@@ -135,6 +136,7 @@ class App extends Component {
         {...routeProps}
         authenticated={tokens.authenticated}
         customer={customer}
+        page="Checkout"
       />
     );
   }
@@ -182,14 +184,14 @@ class App extends Component {
     return (
         <Switch>
           <Route path={"/:locale/:currency"} exact={true}                     render={(props)   => this.renderLayout(props, this.renderLanding)}     />
-          <Route path={"/:locale/:currency/(Category|Search)/:term"}          render={(props)   => this.renderLayout(props, this.renderProducts)}    />
+          <Route path={"/:locale/:currency/(Category|Search)/:term"}          render={(props)   => this.renderLayoutBC(props, this.renderProducts)}    />
           <Route path={"/:locale/:currency/Checkout"} exact={true}            render={(props)   => this.renderLayoutBC(props, this.renderCheckout)}    />
           <Route path={"/:locale/:currency/Cart"} exact={true}                render={(props)   => this.renderLayoutBC(props, this.renderCart)}        />
           <Route path={"/:locale/:currency/Account"} exact={true}             render={(props)   => this.renderLayoutBC(props, this.renderAuth)}        />
-          <Route path={"/:locale/:currency/Wishlist"} exact={true}            render={(props)   => this.renderLayout(props, this.renderWishlist)}    />
-          <Route path={"/:locale/:currency/Contact"} exact={true}             render={(props)   => this.renderLayout(props, this.renderContact)}     />
+          <Route path={"/:locale/:currency/Wishlist"} exact={true}            render={(props)   => this.renderLayoutBC(props, this.renderWishlist)}    />
+          <Route path={"/:locale/:currency/Contact"} exact={true}             render={(props)   => this.renderLayoutBC(props, this.renderContact)}     />
           <Route path={"/:locale/:currency/Auth"} exact={true}                render={(props)   => this.renderLayoutBC(props, this.renderAuth)}        />
-          <Route path={"/:locale/:currency/Product/:productId"} exact={true}  render={(props)   => this.renderLayout(props, this.renderProduct)}     />
+          <Route path={"/:locale/:currency/Product/:productId"} exact={true}  render={(props)   => this.renderLayoutBC(props, this.renderProduct)}     />
           <Redirect from="/" to="/en-GB/HKD" />
           <Route                                                              render={(props)   => this.renderLayout(props, this.renderLanding)}     />
         </Switch>
