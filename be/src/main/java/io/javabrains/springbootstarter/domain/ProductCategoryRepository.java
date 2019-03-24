d10537aa5daf00ce753fe09369d55e80faeeaa0a
dmac088 <danielmackie82@gmail.com>
2019-03-24 13:24:03 +0800

    removed unused imports

--- be/src/main/java/io/javabrains/springbootstarter/domain/ProductCategoryRepository.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/ProductCategoryRepository.java
@@ -3,7 +3,6 @@ package io.javabrains.springbootstarter.domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long> {
--- be/src/main/java/io/javabrains/springbootstarter/domain/ProductCategoryService.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/ProductCategoryService.java
@@ -2,8 +2,6 @@ package io.javabrains.springbootstarter.domain;
 
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
