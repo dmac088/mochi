6ff4a1d94ece9c2fd4347055f3b5832c2fd891f3
dmac088 <danielmackie82@gmail.com>
2019-03-25 13:13:27 +0800

    category breadcrumb now working

--- fe/src/components/BreadCrumb.js
+++ fe/src/components/BreadCrumb.js
@@ -13,11 +13,45 @@ class BreadCrumb extends Component {
 
   }
 
+  findCategoryByName = (categoryList, term) => {
+    return categoryList.filter(function(value, index, arr){
+      return value.categoryDesc === term;
+    })[0];
+  }
+
+  findCategoryById = (categoryList, categoryId) => {
+    return categoryList.filter(function(value, index, arr){
+      return value.categoryId === categoryId;
+    })[0];
+  }
+
+  createLineage = (categoryList, categoryId, result) => {
+    const category = this.findCategoryById(categoryList, categoryId);
+    if(category.categoryLevel === 0) {return;}
+    result.push(this.findCategoryById(categoryList, categoryId));
+    if(!category.parentId) {return;}
+    this.createLineage(categoryList, category.parentId, result);
+  }
+
+  renderCategoryLineage = (categoryList, term) => {
+    const result = [];
+    this.createLineage( categoryList,
+                        this.findCategoryByName(categoryList, term).categoryId,
+                        result);
+    const lineage = result.reverse();
+    return lineage.map(category => {
+      return (
+        <li onClick={this.changeCategory} className="active">
+          {category.categoryDesc}
+        </li>
+      )
+    });
+  }
+
   render() {
     const { locale, currency, term } = this.props.match.params;
     const type = this.props.match.params[0];
-    const { page } =  this.props;
-    console.log(this.props);
+    const { page, categoryList } =  this.props;
     return (
       <div className="breadcrumb-area mb-50">
     		<div className="container">
@@ -26,7 +60,9 @@ class BreadCrumb extends Component {
     					<div className="breadcrumb-container">
     						<ul>
     							<li><Link to={'/'+ locale + '/' + currency}><i className="fa fa-home"></i> Home</Link></li>
-    							<li onClick={this.changeCategory} className="active">{term}</li>
+    							{(!(categoryList.length===0))
+                    ? this.renderCategoryLineage(categoryList, term)
+                    : null}
     						</ul>
     					</div>
     				</div>
