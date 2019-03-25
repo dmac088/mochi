297ac10efa4e89f8e801af257d5a71bfb3ee4d3a
dmac088 <danielmackie82@gmail.com>
2019-03-25 18:50:48 +0800

    asdf

--- fe/src/components/BreadCrumb.js
+++ fe/src/components/BreadCrumb.js
@@ -3,14 +3,9 @@ import { Link } from "react-router-dom";
 
 class BreadCrumb extends Component {
 
-
   constructor(props) {
     super(props);
     this.state = {}
-  }
-
-  componentDidMount() {
-
   }
 
   findCategoryByName = (categoryList, term) => {
