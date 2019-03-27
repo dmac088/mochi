c2f3df1640dcf8d1b6578e5a419e2cd3cc65e43d
Mackie <Daniel.Mackie@dfs.com>
2019-03-27 20:14:54 +0800

    html 5 issues in IE resolved

--- fe/src/components/Products/Product.js
+++ fe/src/components/Products/Product.js
@@ -11,38 +11,40 @@ class Product extends Component {
 
   renderLV = (product, setCurrentProductId, isGrid) => {
     return (
-      <div className="gf-product shop-list-view-product">
-        <div className="image">
-          <a onClick={this.routeSingleProduct} id={product.productId} href="#">
-            <span className="onsale">Sale!</span>
-            <img src={product.productImage} className="img-fluid" alt />
-          </a>
-          <div className="product-hover-icons">
-            <a id={product.productId}
-               onClick={setCurrentProductId}
-               href="#"
-               data-tooltip="Quick view"
-               data-toggle="modal"
-               data-target="#quick-view-modal-container">
-               <span className="icon_search" />
+      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+        <div className="gf-product shop-list-view-product">
+          <div className="image">
+            <a onClick={this.routeSingleProduct} id={product.productId} href="#">
+              <span className="onsale">Sale!</span>
+              <img src={product.productImage} className="img-fluid" alt />
             </a>
+            <div className="product-hover-icons">
+              <a id={product.productId}
+                 onClick={setCurrentProductId}
+                 href="#"
+                 data-tooltip="Quick view"
+                 data-toggle="modal"
+                 data-target="#quick-view-modal-container">
+                 <span className="icon_search" />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="product-content">
-          <div className="product-categories">
-            <a href="shop-left-sidebar.html">Fast Foods</a>,
-            <a href="shop-left-sidebar.html">Vegetables</a>
-          </div>
-          <h3 className="product-title"><a href="single-product.html">{product.productDesc}</a></h3>
-          <div className="price-box mb-20">
-            <span className="main-price">{product.productRrp}</span>
-            <span className="discounted-price">$80.00</span>
-          </div>
-          <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
-          <div className="list-product-icons">
-            <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
-            <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
-            <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
+          <div className="product-content">
+            <div className="product-categories">
+              <a href="shop-left-sidebar.html">Fast Foods</a>,
+              <a href="shop-left-sidebar.html">Vegetables</a>
+            </div>
+            <h3 className="product-title"><a href="single-product.html">{product.productDesc}</a></h3>
+            <div className="price-box mb-20">
+              <span className="main-price">{product.productRrp}</span>
+              <span className="discounted-price">$80.00</span>
+            </div>
+            <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
+            <div className="list-product-icons">
+              <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
+              <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
+              <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
+            </div>
           </div>
         </div>
       </div>
