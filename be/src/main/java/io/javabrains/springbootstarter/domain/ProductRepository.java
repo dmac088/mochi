8dae596a65283a9fec9d7671ae6c6b02551af9ac
Mackie <Daniel.Mackie@dfs.com>
2019-04-02 12:48:51 +0800

    added brand count to navigation of Products

--- be/src/main/java/io/javabrains/springbootstarter/domain/ProductRepository.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/ProductRepository.java
@@ -4,7 +4,9 @@ package io.javabrains.springbootstarter.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
@@ -14,9 +16,15 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 	
 	List<Product> findByPreviewFlag(Long previewFlag);
 	
-	Long countByCategoriesCategoryCode(String categoryCode);
-	
-	Long countByCategoriesCategoryCodeAndBrandBrandCode(String categoryCode, String brandCode);
+	@Query(
+		value = "WITH RECURSIVE MyCTE AS ( SELECT cat_id, cat_cd FROM mochi.category WHERE cat_cd = :categoryCode UNION ALL SELECT c.cat_id, c.cat_cd FROM mochi.category c INNER JOIN MyCTE ON c.cat_prnt_id = MyCTE.cat_id WHERE c.cat_prnt_id IS NOT NULL ) SELECT count(p.prd_id) FROM MyCTE c inner join mochi.product_category pc on c.cat_id = pc.cat_id  inner join mochi.product p  on pc.prd_id = p.prd_id",
+		nativeQuery = true)
+	Long countByCategoriesCategoryCode(@Param("categoryCode") String categoryCode);
+		
+	@Query(
+		value = "WITH RECURSIVE MyCTE AS ( SELECT cat_id, cat_cd FROM mochi.category WHERE cat_cd = :categoryCode UNION ALL SELECT c.cat_id, c.cat_cd FROM mochi.category c INNER JOIN MyCTE ON c.cat_prnt_id = MyCTE.cat_id WHERE c.cat_prnt_id IS NOT NULL ) SELECT count(p.prd_id) FROM MyCTE c inner join mochi.product_category pc on c.cat_id = pc.cat_id  inner join mochi.product p  on pc.prd_id = p.prd_id inner join mochi.brand b on p.bnd_id = b.bnd_id WHERE b.bnd_cd = :brandCode",
+		nativeQuery = true)	
+	Long countByCategoriesCategoryCodeAndBrandBrandCode(@Param("categoryCode") String categoryCode, @Param("brandCode") String brandCode);
 	
 	Optional<Product> findByProductId(Long id);
 }
