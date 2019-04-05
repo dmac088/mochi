b1f66ba7c89359396cf74377708812f7c251966f
dmac088 <danielmackie82@gmail.com>
2019-04-05 23:51:21 +0800

    asdf

--- fe/src/components/Header/HeaderCartSummary.js
+++ fe/src/components/Header/HeaderCartSummary.js
@@ -115,7 +115,7 @@ class Accordion extends React.Component {
               </div>
               <div className="cart-float-single-item-desc">
                 <p className="product-title"> <a href="single-product.html">{product.productDesc} </a></p>
-                <p className="price"><span className="count">{product.quantity}x</span> ${product.productRetail}</p>
+                <p className="price"><span className="count">{product.quantity}x</span> ${product.productMarkdown}</p>
               </div>
             </div>
           )
