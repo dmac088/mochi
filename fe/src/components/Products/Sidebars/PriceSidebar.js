4f0c25299491ad92de10d914a0a3a5667a9e5b28
dmac088 <danielmackie82@gmail.com>
2019-04-07 12:38:15 +0800

    asdf

--- fe/src/components/Products/Sidebars/PriceSidebar.js
+++ fe/src/components/Products/Sidebars/PriceSidebar.js
@@ -4,14 +4,24 @@ import 'rc-slider/assets/index.css';
 
 class PriceSidebar extends Component {
 
+
+  getMaxBrandPrice = (brands, currentBrand) => {
+    return brands.map(brand => {
+      if (currentBrand === brand.brandDesc) {
+          return brand.maxMarkDownPrice;
+      }
+    });
+  }
+
   render() {
-    const { category } = this.props;
+    const { category, brand } = this.props;
     if(!category) { return null }
     console.log(category);
+    console.log(brand);
     return (
       <div className="sidebar mb-35">
         <h3 className="sidebar-title">Filter By Price</h3>
-        <p>Value less than: {category.maxMarkDownPrice}</p>
+        <p>Value less than: {(!brand) ? category.maxMarkDownPrice : this.getMaxBrandPrice(category.categoryBrands, brand)}</p>
           <Slider
             defaultValue={30}
             trackStyle={{ backgroundColor: '#80bb01', height: 10 }}
