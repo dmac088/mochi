38961a201d5587ea85f384594c9bd473e1eb75a6
dmac088 <danielmackie82@gmail.com>
2019-04-07 14:30:50 +0800

    asdf

--- fe/src/components/Products/Sidebars/PriceSidebar.js
+++ fe/src/components/Products/Sidebars/PriceSidebar.js
@@ -4,25 +4,52 @@ import 'rc-slider/assets/index.css';
 
 class PriceSidebar extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      "value": null,
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if(!prevProps.category || !this.props.category) { return }
+    if(prevProps.category.categoryCode === this.props.category.categoryCode
+      && prevProps.brand === this.props.brand) { return }
+    const { category, brand } = this.props;
+    const maxPrice = this.getMaxBrandPrice(category.categoryBrands, brand);
+    this.setState({
+      "value": maxPrice,
+    })
+  }
 
   getMaxBrandPrice = (brands, currentBrand) => {
-    return brands.map(brand => {
+    let maxPrice = 0;
+    brands.map(brand => {
       if (currentBrand === brand.brandDesc) {
-          return brand.maxMarkDownPrice;
+          maxPrice = brand.maxMarkDownPrice;
       }
     });
+    return maxPrice;
+  }
+
+  changeValue = (value) => {
+    this.setState({
+      "value": value,
+    })
   }
 
   render() {
     const { category, brand } = this.props;
-    if(!category) { return null }
-    console.log(category);
     console.log(brand);
+    if(!category) { return null }
+    const maxPrice = ((!brand) ? category.maxMarkDownPrice : this.getMaxBrandPrice(category.categoryBrands, brand));
+    console.log(this.state.value)
     return (
       <div className="sidebar mb-35">
         <h3 className="sidebar-title">Filter By Price</h3>
-        <p>Value less than: {(!brand) ? category.maxMarkDownPrice : this.getMaxBrandPrice(category.categoryBrands, brand)}</p>
+        <p>Value: {(!this.state.value) ? maxPrice : this.state.value}</p>
           <Slider
+            onChange={this.changeValue}
             defaultValue={30}
             trackStyle={{ backgroundColor: '#80bb01', height: 10 }}
             handleStyle={{
@@ -33,7 +60,12 @@ class PriceSidebar extends Component {
               marginTop: -9,
               backgroundColor: 'grey',
             }}
+            min={0}
+            max={maxPrice}
+            defaultValue={maxPrice}
+            value={(!this.state.value) ? maxPrice : this.state.value}
             railStyle={{ height: 10 }}/>
+
       </div>
     );
   }
