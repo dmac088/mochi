ad9f9eb97483aae5826920940363033ef331ef4b
Mackie <Daniel.Mackie@dfs.com>
2019-04-12 16:40:13 +0800

    asdf

--- be/src/main/java/io/javabrains/springbootstarter/domain/LanguageDiscriminator.java
+++ be/src/main/java/io/javabrains/springbootstarter/domain/LanguageDiscriminator.java
@@ -5,7 +5,7 @@ import org.hibernate.search.analyzer.Discriminator;
 public class LanguageDiscriminator implements Discriminator {
 
     public String getAnalyzerDefinitionName(Object value, Object entity, String field) {
-        if ( value == null || !( entity instanceof ProductAttribute || entity instanceof ProductCategoryAttribute)) {
+        if ( value == null || !( entity instanceof ProductAttribute || entity instanceof ProductCategoryAttribute || entity instanceof BrandAttribute)) {
             return null;
         }
         //System.out.println(entity.getClass().toString());
