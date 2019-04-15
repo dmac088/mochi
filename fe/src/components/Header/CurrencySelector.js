9cd50ddd419499dee41d90b1585ec13c1f7494fe
Mackie <Daniel.Mackie@dfs.com>
2019-04-15 12:04:40 +0800

    Language and Currency electors now function components

--- fe/src/components/Header/CurrencySelector.js
+++ fe/src/components/Header/CurrencySelector.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import { updateParams } from '../../services/helpers/Helper';
 
-class LanguageSelector extends Component {
-
-  changeCurr = (e, props) => {
-    const url = props.location.pathname;
-    const search = props.location.search;
-    const { currency } = props.match.params;
-    props.history.push(url.replace(currency || '', event.target.id) + search);
+  const changeCurr = (e, props, routeProps) => {
+    const url = routeProps.location.pathname;
+    const search = routeProps.location.search;
+    const { currency } = routeProps.match.params;
+    routeProps.history.push(url.replace(currency || '', e.target.id) + search);
   }
 
-  export const LanguageSelector = withRouter((history, ...props) => {
+  export const CurrencySelector = withRouter(({history, location, match, ...props}) => {
     return (
       <ul>
-        <li><a id="USD" onClick={this.changeCurr}>USD</a></li>
-        <li><a id="HKD" onClick={this.changeCurr}>HKD</a></li>
+        <li><a id="USD" onClick={(e) => changeCurr(e, props, {history, location, match})}>USD</a></li>
+        <li><a id="HKD" onClick={(e) => changeCurr(e, props, {history, location, match})}>HKD</a></li>
       </ul>
-    )
+    );
   });
