142f8b9a1b9b8ea42e19cf4e509522427fbd11c6
Mackie <Daniel.Mackie@dfs.com>
2019-04-15 18:33:49 +0800

    BrandSidebar is now a function component

--- fe/src/components/Products/Sidebars/BrandSidebar.js
+++ fe/src/components/Products/Sidebars/BrandSidebar.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from "react-router-dom";
 import { changeBrand } from '../../../services/helpers/RouteHelper';
 
-class BrandSidebar extends Component {
-
-  renderBrandListItems = (category, currentBrand) => {
-    const { location, match, history } = this.props;
+  const renderBrandListItems = (category, currentBrand, props) => {
+    const { location, match, history } = props;
     return category.categoryBrands.map(brand => {
       const isActive = (currentBrand === brand.brandDesc)
       return(
@@ -18,9 +16,9 @@ class BrandSidebar extends Component {
     })
   }
 
-  renderAll = (category, isActive) => {
+  const renderAll = (category, isActive, props) => {
       if(category.categoryBrands.length <= 1) {return}
-      const { location, match, history } = this.props;
+      const { location, match, history } = props;
       return (
         <li>
           <a className={(isActive) ? "active" : ""} onClick={(e) => changeBrand(e, location, match, history)} id={"All"} href="#">
@@ -30,10 +28,9 @@ class BrandSidebar extends Component {
       )
   }
 
-
-  render() {
-    const { category } = this.props;
-    const currentBrand = this.props.match.params.brand;
+  export const BrandSidebar = withRouter(({...props}) => {
+    const { category } = props;
+    const currentBrand = props.match.params.brand;
     const isActive = (!currentBrand);
     if (!category.categoryBrands) { return null; }
     if(category.categoryBrands.length <= 1) {return null; }
@@ -41,12 +38,9 @@ class BrandSidebar extends Component {
         <div className="sidebar mb-35">
           <h3 className="sidebar-title">Filter By Brand</h3>
           <ul className="product-categories">
-            {this.renderAll(category, isActive)}
-            {this.renderBrandListItems(category,  currentBrand)}
+            {renderAll(category, isActive, props)}
+            {renderBrandListItems(category, currentBrand, props)}
           </ul>
         </div>
       );
-  }
-}
-
-export default withRouter(BrandSidebar);
+  });
