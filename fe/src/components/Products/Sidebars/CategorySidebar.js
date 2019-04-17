5107e2a3da283857b99330f1ac0e1ef17ca60602
Mackie <Daniel.Mackie@dfs.com>
2019-04-17 16:36:16 +0800

    category facets now showing on search page

--- fe/src/components/Products/Sidebars/CategorySidebar.js
+++ fe/src/components/Products/Sidebars/CategorySidebar.js
@@ -4,7 +4,7 @@ import * as categoryApi from '../../../data/categories/api';
 import { changeCategory } from '../../../services/helpers/RouteHelper';
 
 
-  const renderCategoryListItems = (category, props) => {
+  const renderCategories = (category, props) => {
     const { getMaxPrice } = props;
     return category.children.map(child => {
       return(
@@ -17,16 +17,31 @@ import { changeCategory } from '../../../services/helpers/RouteHelper';
     });
   }
 
-  export const CategorySidebar = withRouter(({...props}) => {
+  const renderCategoryFacets = (categoryFacets, props) => {
+    return categoryFacets.map(categoryFacet => {
+      return(
+        <li key={categoryFacet.value} >
+          <a onClick={(e) => changeCategory(e, props)} id={categoryFacet.value} href="#">
+            {categoryFacet.value} <span className="badge badge-pill badge-secondary">{categoryFacet.count}</span>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  export const CategorySidebar = withRouter(({match, ...props}) => {
     //console.log(props);
-    const { category } = props;
-    if(!category) {return null;}
-    if(category.children.length === 0) {return null;}
+    const { category, categoryFacets } = props;
+    const isSearch = (match.params[0] === "search");
+    const isCategory = (match.params[0] === "category");
+    if(isCategory && !category && !(category.children)) {return null;}
     return (
       <div className="sidebar mb-35">
         <h3 className="sidebar-title">PRODUCT CATEGORIES</h3>
         <ul className="product-categories">
-          {renderCategoryListItems(category, props)}
+          {(isCategory)
+            ? renderCategories(category, props)
+            : renderCategoryFacets(categoryFacets,props)}
         </ul>
       </div>
     );
