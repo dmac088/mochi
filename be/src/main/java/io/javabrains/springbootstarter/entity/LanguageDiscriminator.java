34463721c1a2738ded3f11d1273220e420732306
Mackie <Daniel.Mackie@dfs.com>
2019-04-30 14:58:40 +0800

    indexing fixed

--- be/src/main/java/io/javabrains/springbootstarter/entity/LanguageDiscriminator.java
+++ be/src/main/java/io/javabrains/springbootstarter/entity/LanguageDiscriminator.java
@@ -5,12 +5,11 @@ import org.hibernate.search.analyzer.Discriminator;
 public class LanguageDiscriminator implements Discriminator {
 
     public String getAnalyzerDefinitionName(Object value, Object entity, String field) {
-    	//System.out.println(value);
-    	if (!field.equals("lclCd") || value == null || !( entity instanceof ProductAttribute || entity instanceof CategoryAttribute || entity instanceof BrandAttribute)) {
+
+    	if (value == null || !(entity instanceof ProductAttribute ||  entity instanceof CategoryAttribute || entity instanceof BrandAttribute)) {
             return null;
         }
-//    	System.out.println("made it!");
-//        System.out.println(field + " - " + value);
+        System.out.println(entity.getClass().toGenericString() + " - " + field + " - " + value);
         return (String) value;
     }
 }
