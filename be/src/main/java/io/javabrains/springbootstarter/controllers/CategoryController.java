49d93d714d6c36db01243bd5528cbf3c920fea12
Mackie <Daniel.Mackie@dfs.com>
2019-05-02 19:43:52 +0800

    asdf

--- be/src/main/java/io/javabrains/springbootstarter/controllers/CategoryController.java
+++ be/src/main/java/io/javabrains/springbootstarter/controllers/CategoryController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.javabrains.springbootstarter.domain.Category;
 import io.javabrains.springbootstarter.services.ICategoryService;
 
@@ -16,7 +15,7 @@ import io.javabrains.springbootstarter.services.ICategoryService;
 public class CategoryController {
 
     @Autowired
-    private ICategoryService CategoryService;
+    private ICategoryService categoryService;
 
     public CategoryController() {
         super();
@@ -24,26 +23,26 @@ public class CategoryController {
     
     @GetMapping("/Category/{lcl}/{curr}")
     public List<Category> getCategories(@PathVariable String lcl, @PathVariable String curr) {
-    	return CategoryService.getCategories(lcl, curr);
+    	return categoryService.getCategories(lcl, curr);
     }
     
     @GetMapping("/Category/{lcl}/{curr}/ParentCategory/{parentCat}")
     public List<Category> getCategories(@PathVariable String lcl, @PathVariable String curr, @PathVariable Long parentCat) {
-    	return CategoryService.getCategoryParent(lcl, curr, parentCat);
+    	return categoryService.getCategoryParent(lcl, curr, parentCat);
     }
     
     @GetMapping("/Category/{lcl}/{curr}/level/{level}")
     public List<Category> getCategoriesForLevel(@PathVariable String lcl, @PathVariable String curr, @PathVariable Long level) {
-    	return CategoryService.getCategoriesForLevel(lcl, curr, level);
+    	return categoryService.getCategoriesForLevel(lcl, curr, level);
     }
 
     @GetMapping("/Category/{lcl}/{curr}/id/{categoryId}")
     public Category getCategory(@PathVariable String lcl, @PathVariable String curr, @PathVariable Long categoryId) {
-    	return CategoryService.getCategory(lcl, curr, categoryId);
+    	return categoryService.getCategory(lcl, curr, categoryId);
     }
     
     @GetMapping("/Category/{lcl}/{curr}/desc/{categoryDesc}")
     public Category getCategory(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc) {
-    	return CategoryService.getCategory(lcl, curr, categoryDesc);
+    	return categoryService.getCategory(lcl, curr, categoryDesc);
     }
 }
