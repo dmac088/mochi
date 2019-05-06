8f83e3af94d5cc1a8a701ef6866fd7b9bc087d02
Mackie <Daniel.Mackie@dfs.com>
2019-05-06 19:38:34 +0800

    asdf

--- be/src/main/java/io/javabrains/springbootstarter/services/BrandService.java
+++ be/src/main/java/io/javabrains/springbootstarter/services/BrandService.java
@@ -1,7 +1,6 @@
 package io.javabrains.springbootstarter.services;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.transaction.Transactional;
