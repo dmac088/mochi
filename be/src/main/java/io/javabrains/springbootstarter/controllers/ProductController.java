be1381ba84b13e751cafa31e1570800f18a4648c
dmac088 <danielmackie82@gmail.com>
2019-05-07 22:27:55 +0800

    asdf

--- be/src/main/java/io/javabrains/springbootstarter/controllers/ProductController.java
+++ be/src/main/java/io/javabrains/springbootstarter/controllers/ProductController.java
@@ -29,7 +29,7 @@ public class ProductController {
     	return productService.getProduct(lcl, curr, id);
     }
     
-    @PostMapping("/Product/{lcl}/maxprice")
+    @PostMapping("/Product/{lcl}/{curr}/maxprice")
     public Double getMaxPrice(@PathVariable String lcl, @PathVariable String curr, @RequestBody final List<SidebarFacetDTO> selectedFacets) {
     	return productService.getMaxPrice(lcl, curr, selectedFacets);
     }
