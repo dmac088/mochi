c0d895b044470d5a6046944d1da3c98e2f630f29
Mackie <Daniel.Mackie@dfs.com>
2019-05-07 10:24:55 +0800

    asdf

--- fe/src/data/products/api.js
+++ fe/src/data/products/api.js
@@ -13,53 +13,53 @@ export const get = (locale, currency) => 	fetchApi(
 																					 {
 
 																					 }
-																				 );
+																				 	);
 
 
 export const getMaxPrice(locale, currency, selectedFacets = [])  => fetchApi(
-																				locale +
-																				'/' + currency
-																				selectedFacets,
-																				{},
-																				'POST',
-																				{"Content-Type": "application/json"}
-																				);
+																					locale +
+																					'/' + currency +
+																					'/maxprice' 
+																					selectedFacets,
+																					{},
+																					'POST',
+																					{"Content-Type": "application/json"}
+																					);
 
 
 export const findById = (locale, currency, productId) => fetchApi(
-																				endPoints.get +
-																				locale +
-																				'/' + currency +
-																				'/id/' + productId,
-																				{},
-																				{},
-																				'GET',
-																				{}
-																				);
-
+																					endPoints.get +
+																					locale +
+																					'/' + currency +
+																					'/id/' + productId,
+																					{},
+																					{},
+																					'GET',
+																					{}
+																					);
 
 export const findByCategory = (locale, currency, category, brand, maxPrice, page = 0, size = 10, sortBy = 'nameAsc', selectedFacets = []) => fetchApi(
-																				endPoints.post +
-																				locale +
-																				'/' + currency +
-																				'/category/' + category +
-																				'/maxPrice/' + maxPrice +
-																				'/page/' + page +
-																				'/size/' + size +
-																				'/sortBy/' + sortBy,
-																				selectedFacets,
-																				{},
-																				'POST',
-																				{"Content-Type": "application/json"}
-																			);
+																					endPoints.post +
+																					locale +
+																					'/' + currency +
+																					'/category/' + category +
+																					'/maxPrice/' + maxPrice +
+																					'/page/' + page +
+																					'/size/' + size +
+																					'/sortBy/' + sortBy,
+																					selectedFacets,
+																					{},
+																					'POST',
+																					{"Content-Type": "application/json"}
+																					);
 
 export const findAllFeatured = (locale, currency) => fetchApi(
-																				endPoints.get +
-																				locale +
-																				'/' + currency +
-																				'/featured',
-																				{},
-																				{},
-																				'GET',
-																				{}
-																				);
+																					endPoints.get +
+																					locale +
+																					'/' + currency +
+																					'/featured',
+																					{},
+																					{},
+																					'GET',
+																					{}
+																					);
