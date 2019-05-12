b6ab7a5669f766936af9cf1abb07a2bb9124d93a
dmac088 <danielmackie82@gmail.com>
2019-05-12 22:44:55 +0800

    asdf

--- fe/src/components/Products/Sidebars/PriceSidebar.js
+++ fe/src/components/Products/Sidebars/PriceSidebar.js
@@ -8,24 +8,32 @@ export const PriceSidebar = (props) => {
     return (
       <div className="sidebar mb-35">
         <h3 className="sidebar-title">Filter By Price</h3>
-        <p>Value less than: {(selectedPrice) ? selectedPrice : maxPrice}</p>
-          <Slider
-            onChange={(value) => updateSelectedPrice(value)}
-            trackStyle={{ backgroundColor: '#80bb01', height: 10 }}
-            handleStyle={{
-              borderColor: '#80bb01',
-              height: 28,
-              width: 28,
-              marginLeft: -14,
-              marginTop: -9,
-              backgroundColor: 'grey',
-            }}
-            min={0}
-            max={maxPrice}
-            step={(maxPrice / 4)}
-            defaultValue={maxPrice}
-            value={(selectedPrice) ? selectedPrice : maxPrice}
-            railStyle={{ height: 10 }}/>
+        {priceSlider(maxPrice, selectedPrice, updateSelectedPrice)}
       </div>
     );
   }
+
+const priceSlider = (maxPrice, selectedPrice, updateSelectedPrice ) => {
+  return (
+    <React.Fragment>
+      <p>Value less than: {(selectedPrice) ? selectedPrice : maxPrice}</p>
+      <Slider
+        onChange={(value) => updateSelectedPrice(value)}
+        trackStyle={{ backgroundColor: '#80bb01', height: 10 }}
+        handleStyle={{
+          borderColor: '#80bb01',
+          height: 28,
+          width: 28,
+          marginLeft: -14,
+          marginTop: -9,
+          backgroundColor: 'grey',
+        }}
+        min={(maxPrice / 4)}
+        max={maxPrice}
+        step={(maxPrice / 4)}
+        defaultValue={maxPrice}
+        value={(selectedPrice) ? selectedPrice : maxPrice}
+        railStyle={{ height: 10 }}/>
+    </React.Fragment>
+  );
+}
