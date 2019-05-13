0821f7e67131138201763fc07add866be5954447
dmac088 <danielmackie82@gmail.com>
2019-05-13 19:59:21 +0800

    asdf

--- fe/src/components/Products/ShopBanner.js
+++ fe/src/components/Products/ShopBanner.js
@@ -4,7 +4,7 @@ import React from 'react';
     return (
       <div className="shop-page-banner mb-35">
         <a href="shop-left-sidebar.html">
-          <img src="assets/images/banners/shop-banner.jpg" className="img-fluid" alt="" />
+          <img src="assets/images/categories/veg01.jpg" className="img-fluid" alt="" />
         </a>
       </div>
     );
