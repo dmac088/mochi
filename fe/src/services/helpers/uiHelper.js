99bddbd4deb93cee5daaafc5d5b6927eb10a99f2
Mackie <Daniel.Mackie@dfs.com>
2019-05-15 13:06:06 +0800

    asdf

--- fe/src/services/helpers/uiHelper.js
+++ fe/src/services/helpers/uiHelper.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Velocity from 'velocity-animate';
+import qs from 'query-string';
+
 export const isMobile = () => {
   return  ((window.innerWidth
             || document.documentElement.clientWidth
