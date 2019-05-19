963590f69ddddff67f2ebabe4608529e9881203b
dmac088 <danielmackie82@gmail.com>
2019-05-19 09:08:57 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/ProductTagAttribute.java
@@ -0,0 +1,70 @@
+package io.nzbee.entity;
+
+import java.util.Optional;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.AnalyzerDiscriminator;
+import org.hibernate.search.annotations.Field;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "product_tag_attr_lcl", schema = "mochi")
+@PrimaryKeyJoinColumn(name = "tag_lcl_id")
+public class ProductTagAttribute {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="tag_lcl_id")
+	private Long Id;
+	
+	@Column(name="tsg_id")
+	private Long tagId;
+
+	@Column(name="cat_desc")
+	@Field(analyze = Analyze.YES)
+	private String tagDesc;
+
+	@Column(name="lcl_cd")	
+	@Field
+	@AnalyzerDiscriminator(impl = LanguageDiscriminator.class)
+	private String lclCd;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="tag_id", insertable=false, updatable=false)
+	@JsonBackReference
+	private ProductTag tag;
+	
+	public Long getCategoryId() {
+		return tagId;
+	}
+	
+	public void setTag(Optional<ProductTag> productTag) {
+		this.tag = productTag.get();
+	}
+
+	public String getTagDesc() {
+		return tagDesc;
+	}
+	
+	public void setTagDesc(String tagDesc) {
+		this.tagDesc = tagDesc;
+	}
+	
+	public String getLclCd() {
+		return lclCd;
+	}
+
+	public void setLclCd(String lclCd) {
+		this.lclCd = lclCd;
+	}
+}
