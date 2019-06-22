29a79eee0554456af718ab75204fd11cc256752a
dmac088 <danielmackie82@gmail.com>
2019-06-22 16:04:31 +0800

    asdf

--- be/src/main/java/io/nzbee/dao/ProductDAO.java
+++ be/src/main/java/io/nzbee/dao/ProductDAO.java
@@ -3,21 +3,16 @@ package io.nzbee.dao;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
 import org.hibernate.query.Query;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-
-import com.jayway.jsonpath.Criteria;
-
 import io.nzbee.entity.Category;
 import io.nzbee.entity.CategoryAttribute;
 import io.nzbee.entity.CategoryAttribute_;
@@ -38,10 +33,9 @@ public class ProductDAO {
 		Root<Product> root = cq.from(Product.class);
 		Join<Category, CategoryAttribute> category = root.join(Product_.categories).join(Category_.attributes);
 		
-		
 		cq.select(root).where(cb.equal(category.get(CategoryAttribute_.categoryDesc), "Fruit"));
 		
-		Query<Product> query = (Query<Product>) em.createQuery(cq);
+		TypedQuery<Product> query = em.createQuery(cq);
         //Predicate titlePredicate = cb.like(book.get("title"), "%" + title + "%");
 
 		return new PageImpl<Product>(query.getResultList(), pageable, query.getResultList().size());
