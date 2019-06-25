0ab5bc8b9b2813d6613d1dc663a4e3c0c71b50fa
Dan <daniel.mackie@dfs.com>
2019-06-25 15:09:25 +0800

    added product status to schema

--- be/src/main/java/io/nzbee/entity/ProductStatus.java
+++ be/src/main/java/io/nzbee/entity/ProductStatus.java
@@ -1,12 +1,34 @@
 package io.nzbee.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name = "product_status", schema = "mochi")
 public class ProductStatus {
 	
+	@Id
+	@Column(name="prd_sts_id")
 	private Long productStatusId;
 
+	@Column(name="prd_sts_cd")
 	private String productStatusCode;
 	
+	@Column(name="prd_sts_desc")
 	private String productStatusDesc;
+	
+	@OneToMany(mappedBy="productStatus", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JsonManagedReference
+	private List<Product> products;
 
 	public Long getProductStatusId() {
 		return productStatusId;
@@ -31,4 +53,13 @@ public class ProductStatus {
 	public void setProductStatusDesc(String productStatusDesc) {
 		this.productStatusDesc = productStatusDesc;
 	}
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
 }
