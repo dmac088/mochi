4647906d54681b7a0fb9945cc5281b3c4fc00aaa
Dan <daniel.mackie@dfs.com>
2019-07-10 10:36:39 +0800

    asdf

--- be/src/main/java/io/nzbee/security/Encoders.java
+++ be/src/main/java/io/nzbee/security/Encoders.java
@@ -1,11 +1,10 @@
 	package io.nzbee.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+
 public class Encoders {
 	
 	public final static int userRounds = 8;
