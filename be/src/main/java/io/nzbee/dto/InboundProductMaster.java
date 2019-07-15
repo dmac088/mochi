038e8fe58e1a25d03fb0ca31c8b53ba196058476
dmac088 <danielmackie82@gmail.com>
2019-07-15 23:45:15 +0800

    asdf

--- be/src/main/java/io/nzbee/dto/InboundProductMaster.java
+++ be/src/main/java/io/nzbee/dto/InboundProductMaster.java
@@ -1,10 +1,8 @@
-package io.nzbee.domain;
+package io.nzbee.dto;
 
 import java.util.Date;
-import java.util.Objects;
 
-
-public class Product {
+public class InboundProductMaster {
 
 	private Long productId;
 
@@ -107,32 +105,4 @@ public class Product {
 		this.primaryCategoryPath = primaryCategoryPath;
 	}
 	
-	@Override
-	public boolean equals(Object o) {
-		 if (this == o) return true;
-	     if (o == null || getClass() != o.getClass()) return false;
-	     Product pcDto = (Product) o;
-	     return this.productId == pcDto.productId;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(productId);
-	}
-
-	@Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append("Product [productId=").append(productId)
-        		.append(", productUPC=").append(productUPC)
-        		.append(", productCreateDt=").append(productCreateDt)
-        		.append(", productDesc=").append(productDesc)
-        		.append(", brandDesc=").append(brandDesc)
-        		.append(", productRetail=").append(productRetail)
-        		.append(", productMarkdown=").append(productMarkdown)
-                .append(", ProductImage=").append(productImage)
-                .append(", lclCd=").append(lclCd);
-        return builder.toString();
-    }
-
 }
