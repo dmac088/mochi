e2d36ed9bed83b2e8a18fbb3dcca73ba2925f8ac
Dan <daniel.mackie@dfs.com>
2019-07-17 16:25:19 +0800

    asdf

--- be/src/main/java/io/nzbee/util/ProductMasterController.java
+++ be/src/main/java/io/nzbee/util/ProductMasterController.java
@@ -1,40 +1,41 @@
-package io.nzbee.util;
-
-import java.io.File;
-
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class ProductMasterController {
-
-    @Autowired
-    private ProductMasterService productMasterService;
-	
-	@Autowired
-	private Environment env;
-    
-    public ProductMasterController() {
-        super();
-    }
-    
-    @GetMapping("/Product/Download/product_master.csv")
-    public void downloadProductMaster(HttpServletResponse response) {
-    	try {
-	    	File tempFile = new ClassPathResource("data/" + env.getProperty("util.product-master-output-file")).getFile();
-	    	response.setContentType("text/csv");
-	        response.setHeader("Content-Disposition", "attachment; file=" + tempFile.getAbsolutePath());
-	    	productMasterService.writeProductMaster(response.getWriter());
-    	} catch (Exception e)  {
-    		System.out.println(e);
-    	}
-    }
-    
-}
+//
+//package io.nzbee.util;
+//
+//import java.io.IOException;
+//import javax.servlet.http.HttpServletResponse;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class ProductMasterController {
+//
+//	private static final Logger logger = LoggerFactory.getLogger(ProductMasterController.class);
+//	
+//    @Autowired
+//    private ProductMasterService productMasterService;
+//	
+//	@Autowired
+//	private FileStorageProperties fileStorageProperties;
+//    
+//    public ProductMasterController() {
+//        super();
+//    }
+//    
+//    @GetMapping("/Product/Download/product_master.csv")
+//    public void downloadProductMaster(HttpServletResponse response) {
+//    	try {
+//	    	response.setContentType("text/csv");
+//	        response.setHeader("Content-Disposition", "attachment; file=" + fileStorageProperties.getDownloadDir() + fileStorageProperties.getProductMasterFile());
+//	    	productMasterService.writeProductMaster(response.getWriter());
+//    	} catch (IOException e)  {
+//    		logger.error(e.toString());
+//    	}
+//    }
+//    
+//}
+//
