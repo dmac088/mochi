16645bf0a46bb1c1b45fe82d74d853b92bdb3c27
Dan <daniel.mackie@dfs.com>
2019-07-24 17:40:31 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/IPartyService.java
+++ be/src/main/java/io/nzbee/entity/party/IPartyService.java
@@ -1,7 +1,11 @@
 package io.nzbee.entity.party;
 
+import java.util.List;
+
 import io.nzbee.entity.IService;
 
 public interface IPartyService extends IService<Party> {
-
+	
+	List<Party> findByRoleTypeDesc(String roleTypeDesc);
+	
 }
