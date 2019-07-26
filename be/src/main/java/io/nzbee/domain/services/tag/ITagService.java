ce971473ee56be1e29dea90a6cb1cec9bc1a05ea
Dan <daniel.mackie@dfs.com>
2019-07-26 13:59:17 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/services/tag/ITagService.java
+++ be/src/main/java/io/nzbee/domain/services/tag/ITagService.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import io.nzbee.domain.Tag;
 import io.nzbee.domain.services.IService;
-import io.nzbee.ui.component.web.sidebar.Sidebar;
 
 public interface ITagService extends IService<Tag> {
 
 	//returns a list domain object
-	List<Tag> findAll(String locale, String currency, String categoryDesc, List<Sidebar> selectedFacets);
+	List<Tag> findAll(String locale, String currency, String categoryDesc, List<Long> categoryIds, List<Long> brandIds);
 	
 	//returns a list of domain objects
-	List<Tag> findAll(String locale, String currency, String categoryDesc, Double price, List<Sidebar> selectedFacets);
+	List<Tag> findAll(String locale, String currency, String categoryDesc, Double price, List<Long> categoryIds, List<Long> brandIds);
 	
 }
--- be/src/main/java/io/nzbee/domain/services/tag/TagService.java
+++ be/src/main/java/io/nzbee/domain/services/tag/TagService.java
@@ -1,24 +1,21 @@
 package io.nzbee.domain.services.tag;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import io.nzbee.domain.Tag;
-import io.nzbee.domain.services.category.CategoryService;
-import io.nzbee.entity.category.Category;
 import io.nzbee.entity.category.ICategoryService;
 import io.nzbee.entity.product.IProductService;
 import io.nzbee.entity.product.tag.IProductTagService;
-import io.nzbee.ui.component.web.sidebar.Sidebar;
-import io.nzbee.variables.CategoryVars;
+import io.nzbee.entity.product.tag.ProductTag;
 import io.nzbee.variables.ProductVars;
 
+
+//The Domain object service should be simple and dumb
+//it received primitive types and responds with domain objects,
+//that are constructed from relevant entity objects
+//its simplicity helps us with unit testing
 public class TagService implements ITagService {
 
 	@Autowired
@@ -31,26 +28,28 @@ public class TagService implements ITagService {
 	IProductService productService;
 	
 	@Override
-	public List<Tag> findAll(String locale, String currency, String categoryDesc, List<Sidebar> selectedFacets) {
-			
-			//categories
-			List<Sidebar> selectedCategories = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.PRIMARY_CATEGORY_FACET_NAME);}).collect(Collectors.toList());
-			List<Category> lpc = selectedCategories.stream().map(f-> {
-																		return categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, f.getDesc(), locale).get();
-																	 }).collect(Collectors.toList());
-			List<Category> lpcf = new ArrayList<Category>();
-			lpc.stream().forEach(pc -> { 
-				lpcf.addAll(CategoryService.recurseCategories(new ArrayList<Category>(), pc)); 
-			});
-			List<Long> facetCategoryIds = lpcf.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
+	public List<Tag> findAll(String locale, String currency, String categoryDesc, List<Long> categoryIds, List<Long> brandIds) {
+		return productTagService.findAll(categoryIds, locale, null, null, ProductVars.MARKDOWN_SKU_DESCRIPTION, currency, new Date(), new Date(), brandIds)
+				.stream().map(pt -> {
+					return convertToTagDO(pt, locale);
+				}).collect(Collectors.toList());	
+		
+//			//categories
+//			List<Sidebar> selectedCategories = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.PRIMARY_CATEGORY_FACET_NAME);}).collect(Collectors.toList());
+//			List<Category> lpc = selectedCategories.stream().map(f-> {
+//																		return categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, f.getDesc(), locale).get();
+//																	 }).collect(Collectors.toList());
+//			List<Category> lpcf = new ArrayList<Category>();
+//			lpc.stream().forEach(pc -> { 
+//				lpcf.addAll(CategoryService.recurseCategories(new ArrayList<Category>(), pc)); 
+//			});
+//			List<Long> facetCategoryIds = lpcf.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
+//
+//			//brands
+//			List<Sidebar> selectedBrands = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.BRAND_FACET_NAME);}).collect(Collectors.toList());
+//			List<Long> selectedBrandIds = selectedBrands.stream().map(b -> {return b.getId();}).collect(Collectors.toList());
+//				
 
-			//brands
-			List<Sidebar> selectedBrands = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.BRAND_FACET_NAME);}).collect(Collectors.toList());
-			List<Long> selectedBrandIds = selectedBrands.stream().map(b -> {return b.getId();}).collect(Collectors.toList());
-				
-			
-			return null;
-			//return productTagService.findAll(facetCategoryIds, locale, null, null, ProductVars.MARKDOWN_SKU_DESCRIPTION, currency, new Date(), new Date(), selectedBrandIds);
 			
 		
 //			List<Sidebar> lf = pt.stream().map(t -> {
@@ -80,29 +79,34 @@ public class TagService implements ITagService {
 	}
 	
 	@Override
-	public List<Tag> findAll(String locale, String currency, String categoryDesc, Double price, List<Sidebar> selectedFacets) {
+	public List<Tag> findAll(String locale, String currency, String categoryDesc, Double price, List<Long> categoryIds, List<Long> brandIds) {
+		return productTagService.findAll(categoryIds, locale, new Double(0), price, ProductVars.MARKDOWN_SKU_DESCRIPTION, currency, new Date(), new Date(), brandIds)
+				.stream().map(pt -> {
+					return convertToTagDO(pt, locale);
+				}).collect(Collectors.toList());
 		
 		//all categories (if non selected in facets
-		Optional<Category> parent = categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, categoryDesc, locale);
-		List<Category> allCategories = CategoryService.recurseCategories(new ArrayList<Category>(), parent.get());
-		List<Long> allCategoryIds = allCategories.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
-						
-		//Facets
-		List<Sidebar> selectedCategories = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.PRIMARY_CATEGORY_FACET_NAME);}).collect(Collectors.toList());
-		List<Category> lpc = selectedCategories.stream().map(f-> {return categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, f.getDesc(), locale).get();}).collect(Collectors.toList());
-							
-		List<Category> lpcf = new ArrayList<Category>();
-		lpc.stream().forEach(pc -> { lpcf.addAll(CategoryService.recurseCategories(new ArrayList<Category>(), pc)); });
-
-		List<Long> facetCategoryIds = lpcf.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
-
-		List<Sidebar> selectedBrands = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.BRAND_FACET_NAME);}).collect(Collectors.toList());
-		List<Long> selectedBrandIds = selectedBrands.stream().map(b -> {return b.getId();}).collect(Collectors.toList());
-				     	
-		List<Long> categoryIds = (selectedCategories.size() > 0) ? facetCategoryIds : allCategoryIds;
+//		Optional<Category> parent = categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, categoryDesc, locale);
+//		List<Category> allCategories = CategoryService.recurseCategories(new ArrayList<Category>(), parent.get());
+//		List<Long> allCategoryIds = allCategories.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
+//						
+//		//Facets
+//		List<Sidebar> selectedCategories = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.PRIMARY_CATEGORY_FACET_NAME);}).collect(Collectors.toList());
+//		List<Category> lpc = selectedCategories.stream().map(f-> {return categoryService.findByCategoryDesc(CategoryVars.CATEGORY_TYPE_CODE_PRODUCT, f.getDesc(), locale).get();}).collect(Collectors.toList());
+//							
+//		List<Category> lpcf = new ArrayList<Category>();
+//		lpc.stream().forEach(pc -> { lpcf.addAll(CategoryService.recurseCategories(new ArrayList<Category>(), pc)); });
+//
+//		List<Long> facetCategoryIds = lpcf.stream().map(sc -> { return sc.getCategoryId(); }).collect(Collectors.toList());
+//
+//		List<Sidebar> selectedBrands = selectedFacets.stream().filter(f -> {return f.getFacetingName().equals(CategoryVars.BRAND_FACET_NAME);}).collect(Collectors.toList());
+//		List<Long> selectedBrandIds = selectedBrands.stream().map(b -> {return b.getId();}).collect(Collectors.toList());
+//				     	
+//		List<Long> categoryIds = (selectedCategories.size() > 0) ? facetCategoryIds : allCategoryIds;
+//		
+//		return null;
 		
-		return null;
-		//return productTagService.findAll(categoryIds, locale, new Double(0), price, ProductVars.MARKDOWN_SKU_DESCRIPTION, currency, new Date(), new Date(), selectedBrandIds);
+			
 		
 //		List<ProductTagAttribute> pt = productTagService.findAll(categoryIds, locale, new Double(0), price, ProductVars.MARKDOWN_SKU_DESCRIPTION, currency, new Date(), new Date(), selectedBrandIds);
 		
@@ -118,6 +122,17 @@ public class TagService implements ITagService {
 //									.stream().filter(t -> t.getProductCount() > 0).collect(Collectors.toList());;
 //		
 //		return lf;	
+	}
+	
+	
+	private Tag convertToTagDO(ProductTag pt, String locale) {
+		Tag t = new Tag();
+		t.setTagId(pt.getTagId());
+		t.setTagCode(pt.getCode());
+		t.setLocale(locale);
+		t.setTagDesc(pt.getAttributes().stream().filter(ta -> ta.getLclCd().equals(locale)).collect(Collectors.toList()).get(0).getTagDesc());
+		t.setTagType("Product");
+		return t;
 	}
 	
 	@Override
