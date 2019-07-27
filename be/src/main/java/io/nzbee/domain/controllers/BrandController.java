ee66f2a4ec6cf1ca27460873e927f983397c2eee
dmac088 <danielmackie82@gmail.com>
2019-07-27 22:40:31 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/controllers/BrandController.java
+++ be/src/main/java/io/nzbee/domain/controllers/BrandController.java
@@ -5,15 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.nzbee.domain.Brand;
 import io.nzbee.domain.services.brand.IBrandService;
 import io.nzbee.ui.component.web.sidebar.Sidebar;
-import io.nzbee.variables.CategoryVars;
 
 @RestController
 @RequestMapping("/api")
