8aea66ce1d59191eafd2977b55aac09e8802ba0c
dmac088 <danielmackie82@gmail.com>
2019-08-04 17:14:52 +0800

    asdf

--- be/src/main/java/io/nzbee/ui/component/web/facet/NavFacetController.java
+++ be/src/main/java/io/nzbee/ui/component/web/facet/NavFacetController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.nzbee.domain.Tag;
+
 @RestController
 @RequestMapping("/api")
 public class NavFacetController {
@@ -17,30 +19,30 @@ public class NavFacetController {
 	@Autowired
 	private INavFacetService navFacetService;
 
-//  @PostMapping("/Product/{locale}/{currency}/{category}/{price}/tags")
-//  public List<Tag> getTags(@PathVariable String locale, @PathVariable String currency, @PathVariable String category, @PathVariable Double price, @RequestBody final List<Sidebar> selectedFacets) {
-//  	return tagService.getProductTags(locale, currency, category, price, selectedFacets);
-//  }
+	@PostMapping("/Product/{locale}/{currency}/{category}/{price}/tags")
+    public List<NavFacet<Tag>> getTags(@PathVariable String locale, @PathVariable String currency, @PathVariable String categoryDesc, @PathVariable Double price, @RequestBody final List<NavFacet> selectedFacets) {
+	  return navFacetService.findAll(locale, currency, categoryDesc, selectedFacets).getResult().getTags();
+	}
   
-//	@PostMapping("/Product/{locale}/{currency}/{category}/tags")
-//	public NavFacetResult getTags(@PathVariable String locale, @PathVariable String currency, @PathVariable String categoryDesc, @RequestBody final List<NavFacet> selectedFacets) {
-//	  return navFacetService.findAllTags(locale, currency, categoryDesc, selectedFacets);
-//	} 
-//	
+	@PostMapping("/Product/{locale}/{currency}/{category}/tags")
+	public NavFacetResult getTags(@PathVariable String locale, @PathVariable String currency, @PathVariable String categoryDesc, @RequestBody final List<NavFacet> selectedFacets) {
+	  return navFacetService.findAll(locale, currency, categoryDesc, selectedFacets);
+	} 
+	
     @GetMapping("/Category/{lcl}/{curr}")
     public NavFacetResult getCategories(@PathVariable String lcl, @PathVariable String curr) {
     	return navFacetService.findAll(lcl, curr);
     }
-//
-//    @PostMapping("/Category/{lcl}/{curr}/desc/{categoryDesc}/children")
-//    public NavFacetResult getCategoryChildren(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc, @RequestBody List<NavFacet> selectedFacets) {
-//    	return navFacetService.findAllCategories(lcl, curr, categoryDesc, selectedFacets);
-//    }
-//    
-//    @PostMapping("/Brand/{lcl}/{curr}/category/{categoryDesc}")
-//    public NavFacetResult getBrands(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc, @RequestBody List<NavFacet> selectedFacets) {
-//    	return navFacetService.findAllBrands(lcl, curr, categoryDesc, selectedFacets);
-//    }
+
+    @PostMapping("/Category/{lcl}/{curr}/desc/{categoryDesc}/children")
+    public NavFacetResult getCategoryChildren(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc, @RequestBody List<NavFacet> selectedFacets) {
+    	return navFacetService.findAll(lcl, curr, categoryDesc, selectedFacets);
+    }
+    
+    @PostMapping("/Brand/{lcl}/{curr}/category/{categoryDesc}")
+    public NavFacetResult getBrands(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc, @RequestBody List<NavFacet> selectedFacets) {
+    	return navFacetService.findAll(lcl, curr, categoryDesc, selectedFacets);
+    }
 	
     @PostMapping("/NavFacet/{lcl}/{curr}/category/{categoryDesc}")
     public NavFacetResult getAll(@PathVariable String lcl, @PathVariable String curr, @PathVariable String categoryDesc, @RequestBody List<NavFacet> selectedFacets) {
