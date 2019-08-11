e23ea1f5dbfe437789323eff01b023d078387ae8
dmac088 <danielmackie82@gmail.com>
2019-08-11 19:33:13 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/IProductService.java
+++ be/src/main/java/io/nzbee/entity/product/IProductService.java
@@ -9,32 +9,33 @@ import io.nzbee.entity.IService;
 
 public interface IProductService extends IService<Product> {
 	
-	Page<Product> findAll(String categoryDesc, List<Long> categoryIds, String locale, String priceType, String currency,
-			Date priceDateStart, Date priceDateEnd, Pageable pageable, List<Long> brandIds, List<Long> tagIds);
-
 	List<Product> findAll(String locale, String currency, List<Long> productIds);
 
 
-	Long getCount(String categoryDesc, String locale, String productStatusCode, List<Long> brandIds,
-			int inHandlingBrands, List<Long> categoryIds, int inHandlingCategories);
+	Long getCount(String categoryDesc, String locale, String productStatusCode, List<String> brandCodes,
+			List<String> categoryCodes);
 
-	Long getCountForTags(String categoryDesc, String locale, String productStatusCode, List<Long> brandIds,
-			int inHandlingBrands, List<Long> categoryIds, int inHandlingCategories, List<Long> tagIds,
-			int inHandlingTags);
+	Long getCountForTags(String categoryDesc, String locale, String productStatusCode, List<String> brandCodes,
+			List<String> categoryCodes, List<String> tagCodes);
 
 	Double getMaxMarkDownPrice(String categoryTypeCode, String categoryDesc, String locale, String currencyCode, 
-			String productStatusCode, List<Long> brandIds, int inHandlingBrands,
-			List<Long> categoryIds, int inHandlingCategories);
+			String productStatusCode, List<String> brandCodes, List<String> categoryCodes);
 
 	Double getMaxMarkDownPriceForTags(String categoryTypeCode, String categoryDesc, String locale, String currencyCode,
-			String productStatusCode, List<Long> brandIds, int inHandlingBrands, List<Long> categoryIds,
-			int inHandlingCategories, List<Long> tagIds, int inHandlingTags);
-
-	Double getMaxMarkDownPriceForCategory(Long categoryId, String currencyCode);
+			String productStatusCode, List<String> brandCodes, List<String> categoryCodes, List<String> tagCodes);
+	
+	Double getMaxMarkDownPriceForCategory(String categoryCode, String currencyCode);
 
 	Long getCountForCategory(Long categoryId);
 
 	Page<Product> findAll(String categoryDesc, List<String> categoryCodes, String locale, Double priceStart,
 			Double priceEnd, String priceType, String currency, Date priceDateStart, Date priceDateEnd,
 			Pageable pageable, List<String> brandCodes, List<String> tagCodes);
+
+	Page<Product> findAll(String categoryDesc, List<String> categoryCodes, String locale, String priceType,
+			String currency, Date priceDateStart, Date priceDateEnd, Pageable pageable, List<String> brandCodes,
+			List<String> tagCodes);
+
+
+	
 }
