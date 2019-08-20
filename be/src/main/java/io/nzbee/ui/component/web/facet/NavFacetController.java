4a85b87f44aa0eda604eb279e7c72a50a16daa78
Dan <daniel.mackie@dfs.com>
2019-08-20 19:07:25 +0800

    asdf

--- be/src/main/java/io/nzbee/ui/component/web/facet/NavFacetController.java
+++ be/src/main/java/io/nzbee/ui/component/web/facet/NavFacetController.java
@@ -1,7 +1,5 @@
 package io.nzbee.ui.component.web.facet;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +7,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.nzbee.domain.Brand;
 
 @RestController
 @RequestMapping("/api")
