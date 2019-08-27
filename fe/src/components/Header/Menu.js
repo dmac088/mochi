74b6b374001eb695f20f1720a86e473b6f037562
dmac088 <danielmackie82@gmail.com>
2019-08-27 21:21:26 +0800

    adsf

--- fe/src/components/Header/Menu.js
+++ fe/src/components/Header/Menu.js
@@ -1,46 +1,85 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { routeHome, routeContact, createRouteProps } from '../../services/helpers/routeHelper';
+import { filterCategories } from '../../services/helpers/filterHelper';
 
 export const Menu = withRouter(({history, match, location, ...props}) => {
     const routeProps = createRouteProps(history, match, location);
+    console.log(props);
+    const { brandCategoryList } = props;
     return (
       <div className="main-menu">
         <nav>
           <ul>
-            <li className="active"><a onClick={(e) => routeHome(e, routeProps)} href="#">HOME</a>
-            </li>
-            <li className="menu-item-has-children">
-              <a href="#">Brands</a>
-              <ul className="sub-menu mega-menu three-column">
-                <li><a href="#">Comvita</a></li>
-                <li><a href="#">Airborne</a></li>
-                <li><a href="#">Happy Bee</a></li>
-                <li><a href="#">Antipodes</a></li>
-                <li><a href="#">Wild Ferns</a></li>
-                <li><a href="#">Trilogy</a></li>
-                <li><a href="#">Anchor</a></li>
-              </ul>
-            </li>
-            <li className="menu-item-has-children">
-              <a href="#">BLOG</a>
-              <ul className="sub-menu">
-                <li><a href="blog-3-column.html">Blog 3 column</a></li>
-                <li><a href="blog-grid-left-sidebar.html">Blog Grid Left Sidebar</a></li>
-                <li><a href="blog-grid-right-sidebar.html">Blog Grid Right Sidebar</a></li>
-                <li><a href="blog-list-left-sidebar.html">Blog List Left Sidebar</a></li>
-                <li><a href="blog-list-right-sidebar.html">Blog List Right Sidebar</a></li>
-                <li><a href="blog-post-left-sidebar.html">Blog Post Left Sidebar</a></li>
-                <li><a href="blog-post-right-sidebar.html">Blog Post Right Sidebar</a></li>
-                <li><a href="blog-post-image-format.html">Blog Post Image Format</a></li>
-                <li><a href="blog-post-image-gallery.html">Blog Post Image Gallery Format</a></li>
-                <li><a href="blog-post-audio-format.html">Blog Post Audio Format</a></li>
-                <li><a href="blog-post-video-format.html">Blog Post Video Format</a></li>
-              </ul>
-            </li>
-            <li><a onClick={(e) => routeContact(e, routeProps)} href="#">CONTACT</a></li>
+            <HomeMenu />
+            <BrandMenu
+              brandCategoryList={brandCategoryList}
+             />
+            <BlogMenu />
+            <ContactMenu />
           </ul>
         </nav>
       </div>
     )
   });
+
+const HomeMenu = () => {
+    return (
+      <li className="active">
+        <a onClick={(e) => routeHome(e, routeProps)} href="#">HOME</a>
+      </li>
+    );
+}
+
+const ContactMenu = () => {
+    return (
+      <li>
+        <a onClick={(e) => routeContact(e, routeProps)} href="#">CONTACT</a>
+      </li>
+    );
+}
+
+const BrandMenu = (props) => {
+  const { brandCategoryList } = props;
+  console.log(brandCategoryList);
+  if(!brandCategoryList) { return null; }
+  if(brandCategoryList.length === 0) { return null; }
+  const category = filterCategories(brandCategoryList, 'LNDHM01')[0];
+    return (
+      <li className="menu-item-has-children">
+        <a href="#">{category.facetDisplayValue}</a>
+        <ul class="mega-menu three-column">
+          {renderBrands(category.payload.brands)}
+        </ul>
+      </li>
+    )
+}
+
+const renderBrands = (brandList) => {
+  return brandList.map(b => {
+    return (
+      <li><a href="#">{b.facetDisplayValue}</a></li>
+    );
+  });
+}
+
+const BlogMenu = () => {
+    return (
+      <li className="menu-item-has-children">
+        <a href="#">BLOG</a>
+        <ul className="sub-menu">
+          <li><a href="blog-3-column.html">Blog 3 column</a></li>
+          <li><a href="blog-grid-left-sidebar.html">Blog Grid Left Sidebar</a></li>
+          <li><a href="blog-grid-right-sidebar.html">Blog Grid Right Sidebar</a></li>
+          <li><a href="blog-list-left-sidebar.html">Blog List Left Sidebar</a></li>
+          <li><a href="blog-list-right-sidebar.html">Blog List Right Sidebar</a></li>
+          <li><a href="blog-post-left-sidebar.html">Blog Post Left Sidebar</a></li>
+          <li><a href="blog-post-right-sidebar.html">Blog Post Right Sidebar</a></li>
+          <li><a href="blog-post-image-format.html">Blog Post Image Format</a></li>
+          <li><a href="blog-post-image-gallery.html">Blog Post Image Gallery Format</a></li>
+          <li><a href="blog-post-audio-format.html">Blog Post Audio Format</a></li>
+          <li><a href="blog-post-video-format.html">Blog Post Video Format</a></li>
+        </ul>
+      </li>
+    );
+}
