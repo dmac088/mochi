282bffd9da58c96ad93f27d750aa2047c6e3c014
dmac088 <danielmackie82@gmail.com>
2019-09-14 22:45:31 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/dto/category/ICategoryDao.java
@@ -0,0 +1,21 @@
+package io.nzbee.dto.category;
+
+import java.util.List;
+import java.util.Optional;
+import io.nzbee.entity.IDao;
+
+public interface ICategoryDao extends IDao<Category> {
+
+	Optional<Category> findByCategoryDesc(String categoryTypeCode, String categoryDesc, String locale);
+	
+	Optional<Category> findByCategoryCode(String categoryTypeCode, String categoryCode, String locale);
+	
+	List<Category> findByParent(String hieararchyCode, String categoryTypeCode, Long parentCategoryId, String locale);
+	
+	List<Category> findByLevel(String hieararchyCode, String categoryTypeCode, Long level, String locale);
+
+	List<Category> findChildrenByCriteria(String hieararchyCode, String categoryTypeCode, String parentCategoryDesc,
+			List<String> brandCodes, List<String> tagCodes, String locale);
+
+	
+}
