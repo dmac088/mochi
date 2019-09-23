0bd505e96a96adf2bfcd68106a86f7c188ff97ad
Dan <daniel.mackie@dfs.com>
2019-09-23 10:08:12 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/category/Category_.java
+++ be/.apt_generated/io/nzbee/entity/category/Category_.java
@@ -2,6 +2,7 @@ package io.nzbee.entity.category;
 
 import io.nzbee.entity.category.attribute.CategoryAttribute;
 import io.nzbee.entity.category.type.CategoryType;
+import io.nzbee.entity.product.hierarchy.Hierarchy;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -14,6 +15,7 @@ public abstract class Category_ {
 	public static volatile SingularAttribute<Category, Category> parent;
 	public static volatile SingularAttribute<Category, Long> categoryLevel;
 	public static volatile SingularAttribute<Category, CategoryAttribute> categoryAttribute;
+	public static volatile SingularAttribute<Category, Hierarchy> hierarchy;
 	public static volatile SingularAttribute<Category, String> categoryCode;
 	public static volatile SingularAttribute<Category, Long> categoryId;
 
