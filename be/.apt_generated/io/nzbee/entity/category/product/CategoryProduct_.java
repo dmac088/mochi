171c34724c08e3d0d420eb67dfe0157c9d77adc4
Dan <daniel.mackie@dfs.com>
2019-09-23 14:03:18 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/category/product/CategoryProduct_.java
+++ be/.apt_generated/io/nzbee/entity/category/product/CategoryProduct_.java
@@ -1,12 +1,14 @@
 package io.nzbee.entity.category.product;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(CategoryProduct.class)
 public abstract class CategoryProduct_ extends io.nzbee.entity.category.Category_ {
 
+	public static volatile SingularAttribute<CategoryProduct, Long> productCount;
 
 }
 
