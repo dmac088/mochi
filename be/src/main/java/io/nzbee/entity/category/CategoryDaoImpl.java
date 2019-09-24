cb78e2d9149baca5ddff8f357906c6eb28be8630
dmac088 <danielmackie82@gmail.com>
2019-09-24 21:23:10 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/CategoryDaoImpl.java
+++ be/src/main/java/io/nzbee/entity/category/CategoryDaoImpl.java
@@ -77,14 +77,14 @@ public class CategoryDaoImpl implements ICategoryDao {
 		List<Object[]> results = query.getResultList();
 		List<Category> lc = results.stream().map(c -> (Category) c[0]).collect(Collectors.toList());
 				
-		System.out.println((results.get(0)[8]));
 		
-		lc.stream().forEach(c -> {
-			System.out.println(c.getCategoryCode());
-//			System.out.println(c.getCategoryAttribute().getCategoryDesc());
-//			System.out.println(c.getParent().getCategoryAttribute().getCategoryDesc());
-			System.out.println(c.getClass().getSimpleName());
-			System.out.println(c.getCategoryCode() + " - " + c.getObjectCount());
+		results.stream().forEach(c -> {
+			Category category = (Category) c[0];
+			System.out.println(category.getCategoryCode());
+			System.out.println(((CategoryAttribute) c[1]).getCategoryDesc());
+			//System.out.println(category.getParent().getCategoryAttribute().getCategoryDesc());
+			System.out.println(category.getClass().getSimpleName());
+			System.out.println(category.getCategoryCode() + " - " + c[8]);
 		});
 		
 		return null;
