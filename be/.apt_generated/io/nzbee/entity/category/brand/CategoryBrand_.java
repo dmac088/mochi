03a0aefe0c28b91dbf527c61faa14302c69f7045
Dan <daniel.mackie@dfs.com>
2019-09-24 17:05:32 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/category/brand/CategoryBrand_.java
+++ be/.apt_generated/io/nzbee/entity/category/brand/CategoryBrand_.java
@@ -3,7 +3,6 @@ package io.nzbee.entity.category.brand;
 import io.nzbee.entity.brand.Brand;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.ListAttribute;
-import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
@@ -11,7 +10,6 @@ import javax.persistence.metamodel.StaticMetamodel;
 public abstract class CategoryBrand_ extends io.nzbee.entity.category.Category_ {
 
 	public static volatile ListAttribute<CategoryBrand, Brand> brands;
-	public static volatile SingularAttribute<CategoryBrand, Long> brandCount;
 
 }
 
