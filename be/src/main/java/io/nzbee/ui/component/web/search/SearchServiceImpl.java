4a8c337818cb1081d2a33294379af816c9fc0de9
dmac088 <danielmackie82@gmail.com>
2019-09-29 19:14:21 +0800

    asdf

--- be/src/main/java/io/nzbee/ui/component/web/search/SearchServiceImpl.java
+++ be/src/main/java/io/nzbee/ui/component/web/search/SearchServiceImpl.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
