b35eb9180ac2e6caf8ef920ad89b93c1c64d1c17
dmac088 <danielmackie82@gmail.com>
2019-10-06 11:08:08 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/category/ProductCategory.java
+++ be/src/main/java/io/nzbee/domain/category/ProductCategory.java
@@ -1,20 +1,24 @@
 package io.nzbee.domain.category;
 
+import java.util.List;
+
+import io.nzbee.domain.product.Product;
+
 public class ProductCategory extends Category {
 
 
 	public ProductCategory() {
-		//super();
+		super();
 	}
 
-//	private List<Product> products;
-//	
-//	public List<Product> getProducts() {
-//		return products;
-//	}
-//
-//	public void setProducts(List<Product> products) {
-//		this.products = products;
-//	}
+	private List<Product> products;
+	
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
 	
 }
