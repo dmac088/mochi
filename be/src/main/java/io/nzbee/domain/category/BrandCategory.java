858b756052dae3b1024999f94199a280238e6db6
dmac088 <danielmackie82@gmail.com>
2019-10-06 11:08:08 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/category/BrandCategory.java
+++ be/src/main/java/io/nzbee/domain/category/BrandCategory.java
@@ -1,21 +1,25 @@
 package io.nzbee.domain.category;
 
+import java.util.List;
+
+import io.nzbee.domain.brand.Brand;
+
 public class BrandCategory extends Category {
 
 
 	public BrandCategory() {
-		//super();
+		super();
 	}
-//
-//	private List<Brand> brands;
-//	
-//	public List<Brand> getBrands() {
-//		return brands;
-//	}
-//
-//	public void setBrands(List<Brand> brands) {
-//		this.brands = brands;
-//	}
+
+	private List<Brand> brands;
+	
+	public List<Brand> getBrands() {
+		return brands;
+	}
+
+	public void setBrands(List<Brand> brands) {
+		this.brands = brands;
+	}
 
 	
 }
