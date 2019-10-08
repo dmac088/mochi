2f2dbb7fc92fdfcad75f048726b0a00869820f4e
dmac088 <danielmackie82@gmail.com>
2019-10-08 20:50:29 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/IProductService.java
+++ be/src/main/java/io/nzbee/entity/product/IProductService.java
@@ -9,12 +9,12 @@ import io.nzbee.entity.IService;
 public interface IProductService extends IService<Product> {
 	
 	Page<Product> findAll(String locale, String currency, List<String> productCodes);
-	
-	Page<Product> findAll(String locale, String currency, String priceType, Pageable pageable, String categoryDesc,
+
+	Page<Product> findAll(String locale, String currency, String priceType, int page, int size, String categoryDesc,
 			List<String> categoryCodes, List<String> brandCodes, List<String> tagCodes);
 
 	Page<Product> findAll(String locale, String currency, Double priceStart, Double priceEnd, String priceType,
-			Pageable pageable, String categoryDesc, List<String> categoryCodes, List<String> brandCodes,
+			int page, int size, String categoryDesc, List<String> categoryCodes, List<String> brandCodes,
 			List<String> tagCodes);
 
 	
