04c6d6c8495d4ec9d0845149152d54bb68bb0621
dmac088 <danielmackie82@gmail.com>
2019-10-08 10:59:42 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/ProductDaoImpl.java
+++ be/src/main/java/io/nzbee/entity/product/ProductDaoImpl.java
@@ -14,6 +14,8 @@ import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+
+import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
@@ -44,6 +46,7 @@ import io.nzbee.entity.product.status.ProductStatus;
 import io.nzbee.entity.product.status.ProductStatus_;
 import io.nzbee.entity.product.tag.ProductTag;
 import io.nzbee.entity.product.tag.ProductTag_;
+import io.nzbee.variables.CategoryVars;
 import io.nzbee.variables.ProductVars;
 
 @Component(value = "productEntityDao")
@@ -57,7 +60,12 @@ public class ProductDaoImpl implements IProductDao {
 	@Override
 	public List<Product> findAll(String locale, String currency) {
 		
+		List<String> categories = new ArrayList<String>();
+		categories.add(CategoryVars.PRIMARY_HIERARCHY_ROOT_CODE);
+		
+		
 		Query query = em.createNamedQuery("getProducts")
+				 .setParameter("categoryCodes", categories)
 				 .setParameter("locale", locale)
 				 .setParameter("currency", currency)
 				 .setParameter("activeProductCode", ProductVars.ACTIVE_SKU_CODE)
@@ -87,7 +95,11 @@ public class ProductDaoImpl implements IProductDao {
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public Page<Product> findAll(String locale, String currency, int page, int size, String orderby) {
+	public Page<Product> findAll(	String locale, 
+									String currency, 
+									int page, 
+									int size, 
+									String orderby) {
 		// TODO Auto-generated method stub
 		
 		//first get the result count
@@ -132,67 +144,24 @@ public class ProductDaoImpl implements IProductDao {
 	
 
 	@Override
-	public Page<Product> findAll(List<String> categoryCodes, String locale, Double priceStart,
-			Double priceEnd, String priceType, String currency, Date priceDateStart, Date priceDateEnd,
-			Pageable pageable, List<String> brandCodes, List<String> tagCodes) {
+	public Page<Product> findAll(List<String> categoryCodes, 
+								 String locale, 
+								 Double priceStart,
+								 Double priceEnd, 
+								 String priceType, 
+								 String currency, 
+								 Date priceDateStart, 
+								 Date priceDateEnd,
+								 int page, 
+								 int size,  
+								 List<String> brandCodes, 
+								 List<String> tagCodes) {
+		
 		// TODO Auto-generated method stub
-		CriteriaBuilder cb = em.getCriteriaBuilder();
-		
-		CriteriaQuery<Product> cq = cb.createQuery(Product.class);
-		
-		Root<Product> root 									= cq.from(Product.class);
-		Join<Product, ProductAttribute> productAttribute 	= root.join(Product_.attributes);
-		Join<Product, CategoryProduct> category 					= root.join(Product_.categories);
-		Join<Product, Brand> brand 							= root.join(Product_.brand);
-		Join<Product, ProductStatus> status 				= root.join(Product_.productStatus);
-		Join<Product, ProductPrice> price 					= root.join(Product_.prices);
-		Join<ProductPrice, ProductPriceType> type 			= price.join(ProductPrice_.type);
-		Join<ProductPrice, Currency> curr 					= price.join(ProductPrice_.currency);
-		Join<Brand, BrandAttribute> brandAttribute 			= brand.join(Brand_.brandAttributes);
-		Join<CategoryProduct, CategoryAttribute> categoryAttribute = category.join(CategoryProduct_.attributes);
-		//Join<Category, Hierarchy> categoryHierarchy 		= category.join(Category_.hierarchy);
-		
-		List<Predicate> conditions = new ArrayList<Predicate>();
-		if(!categoryCodes.isEmpty()) {
-			conditions.add(category.get(CategoryProduct_.categoryCode).in(categoryCodes));
-		}
-		if(!brandCodes.isEmpty()) {
-			conditions.add(brand.get(Brand_.brandCode).in(brandCodes));
-		}
-		if(!tagCodes.isEmpty()) {
-			Join<Product, ProductTag> tag = root.join(Product_.tags);
-			conditions.add(tag.get(ProductTag_.productTagCode).in(tagCodes));
-		}
-		conditions.add(cb.equal(brandAttribute.get(BrandAttribute_.lclCd), locale));
-		conditions.add(cb.equal(productAttribute.get(ProductAttribute_.lclCd), locale));
-		conditions.add(cb.equal(categoryAttribute.get(CategoryAttribute_.lclCd), locale));
-		conditions.add(cb.equal(status.get(ProductStatus_.productStatusCode), ProductVars.ACTIVE_SKU_CODE));
-		conditions.add(cb.equal(type.get(ProductPriceType_.desc), priceType));
-		conditions.add(cb.equal(curr.get(Currency_.code), currency));
-		if(priceStart != -1 && priceEnd != -1) {
-			conditions.add(cb.greaterThanOrEqualTo(price.get(ProductPrice_.priceValue), priceStart));
-			conditions.add(cb.lessThanOrEqualTo(price.get(ProductPrice_.priceValue), priceEnd));
-		}
-		conditions.add(cb.lessThanOrEqualTo(price.get(ProductPrice_.startDate), priceDateStart));
-		conditions.add(cb.greaterThanOrEqualTo(price.get(ProductPrice_.endDate), priceDateEnd));
 		
 		
-		Order order = pageable.getSort().stream().map(o -> {
-			return this.getOrder(o.getProperty().replaceAll(".*\\.", ""), o.getDirection(), cb, productAttribute, price);
-		}).collect(Collectors.toList()).get(0);
 		
-		TypedQuery<Product> query = em.createQuery(cq
-				.select(root)
-				.where(conditions.toArray(new Predicate[] {}))
-				.distinct(false)
-				.orderBy(order)
-		);
-
-		PageableUtil pageableUtil = new PageableUtil();
-		query.setFirstResult(pageableUtil.getStartPosition(pageable));
-		query.setMaxResults(pageable.getPageSize());
-		
-		return new PageImpl<Product>(query.getResultList(), pageable, query.getResultList().size());
+		return null;
 
 	}
 	
