412bab3f2de3d3e39fb5c7c551c7bdef11ba05b6
dmac088 <danielmackie82@gmail.com>
2019-10-08 19:58:41 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/IProductService.java
+++ be/src/main/java/io/nzbee/entity/product/IProductService.java
@@ -8,18 +8,6 @@ import io.nzbee.entity.IService;
 
 public interface IProductService extends IService<Product> {
 	
-	Double getMaxMarkDownPrice(String categoryTypeCode, String categoryDesc, String locale, String currencyCode, 
-			String productStatusCode, List<String> brandCodes, List<String> categoryCodes);
-
-	Double getMaxMarkDownPriceForTags(String categoryTypeCode, String categoryDesc, String locale, String currencyCode,
-			String productStatusCode, List<String> brandCodes, List<String> categoryCodes, List<String> tagCodes);
-
-	Long getCount(String categoryDesc, String locale, String currency, String productStatusCode,
-			List<String> brandCodes, List<String> categoryCodes);
-
-	Long getCountForTags(String categoryDesc, String locale, String currency, String productStatusCode,
-			List<String> brandCodes, List<String> categoryCodes, List<String> tagCodes);
-
 	Page<Product> findAll(String locale, String currency, List<String> productCodes);
 	
 	Page<Product> findAll(String locale, String currency, String priceType, Pageable pageable, String categoryDesc,
