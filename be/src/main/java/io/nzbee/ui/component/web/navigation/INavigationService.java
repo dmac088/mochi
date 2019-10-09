94f307f4741b9099881a31744676f20d93aae986
dmac088 <danielmackie82@gmail.com>
2019-10-09 22:23:49 +0800

    asdf

--- be/src/main/java/io/nzbee/ui/component/web/navigation/INavigationService.java
+++ be/src/main/java/io/nzbee/ui/component/web/navigation/INavigationService.java
@@ -5,8 +5,8 @@ import io.nzbee.ui.component.web.search.Search;
 
 public interface INavigationService {
 
-	Search findAll(String locale, String currency, String categoryDesc, int page, int size, String sortBy,
-			NavFacetContainer selectedFacets);
+	Search findAll(String locale, String currency, String categoryDesc, int page, int size,
+			NavFacetContainer selectedFacets, String sortBy);
 
 	Search findAll(String locale, String currency, Double price, int page, int size, String categoryDesc,
 			NavFacetContainer selectedFacets, String sortBy);
