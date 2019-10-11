1facf5ad961e948d9bf5c792d735083bdb9d7a88
Dan <daniel.mackie@dfs.com>
2019-10-11 19:07:38 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/attribute/ProductAttribute.java
+++ be/src/main/java/io/nzbee/entity/product/attribute/ProductAttribute.java
@@ -100,8 +100,8 @@ public class ProductAttribute {
 	@Facet
 	@Field(analyze = Analyze.NO, store=Store.YES)
 	public String getBrandCode() {
-		//return this.product.getBrand().getCode();
-		return "Empty";
+		return this.getProduct().getBrand().getCode();
+	
 	}
 	
 	@Transient
@@ -180,10 +180,7 @@ public class ProductAttribute {
 	
 	@Field(analyze = Analyze.YES, store=Store.YES)
 	public String getBrandDesc() {
-//		List<BrandAttribute> lba = this.product.getBrand().getAttributes();
-//		Optional<BrandAttribute> brandAttribute = lba.stream().filter(ba -> ba.getLclCd().equals(this.getLclCd())).findFirst();
-//		if(brandAttribute.isPresent()) { return brandAttribute.get().getBrandDesc(); }
-		return "Empty";
+		return this.getProduct().getBrand().getBrandAttribute().getBrandDesc();
 	}
 	
 	@Transient
