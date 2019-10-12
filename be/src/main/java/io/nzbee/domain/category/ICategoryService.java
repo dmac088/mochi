122f37f6a23469d40f11abe0db5a6e7b5531caea
dmac088 <danielmackie82@gmail.com>
2019-10-12 15:01:40 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/category/ICategoryService.java
+++ be/src/main/java/io/nzbee/domain/category/ICategoryService.java
@@ -2,30 +2,19 @@ package io.nzbee.domain.category;
 
 
 import java.util.List;
-import java.util.Optional;
-
+import io.nzbee.domain.IService;
 import io.nzbee.domain.brand.Brand;
 import io.nzbee.domain.tag.Tag;
 
 
-public interface ICategoryService {
-	 	 
-	List<Category> findByParent(String locale, String parentCategoryCode);
-
-	List<Category> findAll(String locale, String categoryDesc, List<Brand> brands, List<Tag> tags);
-
-	Optional<Category> findOne(String locale, String categoryCode);
-
-	Optional<Category> findOneByCode(String locale, String categoryCode);
-
-	Optional<Category> findOneByDesc(String locale, String categoryDesc);
+public interface ICategoryService extends IService<Category> {
 
 	List<Category> findAll(String locale, String currency);
-	
-	List<Category> findAllForLevel(Long level, String locale);
 
-	Category convertCategoryDtoToCategoryDO(io.nzbee.dto.category.Category category);
+	List<Category> findByParent(String locale, String currency, String parentCategoryCode);
 
-	io.nzbee.dto.category.Category convertCategoryDOtoCategoryDto(Category category);
+	List<Category> findAllForLevel(Long level, String currency, String locale);
+
+	List<Category> findAll(String locale, String currency, String categoryDesc, List<Brand> brands, List<Tag> tags);
 
 }
