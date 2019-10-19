e5e7b128c8a039305be5ece3ffe5ab1374d2ca59
dmac088 <danielmackie82@gmail.com>
2019-10-19 23:37:04 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/brand/Brand.java
+++ be/src/main/java/io/nzbee/entity/brand/Brand.java
@@ -4,16 +4,13 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
-
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Store;
 import com.fasterxml.jackson.annotation.JsonIgnore;
