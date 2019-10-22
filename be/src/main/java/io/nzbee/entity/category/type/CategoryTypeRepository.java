e6a5e81126488f4cdf9b24601c23c69549f0162d
Dan <daniel.mackie@dfs.com>
2019-10-22 12:14:29 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/type/CategoryTypeRepository.java
+++ be/src/main/java/io/nzbee/entity/category/type/CategoryTypeRepository.java
@@ -1,12 +1,14 @@
 package io.nzbee.entity.category.type;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryTypeRepository extends CrudRepository<CategoryType, Long> {
 	
-	List<CategoryType> findAll();
-	
-	CategoryType findByCategoryTypeId(Long id);
+	List<CategoryType> findAll(); 
+	 
+	Optional<CategoryType> findById(Long id);
 }
 
