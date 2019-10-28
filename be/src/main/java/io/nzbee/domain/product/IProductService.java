c3cb8cc29ceeb0b014c1ad5c842431b56e888cca
dmac088 <danielmackie82@gmail.com>
2019-10-28 19:57:42 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/product/IProductService.java
+++ be/src/main/java/io/nzbee/domain/product/IProductService.java
@@ -1,15 +1,9 @@
 package io.nzbee.domain.product;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.util.Streamable;
-
 import io.nzbee.domain.IService;
-import io.nzbee.dto.IDto;
-import io.nzbee.resources.product.ProductResource;
 import io.nzbee.ui.component.web.facet.IFacet;
 
 public interface IProductService extends IService<Product> {
