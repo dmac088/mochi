5c440e19908a295d554853bc77c3ff5f5ab94560
dmac088 <danielmackie82@gmail.com>
2019-10-30 20:43:55 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/IDomainObject.java
+++ be/src/main/java/io/nzbee/domain/IDomainObject.java
@@ -1,5 +1,13 @@
 package io.nzbee.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeInfo(
+	    use = JsonTypeInfo.Id.CLASS,
+	    include = JsonTypeInfo.As.PROPERTY,
+	    property = "@class")
 public interface IDomainObject {
 
 	public String getCode();
