2b43009740037853fd812028cae9565a895b722b
dmac088 <danielmackie82@gmail.com>
2019-11-05 20:59:55 +0800

    asdf

--- be/src/main/java/io/nzbee/resource/controllers/SearchController.java
+++ be/src/main/java/io/nzbee/resource/controllers/SearchController.java
@@ -1,6 +1,10 @@
-package io.nzbee.ui.component.web.search;
+package io.nzbee.resource.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,7 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.nzbee.domain.product.Product;
 import io.nzbee.ui.component.web.facet.FacetContainer;
+import io.nzbee.ui.component.web.search.ISearchService;
+import io.nzbee.ui.component.web.search.Search;
 
 @RestController
 @RequestMapping("/api")
@@ -26,9 +33,16 @@ public class SearchController {
     						@RequestParam("page") int page,
 					    	@RequestParam("size") int size, 
     						@PathVariable String sortBy, 
-    						@RequestBody  FacetContainer selectedFacets) {
-    	System.out.println(selectedFacets.getFacets().size());
-    	return searchService.findAll(locale, currency, category, term, page, size, sortBy, selectedFacets);
+    						@RequestBody  FacetContainer selectedFacets,
+    						@SuppressWarnings("rawtypes") PagedResourcesAssembler assembler) {
+    	
+    	final Page<Product> pages = searchService.findAll(locale, currency, category, term, page, size, sortBy, selectedFacets);
+
+    	Search s = new Search();
+    	
+    	s.setProducts(assembler.toResource(pages));
+    	
+    	return s;
     }
 	
 }
---
+++ be/src/main/java/io/nzbee/resources/search/SearchResource.java
@@ -0,0 +1,44 @@
+package io.nzbee.resources.search;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import io.nzbee.domain.category.Category;
+import io.nzbee.domain.product.Product;
+import io.nzbee.resource.controllers.CategoryController;
+import io.nzbee.resource.controllers.ProductController;
+import io.nzbee.resource.controllers.SearchController;
+import io.nzbee.ui.component.web.facet.FacetContainer;
+import lombok.Getter;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.web.PagedResourcesAssembler;
+
+@Getter
+public class SearchResource extends ResourceSupport {
+
+	@Autowired
+	private PagedResourcesAssembler<Product> parAssembler;
+	
+	public SearchResource(String locale, 
+						  String currency, 
+						  String category,
+						  String term,
+						  int page,
+						  int size,
+						  String sortBy,
+						  FacetContainer searchFacets) {
+
+		add(linkTo(methodOn(SearchController.class).search(	locale, 
+															currency, 
+															category, 
+															term, 
+															page, 
+															size, 
+															sortBy, 
+															searchFacets,
+															parAssembler)).withRel("products"));
+		
+    }
+	
+}
