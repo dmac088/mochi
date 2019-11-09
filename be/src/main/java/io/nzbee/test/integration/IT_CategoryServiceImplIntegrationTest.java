5a37e7ebb385eb288bcaa21f7ef3d6985d79c1a8
dmac088 <danielmackie82@gmail.com>
2019-11-09 20:58:34 +0800

    finally got unit testing working with JUni mocking in Spring Boot

---
+++ be/src/main/java/io/nzbee/test/integration/IT_CategoryServiceImplIntegrationTest.java
@@ -0,0 +1,72 @@
+package io.nzbee.test.integration;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.entity.category.ICategoryService;
+import io.nzbee.entity.category.product.CategoryProduct;
+import io.nzbee.entity.category.Category;
+import io.nzbee.entity.category.CategoryDaoPostgresImpl;
+import io.nzbee.entity.category.CategoryServiceImpl;
+import io.nzbee.entity.category.ICategoryDao;
+
+@RunWith(SpringRunner.class)
+public class IT_CategoryServiceImplIntegrationTest {
+
+	@TestConfiguration
+    static class CategoryServiceImplIntegrationTestConfiguration {
+		//the beans that we need to run this integration test
+        @Bean(value = "categoryEntityService")
+        public ICategoryService categoryService() {
+            return new CategoryServiceImpl();
+        }
+        
+        @Bean(value = "categoryEntityPostgresDao")
+        public ICategoryDao categoryDao() {
+            return new CategoryDaoPostgresImpl();
+        }
+
+    }
+
+	@Autowired
+	@InjectMocks
+    private CategoryServiceImpl categoryService;
+ 
+	@MockBean
+    private ICategoryDao categoryDao;
+	
+    @Before
+    public void setUp() {
+    	//we setup a mock so that when 
+    	MockitoAnnotations.initMocks(this);
+    	
+        Category testCategory = new CategoryProduct();
+        testCategory.setCategoryCode("TST01");
+     
+        Mockito.when(categoryDao.findByCode("en-GB", "HKD", testCategory.getCategoryCode()))
+          .thenReturn(Optional.ofNullable(testCategory));
+    }
+    
+    @Test
+    public void whenValidName_thenEmployeeShouldBeFound() {
+        String code = "TST01";
+        Optional<Category> found = categoryService.findByCode("en-GB", "HKD", code);
+      
+         assertThat(found.get().getCategoryCode())
+          .isEqualTo(code);
+     }
+    
+}
