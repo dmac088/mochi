9a4a29327164812570c54f64106e83a43ba6e016
dmac088 <danielmackie82@gmail.com>
2019-11-12 15:03:50 +0800

    asdf

--- be/src/main/java/io/nzbee/test/integration/entity/IT_CategoryEntityRepositoryIntegrationTest.java
+++ be/src/main/java/io/nzbee/test/integration/entity/IT_CategoryEntityRepositoryIntegrationTest.java
@@ -2,9 +2,6 @@ package io.nzbee.test.integration.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import javax.persistence.EntityManager;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,15 +10,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import io.nzbee.entity.category.Category;
 import io.nzbee.entity.category.ICategoryService;
-import io.nzbee.entity.category.attribute.CategoryAttribute;
-import io.nzbee.entity.category.product.CategoryProduct;
-import io.nzbee.entity.category.type.CategoryType;
-import io.nzbee.entity.category.type.ICategoryTypeRepository;
-import io.nzbee.entity.product.hierarchy.Hierarchy;
-import io.nzbee.variables.CategoryVars;
+import io.nzbee.test.integration.entity.beans.CategoryEntityBeanFactory;
 import io.nzbee.variables.GeneralVars;
 
 @RunWith(SpringRunner.class)
@@ -29,41 +23,38 @@ import io.nzbee.variables.GeneralVars;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 public class IT_CategoryEntityRepositoryIntegrationTest {
  
+	@TestConfiguration
+    static class CategoryDtoServiceImplIntegrationTest {
+        
+        @Bean(value = "categoryEntityService")
+        public io.nzbee.entity.category.ICategoryService categoryService() {
+            return new io.nzbee.entity.category.CategoryServiceImpl();
+        }
+        
+        @Bean(value = "categoryEntityBeanFactory")
+        public CategoryEntityBeanFactory categoryFactoryBean() {
+            return new CategoryEntityBeanFactory();
+        }
+    }
+	
 	@Autowired
 	@Qualifier("mochiEntityManagerFactory")
 	private EntityManager entityManager;
+	
+	@Autowired
+	private CategoryEntityBeanFactory categoryEntityBeanFactory;
  
     @Autowired
     private ICategoryService categoryService;
     
-    @Autowired
-    private ICategoryTypeRepository categoryTypeRepository;
     
     public Category persistNewCategory() {
-    	Category 		category 		= new CategoryProduct();
-    	CategoryType 	categoryType 	= categoryTypeRepository.findById(new Long(1)).get();
-    	Category 		parentCategory 	= categoryService.findByCode(GeneralVars.LANGUAGE_ENGLISH, 
-    																 GeneralVars.CURRENCY_USD, 
-    																 CategoryVars.PRIMARY_HIERARCHY_ROOT_CODE).get();
-    	Hierarchy 		hierarchy 		= parentCategory.getHierarchy();
     	
-    	category.setCategoryCode("TST01");
-    	category.setCategoryLevel(new Long(1));
-    	category.setCategoryType(categoryType);
-    	category.setParent(parentCategory);
-    	category.setHierarchy(hierarchy);
+    	final Category category = categoryEntityBeanFactory.getCategoryEntityBean();
+    	
+    	System.out.println(category.getCategoryCode());
+    	
     	entityManager.persist(category);
-    	
-    	List<CategoryAttribute> categoryAttributes = new ArrayList<CategoryAttribute>();
-    	CategoryAttribute categoryAttribute = new CategoryAttribute();
-    	categoryAttribute.setCategory(Optional.ofNullable(category));
-    	categoryAttribute.setCategoryId(category.getCategoryId());
-    	categoryAttribute.setCategoryDesc("testCategory");
-    	categoryAttribute.setLclCd(GeneralVars.LANGUAGE_ENGLISH);
-    	categoryAttributes.add(categoryAttribute);
-    	category.setAttributes(categoryAttributes);
-    	category.setCategoryAttribute(categoryAttribute);
-    	entityManager.persist(categoryAttribute);
     	entityManager.flush();
     	
     	return category;
