b497ddf1d488f57d6039eb3de23ee6897271e0dc
dmac088 <danielmackie82@gmail.com>
2019-11-24 22:39:38 +0800

    exception handling now working

--- be/src/main/java/io/nzbee/exceptions/CategoryNotFoundResponse.java
+++ be/src/main/java/io/nzbee/exceptions/CategoryNotFoundResponse.java
@@ -5,7 +5,14 @@ public class CategoryNotFoundResponse {
 	private String categoryNotFound;
 
 	public CategoryNotFoundResponse(String categoryNotFound) {
-		super();
+		this.setCategoryNotFound(categoryNotFound);
+	}
+
+	public String getCategoryNotFound() {
+		return categoryNotFound;
+	}
+
+	public void setCategoryNotFound(String categoryNotFound) {
 		this.categoryNotFound = categoryNotFound;
 	}
 	
