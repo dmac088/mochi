8c30ddb361cf52d8e9c210ce51397c78bd21841f
Dan <daniel.mackie@dfs.com>
2019-12-27 15:04:14 +0800

    asdf

--- be/src/main/java/io/nzbee/test/unit/domain/UT_CategoryTest.java
+++ be/src/main/java/io/nzbee/test/unit/domain/UT_CategoryTest.java
@@ -25,64 +25,71 @@ import io.nzbee.variables.GeneralVars;
 public class UT_CategoryTest {
 
 	@TestConfiguration
-    static class CategoryDomainServiceImplUnitTest {
-		//the beans that we need to run this integration test
-        @Bean(value = "categoryDomainService")
-        public ICategoryService categoryDomainService() {
-            return new CategoryServiceImpl();
-        }
-        
-        @Bean(value = "categoryDtoService")
-        public io.nzbee.dto.category.ICategoryService categoryDtoService() {
-            return new io.nzbee.dto.category.CategoryServiceImpl();
-        }
-        
-        @Bean(value = "categoryEntityBeanFactory")
-        public CategoryEntityBeanFactory categoryFactoryBean() {
-            return new CategoryEntityBeanFactory();
-        }
-    }
-	
-	
+	static class CategoryDomainServiceImplUnitTest {
+		// the beans that we need to run this integration test
+		@Bean(value = "categoryDomainService")
+		public ICategoryService categoryDomainService() {
+			return new CategoryServiceImpl();
+		}
+
+		@Bean(value = "categoryDtoService")
+		public io.nzbee.dto.category.ICategoryService categoryDtoService() {
+			return new io.nzbee.dto.category.CategoryServiceImpl();
+		}
+
+		@Bean(value = "categoryDtoBeanFactory")
+		public CategoryDtoBeanFactory categoryFactoryBean() {
+			return new CategoryDtoBeanFactory();
+		}
+	}
+
 	@Autowired
-    private ICategoryService categoryDomainService;
-	
+	private ICategoryService categoryDomainService;
+
 	@Autowired
 	private CategoryDtoBeanFactory categoryDtoBeanFactory;
- 
+
 	@MockBean
-    private io.nzbee.dto.category.ICategoryService categoryDtoService;
+	private io.nzbee.dto.category.ICategoryService categoryDtoService;
+
+	@Before
+	public void setUp() {
+		// we setup a mock so that when
+		MockitoAnnotations.initMocks(this);
+
+		io.nzbee.dto.category.Category category = categoryDtoBeanFactory.getCategoryDtoBean();
+
+		// need to fill more of the properties here
+		Mockito.when(categoryDtoService.findByCode(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
+				category.getCategoryCode())).thenReturn(Optional.ofNullable(category));
+		
+		Mockito.when(categoryDtoService.findByDesc(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
+				category.getCategoryDesc())).thenReturn(Optional.ofNullable(category));
+	}
+
+	@Test
+	public void whenValidCode_thenCategoryShouldBeFound() {
+		String code = "TST02";
+		String desc = "test category";
+
+		io.nzbee.domain.category.Category found = categoryDomainService.findByCode(GeneralVars.LANGUAGE_ENGLISH,
+				GeneralVars.CURRENCY_HKD, code);
+
+		assertThat(found.getCode()).isEqualTo(code);
+		assertThat(found.getDesc()).isEqualTo(desc);
+	}
 	
-	 @Before
-	 public void setUp() {
-	    //we setup a mock so that when 
-	    MockitoAnnotations.initMocks(this);
-	        
-	    io.nzbee.dto.category.Category category = categoryDtoBeanFactory.getCategoryDtoBean();
-	        
-	    //need to fill more of the properties here
-	    Mockito.when(categoryDtoService.findByCode(GeneralVars.LANGUAGE_ENGLISH, 
-	    										   GeneralVars.CURRENCY_HKD, 
-	    										   category.getCategoryCode()))
-	           .thenReturn(Optional.ofNullable(category));
-	 }
-	 
-	 
-	    @Test
-	    public void whenValidCode_thenCategoryShouldBeFound() {
-	        String code = "TST02";
-	        String desc = "test category";
-	        
-	        io.nzbee.domain.category.Category found = categoryDomainService.findByCode(
-	        												  GeneralVars.LANGUAGE_ENGLISH, 
-	        											   	  GeneralVars.CURRENCY_HKD, 
-	        											   	  code);
-	      
-	         assertThat(found.getCode())
-	          .isEqualTo(code);
-	         assertThat(found.getDesc())
-	          .isEqualTo(desc);
-	     }
-	
-	
+	@Test
+	public void whenValidDesc_thenCategoryShouldBeFound() {
+		String code = "TST02";
+		String desc = "test category";
+
+		io.nzbee.domain.category.Category found = categoryDomainService.findByDesc(GeneralVars.LANGUAGE_ENGLISH,
+				GeneralVars.CURRENCY_HKD, desc);
+
+		assertThat(found.getCode()).isEqualTo(code);
+		assertThat(found.getDesc()).isEqualTo(desc);
+	}
+
+
 }
