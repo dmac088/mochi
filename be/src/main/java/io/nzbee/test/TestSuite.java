efbbefba3910a6d34da0a0c3d8a3ea14444eabc2
Dan <daniel.mackie@dfs.com>
2019-12-27 14:18:27 +0800

    sdf

---
+++ be/src/main/java/io/nzbee/test/TestSuite.java
@@ -0,0 +1,10 @@
+package io.nzbee.test;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("io.nzbee.test")
+public class TestSuite {
+}
