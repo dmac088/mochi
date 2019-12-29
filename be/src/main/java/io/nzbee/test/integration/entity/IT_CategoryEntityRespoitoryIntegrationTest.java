d0668d21c6b35f1a3bee4eeeaeab0b560a6156ef
dmac088 <danielmackie82@gmail.com>
2019-12-29 16:32:03 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/test/integration/entity/IT_CategoryEntityRespoitoryIntegrationTest.java
@@ -0,0 +1,62 @@
+package io.nzbee.test.integration.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.entity.category.Category;
+import io.nzbee.entity.category.ICategoryService;
+import io.nzbee.variables.GeneralVars;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles(profiles = "dev")
+public class IT_CategoryEntityRespoitoryIntegrationTest {
+
+	@TestConfiguration
+    static class CategoryEntityRepositoryIntegrationTest {
+        
+        @Bean(value = "categoryEntityService")
+        public io.nzbee.entity.category.ICategoryService categoryService() {
+            return new io.nzbee.entity.category.CategoryServiceImpl();
+        }
+        
+    }
+	
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager entityManager;
+	
+ 
+    @Autowired
+    private ICategoryService categoryService;
+    
+    @Test
+    public void whenFindAll_thenReturnAllCategories() {
+    	
+        // when
+    	List<Category> found = categoryService.findAll( GeneralVars.LANGUAGE_ENGLISH, 
+												  		GeneralVars.CURRENCY_USD);
+     
+        // then
+    	assertFound(found);
+    }
+	
+    private void assertFound(final List<Category> found) {
+    	
+    	assertThat(found).isNotNull();
+    	assertThat(found).size().isEqualTo(43);
+    }
+    
+}
