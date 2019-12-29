aaf6218f246818f13c0e97e1f7ce02c848abac51
dmac088 <danielmackie82@gmail.com>
2019-12-29 15:04:43 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/test/integration/domain/IT_BrandCategoryDoServiceImplIntegrationTest.java
@@ -0,0 +1,116 @@
+package io.nzbee.test.integration.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.dto.category.ICategoryService;
+import io.nzbee.dto.category.Category;
+import io.nzbee.dto.category.CategoryServiceImpl;
+import io.nzbee.test.entity.beans.CategoryEntityBeanFactory;
+import io.nzbee.entity.category.CategoryDaoPostgresImpl;
+import io.nzbee.entity.category.ICategoryDao;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles(profiles = "dev")
+public class IT_BrandCategoryDoServiceImplIntegrationTest {
+
+	@TestConfiguration
+    static class BrandCategoryEntityServiceImplIntegrationTestConfiguration {
+		//the beans that we need to run this integration test
+        @Bean(value = "categoryDtoService")
+        public ICategoryService categoryService() {
+            return new CategoryServiceImpl();
+        }
+        
+        @Bean(value = "categoryEntityService")
+        public io.nzbee.entity.category.ICategoryService categoryEntityService() {
+        	return new io.nzbee.entity.category.CategoryServiceImpl();
+        }
+        
+        @Bean(value = "categoryEntityPostgresDao")
+        public ICategoryDao categoryDao() {
+            return new CategoryDaoPostgresImpl();
+        }
+        
+        @Bean(value = "categoryEntityBeanFactory")
+        public CategoryEntityBeanFactory categoryFactoryBean() {
+            return new CategoryEntityBeanFactory();
+        }
+
+    }
+	
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager entityManager;
+	
+	@Autowired
+    private ICategoryService categoryService;
+	
+	@Autowired
+	private CategoryEntityBeanFactory categoryEntityBeanFactory;
+	
+	private io.nzbee.entity.category.Category category = null;
+	
+
+	public io.nzbee.entity.category.Category persistNewCategory() {
+	    	
+		category = categoryEntityBeanFactory.getBrandCategoryEntityBean();
+	    	
+	    //persist a new transient test hierarchy
+	    entityManager.persist(category.getHierarchy());
+	    	
+	    //persist a new transient test category
+	    entityManager.persist(category);
+	    entityManager.flush();
+	    	
+	    return category;
+	}
+	
+    @Before
+    public void setUp() { 
+    	this.persistNewCategory();
+    }
+ 
+    
+    @Test
+    public void whenValidCode_thenBrandCategoryShouldBeFound() {
+        String code = "TST02";
+        Optional<Category> found = categoryService.findByCode("en-GB", "HKD", code);
+      
+        assertFound(found.get());
+     }
+    
+    @Test
+    public void whenValidDesc_thenBrandCategoryShouldBeFound() {
+        Optional<Category> found = categoryService.findByDesc("en-GB", "HKD", "test brand category");
+      
+        assertFound(found.get());
+     }
+    
+    private void assertFound(final Category found) {
+
+    	assertThat(found.getCategoryCode())
+        .isEqualTo("TST02");
+    	
+	    assertThat(found.getCategoryLevel())
+	    .isEqualTo(new Long(2));
+	    
+	    assertThat(found.getCategoryDesc())
+	    .isEqualTo("test brand category");
+    }
+    
+}
