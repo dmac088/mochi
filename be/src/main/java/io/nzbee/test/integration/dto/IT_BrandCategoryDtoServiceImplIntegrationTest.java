3e45f3c516c67a3bcf40b36377c56b1f4e9ddce9
dmac088 <danielmackie82@gmail.com>
2019-12-29 14:26:48 +0800

    asdf

--- be/src/main/java/io/nzbee/test/integration/dto/IT_BrandCategoryDtoServiceImplIntegrationTest.java
+++ be/src/main/java/io/nzbee/test/integration/dto/IT_BrandCategoryDtoServiceImplIntegrationTest.java
@@ -1,36 +1,44 @@
 package io.nzbee.test.integration.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Optional;
+import javax.persistence.EntityManager;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import io.nzbee.entity.category.ICategoryService;
+import io.nzbee.dto.category.ICategoryService;
+import io.nzbee.dto.category.Category;
+import io.nzbee.dto.category.CategoryServiceImpl;
 import io.nzbee.test.entity.beans.CategoryEntityBeanFactory;
-import io.nzbee.entity.category.Category;
 import io.nzbee.entity.category.CategoryDaoPostgresImpl;
-import io.nzbee.entity.category.CategoryServiceImpl;
 import io.nzbee.entity.category.ICategoryDao;
 
 @RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(profiles = "dev")
 public class IT_BrandCategoryDtoServiceImplIntegrationTest {
 
 	@TestConfiguration
     static class BrandCategoryEntityServiceImplIntegrationTestConfiguration {
 		//the beans that we need to run this integration test
-        @Bean(value = "categoryEntityService")
+        @Bean(value = "categoryDtoService")
         public ICategoryService categoryService() {
             return new CategoryServiceImpl();
+        }
+        
+        @Bean(value = "categoryEntityService")
+        public io.nzbee.entity.category.ICategoryService categoryEntityService() {
+        	return new io.nzbee.entity.category.CategoryServiceImpl();
         }
         
         @Bean(value = "categoryEntityPostgresDao")
@@ -44,59 +52,65 @@ public class IT_BrandCategoryDtoServiceImplIntegrationTest {
         }
 
     }
-
+	
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager entityManager;
+	
 	@Autowired
     private ICategoryService categoryService;
- 
-	@MockBean
-    private ICategoryDao categoryDao;
 	
 	@Autowired
 	private CategoryEntityBeanFactory categoryEntityBeanFactory;
 	
+	private io.nzbee.entity.category.Category category = null;
+	
+
+	public io.nzbee.entity.category.Category persistNewCategory() {
+	    	
+		category = categoryEntityBeanFactory.getBrandCategoryEntityBean();
+	    	
+	    //persist a new transient test hierarchy
+	    entityManager.persist(category.getHierarchy());
+	    	
+	    //persist a new transient test category
+	    entityManager.persist(category);
+	    entityManager.flush();
+	    	
+	    return category;
+	}
+	
     @Before
-    public void setUp() {
-    	//we setup a mock so that when 
-    	MockitoAnnotations.initMocks(this);
-    	final Category testCategory = categoryEntityBeanFactory.getBrandCategoryEntityBean();
-    	
-        Mockito.when(categoryDao.findByCode("en-GB", "HKD", "TST02"))
-          .thenReturn(Optional.ofNullable(testCategory));
-        
-        Mockito.when(categoryDao.findById("en-GB", "HKD", new Long(1)))
-        .thenReturn(Optional.ofNullable(testCategory));
-        
-        Mockito.when(categoryDao.findByDesc("en-GB", "HKD", "test brand category"))
-        .thenReturn(Optional.ofNullable(testCategory));
+    public void setUp() { 
+    	this.persistNewCategory();
     }
-   
-    
-    @Test
-    public void whenValidId_thenBrnadCategoryShouldBeFound() {
-        Long Id = new Long(1);
-        String code = "TST02";
-        Optional<Category> found = categoryService.findById("en-GB", "HKD", Id);
-      
-         assertThat(found.get().getCategoryCode())
-          .isEqualTo(code);
-     }
+ 
     
     @Test
     public void whenValidCode_thenBrandCategoryShouldBeFound() {
         String code = "TST02";
         Optional<Category> found = categoryService.findByCode("en-GB", "HKD", code);
       
-         assertThat(found.get().getCategoryCode())
-          .isEqualTo(code);
+        assertFound(found.get());
      }
     
     @Test
     public void whenValidDesc_thenBrandCategoryShouldBeFound() {
-    	String code = "TST02";
         Optional<Category> found = categoryService.findByDesc("en-GB", "HKD", "test brand category");
       
-         assertThat(found.get().getCategoryCode())
-          .isEqualTo(code);
+        assertFound(found.get());
      }
+    
+    private void assertFound(final Category found) {
+
+    	assertThat(found.getCategoryCode())
+        .isEqualTo("TST02");
+    	
+	    assertThat(found.getCategoryLevel())
+	    .isEqualTo(new Long(2));
+	    
+	    assertThat(found.getCategoryDesc())
+	    .isEqualTo("test brand category");
+    }
     
 }
