589821b55d45056cf2e9ad422638c99426817196
Dan <daniel.mackie@dfs.com>
2019-12-30 15:15:03 +0800

    asdf

--- be/src/main/java/io/nzbee/test/integration/controller/IT_CategoryControllerIntegrationTest.java
+++ be/src/main/java/io/nzbee/test/integration/controller/IT_CategoryControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package io.nzbee.test.integration.controller;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import io.nzbee.domain.category.CategoryServiceImpl;
 import io.nzbee.resources.controllers.CategoryController;
 import io.nzbee.security.WebSecurityConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@AutoConfigureMockMvc()
+@AutoConfigureMockMvc(addFilters = false)
 @ContextConfiguration(classes = {CategoryController.class, 
 							     CategoryServiceImpl.class,
 							     io.nzbee.dto.category.CategoryServiceImpl.class,
@@ -43,15 +46,14 @@ public class IT_CategoryControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
     
-    
     @Test
     public void testFindAll() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/Category/en-GB/HKD")
                 //.with(user(TEST_USER_ID))
                //.with(csrf())
                 //.content(birthday)
-               // .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.ALL))
                 .andExpect(status().isOk())
                 .andReturn();
         
