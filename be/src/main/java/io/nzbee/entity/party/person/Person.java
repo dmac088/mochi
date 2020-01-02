720a270990458aba2a4c4fa1c95c7ec1a797010a
Dan <daniel.mackie@dfs.com>
2020-01-02 16:57:33 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/person/Person.java
+++ be/src/main/java/io/nzbee/entity/party/person/Person.java
@@ -6,29 +6,19 @@ import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import io.nzbee.entity.party.Party;
-import io.nzbee.entity.party.PartyType;
 
 @Entity
 @Table(name = "person", schema = "mochi")
 @PrimaryKeyJoinColumn(name = "psn_id")
 @DiscriminatorValue("1")
 public class Person extends Party {
-
-	@Transient
-	private Long partyTypeId = (long) 1;
 	
 	@Column(name="psn_gvn_nm")
 	private String GivenName;
 	
 	@Column(name="psn_fml_nm")
 	private String FamilyName;
-	
-	public Person() {
-		super();
-		this.setPartyType(new PartyType(partyTypeId));
-	}
 	
 	public String getGivenName() {
 		return GivenName; 
