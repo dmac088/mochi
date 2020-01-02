94c8fa55f22f24ec5ed6964e3f49f68620542256
Dan <daniel.mackie@dfs.com>
2020-01-02 15:25:28 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/PartyDaoImpl.java
+++ be/src/main/java/io/nzbee/entity/party/PartyDaoImpl.java
@@ -1,10 +1,56 @@
 package io.nzbee.entity.party;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import io.nzbee.entity.role.Role;
+import io.nzbee.entity.role.RoleType;
+import io.nzbee.entity.role.RoleType_;
+import io.nzbee.entity.role.Role_;
 
+
+@Component(value="partyDao")
 public class PartyDaoImpl implements IPartyDao {
 
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager em;
+	
+	@Override
+	public List<Party> findAllByRoleName(Class<Role> roleClassType) {
+		// TODO Auto-generated method stub
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		
+		CriteriaQuery<Party> cq = cb.createQuery(Party.class);
+		
+		Root<Party> root = cq.from(Party.class);
+		
+		Join<Party, Role> partyRole = root.join(Party_.partyRoles);
+		Join<Role, RoleType> roleType = partyRole.join(Role_.roleType);
+		
+		List<Predicate> conditions = new ArrayList<Predicate>();
+		
+		conditions.add(cb.equal(roleType.get(RoleType_.roleTypeDesc), roleClassType.getSimpleName()));
+		
+		TypedQuery<Party> query = em.createQuery(cq
+				.select(root)
+				.where(conditions.toArray(new Predicate[] {}))
+				.distinct(true)
+		);
+		
+		return query.getResultList();
+	}
+	
 	@Override
 	public Optional<Party> findById(long id) {
 		// TODO Auto-generated method stub
@@ -46,23 +92,4 @@ public class PartyDaoImpl implements IPartyDao {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public List<Party> findAll(String locale, String currency) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Party> findByRoleName(String RoleName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Party> findByRoleCode(String RoleCode) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- be/src/main/java/io/nzbee/test/unit/entity/UT_CustomerTest.java
+++ be/src/main/java/io/nzbee/test/unit/entity/UT_CustomerTest.java
@@ -1,34 +1,43 @@
 package io.nzbee.test.unit.entity;
 
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import io.nzbee.entity.party.IPartyDao;
+import io.nzbee.entity.party.IPartyService;
+import io.nzbee.entity.party.PartyDaoImpl;
+import io.nzbee.entity.party.PartyServiceImpl;
 
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles(profiles = "dev")
 public class UT_CustomerTest {
 
-	/*
+	
 	@TestConfiguration
     static class BrandCategoryServiceImplIntegrationTestConfiguration {
 		//the beans that we need to run this integration test
-        @Bean(value = "customerEntityService")
-        public IPartyService categoryService() {
+        @Bean(value = "partyyService")
+        public IPartyService partyService() {
             return new PartyServiceImpl();
         }
         
-        @Bean(value = "customerEntityPostgresDao")
+        @Bean(value = "partyDao")
         public IPartyDao partyDao() {
-            return new PartyDaoPostgresImpl();
+            return new PartyDaoImpl();
         }
         
-        @Bean(value = "customerEntityBeanFactory")
-        public PartyEntityBeanFactory partyFactoryBean() {
-            return new PartyEntityBeanFactory();
-        }
+//        @Bean(value = "customerEntityBeanFactory")
+//        public PartyEntityBeanFactory partyFactoryBean() {
+//            return new PartyEntityBeanFactory();
+//        }
 
     }
-    */
+    
+	
+	
 	
 }
