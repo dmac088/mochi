d2cfd0266deaf1184cbd98c61fcfe3a6adc72bb8
Dan <daniel.mackie@dfs.com>
2020-01-02 18:47:47 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/IPartyDao.java
+++ be/src/main/java/io/nzbee/entity/party/IPartyDao.java
@@ -3,10 +3,9 @@ package io.nzbee.entity.party;
 import java.util.List;
 
 import io.nzbee.entity.IDao;
-import io.nzbee.entity.role.Role;
 
 public interface IPartyDao extends IDao<Party> {
 	
-	List<Party> findAllByRoleName(Class<Role> roleClassType);
+	List<Party> findAllByRoleName(Class<?> roleClassType);
 
 }
