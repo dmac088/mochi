0ed5e09847e06010fc4ed8d06e3d3ac91b9a69f1
Dan <daniel.mackie@dfs.com>
2020-01-03 16:01:45 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/test/entity/beans/ProductEntityBeanFactory.java
@@ -0,0 +1,5 @@
+package io.nzbee.test.entity.beans;
+
+public class ProductEntityBeanFactory {
+
+}
---
+++ be/src/main/java/io/nzbee/test/integration/entity/IT_ProductEntityRepositoryIntegrationTest.java
@@ -0,0 +1,33 @@
+package io.nzbee.test.integration.entity;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.test.entity.beans.ProductEntityBeanFactory;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles(profiles = "dev")
+public class IT_ProductEntityRepositoryIntegrationTest {
+
+	@TestConfiguration
+    static class ProductEntityRepositoryIntegrationTest {
+        
+        @Bean(value = "productEntityService")
+        public io.nzbee.entity.product.IProductService productService() {
+            return new io.nzbee.entity.product.ProductServiceImpl();
+        }
+        
+        @Bean(value = "productEntityBeanFactory")
+        public ProductEntityBeanFactory productFactoryBean() {
+            return new ProductEntityBeanFactory();
+        }
+    }
+	
+}
