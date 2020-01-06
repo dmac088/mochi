a8fae3db257af244a282b81fa6f90e3f17a818b1
Dan <daniel.mackie@dfs.com>
2020-01-06 10:45:47 +0800

    asdf

--- be/src/main/java/io/nzbee/test/SuiteIntegrationTest.java
+++ be/src/main/java/io/nzbee/test/SuiteIntegrationTest.java
@@ -5,6 +5,6 @@ import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitPlatform.class)
-@SelectPackages("io.nzbee.test")
-public class TestSuite {
+@SelectPackages("io.nzbee.test.integration")
+public class SuiteIntegrationTest {
 }
