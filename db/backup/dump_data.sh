9694ce978395d7f76b01963edd1865743867b883
dmac088 <danielmackie82@gmail.com>
2020-01-08 23:42:48 +0800

    asdf

--- db/backup/dump_data.sh
+++ db/backup/dump_data.sh
@@ -1,2 +1,2 @@
 set PGPASSWORD=password
-pg_dump -U mochidb_owner --data-only --schema "mochi" mochidb > ../../be/src/main/resources/database/mochi_schema.sql
+pg_dump -U mochidb_owner --data-only --inserts  --schema "mochi" mochidb > ../../be/src/main/resources/database/mochi_data.sql
