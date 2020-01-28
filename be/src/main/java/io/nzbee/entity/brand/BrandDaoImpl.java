3a6fc5b85cb316fa6b03a2bab1ee0c0e5c4b5228
dmac088 <danielmackie82@gmail.com>
2020-01-28 22:53:11 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/brand/BrandDaoImpl.java
+++ be/src/main/java/io/nzbee/entity/brand/BrandDaoImpl.java
@@ -173,7 +173,7 @@ public class BrandDaoImpl  implements IBrandDao {
 		Join<Brand, BrandAttribute> attribute = root.join(Brand_.attributes);
 		
 		List<Predicate> conditions = new ArrayList<Predicate>();
-		conditions.add(cb.equal(status.get(ProductStatus_.code), ProductVars.ACTIVE_SKU_CODE));
+		conditions.add(cb.equal(status.get(ProductStatus_.productStatusCode), ProductVars.ACTIVE_SKU_CODE));
 		conditions.add(cb.equal(attribute.get(BrandAttribute_.lclCd), locale));
 		conditions.add(root.in(Brand_.brandCode).in(brandCodes));
 
