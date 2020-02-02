9bf631aa995494ad01c482e11c188a1ec53bf369
dmac088 <danielmackie82@gmail.com>
2020-02-02 11:19:03 +0800

    falling back to using jwt tokens

--- be/src/main/java/io/nzbee/security/OAuth2AuthorizationServerConfig.java
+++ be/src/main/java/io/nzbee/security/OAuth2AuthorizationServerConfig.java
@@ -1,51 +1,88 @@
 package io.nzbee.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@Import(AuthorizationServerSecurityConfigurer.class)
 public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-	
+
+    @Autowired
+    @Qualifier("securityDataSource")
+    private DataSource dataSource;
+    
     @Autowired
     private AuthenticationManager authenticationManager;
+    
+    @Autowired
+    private UserDetailsService userDetailsService;
+    
+    @Autowired
+    private PasswordEncoder oauthClientPasswordEncoder;
+    
+    
+	@Bean
+    public JdbcTokenStore tokenStore() {
+    	return new JdbcTokenStore(dataSource);
+    }    
 	
+    @Bean
+    public OAuth2AccessDeniedHandler oauthAccessDeniedHandler() {
+        return new OAuth2AccessDeniedHandler();
+    }
+    
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) 
-      throws Exception {
-        endpoints.tokenStore(tokenStore())
-                 .accessTokenConverter(accessTokenConverter())
-                 .authenticationManager(authenticationManager);
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()").passwordEncoder(oauthClientPasswordEncoder);
     }
- 
+    
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+    
     @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
+    public TokenEnhancer tokenEnhancer() {
+       return new CustomTokenEnhancer();
     }
- 
+    
     @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("123");
-        return converter;
+    public DefaultAccessTokenConverter accessTokenConverter() {
+        return new DefaultAccessTokenConverter();
     }
- 
-    @Bean
-    @Primary
-    public DefaultTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        defaultTokenServices.setSupportRefreshToken(true);
-        return defaultTokenServices;
+    
+   
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+
+        endpoints
+        	.tokenStore(tokenStore())
+        	.reuseRefreshTokens(false)
+        	.tokenEnhancer(tokenEnhancer())
+        	.accessTokenConverter(accessTokenConverter())
+        	.authenticationManager(authenticationManager)
+        	.userDetailsService(userDetailsService);
+        	
     }
+   
 }
--- be/src/main/java/io/nzbee/security/OAuth2ResourceServerConfig.java
+++ be/src/main/java/io/nzbee/security/OAuth2ResourceServerConfig.java
@@ -1,52 +1,25 @@
 package io.nzbee.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
-	
-    //private static final String RESOURCE_ID = "resource-server-rest-api";
+    private static final String RESOURCE_ID = "resource-server-rest-api";
     private static final String SECURED_READ_SCOPE = "#oauth2.hasScope('read')";
     private static final String SECURED_WRITE_SCOPE = "#oauth2.hasScope('write')";
     private static final String SECURED_PATTERN = "/api/**";
-	
+    
     @Override
-    public void configure(ResourceServerSecurityConfigurer config) {
-        config.tokenServices(tokenServices());
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(RESOURCE_ID);
     }
- 
-    @Bean
-    public TokenStore tokenStore() {
-        return new JwtTokenStore(accessTokenConverter());
-    }
- 
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey("123");
-        return converter;
-    }
- 
-    @Bean
-    @Primary
-    public ResourceServerTokenServices tokenServices() {
-        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
-        defaultTokenServices.setTokenStore(tokenStore());
-        return defaultTokenServices;
-    }
+    
     
     @Override
     public void configure(HttpSecurity http) throws Exception {
