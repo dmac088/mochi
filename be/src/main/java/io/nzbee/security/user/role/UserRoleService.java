81ee199f120f7bbbc5804f52b31d340b37afb516
dmac088 <danielmackie82@gmail.com>
2020-02-18 00:44:18 +0800

    asdf

--- be/src/main/java/io/nzbee/security/user/role/UserRoleService.java
+++ be/src/main/java/io/nzbee/security/user/role/UserRoleService.java
@@ -4,7 +4,7 @@ package io.nzbee.security.user.role;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service(value = "userRoleService")
 public class UserRoleService implements IUserRoleService  {
 
     @Autowired
