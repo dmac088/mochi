902641ab4b3b8c738c07901b53de0e14ec4b70c2
dmac088 <danielmackie82@gmail.com>
2020-03-01 14:22:39 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/category/CategoryResource.java
+++ be/src/main/java/io/nzbee/resources/category/CategoryResource.java
@@ -2,6 +2,18 @@ package io.nzbee.resources.category;
 
 import org.springframework.hateoas.ResourceSupport;
 
+import io.nzbee.domain.category.Category;
+
 public class CategoryResource  extends ResourceSupport  {
+	
+	private final Category data;
+	
+	public CategoryResource(Category category) {
+		this.data = category;
+	}
+
+	public Category getData() {
+		return data;
+	}
 	
 }
