9cdae8e9c96480df42aad8caad6a7a7228a37c17
dmac088 <danielmackie82@gmail.com>
2020-03-18 21:52:27 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/beans/ProductEntityBeanFactory.java
+++ be/src/test/java/io/nzbee/test/integration/beans/ProductEntityBeanFactory.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import io.nzbee.entity.product.Product;
+import io.nzbee.entity.product.attribute.ProductAttribute;
 
 @Service(value = "productEntityBeanFactory")
 @Profile(value = "dev")
@@ -19,6 +20,24 @@ public class ProductEntityBeanFactory {
 		product.setUPC("123456789");
 		
 		return product;
+	}
+	
+	@Bean
+	public final ProductAttribute getProductAttributeEntityBean() {
+		
+		Product p = new Product();
+		ProductAttribute paEng = new ProductAttribute();
+		paEng.setProductDesc("test product");
+		paEng.setLclCd("en-GB");
+		paEng.setProduct(p);
+		
+		p.setProductCreateDt(new Date());
+		p.setUPC("123456789");
+		
+		p.addProductAttribute(paEng);
+		p.getAttributes().add(paEng);
+		
+		return paEng;
 	}
 	
 }
