b6492e39002eb97d87e47b1ee5f69405e018c6d7
dmac088 <danielmackie82@gmail.com>
2020-03-29 14:39:30 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/product/jewellery/IJewelleryService.java
@@ -0,0 +1,5 @@
+package io.nzbee.entity.product.jewellery;
+
+public interface IJewelleryService {
+
+}
---
+++ be/src/main/java/io/nzbee/entity/product/jewellery/JewelleryDaoPostgresImpl.java
@@ -0,0 +1,5 @@
+package io.nzbee.entity.product.jewellery;
+
+public class JewelleryDaoPostgresImpl {
+
+}
