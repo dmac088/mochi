49aefbc6aace75cedeae485052cdf209c068dc07
dmac088 <danielmackie82@gmail.com>
2020-04-02 12:21:56 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/price/IProductPriceTypeService.java
+++ be/src/main/java/io/nzbee/entity/product/price/IProductPriceTypeService.java
@@ -3,11 +3,11 @@ package io.nzbee.entity.product.price;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductPriceTypeRepository extends CrudRepository<ProductPriceType, Long> {
+public interface IProductPriceTypeService {
 
 	List<ProductPriceType> findAll();
+
+	Optional<ProductPriceType> findById(Long Id);
 	
 	Optional<ProductPriceType> findByCode(String code);
 
