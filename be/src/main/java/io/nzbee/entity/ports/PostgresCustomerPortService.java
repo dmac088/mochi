52923fa0b4ce6425d95a1fed634346a8c6fdba5d
dmac088 <danielmackie82@gmail.com>
2020-04-06 11:34:06 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/ports/PostgresCustomerPortService.java
+++ be/src/main/java/io/nzbee/entity/ports/PostgresCustomerPortService.java
@@ -3,52 +3,52 @@ package io.nzbee.entity.ports;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import io.nzbee.domain.adapters.ICustomerPortService;
+import io.nzbee.domain.customer.Customer;
 
-import io.nzbee.domain.adapters.IBrandPortService;
-import io.nzbee.domain.brand.Brand;
-
-public class PostgresCustomerPortService implements IBrandPortService {
+public class PostgresCustomerPortService implements ICustomerPortService {
 
 	@Override
-	public Optional<Brand> findByCode(String code) {
+	public Optional<Customer> findByCode(String code) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void save(Brand tag) {
+	public Set<Customer> findAll(String locale, String currency) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findByCode(String locale, String currency, String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer findByDesc(String locale, String currency, String desc) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Customer> findAll(String locale, String currency, List<String> codes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(Customer domainObject) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Set<Brand> findAll(String locale, String currency) {
+	public Set<Customer> findAll(String locale, String currency, String category) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Brand findByCode(String locale, String currency, String code) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Brand findByDesc(String locale, String currency, String desc) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Brand> findAll(String locale, String currency, List<String> codes) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Set<Brand> findAll(String locale, String currency, String category) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
