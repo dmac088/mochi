525f72a539290add1a965867d968022ce40b2f06
dmac088 <danielmackie82@gmail.com>
2020-04-11 22:09:35 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/product/Jewellery.java
+++ be/src/main/java/io/nzbee/domain/product/Jewellery.java
@@ -1,5 +1,35 @@
 package io.nzbee.domain.product;
 
+import java.util.Date;
+
+import io.nzbee.domain.brand.Brand;
+import io.nzbee.domain.department.Department;
+
 public class Jewellery extends Product {
+
+	public Jewellery(String productUPC,
+				   	 Date   productCreateDt,
+				   	 String productDesc,
+				   	 Double productRetail,
+				   	 Double productMarkdown,
+				   	 String productImage,
+				   	 String displayCategories,
+				   	 String lclCd,
+				   	 String currency,
+				   	 Brand brand,
+				   	 Department department) {
+	
+	super(productUPC,
+		  productCreateDt,
+		  productDesc,
+		  productRetail,
+		  productMarkdown,
+		  productImage,
+		  displayCategories,
+		  lclCd,
+		  currency,
+		  brand,
+		  department);
+	}
 
 }
