0d671d3f08fe65f981911e57f35ed0f992199ba9
dmac088 <danielmackie82@gmail.com>
2020-04-11 10:53:16 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/beans/ProductDoBeanFactory.java
+++ be/src/test/java/io/nzbee/test/integration/beans/ProductDoBeanFactory.java
@@ -3,22 +3,36 @@ package io.nzbee.test.integration.beans;
 import java.util.Date;
 
 import org.springframework.stereotype.Service;
+
+import io.nzbee.domain.brand.Brand;
+import io.nzbee.domain.department.Department;
+import io.nzbee.domain.product.Food;
 import io.nzbee.domain.product.Product;
 
 @Service
 public class ProductDoBeanFactory {
 	
-	
 	public Product getProductDoBean() {
-		return new Product(		   "3254354673",
+		return new Food(		   "3254354673",
 								   new Date(),
 								   "Test Product Description",
 								   new Double(78),
 								   new Double(71),
 								   "test_image.jpg",
+								   "NZL",
+								   new Date(),
 								   "test, test",
 								   "en-GB",
-								   "HKD");
+								   "HKD",
+								   new Brand("",
+											 "",
+											 20,
+											 "en-GB",
+											 "HKD"),
+								   new Department("",
+												  "",
+												  "en-GB",
+												  "HKD"));
 	}
 		
 	
