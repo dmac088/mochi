0260b4b131b150cfa2a8ca354f9fe8126320265f
dmac088 <danielmackie82@gmail.com>
2020-04-15 18:16:48 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/product/CategoryProductDao.java
+++ be/src/main/java/io/nzbee/entity/category/product/CategoryProductDao.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
+import javax.persistence.Tuple;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -17,6 +19,8 @@ import org.springframework.stereotype.Service;
 import io.nzbee.entity.category.Category_;
 import io.nzbee.entity.category.attribute.CategoryAttribute;
 import io.nzbee.entity.category.attribute.CategoryAttribute_;
+import io.nzbee.entity.category.type.CategoryType;
+import io.nzbee.entity.category.type.CategoryType_;
 import io.nzbee.entity.product.Product;
 import io.nzbee.entity.product.Product_;
 
@@ -31,28 +35,58 @@ public class CategoryProductDao implements ICategoryProductDao {
 	public List<CategoryProduct> findAllByProductCode(String locale, String currency, String productCode) {
 		CriteriaBuilder cb = em.getCriteriaBuilder();
 		
-		CriteriaQuery<CategoryProduct> cq = cb.createQuery(CategoryProduct.class);
+		CriteriaQuery<Tuple> cq = cb.createQuery(Tuple.class);
 		
 		Root<CategoryProduct> root = cq.from(CategoryProduct.class);
 		
-		Join<CategoryProduct, CategoryAttribute> categoryAttribute = root.join(Category_.attributes);
+		Join<CategoryProduct, CategoryAttribute> attribute = root.join(Category_.attributes);
 		Join<CategoryProduct, Product> products = root.join(CategoryProduct_.products);
+		Join<CategoryProduct, CategoryType> type = root.join(Category_.categoryType);
+		
+		cq.multiselect(	root.get(CategoryProduct_.categoryId).alias("categoryId"),
+						root.get(CategoryProduct_.categoryCode).alias("categoryCode"),
+						attribute.get(CategoryAttribute_.categoryAttributeId).alias("categoryAttributeId"),
+						attribute.get(CategoryAttribute_.categoryDesc).alias("categoryDesc"),
+						type.get(CategoryType_.categoryTypeCode).alias("categoryTypeCode"),
+						type.get(CategoryType_.categoryTypeDesc).alias("categoryTypeDesc")
+		);
 		
 		List<Predicate> conditions = new ArrayList<Predicate>();
 	
 		if(!(productCode == null)) {
+			
+			cq.where(cb.and(
+					cb.equal(products.get(Product_.productUPC), productCode),
+					cb.equal(attribute.get(CategoryAttribute_.lclCd), locale)
+					)
+			);
+			
 			conditions.add(cb.equal(products.get(Product_.productUPC), productCode));
 		}
 		
-		conditions.add(cb.equal(categoryAttribute.get(CategoryAttribute_.lclCd), locale));
+		TypedQuery<Tuple> query = em.createQuery(cq);
+		List<Tuple> tuples = query.getResultList();
 		
-		TypedQuery<CategoryProduct> query = em.createQuery(cq
-				.select(root)
-				.where(conditions.toArray(new Predicate[] {}))
-				.distinct(true)
-		);
-		
-		return query.getResultList();
+		return tuples.stream().map(t -> {
+
+			CategoryAttribute ca = new CategoryAttribute();
+			ca.setCategoryDesc(t.get("categoryDesc").toString());
+			ca.setLclCd(locale);
+			
+			CategoryType ct = new CategoryType();
+			ct.setCategoryTypeCode(t.get("categoryTypeCode").toString());
+			ct.setCategoryTypeDesc(t.get("categoryTypeDesc").toString());
+			
+			CategoryProduct cp = new CategoryProduct();
+			cp.setCategoryId(Long.parseLong(t.get("categoryId").toString()));
+			cp.setCategoryCode(t.get("categoryCode").toString());
+			cp.setLocale(locale);
+			cp.setCurrency(currency);
+			cp.setCategoryAttribute(ca);
+			cp.setCategoryType(ct);
+			
+			return cp; 
+		}).collect(Collectors.toList());
 	}
 	
 	@Override
