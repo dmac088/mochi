a60eb35fcbbe97467ddf6437143f5a78a764a63e
dmac088 <danielmackie82@gmail.com>
2020-04-15 15:44:21 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntityRepositoryIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntityRepositoryIntegrationTest.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
@@ -96,7 +95,6 @@ public class IT_ProductEntityRepositoryIntegrationTest {
 	
 	
 	@Test
-	@Rollback(false)
 	public void whenFindByCode_thenReturnProduct() {
 		 // when
     	Product found = productService.findByCode(GeneralVars.LANGUAGE_ENGLISH, 
