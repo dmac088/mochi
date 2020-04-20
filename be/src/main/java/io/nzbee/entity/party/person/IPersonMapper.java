f3203bcfbae5daab04b5794022ea929589d62040
dmac088 <danielmackie82@gmail.com>
2020-04-20 15:35:42 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/person/IPersonMapper.java
+++ be/src/main/java/io/nzbee/entity/party/person/IPersonMapper.java
@@ -1,7 +1,10 @@
 package io.nzbee.entity.party.person;
 
+import io.nzbee.domain.customer.Customer;
 import io.nzbee.entity.IMapper;
 
-public interface IPersonMapper extends IMapper<Person, io.nzbee.entity.party.person.Person> {
+public interface IPersonMapper extends IMapper<Customer, io.nzbee.entity.party.person.Person> {
 
+	Customer entityToDo(io.nzbee.entity.party.person.Person e);
+	
 }
