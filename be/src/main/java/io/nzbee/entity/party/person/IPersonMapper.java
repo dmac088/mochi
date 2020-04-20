3332edf580ae750cb24e4fe5b7c6e0c157acd556
dmac088 <danielmackie82@gmail.com>
2020-04-20 15:29:48 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/party/person/IPersonMapper.java
@@ -0,0 +1,7 @@
+package io.nzbee.entity.party.person;
+
+import io.nzbee.entity.IMapper;
+
+public interface IPersonMapper extends IMapper<Person, io.nzbee.entity.party.person.Person> {
+
+}
---
+++ be/src/main/java/io/nzbee/entity/party/person/PersonMapper.java
@@ -0,0 +1,14 @@
+package io.nzbee.entity.party.person;
+
+
+import io.nzbee.domain.customer.Customer;
+import org.springframework.stereotype.Component;
+
+@Component(value="personMapper")
+public class PersonMapper implements IPersonMapper {
+
+	public Customer entityToDo(io.nzbee.entity.category.Category e) {
+			return null;
+	}
+
+}
