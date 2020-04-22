5b35c74920f794458f3f61dcd0fef9706d28593a
dmac088 <danielmackie82@gmail.com>
2020-04-22 15:15:27 +0800

    sadf

--- be/src/main/java/io/nzbee/entity/category/CategoryDaoPostgresImpl.java
+++ be/src/main/java/io/nzbee/entity/category/CategoryDaoPostgresImpl.java
@@ -41,7 +41,6 @@ public class CategoryDaoPostgresImpl implements ICategoryDao {
 	@Override
 	public <T> List<Category> findByCodeAndType(String locale, String currency, Class<T> cls) {
 		
-		
 		Session session = em.unwrap(Session.class);
 		
 		Query query = session.createNativeQuery(constructSQL(false, 
