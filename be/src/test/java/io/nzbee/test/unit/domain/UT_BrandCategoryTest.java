c74d5d1cbf194a3159f6f13b5be0d3afc33c77cd
dmac088 <danielmackie82@gmail.com>
2020-04-23 18:40:42 +0800

    asdf

--- be/src/test/java/io/nzbee/test/unit/domain/UT_BrandCategoryTest.java
+++ be/src/test/java/io/nzbee/test/unit/domain/UT_BrandCategoryTest.java
@@ -1,97 +1,97 @@
-package io.nzbee.test.unit.domain;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-import io.nzbee.domain.category.Category;
-import io.nzbee.domain.category.CategoryServiceImpl;
-import io.nzbee.domain.category.ICategoryService;
-import io.nzbee.test.integration.beans.CategoryDoBeanFactory;
-import io.nzbee.variables.GeneralVars;
-
-@RunWith(SpringRunner.class)
-@ActiveProfiles(profiles = "dev")
-public class UT_BrandCategoryTest {
-
-	@TestConfiguration
-	static class BrandCategoryDomainServiceImplUnitTest {
-		// the beans that we need to run this integration test
-		@Bean
-		public ICategoryService categoryDomainService() {
-			return new CategoryServiceImpl();
-		}
-		
-		@Bean
-		public CategoryDoBeanFactory categoryDoBeanFactory() {
-			return new CategoryDoBeanFactory();
-		}
-		
-	}
-
-	@MockBean
-	private ICategoryService categoryDoService;
-
-	@Autowired
-	private CategoryDoBeanFactory categoryDoBeanFactory;
-	
-	@Before
-	public void setUp() {
-		// we setup a mock so that when
-		MockitoAnnotations.initMocks(this);
-
-		Category category = categoryDoBeanFactory.getBrandCategoryDoBean();
-
-		// need to fill more of the properties here
-		Mockito.when(categoryDoService.findByCode(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
-				category.getCategoryCode())).thenReturn(category);
-		
-		Mockito.when(categoryDoService.findByDesc(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
-				category.getCategoryDesc())).thenReturn(category);
-	}
-
-	@Test
-	public void whenValidCode_thenBrandCategoryShouldBeFound() {
-		String code = "TST02";
-
-		Category found = categoryDoService.findByCode(GeneralVars.LANGUAGE_ENGLISH,
-													  GeneralVars.CURRENCY_HKD, 
-													  code);
-
-		assertFound(found);
-	}
-	
-	@Test
-	public void whenValidDesc_thenBrandCategoryShouldBeFound() {
-		String desc = "test brand category";
-
-		io.nzbee.domain.category.Category found = categoryDoService.findByDesc(	GeneralVars.LANGUAGE_ENGLISH,
-																					GeneralVars.CURRENCY_HKD, 
-																					desc);
-
-		assertFound(found);
-	}
-	
-    private void assertFound(final io.nzbee.domain.category.Category found) {
-
-    	assertThat(found.getCode())
-        .isEqualTo("TST02");
-    	
-	    assertThat(found.getLevel())
-	    .isEqualTo(new Long(0));
-	    
-	    assertThat(found.getDesc())
-	    .isEqualTo("test brand category");
-    }
-
-
-}
+//package io.nzbee.test.unit.domain;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mockito;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.TestConfiguration;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import io.nzbee.domain.category.Category;
+//import io.nzbee.domain.category.CategoryServiceImpl;
+//import io.nzbee.domain.category.ICategoryService;
+//import io.nzbee.test.integration.beans.CategoryDoBeanFactory;
+//import io.nzbee.variables.GeneralVars;
+//
+//@RunWith(SpringRunner.class)
+//@ActiveProfiles(profiles = "dev")
+//public class UT_BrandCategoryTest {
+//
+//	@TestConfiguration
+//	static class BrandCategoryDomainServiceImplUnitTest {
+//		// the beans that we need to run this integration test
+//		@Bean
+//		public ICategoryService categoryDomainService() {
+//			return new CategoryServiceImpl();
+//		}
+//		
+//		@Bean
+//		public CategoryDoBeanFactory categoryDoBeanFactory() {
+//			return new CategoryDoBeanFactory();
+//		}
+//		
+//	}
+//
+//	@MockBean
+//	private ICategoryService categoryDoService;
+//
+//	@Autowired
+//	private CategoryDoBeanFactory categoryDoBeanFactory;
+//	
+//	@Before
+//	public void setUp() {
+//		// we setup a mock so that when
+//		MockitoAnnotations.initMocks(this);
+//
+//		Category category = categoryDoBeanFactory.getBrandCategoryDoBean();
+//
+//		// need to fill more of the properties here
+//		Mockito.when(categoryDoService.findByCode(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
+//				category.getCategoryCode())).thenReturn(category);
+//		
+//		Mockito.when(categoryDoService.findByDesc(GeneralVars.LANGUAGE_ENGLISH, GeneralVars.CURRENCY_HKD,
+//				category.getCategoryDesc())).thenReturn(category);
+//	}
+//
+//	@Test
+//	public void whenValidCode_thenBrandCategoryShouldBeFound() {
+//		String code = "TST02";
+//
+//		Category found = categoryDoService.findByCode(GeneralVars.LANGUAGE_ENGLISH,
+//													  GeneralVars.CURRENCY_HKD, 
+//													  code);
+//
+//		assertFound(found);
+//	}
+//	
+//	@Test
+//	public void whenValidDesc_thenBrandCategoryShouldBeFound() {
+//		String desc = "test brand category";
+//
+//		io.nzbee.domain.category.Category found = categoryDoService.findByDesc(	GeneralVars.LANGUAGE_ENGLISH,
+//																					GeneralVars.CURRENCY_HKD, 
+//																					desc);
+//
+//		assertFound(found);
+//	}
+//	
+//    private void assertFound(final io.nzbee.domain.category.Category found) {
+//
+//    	assertThat(found.getCode())
+//        .isEqualTo("TST02");
+//    	
+//	    assertThat(found.getLevel())
+//	    .isEqualTo(new Long(0));
+//	    
+//	    assertThat(found.getDesc())
+//	    .isEqualTo("test brand category");
+//    }
+//
+//
+//}
