00c5d8c1301437463d44d6d3348db9aadaae1b32
dmac088 <danielmackie82@gmail.com>
2020-04-23 15:52:05 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/domain/IT_CustomerDoServiceImplIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/domain/IT_CustomerDoServiceImplIntegrationTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import io.nzbee.domain.customer.Customer;
