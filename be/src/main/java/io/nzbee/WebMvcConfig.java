40fdfc1cf8cd2af0443b2433f951c7a66e7d5918
dmac088 <danielmackie82@gmail.com>
2020-04-25 15:40:05 +0800

    asdf

--- be/src/main/java/io/nzbee/WebMvcConfig.java
+++ be/src/main/java/io/nzbee/WebMvcConfig.java
@@ -1,6 +1,5 @@
 package io.nzbee;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
