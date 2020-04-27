dd8df9ecb1fa456410cc825b713e6cac4987736f
dmac088 <danielmackie82@gmail.com>
2020-04-27 12:08:39 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/controllers/ProductController.java
+++ be/src/main/java/io/nzbee/resources/controllers/ProductController.java
@@ -3,13 +3,11 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedResources;
@@ -25,9 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 import io.nzbee.domain.ports.IProductPortService;
-import io.nzbee.domain.product.IProductService;
 import io.nzbee.domain.product.Product;
 import io.nzbee.dto.facet.IFacet;
 import io.nzbee.resources.product.ProductResource;
@@ -44,37 +40,38 @@ public class ProductController {
     @Autowired
     private ResourceAssembler<Product, ProductResource> prodAssembler;
     
-//    @SuppressWarnings("unchecked")
-//	@GetMapping(value = "/Product/{locale}/{currency}/category/{categoryCode}", 
-//    			params = { "page", "size" })
-//    public ResponseEntity<PagedResources<ProductResource>> getProducts(@PathVariable String locale, 
-//															    	   @PathVariable String currency, 
-//															    	   @PathVariable String categoryCode,
-//															    	   @RequestParam("page") int page,
-//															    	   @RequestParam("size") int size,
-//															    	   @SuppressWarnings("rawtypes") PagedResourcesAssembler assembler) {
-//    	
-//    	LOGGER.debug("Fetching products for parameters : {}, {}, {}, {}, {}", locale, currency, categoryCode, page, size);
-//    	
-//    	final Page<Product> pages =
-//    					productService.findAll(	
-//    									locale, 
-//    									currency,
-//    									PageRequest.of(page, size), 
-//    									categoryCode, 
-//    									new HashSet<IFacet>(), 
-//    									"1");
-//    			
-//
-//    	//convert the page of products to a page of product resources
-//    	final Page<ProductResource> prPages = new PageImpl<ProductResource>(pages.stream()
-//							    											.map(p -> prodAssembler.toResource(p))
-//							    											.collect(Collectors.toList()),
-//							    											pages.getPageable(),
-//							    											pages.getTotalElements());
-//    	
-//    	return new ResponseEntity< >(assembler.toResource(prPages), HttpStatus.OK);
-//    }
+    @SuppressWarnings("unchecked")
+	@GetMapping(value = "/Product/{locale}/{currency}/category/{categoryCode}", 
+    			params = { "page", "size" })
+    public ResponseEntity<PagedResources<ProductResource>> getProducts(@PathVariable String locale, 
+															    	   @PathVariable String currency, 
+															    	   @PathVariable String categoryCode,
+															    	   @RequestParam("page") int page,
+															    	   @RequestParam("size") int size,
+															    	   @SuppressWarnings("rawtypes") PagedResourcesAssembler assembler) {
+    	
+    	LOGGER.debug("Fetching products for parameters : {}, {}, {}, {}, {}", locale, currency, categoryCode, page, size);
+    	
+    	final Page<Product> pages =
+    					productService.findAll(	
+    									locale, 
+    									currency,
+    									categoryCode, 
+    									page, 
+    									size,
+    									new HashSet<IFacet>(), 
+    									"1");
+    			
+
+    	//convert the page of products to a page of product resources
+    	final Page<ProductResource> prPages = new PageImpl<ProductResource>(pages.stream()
+							    											.map(p -> prodAssembler.toResource(p))
+							    											.collect(Collectors.toList()),
+							    											pages.getPageable(),
+							    											pages.getTotalElements());
+    	
+    	return new ResponseEntity< >(assembler.toResource(prPages), HttpStatus.OK);
+    }
     
 	@GetMapping(value = "/Product/{locale}/{currency}/brand/code/{brandCode}", 
     			params = { "page", "size" })
