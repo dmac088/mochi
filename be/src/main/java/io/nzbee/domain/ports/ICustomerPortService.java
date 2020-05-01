9f75af51b79912473fc4e90473384e47be1bc623
dmac088 <danielmackie82@gmail.com>
2020-05-01 22:02:32 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/ports/ICustomerPortService.java
+++ be/src/main/java/io/nzbee/domain/ports/ICustomerPortService.java
@@ -3,11 +3,12 @@ package io.nzbee.domain.ports;
 import java.util.Optional;
 
 import io.nzbee.domain.customer.Customer;
+import io.nzbee.dto.customer.CustomerDTO;
 
 public interface ICustomerPortService extends IPortService<Customer> {
 
 	public Optional<Customer> findByUsername(String userName);
 	
-	public void registerNewCustomer(Customer customer);
+	void registerNewCustomer(CustomerDTO customer);
 	
 }
