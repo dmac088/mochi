7190ca33b73a20cca0a15aff373bbbde2749a282
dmac088 <danielmackie82@gmail.com>
2020-05-06 23:01:54 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/adapters/PostgresCategoryAdapter.java
+++ be/src/main/java/io/nzbee/entity/adapters/PostgresCategoryAdapter.java
@@ -1,12 +1,10 @@
 package io.nzbee.entity.adapters;
 
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 import io.nzbee.domain.category.BrandCategory;
 import io.nzbee.domain.category.Category;
 import io.nzbee.domain.category.ProductCategory;
@@ -18,7 +16,7 @@ import io.nzbee.entity.category.brand.CategoryBrand;
 import io.nzbee.entity.category.brand.ICategoryBrandService;
 import io.nzbee.entity.category.product.CategoryProduct;
 import io.nzbee.entity.category.product.ICategoryProductService;
-
+import io.nzbee.exceptions.category.CategoryException;
 @Component
 public class PostgresCategoryAdapter implements ICategoryPortService {
 
@@ -42,6 +40,20 @@ public class PostgresCategoryAdapter implements ICategoryPortService {
 	}
 	
 	@Override
+	public Category findByCode(String locale, String currency, String code) {
+		io.nzbee.entity.category.Category cp = categoryService.findByCode(locale, currency, code)
+				.orElseThrow(() -> new CategoryException("Primary category for code " + code + " not found!"));
+		return categoryMapper.entityToDo(cp, locale, currency);
+	}	
+	
+	@Override
+	public Category findByDesc(String locale, String currency, String desc) {
+		io.nzbee.entity.category.Category cp = categoryService.findByDesc(locale, currency, desc)
+				.orElseThrow(() -> new CategoryException("Primary category for desc " + desc + " not found!"));
+		return categoryMapper.entityToDo(cp, locale, currency);
+	}
+	
+	@Override
 	public Set<ProductCategory> findAllByProductCode(String locale, String currency, String productCode) {
 		return categoryProductService.findAllByProductCode(locale, currency, productCode)
 				.stream().map(c -> (ProductCategory) categoryMapper.entityToDo(c, locale, currency)).collect(Collectors.toSet());
@@ -49,7 +61,9 @@ public class PostgresCategoryAdapter implements ICategoryPortService {
 	
 	@Override
 	public ProductCategory findPrimaryByProductCode(String locale, String currency, String productCode) {
-		return (ProductCategory) categoryMapper.entityToDo(categoryProductService.findPrimaryByProductCode(locale, currency, productCode).get(), locale, currency);		
+		CategoryProduct cp = categoryProductService.findPrimaryByProductCode(locale, currency, productCode)
+				.orElseThrow(() -> new CategoryException("Primary category for product code " + productCode + " not found!"));
+		return (ProductCategory) categoryMapper.entityToDo(cp, locale, currency);		
 	}
 	
 	@Override
@@ -75,18 +89,7 @@ public class PostgresCategoryAdapter implements ICategoryPortService {
 		return categoryService.findAll(locale, currency)
 				.stream().map(c -> categoryMapper.entityToDo(c, locale, currency)).collect(Collectors.toSet());
 	}
-
-	@Override
-	public Category findByCode(String locale, String currency, String code) {
-		Optional<io.nzbee.entity.category.Category> cp = categoryService.findByCode(locale, currency, code);
-		return categoryMapper.entityToDo(cp.get(), locale, currency);
-	}	
 	
-	@Override
-	public Category findByDesc(String locale, String currency, String desc) {
-		return categoryMapper.entityToDo(categoryService.findByDesc(locale, currency, desc).get(), locale, currency);
-	}
-
 	@Override
 	public void save(Category domainObject) {
 		
