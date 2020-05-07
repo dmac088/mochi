2201e159da272f0db4c0c3c139c232ef179c7fed
dmac088 <danielmackie82@gmail.com>
2020-05-07 15:20:14 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/exceptions/brand/BrandException.java
@@ -0,0 +1,13 @@
+package io.nzbee.exceptions.brand;
+
+public class BrandException extends RuntimeException {
+
+	private static final long serialVersionUID = -1224349429646369198L;
+
+	public BrandException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
