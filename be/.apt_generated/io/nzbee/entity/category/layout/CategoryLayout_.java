023cbca589dabe7a8f5c1cac1aaf907a7705e3c5
dmac088 <danielmackie82@gmail.com>
2020-05-07 18:19:17 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/category/layout/CategoryLayout_.java
+++ be/.apt_generated/io/nzbee/entity/category/layout/CategoryLayout_.java
@@ -1,12 +1,19 @@
 package io.nzbee.entity.category.layout;
 
+import io.nzbee.entity.category.Category;
+import io.nzbee.entity.layout.Layout;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(CategoryLayout.class)
 public abstract class CategoryLayout_ {
 
+	public static volatile SingularAttribute<CategoryLayout, Layout> layout;
+	public static volatile SingularAttribute<CategoryLayout, CategoryLayoutId> id;
+	public static volatile SingularAttribute<CategoryLayout, Category> category;
+	public static volatile SingularAttribute<CategoryLayout, Long> order;
 
 }
 
--- be/src/main/java/io/nzbee/entity/category/layout/CategoryLayout.java
+++ be/src/main/java/io/nzbee/entity/category/layout/CategoryLayout.java
@@ -1,8 +1,34 @@
 package io.nzbee.entity.category.layout;
 
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import io.nzbee.entity.category.Category;
+import io.nzbee.entity.layout.Layout;
 
 @Entity
 public class CategoryLayout {
 
+	@EmbeddedId
+    private CategoryLayoutId id = new CategoryLayoutId();
+ 
+    @ManyToOne
+    @MapsId("categoryId")
+    private Category category;
+ 
+    @ManyToOne
+    @MapsId("layoutId")
+    private Layout layout;
+ 
+    private Long order;
+
+	public Long getOrder() {
+		return order;
+	}
+
+	public void setOrder(Long order) {
+		this.order = order;
+	}
+	
 }
