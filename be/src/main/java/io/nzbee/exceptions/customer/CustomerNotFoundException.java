6f34b71807df222d07bc1b3d9416cca85455f6a7
dmac088 <danielmackie82@gmail.com>
2020-05-08 18:09:35 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/exceptions/customer/CustomerNotFoundException.java
@@ -0,0 +1,24 @@
+package io.nzbee.exceptions.customer;
+
+
+public final class CustomerNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public CustomerNotFoundException() {
+        super();
+    }
+
+    public CustomerNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public CustomerNotFoundException(final String message) {
+        super(message);
+    }
+
+    public CustomerNotFoundException(final Throwable cause) {
+        super(cause);
+    }
+
+}
