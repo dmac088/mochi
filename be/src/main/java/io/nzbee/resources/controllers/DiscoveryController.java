619923d296adfc0c41a3a5b56df153f8fa0b8e38
dmac088 <danielmackie82@gmail.com>
2020-05-10 20:32:36 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/resources/controllers/DiscoveryController.java
@@ -0,0 +1,19 @@
+package io.nzbee.resources.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.nzbee.resources.discovery.DiscoveryResource;
+
+@RestController
+@RequestMapping("/api")
+public class DiscoveryController {
+
+	@GetMapping("/Discovery/{locale}/{currency}")
+	public ResponseEntity<DiscoveryResource> getCategory(@PathVariable String locale, @PathVariable String currency) {
+		return null;
+	}
+	
+}
