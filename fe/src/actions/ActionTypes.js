a089d4db246b3b05f5da628ab958d08fb0905cdb
dmac088 <danielmackie82@gmail.com>
2020-05-11 22:06:56 +0800

    asdf

--- fe/src/actions/ActionTypes.js
+++ fe/src/actions/ActionTypes.js
@@ -1,3 +1,5 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES';
 export const GET_DISCOVERY = 'GET_DISCOVERY';
 export const GET_SESSION = 'GET_SESSION';
+export const SET_USERNAME = 'SET_USERNAME';
+export const SET_PASSWORD = 'SET_PASSWORD';
--- fe/src/reducers/index.js
+++ fe/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import categoryReducer from "./categoryReducer";
 import discoveryReducer from "./discoveryReducer";
+import sessionReducer from "./sessionReducer";
 
 export default combineReducers({
   category: categoryReducer,
   discovery: discoveryReducer,
+  session: sessionReducer,
 });
