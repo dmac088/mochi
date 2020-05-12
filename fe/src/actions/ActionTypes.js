4ce04a608a6d0ed3e73ff5fa724db0ec6d2a00dc
dmac088 <danielmackie82@gmail.com>
2020-05-12 15:06:47 +0800

    asdf

--- fe/src/actions/ActionTypes.js
+++ fe/src/actions/ActionTypes.js
@@ -1,5 +1,3 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES';
 export const GET_DISCOVERY = 'GET_DISCOVERY';
 export const GET_SESSION = 'GET_SESSION';
-export const SET_USERNAME = 'SET_USERNAME';
-export const SET_PASSWORD = 'SET_PASSWORD';
