a393e9bce73f769d1c90081a8048f5f72c36e002
dmac088 <danielmackie82@gmail.com>
2020-05-14 16:44:59 +0800

    added Layout to the domain model

---
+++ be/src/main/java/io/nzbee/domain/layout/category/LayoutCategory.java
@@ -0,0 +1,19 @@
+package io.nzbee.domain.layout.category;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.nzbee.domain.category.Category;
+import io.nzbee.domain.layout.Layout;
+
+public class LayoutCategory extends Layout {
+	
+	private List<Category> categories;
+	
+	public LayoutCategory(String layoutCode, String layoutDesc, Category category) {
+		super(layoutCode, layoutDesc);
+		categories = new ArrayList<Category>();
+		categories.add(category);
+	}
+
+}
