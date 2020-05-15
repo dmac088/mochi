7a7fd4c9ac404943f778ef61b5c21db62d2e514e
dmac088 <danielmackie82@gmail.com>
2020-05-15 15:04:18 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/category/LayoutCategory.java
+++ be/src/main/java/io/nzbee/domain/category/LayoutCategory.java
@@ -5,10 +5,28 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName("layoutcategory")
 public class LayoutCategory extends Category {
 
-	public LayoutCategory(String categoryCode, String categoryDesc, Long categoryLevel, String lclCd, String currency,
-			int objectCount) {
-		super(categoryCode, categoryDesc, categoryLevel, lclCd, currency, objectCount);
-		// TODO Auto-generated constructor stub
+	private Long orderNumber;
+	
+	public LayoutCategory(String categoryCode, 
+						  String categoryDesc, 
+						  Long categoryLevel, 
+						  String lclCd, 
+						  String currency,
+						  int objectCount,
+						  Long orderNumber) {
+		
+		super(	categoryCode, 
+				categoryDesc, 
+				categoryLevel, 
+				lclCd, 
+				currency, 
+				objectCount);
+		
+		this.orderNumber = orderNumber;
+	}
+
+	public Long getOrderNumber() {
+		return orderNumber;
 	}
 
 }
