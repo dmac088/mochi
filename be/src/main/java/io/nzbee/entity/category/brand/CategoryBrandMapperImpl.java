21554f40e5a9ee1e8cd2ba725c321f9b8e90d96b
dmac088 <danielmackie82@gmail.com>
2020-05-15 15:52:23 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/brand/CategoryBrandMapperImpl.java
+++ be/src/main/java/io/nzbee/entity/category/brand/CategoryBrandMapperImpl.java
@@ -2,6 +2,7 @@ package io.nzbee.entity.category.brand;
 
 import org.springframework.stereotype.Component;
 import io.nzbee.domain.category.BrandCategory;
+import io.nzbee.entity.category.attribute.CategoryAttribute;
 
 @Component
 public class CategoryBrandMapperImpl implements ICategoryBrandMapper {
@@ -21,8 +22,22 @@ public class CategoryBrandMapperImpl implements ICategoryBrandMapper {
 
 	@Override
 	public CategoryBrand doToEntity(BrandCategory d) {
-		// TODO Auto-generated method stub
-		return null;
+		BrandCategory bc = (BrandCategory) d;
+		
+		CategoryBrand cb = new CategoryBrand();
+		cb.setCategoryCode(bc.getCategoryCode());
+		cb.setLocale(bc.getLocale());
+		cb.setCurrency(bc.getCurrency());
+		cb.setCategoryLevel(bc.getCategoryLevel());
+		cb.setObjectCount(bc.getCount());
+		
+		CategoryAttribute ca = new CategoryAttribute();
+		ca.setCategoryDesc(bc.getCategoryDesc());
+		ca.setLclCd(bc.getLocale());
+		
+		ca.setCategory(cb);
+		cb.addAttribute(ca);
+		return cb;
 	}
 
 }
