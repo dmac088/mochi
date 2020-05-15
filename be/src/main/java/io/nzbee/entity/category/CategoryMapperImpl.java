61febb971917df561f72961144010945425aee1e
dmac088 <danielmackie82@gmail.com>
2020-05-15 18:41:08 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/CategoryMapperImpl.java
+++ be/src/main/java/io/nzbee/entity/category/CategoryMapperImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import io.nzbee.entity.category.brand.CategoryBrand;
 import io.nzbee.entity.category.brand.ICategoryBrandMapper;
+import io.nzbee.entity.category.layout.CategoryLayout;
+import io.nzbee.entity.category.layout.ICategoryLayoutMapper;
 import io.nzbee.entity.category.product.CategoryProduct;
 import io.nzbee.entity.category.product.ICategoryProductMapper;
 
@@ -15,6 +17,9 @@ public class CategoryMapperImpl implements ICategoryMapper {
 	
 	@Autowired
 	private ICategoryBrandMapper categoryBrandMapper;
+	
+	@Autowired
+	private ICategoryLayoutMapper categoryLayoutMapper;
 
 	public io.nzbee.domain.category.Category entityToDo(Category e) {
 	
@@ -23,6 +28,9 @@ public class CategoryMapperImpl implements ICategoryMapper {
 		}
 		if(e instanceof CategoryBrand) {
 			return categoryBrandMapper.entityToDo((CategoryBrand) e);
+		}
+		if(e instanceof CategoryLayout) {
+			return categoryLayoutMapper.entityToDo((CategoryLayout) e);
 		}
 		return null;
 		
