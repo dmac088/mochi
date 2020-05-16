67b9a46e3db2dd5b98dfbe88bc4c238ad6136de4
dmac088 <danielmackie82@gmail.com>
2020-05-16 14:12:59 +0800

    asdf

--- fe/src/components/Layout/MainMenu/LayoutHelper.js
+++ fe/src/components/Layout/MainMenu/LayoutHelper.js
@@ -5,7 +5,9 @@
 //categories can be brand categories or product categories
 //for categories with children we want to render the children hierarchically 
 //in cascading menu items, brand are non hierarchical so we just want to display
-//the brands within the brand category
+//the brands within the brand category. Additionally we need an API url in our 
+//HATEOAS implementation to get all the ancestors of a particular productCategory 
+//code
 export const menuCategories = 
 [
     'FRT01', //fruit
