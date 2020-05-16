93963eda60b22a1f44b6267c151d6a3d2e057cc5
dmac088 <danielmackie82@gmail.com>
2020-05-16 13:42:22 +0800

    asdf

--- fe/src/components/Layout/MainMenu/Helper.js
+++ fe/src/components/Layout/MainMenu/Helper.js
@@ -1,3 +1,11 @@
+
+//the menuCategories array
+//defines which categories will be shown in the main menu
+//including the sort order of such categories
+//categories can be brand categories or product categories
+//for categories with children we want to render the children hierarchically 
+//in cascading menu items, brand are non hierarchical so we just want to display
+//the brands within the brand category
 export const menuCategories = 
 [
     'FRT01', //fruit
