0f64107f604ee76f1e82a1d77ff12acac5e2f28c
dmac088 <danielmackie82@gmail.com>
2020-05-18 18:25:43 +0800

    asdf

--- fe/src/components/Layout/Header/MainMenu/Web/HomeMenu.js
+++ fe/src/components/Layout/Header/MainMenu/Web/HomeMenu.js
@@ -3,7 +3,6 @@ import { withRouter } from 'react-router-dom';
 
 export const route = (e, props) => {
     e.preventDefault();
-    console.log(props);
     const { lang, curr } = props.match.params;
     props.history.push('/' + lang + '/' + curr );
 }
