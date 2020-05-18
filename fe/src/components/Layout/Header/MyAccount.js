aa4fb075bb55f57adb28493d7b97f86562589d84
dmac088 <danielmackie82@gmail.com>
2020-05-18 16:07:06 +0800

    asdf

--- fe/src/components/Layout/Header/MyAccount.js
+++ fe/src/components/Layout/Header/MyAccount.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-export const MyAccount = () => {
-    return (
-        <li>
-            <Link to="/myaccount">
-                My Account
-            </Link>
-        </li>
-    );
+const routeAccount = (e, props) => {
+    e.preventDefault();
+    const { lang, curr } = props.match.params;
+    props.history.push('/' + lang + '/' + curr + '/myaccount');
 }
 
+export const MyAccount = withRouter(({...props}) => {
+    return (
+        <li> 
+            <a href="#" onClick={(e) => routeAccount(e, props)}>My Account</a>
+        </li>
+    );
+});
+
 export default MyAccount;
+
 
 
