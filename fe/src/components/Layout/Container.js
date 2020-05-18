5be96d54fc4359ca6a3625308ef9609c036bf6db
dmac088 <danielmackie82@gmail.com>
2020-05-18 15:59:34 +0800

    asdf

--- fe/src/components/Layout/Container.js
+++ fe/src/components/Layout/Container.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 function Container(props) {
     return (
         <React.Fragment>
             <Header />
-            {props.children}
+                {props.children}
+            <Footer />
         </React.Fragment>
     );
 }
