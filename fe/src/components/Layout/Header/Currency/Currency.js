ceb212e06c2531d2e70d56c1fa2b25be668324bc
dmac088 <danielmackie82@gmail.com>
2020-05-18 15:15:43 +0800

    asdf

---
+++ fe/src/components/Layout/Header/Currency/Currency.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { withRouter } from 'react-router-dom';
+
+const changeCurr = (e, props) => {
+    e.preventDefault();
+    const { lang } = props.match.params;
+    props.history.push('/' +lang + '/' + e.currentTarget.id);
+}
+
+export const Currency = withRouter(({...props}) => {
+    return (
+        <li> <a href="#">HKD <i className="fa fa-chevron-down"></i></a>
+            <ul>
+                <li><a href="#" id="HKD" onClick={(e) => changeCurr(e, props)}>HKD</a></li>
+                <li><a href="#" id="USD" onClick={(e) => changeCurr(e, props)}>USD</a></li>
+            </ul>
+        </li>
+    );
+});
+
+export default Currency;
+
+
+
+
+
+
