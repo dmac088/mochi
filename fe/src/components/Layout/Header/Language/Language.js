e0012ecbe2fa84075b974b7ab1f5ff239a0a3972
dmac088 <danielmackie82@gmail.com>
2020-05-19 16:08:19 +0800

    asdf

--- fe/src/components/Layout/Header/Language/Language.js
+++ fe/src/components/Layout/Header/Language/Language.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 
-
-
-
 const changeLang = (e, curr, history) => {
     e.preventDefault();
     history.push('/' + e.currentTarget.id + '/' + curr);
@@ -12,17 +9,18 @@ const changeLang = (e, curr, history) => {
 export const Language = withRouter(({...props}) => {
     const { params } = props.match;
     const { lang, curr } = params;
+    const{ history } = props;
     return (
         <LanguageBase lang={lang}
                       curr={curr}
-                      history={props.history} />
+                      history={history}/>
     );
 });
 
 export const LanguageBase = (props) => {
     const { lang, curr, history } = props;
     return (
-        <li data-testid="currency"> <a href="#">{lang} <i className="fa fa-chevron-down"></i></a>
+        <li data-testid="language"> <a href="#">{lang} <i className="fa fa-chevron-down"></i></a>
             <ul>
                 <li><a href="#" id="en-GB" onClick={(e) => changeLang(e, curr, history)}>English</a></li>
                 <li><a href="#" id="zh-HK" onClick={(e) => changeLang(e, curr, history)}>Chinese</a></li>
