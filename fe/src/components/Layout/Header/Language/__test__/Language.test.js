43754952872c9febf180d773c4797f7cfea6be83
dmac088 <danielmackie82@gmail.com>
2020-05-19 17:36:36 +0800

    iasdf

--- fe/src/components/Layout/Header/Language/__test__/Language.test.js
+++ fe/src/components/Layout/Header/Language/__test__/Language.test.js
@@ -5,52 +5,68 @@ import { BrowserRouter } from 'react-router-dom';
 import { createMemoryHistory } from 'history'
 import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
-
 import renderer from 'react-test-renderer';
 
-afterEach(cleanup);
+describe('Test language menu', () => {
 
-it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(
-        <BrowserRouter>
-            <LanguageBase />
-        </BrowserRouter >, div);
-});
+    afterEach(cleanup);
 
-it("renders english in language menu correctly", () => {
-    const history = createMemoryHistory('/en-GB/HKD')
-    const { getByTestId } = render(
-        <BrowserRouter>
-            <LanguageBase
-                lang='en-GB'
-                curr='HKD'
-                history={history} />
-        </BrowserRouter >);
-    expect(getByTestId('language')).toHaveTextContent("English");
-});
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        ReactDOM.render(
+            <BrowserRouter>
+                <LanguageBase />
+            </BrowserRouter >, div);
+    });
 
-it("renders chinese in language menu correctly", () => {
-    const history = createMemoryHistory('/en-GB/HKD')
-    const { getByTestId } = render(
-        <BrowserRouter>
-            <LanguageBase
-                lang='zh-HK'
-                curr='HKD'
-                history={history} /> 
-        </BrowserRouter >);
-    expect(getByTestId('language')).toHaveTextContent("Chinese");
-});
+    it("renders english in language menu correctly", () => {
+        const history = createMemoryHistory('/en-GB/HKD')
+        const { getByTestId } = render(
+            <BrowserRouter>
+                <LanguageBase
+                    lang='en-GB'
+                    curr='HKD'
+                    history={history} />
+            </BrowserRouter >);
+        expect(getByTestId('language')).toHaveTextContent("English");
+    });
 
-it("matches snapshot", () => {
-    const history = createMemoryHistory('/en-GB/HKD') 
-    const tree = renderer.create(
-        <BrowserRouter>
-            <LanguageBase
-                lang='zh-HK'
-                curr='HKD'
-                history={history} />
-        </BrowserRouter >
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    it("renders chinese in language menu correctly", () => {
+        const history = createMemoryHistory('/en-GB/HKD')
+        const { getByTestId } = render(
+            <BrowserRouter>
+                <LanguageBase
+                    lang='zh-HK'
+                    curr='HKD'
+                    history={history} /> 
+            </BrowserRouter >);
+        expect(getByTestId('language')).toHaveTextContent("Chinese");
+    });
+
+    it("matches snapshot 1", () => {
+        const history = createMemoryHistory('/en-GB/HKD') 
+        const tree = renderer.create(
+            <BrowserRouter>
+                <LanguageBase
+                    lang='en-GB'
+                    curr='HKD'
+                    history={history} />
+            </BrowserRouter >
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    }); 
+
+
+    it("matches snapshot 2", () => {
+        const history = createMemoryHistory('/zh-HK/USD') 
+        const tree = renderer.create(
+            <BrowserRouter>
+                <LanguageBase
+                    lang='zh-HK'
+                    curr='USD'
+                    history={history} />
+            </BrowserRouter >
+        ).toJSON();
+        expect(tree).toMatchSnapshot();
+    }); 
 });
