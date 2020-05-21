72db641bad05fcd4697d46a743d63151a1cba0ec
dmac088 <danielmackie82@gmail.com>
2020-05-21 23:30:46 +0800

    asdf

--- fe/src/services/Routing/Helper.js
+++ fe/src/services/Routing/Helper.js
@@ -2,5 +2,6 @@
 
 export const routeToPage = (history, params, page) => {
     const {lang, curr} = params;
-    history.push('/' + lang + '/' + curr + '/' + page);
+    history.push({
+    pathname: '/' + lang + '/' + curr + '/' + page});
 }
