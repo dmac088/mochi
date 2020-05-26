7367470ff010f88aaf45140591b39bb846c3936f
dmac088 <danielmackie82@gmail.com>
2020-05-26 14:20:47 +0800

    asdf

---
+++ fe/src/components/Layout/Landing/Highlights/Highlights.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+function Highlights() {
+
+  return (
+    <div className="slider tab-slider mb-35">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="tab-slider-wrapper">
+              <nav>
+                <div className="nav nav-tabs" id="nav-tab" role="tablist">
+
+                </div>
+              </nav>
+              <div className="tab-content" id="nav-tabContent">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Highlights;
