d10af71ead8ba7b9663358934cfa8151b6d84200
dmac088 <danielmackie82@gmail.com>
2020-05-26 11:49:22 +0800

    asdf

---
+++ fe/src/components/Layout/BreadCrumb/BreadCrumb.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function BreadCrumb() {
+    return (
+        <div className="breadcrumb-area mb-50">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="breadcrumb-container">
+                            <ul>
+                                <li><i className="fa fa-home"></i> Home</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BreadCrumb;
