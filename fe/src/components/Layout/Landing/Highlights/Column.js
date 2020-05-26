56641cc0e2c416aa5c3e307d848126bea372a27b
dmac088 <danielmackie82@gmail.com>
2020-05-26 14:40:17 +0800

    asdf

--- fe/src/components/Layout/Landing/Highlights/Column.js
+++ fe/src/components/Layout/Landing/Highlights/Column.js
@@ -3,25 +3,11 @@ import { withRouter } from 'react-router-dom';
 import Product from './Product';
 const $ = window.$;
 
-const renderProducts = (props) => {
-  const { category, match, history, setCurrentProductId, products } = props;
-  if (products === undefined) { return; }
-  return products.map(product => {
-    return (
-      <Product
-        key={product.productUPC}
-        product={product}
-        category={category}
-        setCurrentProductId={setCurrentProductId}
-      />
-    )
-  });
-}
-
 const Column = () => {
   return (
     <div className="single-tab-slider-item">
-      
+        <Product />
+        <Product />
     </div>
   )
 }
