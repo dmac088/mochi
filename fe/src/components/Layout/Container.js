2cf60abebe1cae29bca674de0400779e6f5ce4bf
dmac088 <danielmackie82@gmail.com>
2020-05-26 11:49:22 +0800

    asdf

--- fe/src/components/Layout/Container.js
+++ fe/src/components/Layout/Container.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import BreadCrumb from "./BreadCrumb/BreadCrumb";
 
 function Container(props) {
     return (
         <React.Fragment>
             <Header />
+            <BreadCrumb />
                 {props.children}
             <Footer />
         </React.Fragment>
