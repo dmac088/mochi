8b4acaf188fd11b9b763a5269bae281c6bbefd82
dmac088 <danielmackie82@gmail.com>
2020-05-27 16:57:16 +0800

    asdf

---
+++ fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuContainer.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import { TransitionGroup, Transition } from 'react-transition-group'
+import CategoryMenu from "./CategoryMenu";
+
+function CategoryMenuContainer() {
+
+    const [stateObject, setObjectState] = useState({
+        menuVisible: true,
+        isMobile: false
+    });
+
+    const isMobile = () => {
+        return ((window.innerWidth
+            || document.documentElement.clientWidth
+            || document.body.clientWidth) <= 991);
+    }
+
+    const renderMenu = (isMounting = false) => {
+        if (isMobile() && !stateObject.isMobile) {
+            setObjectState({
+                isMobile: true,
+                menuVisible: (!isMounting),
+            });
+        }
+        if (!isMobile() && stateObject.isMobile) {
+            setObjectState({
+                isMobile: false,
+                menuVisible: true,
+            });
+        }
+    }
+
+    const toggleVisible = () => {
+        setObjectState((prevState) => ({
+            ...prevState,
+            menuVisible: !prevState.menuVisible,
+        }));
+    }
+
+    useEffect(() => {
+        renderMenu(true);
+        window.addEventListener('resize',  renderMenu , { passive: true });
+    });
+
+    return (
+        <div className="hero-side-category">
+            <div className="category-toggle-wrap">
+                <button onClick={toggleVisible} className="category-toggle">
+                    <span className="arrow_carrot-right_alt2 mr-2"></span> All Categories</button>
+            </div>
+            <nav className="category-menu" >
+                <TransitionGroup
+                    component="nav"
+                    className="category-menu">
+                    {((stateObject.menuVisible)
+                        ? <CategoryMenu />
+                        : null)}
+                </TransitionGroup>
+            </nav>
+        </div>
+    );
+}
+
+export default CategoryMenuContainer;
