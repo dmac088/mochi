6b9294de7ce0333907bbfb190d9306c49d096425
dmac088 <danielmackie82@gmail.com>
2020-05-28 17:36:31 +0800

    asdf

--- fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItemSublist.js
+++ fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItemSublist.js
@@ -2,28 +2,18 @@ import React from 'react';
 
 function CategoryMenuItemSubList(props) {
 
-    // componentWillEnter(callback) {
-    //     if (!this.container) { return }
-    //     slide(this.container, 'slideDown', { duration: 500, "display": "" }, callback);
-    // }
-
-    // componentWillLeave(callback) {
-    //     if (!this.container) { return }
-    //     slide(this.container, 'slideUp', null, callback);
-    // }
-    
     let container = null;
 
     const setContainer = (c) => {
         container = c;
     }
 
-    const { isMobile, displayList, dataList, renderCategoryList, itemCounter, children } = props;
+    const { isMobile, fullList, renderCategoryList, itemCounter, children } = props;
     
     return (
         <ul ref={setContainer}
             className="category-mega-menu">
-             {renderCategoryList(isMobile, children, dataList, false, itemCounter)} 
+             {renderCategoryList(isMobile, children, fullList, false, itemCounter)} 
         </ul>
     )
 }
