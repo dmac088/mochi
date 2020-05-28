7a78fcf8270997c3af7549847e86e86d6fa8743d
dmac088 <danielmackie82@gmail.com>
2020-05-28 15:49:51 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/CategoryDaoPostgresImpl.java
+++ be/src/main/java/io/nzbee/entity/category/CategoryDaoPostgresImpl.java
@@ -364,7 +364,6 @@ public class CategoryDaoPostgresImpl implements ICategoryDao {
 		}
 		category.setObjectCount(((BigDecimal)o[6]).intValue());
 		category.setChildCount(((BigInteger)o[7]).longValue());
-		System.out.println(category.getChildCount());
 		category.setLocale(locale);
 		category.setCurrency(currency);
 		
