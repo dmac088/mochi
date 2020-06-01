2a1605e1b263fca89fda537dbab8bfbc4b4222fa
dmac088 <danielmackie82@gmail.com>
2020-06-01 12:23:54 +0800

    asdf

--- fe/src/components/Layout/Header/MainMenu/Mob/BrandMenu.js
+++ fe/src/components/Layout/Header/MainMenu/Mob/BrandMenu.js
@@ -1,34 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Transition } from 'react-transition-group';
+import { slide } from '../../../Helpers/Animation/Slide';
 
 function BrandMenu() {
 
-    // componentWillEnter(callback) {
-    //     if (!this.container) { return }
-    //     slide(this.container, 'slideDown', null, callback);
-    // }
-
-    // componentWillLeave(callback) {
-    //     if (!this.container) { return }
-    //     slide(this.container, 'slideUp', null, callback);
-    // }
+    const [revealMenu, setRevealMenu] = useState(false);
 
     let container = null;
-    
     const setContainer = (c) => {
         container = c;
     }
 
+    const toggleMenu = (e) => {
+        e.preventDefault();
+        setRevealMenu(!revealMenu);
+    }
+
     return (
-        <ul ref={setContainer} className="sub-menu mega-menu three-column" style={{ display: 'block' }}>
-            <li><a href="#">Comvita</a></li>
-            <li><a href="#">Airborne</a></li>
-            <li><a href="#">Happy Bee</a></li>
-            <li><a href="#">Antipodes</a></li>
-            <li><a href="#">Wild Ferns</a></li>
-            <li><a href="#">Trilogy</a></li>
-            <li><a href="#">Anchor</a></li>
-        </ul>
+        <React.Fragment>
+            <a href="#">Brands</a>
+            <Transition
+                in={revealMenu}
+                timeout={2000}
+                onEntering={() => { slide(container, 'slideDown', { display: "block" }); }}
+                onExiting={() => { slide(container, 'slideUp', { display: "none" }); }}>
+                <ul ref={setContainer} className="sub-menu mega-menu three-column" >
+                    <li><a href="#">Comvita</a></li>
+                    <li><a href="#">Airborne</a></li>
+                    <li><a href="#">Happy Bee</a></li>
+                    <li><a href="#">Antipodes</a></li>
+                    <li><a href="#">Wild Ferns</a></li>
+                    <li><a href="#">Trilogy</a></li>
+                    <li><a href="#">Anchor</a></li>
+                </ul>
+            </Transition>
+            <a onClick={toggleMenu}
+               className="mean-expand"
+               href="#"
+               style={{ fontSize: 0 }}>
+                {(revealMenu) ? "-" : "+"}
+            </a>
+        </React.Fragment>
     )
 }
 
-export default BrandMenu;
+export default BrandMenu;   
