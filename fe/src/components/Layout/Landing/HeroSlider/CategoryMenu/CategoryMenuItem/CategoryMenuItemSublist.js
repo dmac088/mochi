622c71531d180c44c0f89f736667f61e5bf4ae05
dmac088 <danielmackie82@gmail.com>
2020-06-01 10:23:37 +0800

    asdf

--- fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItemSublist.js
+++ fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItemSublist.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { Transition } from 'react-transition-group';
+import { slide } from '../../../../Helpers/Animation/Slide';
 
 function CategoryMenuItemSubList(props) {
 
-    const { isMobile, fullList, renderCategoryList, itemCounter, children } = props;
+    const { isMobile, fullList, renderCategoryList, itemCounter, children, expand } = props;
     
+    let container = null;
+    const setScope = (c) => {
+        container = c;
+    }
+
     return (
-        <React.Fragment>
-             {renderCategoryList(isMobile, children, fullList, false, itemCounter)} 
-        </React.Fragment>
+        <Transition
+                in={(expand)}
+                timeout={2000}
+                onEntering={() => { slide(container, 'slideDown'); }}
+                onExiting={() => { slide(container, 'slideUp', {display:"none"}); }}>
+            <ul ref={setScope} className="category-mega-menu" style={{display:"none"}}>
+                {renderCategoryList(isMobile, children, fullList, false, itemCounter)} 
+            </ul>                        
+        </Transition>
     )
 }
 
