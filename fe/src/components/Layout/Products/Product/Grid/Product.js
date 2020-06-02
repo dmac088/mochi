1ca0393bbba938751c1b30fa846941f28303efda
dmac088 <danielmackie82@gmail.com>
2020-06-02 17:01:40 +0800

    asdf

--- fe/src/components/Layout/Products/Product/Grid/Product.js
+++ fe/src/components/Layout/Products/Product/Grid/Product.js
@@ -1,42 +1,41 @@
 import React from 'react';
 
-
- function Product() {
-    return (
-      <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
-        <div className="gf-product shop-grid-view-product">
-          <div className="image">
-            <a onClick={(e) => {}} id={product.productUPC} href="#">
-              <span className="onsale">Sale!</span>
-              <img src={productImagePath + product.productImage} className="img-fluid" alt />
+function Product() {
+  return (
+    <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
+      <div className="gf-product shop-grid-view-product">
+        <div className="image">
+          <a onClick={(e) => { }} id={product.productUPC} href="#">
+            <span className="onsale">Sale!</span>
+            <img src={productImagePath + product.productImage} className="img-fluid" alt />
+          </a>
+          <div className="product-hover-icons">
+            <a onClick={(e) => { }} href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
+            <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
+            <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
+            <a id={product.productUPC}
+              onClick={setCurrentProductId}
+              href="#"
+              data-tooltip="Quick view"
+              data-toggle="modal"
+              data-target="#quick-view-modal-container"> <span className="icon_search" />
             </a>
-            <div className="product-hover-icons">
-              <a onClick={(e) => {}} href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
-              <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
-              <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
-              <a  id={product.productUPC}
-                  onClick={setCurrentProductId}
-                  href="#"
-                  data-tooltip="Quick view"
-                  data-toggle="modal"
-                  data-target="#quick-view-modal-container"> <span className="icon_search" />
-              </a>
-            </div>
           </div>
-          <div className="product-content">
-            <div className="product-categories">
-              <a href="shop-left-sidebar.html">{product.primaryCategoryPath}</a>,<span> </span>
-              <a href="shop-left-sidebar.html">{product.brandDesc}</a>
-            </div>
-            <h3 className="product-title"><a href="#">{product.productDesc}</a></h3>
-            <div className="price-box">
-              <span className="main-price">${product.productRetail}</span>
-              <span className="discounted-price">${product.productMarkdown}</span>
-            </div>
+        </div>
+        <div className="product-content">
+          <div className="product-categories">
+            <a href="shop-left-sidebar.html">{product.primaryCategoryPath}</a>,<span> </span>
+            <a href="shop-left-sidebar.html">{product.brandDesc}</a>
+          </div>
+          <h3 className="product-title"><a href="#">{product.productDesc}</a></h3>
+          <div className="price-box">
+            <span className="main-price">${product.productRetail}</span>
+            <span className="discounted-price">${product.productMarkdown}</span>
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 
-  export default Product;
+export default Product;
