b453c3647d699b46a264130d4ff9f91567142f51
dmac088 <danielmackie82@gmail.com>
2020-06-02 16:35:53 +0800

    asdf

---
+++ fe/src/components/Layout/Products/Products.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Products() {
+
+    return (
+        <div className="shop-page-container mb-50">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 order-2 order-lg-1">
+                        <div className="sidebar-area">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Products;
