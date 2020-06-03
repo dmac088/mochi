f9edf7cff5b211a8bb9f08af0ff2f1ee5e3ba52e
dmac088 <danielmackie82@gmail.com>
2020-06-03 16:09:27 +0800

    asdf

--- fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItem.js
+++ fe/src/components/Layout/Landing/HeroSlider/CategoryMenu/CategoryMenuItem/CategoryMenuItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MobileCategoryMenuItemSubList from './MobileCategoryMenuItemSublist';
 import WebCategoryMenuItemSubList from './WebCategoryMenuItemSublist';
 import { Link } from "react-router-dom";
