54128b6615cb2d85ac39b15a0d7729f173c3a4ff
dmac088 <danielmackie82@gmail.com>
2020-06-04 11:44:47 +0800

    asdf

--- fe/src/components/Layout/Header/MyAccount.js
+++ fe/src/components/Layout/Header/MyAccount.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 import { routeToPage } from '../../../services/Routing/Helper';
+import { localization } from './Localization/Localization';
+
+const routePage = 'myaccount';
 
 export const MyAccount = withRouter(({...props}) => {
     const { params } = props.match;
@@ -14,15 +17,16 @@ export const MyAccount = withRouter(({...props}) => {
 
 export const MyAccountBase = (props) => {
     const { params, history } = props;
+    const {lang, curr} =  params;
 
     const routeAccount = (e) => {
         e.preventDefault();
-        routeToPage(history, params, 'myaccount');
+        routeToPage(history, params, routePage);
     }
 
     return (
         <li> 
-            <a href="#" onClick={routeAccount}>My Account</a>
+            <a href="#" onClick={routeAccount}>{localization[lang][routePage]}</a>
         </li>
     );
 };
