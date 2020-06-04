051d98aa858e513a52ac7f30f0ff895b485e4f8d
dmac088 <danielmackie82@gmail.com>
2020-06-04 09:44:20 +0800

    asdf

---
+++ fe/src/components/Layout/BreadCrumb/lang/lang.js
@@ -0,0 +1,17 @@
+
+export const lang = {
+    "en-GB": {
+        "category": "Category",
+        "mybag": "My Bag",
+        "contact": "Contact Us",
+        "checkout": "Checkout",
+        "myaccount": "My Account",
+    },
+    "zh-HK": {
+        "category": "類別",
+        "mybag": "我的包",
+        "contact": "聯繫我們",
+        "checkout": "查看",
+        "myaccount": "我的帳戶",
+    }
+}
