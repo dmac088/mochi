dad8fb9b1b5437db946b0f4528807908166059fa
dmac088 <danielmackie82@gmail.com>
2020-06-04 13:07:37 +0800

    asdf

--- fe/src/components/Layout/Header/HeaderItem.js
+++ fe/src/components/Layout/Header/HeaderItem.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { withRouter } from 'react-router-dom';
 import { routeToPage } from '../../../services/Routing/Helper';
-import { localization } from './Localization/Localization';
+import { localization } from '../Localization/Localization';
 
 
 export const HeaderItem = withRouter(({...props}) => {
     const { params } = props.match;
     const{ history, routePage } = props;
-    
+
     return (
         <HeaderItemBase  params={params}
                         history={history} 
