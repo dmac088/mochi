ac3c1aac29f3fb1be67bc9e41c87b594d544c1e7
dmac088 <danielmackie82@gmail.com>
2020-06-04 13:01:27 +0800

    removed redundant components

---
+++ fe/src/components/Layout/Header/HeaderItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { withRouter } from 'react-router-dom';
+import { routeToPage } from '../../../services/Routing/Helper';
+import { localization } from './Localization/Localization';
+
+
+export const HeaderItem = withRouter(({...props}) => {
+    const { params } = props.match;
+    const{ history, routePage } = props;
+    
+    return (
+        <HeaderItemBase  params={params}
+                        history={history} 
+                        routePage={routePage}/>
+    );
+});
+
+export const HeaderItemBase = (props) => {
+    const { params, history, routePage } = props;
+    const {lang} =  params;
+
+
+    const routeHeaderItem = (e) => {
+        e.preventDefault();
+        routeToPage(history, params, routePage);
+    }
+
+    return (
+        <li> 
+            <a href="#" onClick={routeHeaderItem}>{localization[lang][routePage]}</a>
+        </li>
+    );
+};
+
+export default HeaderItem;
