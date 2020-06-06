25521f7da90bf83e6620da12ce0054340bc73cec
dmac088 <danielmackie82@gmail.com>
2020-06-06 11:29:52 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/token/TokenResourceAssembler.java
+++ be/src/main/java/io/nzbee/resources/token/TokenResourceAssembler.java
@@ -1,20 +1,20 @@
 package io.nzbee.resources.token;
 
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
-
+import io.nzbee.resources.controllers.TokenController;
 import io.nzbee.security.CustomTokenEnhancer;
 
 public class TokenResourceAssembler extends ResourceAssemblerSupport<CustomTokenEnhancer, TokenResource> {
 
-	public TokenResourceAssembler(Class<?> controllerClass, Class<TokenResource> resourceType) {
-		super(controllerClass, resourceType);
+	public TokenResourceAssembler() {
+		super(TokenController.class, TokenResource.class);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public TokenResource toResource(CustomTokenEnhancer entity) {
-		// TODO Auto-generated method stub
-		return null;
+		TokenResource tr = new TokenResource(entity); 
+		return tr;
 	}
 
 }
