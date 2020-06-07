98c09ae95769823d7a13f1ef568d28bc551a71cc
dmac088 <danielmackie82@gmail.com>
2020-06-07 15:41:13 +0800

    asdf

--- fe/src/actions/ActionTypes.js
+++ fe/src/actions/ActionTypes.js
@@ -3,3 +3,4 @@ export const GET_DISCOVERY = 'GET_DISCOVERY';
 export const GET_SESSION = 'GET_SESSION';
 export const RESET_SESSION = 'RESET_SESSION';
 export const GET_ERROR = 'GET_ERROR';
+export const GET_CUSTOMER = 'GET_CUSTOMER';
