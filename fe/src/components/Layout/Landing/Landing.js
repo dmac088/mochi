9c5bcfa5efc3abfb5336de6c8ab914fc29a3a615
dmac088 <danielmackie82@gmail.com>
2020-06-07 18:08:15 +0800

    asdf

--- fe/src/components/Layout/Landing/Landing.js
+++ fe/src/components/Layout/Landing/Landing.js
@@ -10,6 +10,7 @@ import BestSeller from "./BestSeller/BestSeller";
 import BrandSlider from "./BrandSlider/BrandSlider";
 
 function Landing(props) { 
+  
   return (
     <React.Fragment>
       <HeroSlider 
@@ -25,43 +26,6 @@ function Landing(props) {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">Introduction To HATEOAS</h1>
-                {/* <form onSubmit={(e) => handleSubmit(e)}>
-                  <input
-                    className="form-control mr-sm-2"
-                    type="input"
-                    onChange={e => setUsername(e.target.value)}
-                    id="customer.userName"
-                    placeholder="User Name"
-                    aria-label="userName" />
-                  <br />
-                  <input
-                    className="form-control mr-sm-2"
-                    id="customer.password"
-                    type="password"
-                    onChange={e => setPassword(e.target.value)}
-                    placeholder="Password"
-                    aria-label="Password" />
-                  <button
-                    className="btn btn-outline-success mr-sm-2 my-2 my-sm-0"
-                    type="submit">
-                    Login
-                  </button>
-                </form> */}
-                {/* <hr /> */}
-                {/* <button
-                  onClick={logout}
-                  className="btn btn-outline-success mr-sm-2 my-2 my-sm-0"
-                  type="submit">
-                  Logout
-                </button>
-                <br />
-                <br />  */}
-            
-                {/* <Link className="btn btn-lg btn-primary mr-2" to="/dashboard">
-                  Load Dashboard
-                </Link>
-                <br /> */}
-                {/* {tokens.access_token} */}
               </div>
             </div>
           </div>
