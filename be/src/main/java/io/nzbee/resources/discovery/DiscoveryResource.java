6a856d2466a6b861405d94065bd885409685093d
dmac088 <danielmackie82@gmail.com>
2020-06-07 11:19:53 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/discovery/DiscoveryResource.java
+++ be/src/main/java/io/nzbee/resources/discovery/DiscoveryResource.java
@@ -6,6 +6,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
 import io.nzbee.resources.controllers.BrandController;
 import io.nzbee.resources.controllers.CategoryController;
+import io.nzbee.resources.controllers.CustomerController;
 
 public class DiscoveryResource extends ResourceSupport {
 
@@ -14,6 +15,7 @@ public class DiscoveryResource extends ResourceSupport {
 		this.add(linkTo(methodOn(CategoryController.class).getCategories(locale, currency)).withRel("allCategories"));
 		this.add(linkTo(methodOn(CategoryController.class).getProductCategories(locale, currency)).withRel("productCategories"));
 		this.add(linkTo(methodOn(CategoryController.class).getBrandCategories(locale, currency)).withRel("brandCategories"));
+		this.add(linkTo(methodOn(CustomerController.class).getCustomer(null)).withRel("brandCategories"));
 		this.add(new Link(rootURL + "/oauth/token").withRel("accessTokens"));
 	}
 	
