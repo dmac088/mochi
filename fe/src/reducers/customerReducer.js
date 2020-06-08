028500d8ca769a6ad898f92e9210df8588a17623
dmac088 <danielmackie82@gmail.com>
2020-06-08 11:54:41 +0800

    asdf

--- fe/src/reducers/customerReducer.js
+++ fe/src/reducers/customerReducer.js
@@ -1,22 +1,15 @@
 import {
-  GET_CUSTOMER,
   GET_CUSTOMER_STARTED,
   GET_CUSTOMER_SUCCESS
 } from "../actions/ActionTypes";
 
 const initialState = {
-  customer: {
     loading: false,
-  },
 };
 
-
-
 export default function (state = initialState, action) {
-  console.log(state);
-  console.log(action);
   switch (action.type) {
-    
+
     case GET_CUSTOMER_STARTED:
       return {
         ...state,
