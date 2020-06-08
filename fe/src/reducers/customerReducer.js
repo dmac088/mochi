1d79c0f0e0430a26b31f86427e53f749623fc612
dmac088 <danielmackie82@gmail.com>
2020-06-08 10:29:18 +0800

    asdf

--- fe/src/reducers/customerReducer.js
+++ fe/src/reducers/customerReducer.js
@@ -1,18 +1,43 @@
-import { GET_CUSTOMER } from "../actions/ActionTypes";
+import {
+  GET_CUSTOMER,
+  GET_CUSTOMER_STARTED,
+  GET_CUSTOMER_FINISHED
+} from "../actions/ActionTypes";
 
 const initialState = {
-  customer: {},
+  customer: {
+    loading: false,
+  },
 };
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   switch (action.type) {
     //GET CAPABILITIES
     //OTHER REDUCER ACTIONS.
+    case GET_CUSTOMER_STARTED:
+      return {
+        ...state,
+        customer: {
+          ...state.customer,
+          loading: true,
+        }
+      }
     case GET_CUSTOMER:
       return {
         ...state,
-        ...action.payload,
+        customer: {
+          ...state.customer,
+          ...action.payload.customer
+        }
       };
+    case GET_CUSTOMER_FINISHED:
+      return {
+        ...state,
+        customer: {
+          ...state.customer,
+          loading: true,
+        }
+      }
 
     default:
       return state;
