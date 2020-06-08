2b6ce1f32e330fdcfb8a0b07a2591d93cf56c8ed
dmac088 <danielmackie82@gmail.com>
2020-06-08 11:57:15 +0800

    asdf

--- fe/src/actions/CustomerActions.js
+++ fe/src/actions/CustomerActions.js
@@ -10,10 +10,6 @@ export const findByUserName = dispatch => {
     const { userName, access_token } = state.session.tokens;
     const { href } = state.discovery.links.customer;
 
-    console.log(userName);
-    console.log(access_token);
-    console.log(href);
-
     dispatch(getCustomerStarted());
 
     axios({
@@ -29,32 +25,6 @@ export const findByUserName = dispatch => {
       // });
     });
   }
-
-/*
-  const state = store.getState();
-  const { userName, access_token } = state.session.tokens;
-  const { href } = state.discovery.links.customer;
-  axios({
-    method: 'get',
-    url: href.replace('{username}', userName),
-    headers: {'Authorization': 'Bearer ' + access_token}
-  }).then((payload) => {
-    return payload.data;
-  }).then((customer) => {
-    dispatch({
-      type: GET_CUSTOMER,
-      payload: customer,
-    });
-  }).catch((error) => {
-    dispatch({
-      type: GET_ERROR,
-      payload: error.response,
-    });
-  });
-  */
-}
-
-
 
 const getCustomerStarted = () => ({
   type: GET_CUSTOMER_STARTED
