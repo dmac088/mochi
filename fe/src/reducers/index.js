0ac47f03caa0bf00eba1a72c3bc7365c656e8283
dmac088 <danielmackie82@gmail.com>
2020-06-09 15:11:53 +0800

    asdf

--- fe/src/reducers/index.js
+++ fe/src/reducers/index.js
@@ -2,13 +2,11 @@ import { combineReducers } from "redux";
 import categoryReducer from "./categoryReducer";
 import discoveryReducer from "./discoveryReducer";
 import sessionReducer from "./sessionReducer";
-import errorReducer from "./errorReducer";  
 import customerReducer from "./customerReducer";
 
 export default combineReducers({
   category: categoryReducer,
   discovery: discoveryReducer,
   session: sessionReducer,
-  error: errorReducer,
   customer: customerReducer,
 });
