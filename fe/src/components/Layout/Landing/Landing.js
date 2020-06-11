7b64563b3ffdfa4b07f9831aa6c0dab894f052f7
dmac088 <danielmackie82@gmail.com>
2020-06-11 14:54:39 +0800

    asdf

--- fe/src/components/Layout/Landing/Landing.js
+++ fe/src/components/Layout/Landing/Landing.js
@@ -17,17 +17,6 @@ function Landing(props) {
       <Banner />
       <BestSeller />
       <BrandSlider />
-       <div className="landing">
-        <div className="light-overlay landing-inner text-dark">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Introduction To HATEOAS</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </React.Fragment>
   );
 }
