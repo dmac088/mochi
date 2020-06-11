7145e763a0a56c38429933cbd63247a1a227d907
dmac088 <danielmackie82@gmail.com>
2020-06-11 17:05:57 +0800

    asdf

---
+++ fe/src/components/Layout/Account/Default/Default.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+function Default() {
+    return (
+        <React.Fragment>
+            <h3>Dashboard</h3>
+
+            <div className="welcome"><p>Hello, <strong></strong> (If Not <strong>{customer.givenName} </strong><a href="login-register.html" className="logout"> Logout</a>)</p>
+            </div>
+
+            <p className="mb-0">From your account dashboard. you can easily check &amp; view your
+            recent orders, manage your shipping and billing addresses and edit your
+    password and account details.</p>
+        </React.Fragment>
+    )
+}
---
+++ fe/src/components/Layout/Account/Orders/Orders.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function Orders() {
+    return (
+        <h3>Orders</h3>
+
+        <div className="myaccount-table table-responsive text-center">
+            <table className="table table-bordered">
+                <thead className="thead-light">
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                        <th>Total</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mostarizing Oil</td>
+                        <td>Aug 22, 2018</td>
+                        <td>Pending</td>
+                        <td>$45</td>
+                        <td><a href="cart.html" className="btn">View</a></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Katopeno Altuni</td>
+                        <td>July 22, 2018</td>
+                        <td>Approved</td>
+                        <td>$100</td>
+                        <td><a href="cart.html" className="btn">View</a></td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Murikhete Paris</td>
+                        <td>June 12, 2017</td>
+                        <td>On Hold</td>
+                        <td>$99</td>
+                        <td><a href="cart.html" className="btn">View</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Orders;
