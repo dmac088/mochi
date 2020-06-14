8729a42eb0fede99cd356c6c564db3557934bd46
dmac088 <danielmackie82@gmail.com>
2020-06-14 18:15:18 +0800

    asdf

---
+++ fe/src/components/Layout/Account/Admin/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Admin() {
+    return (
+        <React.Fragment>
+            <h3>Admin</h3>
+
+            <p>Admin.</p>
+
+        </React.Fragment>
+    );
+}
+
+export default Admin;
