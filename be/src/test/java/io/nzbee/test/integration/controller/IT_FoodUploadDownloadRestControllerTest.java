d177367182063c5a36b822332669c80f99bfb23a
dmac088 <danielmackie82@gmail.com>
2020-06-17 09:02:48 +0800

    removed unused imports

--- be/src/test/java/io/nzbee/test/integration/controller/IT_FoodUploadDownloadRestControllerTest.java
+++ be/src/test/java/io/nzbee/test/integration/controller/IT_FoodUploadDownloadRestControllerTest.java
@@ -12,7 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.Spy;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
