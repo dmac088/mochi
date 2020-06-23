6ce667819b81ae19b15ccbd6c834d5cbca6240ab
dmac088 <danielmackie82@gmail.com>
2020-06-23 13:49:54 +0800

    removed exact param matching

--- fe/src/components/Layout/Helpers/Route/History.js
+++ fe/src/components/Layout/Helpers/Route/History.js
@@ -1,8 +1,4 @@
 import { createBrowserHistory } from 'history';
-import { matchPath } from 'react-router'
 
 export const history = createBrowserHistory();
 
-export const { params } = matchPath(history.location.pathname, {
-    path: "/:lang/:curr"
- })
