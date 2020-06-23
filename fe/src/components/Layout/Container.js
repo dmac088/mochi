8ec6065c5bf4e64f93b6c3e8fc2df23bdb2ba73f
dmac088 <danielmackie82@gmail.com>
2020-06-23 14:15:44 +0800

    added side-effect on Container to reinitialize data on language change

--- fe/src/components/Layout/Container.js
+++ fe/src/components/Layout/Container.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
@@ -9,28 +9,43 @@ import { initialize } from "../../actions/DiscoveryActions";
 import { reauthenticate } from "../../actions/SessionActions";
 
 function Container(props) {
-    const { path } = props.match;
+    const { path, params } = props.match;
+    const { lang, curr } = params;
+    const prevParams = usePrevious({lang, curr});
+    console.log(prevParams);    
+
     const dispatch = useDispatch();
+
+    function usePrevious(value) {
+        const ref = useRef();
+        useEffect(() => {
+            ref.current = value;
+        });
+        return ref.current;
+    }
 
     useEffect(() => {
         dispatch(reauthenticate());
+        dispatch(initialize());
     }, []);
 
     //we really only want to reinitialize on a change of locale or currency
     //not on every reload, but worry about this later
     useEffect(() => {
-        dispatch(initialize());
-    });
+        if(prevParams && (lang !== prevParams.lang || curr !== prevParams.curr)) {
+            dispatch(initialize());
+        }
+    }, lang, curr);
 
     return (
         <React.Fragment>
-            <Header 
-                {...props}/>
+            <Header
+                {...props} />
             {isHomePath(path)
-            ? <React.Fragment />
-            : <BreadCrumb 
-                {...props} />}
-                {props.children}
+                ? <React.Fragment />
+                : <BreadCrumb
+                    {...props} />}
+            {props.children}
             <Scroller />
             <Footer />
         </React.Fragment>
