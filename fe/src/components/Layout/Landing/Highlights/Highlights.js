26ed35b5206dbfde2154df1bfe887510d25fe66d
dmac088 <danielmackie82@gmail.com>
2020-06-25 15:10:54 +0800

    added categories to highlights

--- fe/src/components/Layout/Landing/Highlights/Highlights.js
+++ fe/src/components/Layout/Landing/Highlights/Highlights.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Category from './Tabs/Category';
+import { categoryMaster } from './Categories';
 
 function Highlights() {
 
   const categories = useSelector(state => state.categories.list);
 
-  console.log(categories);
+  const renderCategories = (categories) => {
+    if(!categories) { return null; }
+    return categories.filter(c => categoryMaster.includes(c.data.categoryCode)).map(c => {
+      // <a className="nav-item nav-link active" data-toggle="tab" href="#" role="tab" aria-selected="true">
+        return (
+          <a className="nav-item nav-link" data-toggle="tab" href="#" role="tab" aria-selected="true">
+            {c.data.categoryDesc}
+          </a>
+        )
+    }) 
+  }
 
   return (
     <div className="slider tab-slider mb-35">
@@ -16,8 +27,7 @@ function Highlights() {
             <div className="tab-slider-wrapper">
               <nav>
                 <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                  <a className="nav-item nav-link active" data-toggle="tab" href="#" role="tab" aria-selected="true">Category A</a>
-                  <a className="nav-item nav-link" data-toggle="tab" href="#" role="tab" aria-selected="true">Category B</a>
+                  {renderCategories(categories)}
                 </div>
               </nav>
               <div className="tab-content" id="nav-tabContent">
