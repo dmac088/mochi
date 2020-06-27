ebb3d458346bee70c8e312b66eaa7e5fbcb5a87f
dmac088 <danielmackie82@gmail.com>
2020-06-27 22:08:55 +0800

    banner image added to product browsing page

--- fe/src/components/Layout/Products/ShopBanner.js
+++ fe/src/components/Layout/Products/ShopBanner.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import veg01 from '../../../assets/images/categories/VEG01.jpg';
 
  function ShopBanner(props) {
     return (
       <div className="shop-page-banner mb-35">
         <a href="shop-left-sidebar.html">
-          <img src="assets/images/categories/veg01.jpg" className="img-fluid" alt="" />
+          <img src={veg01} className="img-fluid" alt="" />
         </a>
       </div>
     );
