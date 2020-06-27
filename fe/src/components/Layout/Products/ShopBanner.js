1b44da95bb39f95c96536f990d18dbc94247ed42
dmac088 <danielmackie82@gmail.com>
2020-06-27 21:56:20 +0800

    added shopbanner and shopheader

---
+++ fe/src/components/Layout/Products/ShopBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ function ShopBanner(props) {
+    return (
+      <div className="shop-page-banner mb-35">
+        <a href="shop-left-sidebar.html">
+          <img src="assets/images/categories/veg01.jpg" className="img-fluid" alt="" />
+        </a>
+      </div>
+    );
+  }
+
+  export default ShopBanner;
