37adbcd69b672b85a48f8cf8a196fb74ad24f35b
dmac088 <danielmackie82@gmail.com>
2020-06-27 17:28:03 +0800

    formatting

--- fe/src/services/Category/index.js
+++ fe/src/services/Category/index.js
@@ -1,23 +1,24 @@
 
 
-export  const findByCode = (categories, code) => {
-    if(!categories) { return; }
-    return categories.filter( o => o.data.categoryCode === code )[0];
+export const findByCode = (categories, code) => {
+    if (!categories) { return; }
+    return categories.filter(o => o.data.categoryCode === code)[0];
 }
 
-export  const findByDesc = (categories, desc) => {
-    if(!categories) { return; }
-    return categories.filter( o => o.data.categoryDesc === desc )[0];
+export const findByDesc = (categories, desc) => {
+    if (!categories) { return; }
+    return categories.filter(o => o.data.categoryDesc === desc)[0];
 }
 
 export const getChildren = (parent, categories, children) => {
-    if(!categories) { return; }
+    if (!categories) { return; }
+    if (!parent) { return; }
     const c = categories.filter(o => o.data.parentCode === parent.data.categoryCode);
-    if(!c) {return children;}
+    if (!c) { return children; }
     c.map((child) => {
-                      children.push(child);
-                      getChildren(child, categories, children);
-                      });
+        children.push(child);
+        getChildren(child, categories, children);
+    });
 
     return c;
-  }
+}
