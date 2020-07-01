afa69ed0e9ec6edd8e5000d2496ee887bfa0bbc7
dmac088 <danielmackie82@gmail.com>
2020-07-01 10:19:56 +0800

    added removeFacet function

---
+++ fe/src/components/Layout/Products/Sidebars/SelectionSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SelectionSidebar(props) {
+ 
+    return (
+        <React.Fragment>
+             
+        </React.Fragment>
+    )
+}
+
+export default SelectionSidebar;
