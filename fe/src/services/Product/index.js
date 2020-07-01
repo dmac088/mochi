197961a4595f48dfb8aa72e28329cd44d712bbc9
dmac088 <danielmackie82@gmail.com>
2020-07-01 18:03:47 +0800

    asdf

--- fe/src/services/Product/index.js
+++ fe/src/services/Product/index.js
@@ -1,9 +1,10 @@
 
 
-export const newFacet = (facetId, facetName) => {
+export const newFacet = (facetId, facetName, display) => {
     return {
         "type": "EntityFacet",
         "facetingName": facetName,
         "id": facetId,
+        "display": display,
     }
 }
