58312255971ab1c60642a94e44736fc7d4bfe2e0
dmac088 <danielmackie82@gmail.com>
2020-07-05 09:30:36 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/brand/IBrandService.java
+++ be/src/main/java/io/nzbee/domain/brand/IBrandService.java
@@ -2,6 +2,7 @@ package io.nzbee.domain.brand;
 
 import java.util.Set;
 import io.nzbee.domain.ILocalizedService;
+import io.nzbee.search.dto.facet.IFacet;
 
 public interface IBrandService extends ILocalizedService<Brand> {
 	
@@ -9,7 +10,7 @@ public interface IBrandService extends ILocalizedService<Brand> {
 	
 	Brand findByProductCode(String locale, String currency, String productCode);
 
-	Set<Brand> findAll(String locale, String currency, String categoryCode, Set<String> categoryCodes,
-			Set<String> tagCodes, Double maxPrice);
+
+	Set<Brand> findAll(String locale, String currency, String categoryCode, Set<IFacet> selectedFacets);
 	
 }
