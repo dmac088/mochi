13c89176728cca3387c89fbf92054dc058e06167
dmac088 <danielmackie82@gmail.com>
2020-07-05 17:46:51 +0800

    removed unused imports

--- be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntityRepositoryIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntityRepositoryIntegrationTest.java
@@ -3,9 +3,7 @@ package io.nzbee.test.integration.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
 import java.util.List;
-
 import javax.persistence.EntityManager;
 import org.junit.After;
 import org.junit.Before;
@@ -17,14 +15,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.jdbc.SqlConfig.TransactionMode;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import io.nzbee.Globals;
 import io.nzbee.entity.category.product.CategoryProduct;
 import io.nzbee.entity.category.product.ICategoryProductService;
