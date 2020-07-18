aa2c2c2ca56d0b213adf9253c2c7147539b19eb8
dmac088 <danielmackie82@gmail.com>
2020-07-18 16:43:44 +0800

    asdf

--- fe/src/components/Layout/Products/Product/List/ProductList.js
+++ fe/src/components/Layout/Products/Product/List/ProductList.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
-function Product() {
+const images = require.context('../../../../../assets/images/products', true);
+
+function ProductList(props) {
+  const { data } = props.product;
+
   return (
+
     <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
       <div className="gf-product shop-list-view-product">
         <div className="image">
-          <a onClick={(e) => { }} id={product.productUPC} href="#">
+          <a onClick={(e) => { }} id={data.productUPC} href="#">
             <span className="onsale">Sale!</span>
-            <img src={productImagePath + product.productImage} className="img-fluid" alt />
+            <img src={images(`./${data.productImage}`)} className="img-fluid" alt />
           </a>
           <div className="product-hover-icons">
-            <a id={product.productUPC}
-              onClick={setCurrentProductId}
+            <a id={data.productUPC}
+              onClick={() => {}}
               href="#"
               data-tooltip="Quick view"
               data-toggle="modal"
@@ -22,13 +27,13 @@ function Product() {
         </div>
         <div className="product-content">
           <div className="product-categories">
-            <a href="shop-left-sidebar.html">{product.primaryCategoryPath}</a>,<span> </span>
-            <a href="shop-left-sidebar.html">{product.brandDesc}</a>
+            <a href="shop-left-sidebar.html">{data.primaryCategoryPath}</a>,<span> </span>
+            <a href="shop-left-sidebar.html">{data.brandDesc}</a>
           </div>
-          <h3 className="product-title"><a href="single-product.html">{product.productDesc}</a></h3>
+          <h3 className="product-title"><a href="single-data.html">{data.productDesc}</a></h3>
           <div className="price-box mb-20">
-            <span className="main-price">${product.productRetail}</span>
-            <span className="discounted-price">${product.productMarkdown}</span>
+            <span className="main-price">${data.productRetail}</span>
+            <span className="discounted-price">${data.productMarkdown}</span>
           </div>
           <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
           <div className="list-product-icons">
@@ -42,4 +47,4 @@ function Product() {
   )
 }
 
-export default Product;
+export default ProductList;
