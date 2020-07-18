1dbefa1126358d2406adc36adf4d57ba6fecfc5f
dmac088 <danielmackie82@gmail.com>
2020-07-18 17:01:59 +0800

    asdf

--- fe/src/components/Layout/Products/Product/List/ProductList.js
+++ fe/src/components/Layout/Products/Product/List/ProductList.js
@@ -10,13 +10,12 @@ function ProductList(props) {
     <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12">
       <div className="gf-product shop-list-view-product">
         <div className="image">
-          <a onClick={(e) => { }} id={data.productUPC} href="#">
+          <a id={data.productUPC} href="#">
             <span className="onsale">Sale!</span>
             <img src={images(`./${data.productImage}`)} className="img-fluid" alt />
           </a>
           <div className="product-hover-icons">
             <a id={data.productUPC}
-              // onClick={() => {}}
               href="#"
               data-tooltip="Quick view"
               data-toggle="modal"
@@ -37,7 +36,7 @@ function ProductList(props) {
           </div>
           <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
           <div className="list-product-icons">
-            <a onClick={(e) => { }} href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
+            <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
             <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
             <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
           </div>
