15e987eb326a5e0904b97ffc1a4d34493aa7b364
dmac088 <danielmackie82@gmail.com>
2020-07-19 20:32:23 +0800

    started developing the bag reducer

--- fe/src/reducers/bagReducer.js
+++ fe/src/reducers/bagReducer.js
@@ -9,17 +9,20 @@ export default function (state = initialState, action) {
     switch (action.type) {
         case ADD_ITEM:
             return {
-
+                ...state,
+                items: [...state.items, action.item]
             };
 
         case UPDATE_ITEM:
             return {
-
+                ...state,
+                items: [...(state.items.filter(i => i !== action.item)), action.item]
             };
 
         case REMOVE_ITEM:
             return {
-
+                ...state,
+                items: state.items.filter(i => i !== action.item),
             };
             
         case UPDATE_CART_TOTALS:
