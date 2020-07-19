cccc4305d19af0661dbc6a658436d1b8b3e24f4a
dmac088 <danielmackie82@gmail.com>
2020-07-19 20:40:32 +0800

    added bagReducer to root reducer

--- fe/src/reducers/index.js
+++ fe/src/reducers/index.js
@@ -3,10 +3,12 @@ import categoryReducer from "./categoryReducer";
 import discoveryReducer from "./discoveryReducer";
 import sessionReducer from "./sessionReducer";
 import customerReducer from "./customerReducer";
+import bagReducer from "./bagReducer";
 
 export default combineReducers({
   categories: categoryReducer,
   discovery: discoveryReducer,
   session: sessionReducer,
   customer: customerReducer,
+  bag: bagReducer,
 });
