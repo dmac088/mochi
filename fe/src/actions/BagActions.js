ad7c9781770e4bd7cb5acbbcd2531ced098c21f7
dmac088 <danielmackie82@gmail.com>
2020-07-25 15:31:22 +0800

    asdf

--- fe/src/actions/BagActions.js
+++ fe/src/actions/BagActions.js
@@ -1,5 +1,3 @@
-import { instance as axios } from "../components/Layout/Helpers/api/axios";
-//import * as bagService from "../services/Bag/index"
 
 import {
   GET_BAG_ITEMS_STARTED,
@@ -7,35 +5,11 @@ import {
   GET_BAG_ITEMS_FAILURE
 } from "./ActionTypes";
 
-export const getBagItems = (bagItems) => {
-  return (dispatch, getState) => {
-
-    dispatch(getBagItemsStarted());
-    const state = getState();
-    //const bagItems = bagService.getItems();
-
-    axios.post(state.discovery.links.getProducts.href, bagItems.map(i => i.productCode))
-      .then((payload) => {
-        return payload.data._embedded.productResources;
-      }).then((products) => {
-        const items = products.map(p => {
-          return {
-            ...p,
-            'quantity': bagItems.filter(i => i.productCode === p.data.productUPC)[0].quantity,
-          }
-        });
-        dispatch(getBagItemsSuccess(items));
-      }).catch((error) => {
-        dispatch(getBagItemsFailure(error.response));
-      });
-  }
-}
-
-const getBagItemsStarted = () => ({
+export const getBagItemsStarted = () => ({
   type: GET_BAG_ITEMS_STARTED
 });
 
-const getBagItemsSuccess = items => ({
+export const getBagItemsSuccess = items => ({
   type: GET_BAG_ITEMS_SUCCESS,
   payload: {
     items: items,
@@ -43,7 +17,7 @@ const getBagItemsSuccess = items => ({
   }
 });
 
-const getBagItemsFailure = error => ({
+export const getBagItemsFailure = error => ({
   type: GET_BAG_ITEMS_FAILURE,
   payload: {
     error,
