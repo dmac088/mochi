66c9b81e05bd286db67e095a0951465600dcccf8
dmac088 <danielmackie82@gmail.com>
2020-07-26 13:58:11 +0800

    asdf

--- fe/src/components/Layout/Header/Bag/BagMenu.js
+++ fe/src/components/Layout/Header/Bag/BagMenu.js
@@ -5,12 +5,14 @@ import { Link } from "react-router-dom";
 import { slide } from '../../Helpers/Animation/Slide';
 import { getBagPath } from '../../Helpers/Route/Route';
 import { localization } from '../../Localization/Localization';
-
+import { useSelector } from 'react-redux';
 
 function BagMenu(props) {
 
   const { match } = props;
   const { lang } = match.params;
+
+  const bag = useSelector(state => state.bag);
 
   const [stateInContainer, setInContainer] = useState(false);
 
@@ -53,7 +55,7 @@ function BagMenu(props) {
         </div>
         <div className="cart-info d-inline-block">
           <p>{localization[lang]['mybag']}
-						<span>0 {localization[lang]['items']} - $0.00</span>
+          <span>{bag.totalItems} {localization[lang]['items']} - ${bag.totalAmount}</span>
           </p>
         </div>
         </Link>
@@ -64,7 +66,8 @@ function BagMenu(props) {
         onExiting={() => { slide(container, 'slideUp', { duration: 500 }); }}>
           <div className="cart-floating-box" id="cart-floating-box" ref={setContainer}>
             <Accordion 
-              {...props}/>
+              {...props}
+              bag={bag}/>
           </div>
       </Transition>
     </div>
