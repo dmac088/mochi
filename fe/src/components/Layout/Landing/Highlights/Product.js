10938bd70d6960d8ef3855b13e311d04eeee1a09
dmac088 <danielmackie82@gmail.com>
2020-07-26 15:25:06 +0800

    asdf

--- fe/src/components/Layout/Landing/Highlights/Product.js
+++ fe/src/components/Layout/Landing/Highlights/Product.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as bagService from "../../../../services/Bag/index";
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 
 function Product(props) {
