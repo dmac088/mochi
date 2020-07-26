ee743bcdce459ce4bd3b8a02be620588059c3b57
dmac088 <danielmackie82@gmail.com>
2020-07-27 00:28:16 +0800

    asdf

--- fe/src/components/Layout/Landing/Highlights/Column.js
+++ fe/src/components/Layout/Landing/Highlights/Column.js
@@ -3,12 +3,14 @@ import Product from './Product';
 const $ = window.$;
 
 const renderProducts = (props) => {
-  const { products } = props;
+  const { products, category } = props;
   if (!products) { return; }
   return products.map((product, index) => {
     return (
         <Product
+          {...props}
           key={index}
+          category={category}
           product={product}
         />
       )
