dea1b41f977244b9681169f6f5691722ec199318
dmac088 <danielmackie82@gmail.com>
2020-07-26 15:19:39 +0800

    asdf

--- fe/src/components/Layout/Header/Search/Search.js
+++ fe/src/components/Layout/Header/Search/Search.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Typeahead } from 'react-bootstrap-typeahead';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import { render } from 'react-dom';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 import { localization } from '../../Localization/Localization';
 
 function Search(props) {
