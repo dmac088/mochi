5b4760ed5f2218cf46ec8ed238e6fb6991792e85
dmac088 <danielmackie82@gmail.com>
2020-07-29 19:01:51 +0800

    asdf

--- fe/src/components/Layout/Products/Sidebars/Layout/ButtonSidebar.js
+++ fe/src/components/Layout/Products/Sidebars/Layout/ButtonSidebar.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
-const renderItems = (items, modFacet, filterType) => {
-    return items.map((i, index) => {
-        return (
-            <li key={index}>
-                <a onClick={(e) => {
-                    e.preventDefault();
-                    modFacet(i.code, filterType, i.display);
-                }}>{i.display}</a>
-                {/* <Link to={i.path}>
-          {i.name}
-        </Link> */}
-            </li>
-        )
-    })
+const renderItems = (items, modFacet) => {
+  return items.map((i, index) => {
+    return (
+      <li key={index}>
+        <a onClick={(e) => {
+                              e.preventDefault();
+                              modFacet(i);
+                            }}>{`${i.data.desc} (${i.data.count})`}
+        </a>
+      </li>
+    )
+  })
 }
 
 export const ButtonSidebar = (props) => {
-    return (
-        <div className="sidebar">
-            <h3 className="sidebar-title">{props.heading}</h3>
-            <ul className="tag-container">
-                {renderItems(props.items, props.modFacet, props.filterType)}
-            </ul>
-        </div>
-    )
+  return (
+    <div className="sidebar mb-35">
+      <h3 className="sidebar-title">{props.heading}</h3>
+      <ul className="tag-container">
+        {renderItems(props.items, props.modFacet)}
+      </ul>
+    </div>
+  )
 }
+
