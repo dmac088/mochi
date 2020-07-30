b0218f1b3f3fa5f74982cfc94a35fc32c514a04d
dmac088 <danielmackie82@gmail.com>
2020-07-30 09:26:02 +0800

    asdf

--- fe/src/components/Layout/Products/Product/List/ProductList.js
+++ fe/src/components/Layout/Products/Product/List/ProductList.js
@@ -38,8 +38,6 @@ function ProductList(props) {
           <p className="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere esse tempora magnam dolorem tenetur eos eligendi non temporibus qui enim. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, magni.</p>
           <div className="list-product-icons">
             <a href="#" data-tooltip="Add to cart"> <span className="icon_cart_alt" /></a>
-            <a href="#" data-tooltip="Add to wishlist"> <span className="icon_heart_alt" /> </a>
-            <a href="#" data-tooltip="Compare"> <span className="arrow_left-right_alt" /> </a>
           </div>
         </div>
       </div>
