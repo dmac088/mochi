5635f213bc28085d2932a3e9964c0c5c9d2dcd6c
dmac088 <danielmackie82@gmail.com>
2020-07-30 17:41:37 +0800

    asdf

--- fe/src/components/Layout/Header/Language/Language.js
+++ fe/src/components/Layout/Header/Language/Language.js
@@ -3,16 +3,17 @@ import  selector  from './lang/selector';
 import { generatePath } from 'react-router';
 
 function Language(props) {
-    const { match, history } = props; 
+    const { match } = props; 
     const { lang } = match.params;
 
-    const changeLang = (e, match, history) => {
+    const changeLang = (e, props) => {
         e.preventDefault();
+        const { match, history, location } = props;
         const lang = e.currentTarget.id;
         const { path } = match;
-        const newPath = generatePath(path, {    ...match.params,
+        const newPath = generatePath(path,  {    ...match.params,
                                                 lang: lang,
-                                            } );
+                                            }) + location.search;
            
         history.replace(newPath);
     }
@@ -20,8 +21,8 @@ function Language(props) {
     return (
         <li data-testid="language"> <a id="language" href="#">{selector[lang]}<i className="fa fa-chevron-down"></i></a>
             <ul>
-                <li><a href="#" id="en-GB" onClick={(e) => changeLang(e, match, history)}>English</a></li>
-                <li><a href="#" id="zh-HK" onClick={(e) => changeLang(e, match, history)}>Chinese</a></li>
+                <li><a href="#" id="en-GB" onClick={(e) => changeLang(e, props)}>English</a></li>
+                <li><a href="#" id="zh-HK" onClick={(e) => changeLang(e, props)}>Chinese</a></li>
             </ul>
         </li>
     )
