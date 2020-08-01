6fa87cb38e2ea36f41bb77ec4305724ca96d24bf
dmac088 <danielmackie82@gmail.com>
2020-08-01 18:02:36 +0800

    brand upload endpoint now working

---
+++ be/src/main/java/io/nzbee/entity/brand/attribute/IBrandAttributeService.java
@@ -0,0 +1,14 @@
+package io.nzbee.entity.brand.attribute;
+
+import java.util.Optional;
+
+import io.nzbee.entity.ILocalizedDao;
+
+public interface IBrandAttributeService extends ILocalizedDao<BrandAttribute> {
+	
+	Optional<BrandAttribute> getBrandAttributeEN(Long id);
+	
+	Optional<BrandAttribute> getBrandAttributeHK(Long id);
+
+	Optional<BrandAttribute> getBrandAttribute(Long id, String locale);
+}
