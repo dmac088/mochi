4136b4a98bf82600ddd1f9abfa7093315b426e9d
dmac088 <danielmackie82@gmail.com>
2020-08-09 13:29:22 +0800

    model and repository objects added to complement new security tables in DB

---
+++ be/.apt_generated/io/nzbee/security/devicemetadata/DeviceMetadata_.java
@@ -0,0 +1,27 @@
+package io.nzbee.security.devicemetadata;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+import io.nzbee.security.user.devicemetadata.DeviceMetadata;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(DeviceMetadata.class)
+public abstract class DeviceMetadata_ {
+
+	public static volatile SingularAttribute<DeviceMetadata, String> deviceDetails;
+	public static volatile SingularAttribute<DeviceMetadata, Date> lastLoggedIn;
+	public static volatile SingularAttribute<DeviceMetadata, String> location;
+	public static volatile SingularAttribute<DeviceMetadata, Long> id;
+	public static volatile SingularAttribute<DeviceMetadata, Long> userId;
+
+	public static final String DEVICE_DETAILS = "deviceDetails";
+	public static final String LAST_LOGGED_IN = "lastLoggedIn";
+	public static final String LOCATION = "location";
+	public static final String ID = "id";
+	public static final String USER_ID = "userId";
+
+}
+
