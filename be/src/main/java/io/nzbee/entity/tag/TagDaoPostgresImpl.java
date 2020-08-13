b78d392ca4d0e441882573bfbf82a4f72de8d5f8
dmac088 <danielmackie82@gmail.com>
2020-08-13 16:44:13 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/tag/TagDaoPostgresImpl.java
+++ be/src/main/java/io/nzbee/entity/tag/TagDaoPostgresImpl.java
@@ -306,7 +306,7 @@ public class TagDaoPostgresImpl implements ITagDao {
 				"                              || '/' AS text) node " + 
 				"          FROM      mochi.category            AS t " + 
 				"          WHERE     0=0 " + 
-				"          AND t.cat_prnt_cd = :categoryCode " + 
+				"          AND coalesce(t.cat_cd, t.cat_prnt_cd) = :categoryCode " + 
 				"          UNION ALL " + 
 				"          SELECT t.cat_id, " + 
 				"                 t.cat_cd, " + 
@@ -322,15 +322,16 @@ public class TagDaoPostgresImpl implements ITagDao {
 				"          ON     t.cat_prnt_id = d.cat_id )" + 
 				", categories AS " + 
 				"( " + 
-				"          SELECT    s1.node," + 
-				"					 coalesce(s2.cat_id,s1.cat_id) as cat_id, " +
+				"          SELECT    coalesce(s2.cat_id,s1.cat_id) as cat_id, " +
 				"		   			 coalesce(s2.cat_cd,s1.cat_cd) as cat_cd " +
 				"          FROM      descendants s1 " + 
 				"          LEFT JOIN descendants s2 " + 
 				"          ON        s1.node <> s2.node " + 
-				"          AND       LEFT(s2.node, length(s1.node)) = s1.node " + 
+				"          AND       LEFT(s2.node, length(s1.node)) = s1.node " +
 				"		   WHERE 0=0 " + 
-				((hasCategories) ? 	"AND  s1.cat_cd IN (:categoryCodes) " : "") +	
+				((hasCategories) ? 	"AND  s1.cat_cd IN (:categoryCodes) " : "") +
+				"          GROUP BY  coalesce(s2.cat_id,s1.cat_id), " +
+				"		   			 coalesce(s2.cat_cd,s1.cat_cd)" +
 				")" + 
 			"			select t.tag_id,  " + 
 			"				   t.tag_cd, " + 
