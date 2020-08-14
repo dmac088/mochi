24003d93d98132e24911bb5de52d9e4cf1b1bb18
dmac088 <danielmackie82@gmail.com>
2020-08-14 20:29:32 +0800

    added Bag to the model

---
+++ be/.apt_generated/io/nzbee/entity/bag/Bag_.java
@@ -0,0 +1,13 @@
+package io.nzbee.entity.bag;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Bag.class)
+public abstract class Bag_ {
+
+
+
+}
+
---
+++ be/src/main/java/io/nzbee/entity/bag/Bag.java
@@ -0,0 +1,8 @@
+package io.nzbee.entity.bag;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Bag {
+
+}
