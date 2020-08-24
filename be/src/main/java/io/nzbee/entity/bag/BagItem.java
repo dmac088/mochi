f69ce23220cf6acaa95e287aa439893958bbd36f
dmac088 <danielmackie82@gmail.com>
2020-08-24 11:48:27 +0800

    moved quanity from bag attribute to bagitem attribute

--- be/src/main/java/io/nzbee/entity/bag/BagItem.java
+++ be/src/main/java/io/nzbee/entity/bag/BagItem.java
@@ -1,7 +1,10 @@
 package io.nzbee.entity.bag;
 
+import javax.persistence.Column;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import io.nzbee.entity.product.Product;
 
 @Table(name = "bag_item", schema = "mochi")
 public class BagItem {
@@ -9,6 +12,22 @@ public class BagItem {
 	@ManyToOne
 	private Bag bag;
 	
+	@Column(name="prd_id")
+	private Product product;
 	
+	@Column(name="qty")
+	private double quantity;
+	
+	public BagItem(Product p) {
+		this.product = p;
+	}
+	
+	public double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(double quantity) {
+		this.quantity = quantity;
+	}
 	
 }
