184fa71e21c0c9bcd650073652d00d68d7549b23
dmac088 <danielmackie82@gmail.com>
2020-08-24 11:11:37 +0800

    added minimalist bagItem to the domain model

--- be/src/main/java/io/nzbee/domain/bag/BagItem.java
+++ be/src/main/java/io/nzbee/domain/bag/BagItem.java
@@ -1,5 +1,36 @@
 package io.nzbee.domain.bag;
 
+import io.nzbee.domain.product.Product;
+
 public class BagItem {
 
+	private Bag bag;
+	private Product product;
+	private double qty;
+	private BagStatus bagStatus;
+	
+	public BagItem(Bag bag, Product p, int qty) {
+		this.bag = bag;
+		this.product = p;
+		this.qty = qty;
+		bagStatus = BagStatus.NEW;
+	}
+
+	public Bag getBag() {
+		return bag;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public double getQty() {
+		return qty;
+	}
+
+	public BagStatus getBagStatus() {
+		return bagStatus;
+	}
+
+	
 }
