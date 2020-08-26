988637b6881dfc18c4d4b7f62cc6723b10543968
dmac088 <danielmackie82@gmail.com>
2020-08-26 18:03:26 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/bag/item/BagItemMapperImpl.java
+++ be/src/main/java/io/nzbee/entity/bag/item/BagItemMapperImpl.java
@@ -6,6 +6,7 @@ import io.nzbee.domain.bag.Bag;
 import io.nzbee.domain.bag.BagItem;
 import io.nzbee.domain.product.Product;
 import io.nzbee.entity.bag.IBagMapper;
+import io.nzbee.entity.bag.IBagService;
 import io.nzbee.entity.product.IProductMapper;
 import io.nzbee.entity.product.IProductService;
 import java.util.Optional;
@@ -22,6 +23,9 @@ public class BagItemMapperImpl implements IBagItemMapper {
 	@Autowired
 	private IProductService productService;
 	
+	@Autowired
+	private IBagService bagService;
+	
 	@Override
 	public BagItem entityToDo(io.nzbee.entity.bag.item.BagItem e) {
 		Bag b = bagMapper.entityToDo(e.getBag());
@@ -33,7 +37,9 @@ public class BagItemMapperImpl implements IBagItemMapper {
 	@Override
 	public io.nzbee.entity.bag.item.BagItem doToEntity(BagItem d) {
 		Optional<io.nzbee.entity.product.Product> p = productService.findByCode(d.getProduct().getProductUPC());
+		Optional<io.nzbee.entity.bag.Bag> b = bagService.findByCode(d.getBag().getCustomer().getUserName()); 
 		io.nzbee.entity.bag.item.BagItem bi = new io.nzbee.entity.bag.item.BagItem(p.get());
+		bi.setBag(b.get());
 		return bi;
 	}
 
