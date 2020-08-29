34ab8c95cfa6439f41325581fb45f7a51a987f6c
dmac088 <danielmackie82@gmail.com>
2020-08-29 14:09:44 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/dto/product/ProductDTOMapperImpl.java
@@ -0,0 +1,37 @@
+package io.nzbee.dto.product;
+
+import org.springframework.stereotype.Component;
+import io.nzbee.domain.product.Product;
+
+@Component
+public class ProductDTOMapperImpl implements IProductDTOMapper {
+
+	@Override
+	public ProductDTO doToDto(Product d) {
+		ProductDTO pdto = new ProductDTO();
+		
+		//brand
+		pdto.setBrandCode(d.getBrand().getBrandCode());
+		pdto.setBrandDesc(d.getBrand().getBrandDesc());
+		
+		//cateogry
+		pdto.setPrimaryCategoryCode(d.getPrimaryCategory().getCategoryCode());
+		pdto.setPrimaryCategoryDesc(d.getPrimaryCategory().getCategoryDesc());
+		
+		//product 
+		pdto.setProductUPC(d.getProductUPC());
+		pdto.setProductDesc(d.getProductDesc());
+		pdto.setProductLongDesc(d.getProductLongDesc());
+		pdto.setProductMarkdown(d.getProductMarkdown());
+		pdto.setProductRetail(d.getProductRetail());
+		
+		return pdto;
+	}
+
+	@Override
+	public Product dtoToDo(ProductDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
