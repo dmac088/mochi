c25c003d1ad6fd4c1d190b775a0624ad882d0f33
dmac088 <danielmackie82@gmail.com>
2020-08-29 13:39:37 +0800

    added DTO classes for product to preserve the purity of the domain model i.e. no Jackson annotations in the domain model that deal with UI concerns

---
+++ be/src/main/java/io/nzbee/dto/product/IProductDTOMapper.java
@@ -0,0 +1,8 @@
+package io.nzbee.dto.product;
+
+import io.nzbee.domain.product.Product;
+import io.nzbee.dto.IMapper;
+
+public interface IProductDTOMapper extends IMapper<ProductDTO, Product> {
+
+}
--- be/src/main/java/io/nzbee/dto/product/ProductDTO.java
+++ be/src/main/java/io/nzbee/dto/product/ProductDTO.java
@@ -2,6 +2,6 @@ package io.nzbee.dto.product;
 
 //this class sits atop the domain model objects and manages the mapping 
 //between the external application model and the internal domain model
-public class Product {
+public class ProductDTO {
 
 }
