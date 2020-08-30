31e1d44976fd1835e4dce033cd851d90d910cfb3
dmac088 <danielmackie82@gmail.com>
2020-08-30 12:08:18 +0800

    added BagResource and BagResourceAssembler with Bag self link

---
+++ be/src/main/java/io/nzbee/resources/bag/BagResource.java
@@ -0,0 +1,22 @@
+package io.nzbee.resources.bag;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.nzbee.dto.bag.BagDTO;
+
+public class BagResource extends RepresentationModel<BagResource> {
+
+	private final BagDTO data;
+	
+	@JsonCreator
+	public BagResource(@JsonProperty("bag") BagDTO bag) {
+		this.data = bag;
+		
+	}
+	
+	public BagDTO getData() {
+		return data;
+	}
+}
---
+++ be/src/main/java/io/nzbee/resources/bag/BagResourceAssembler.java
@@ -0,0 +1,24 @@
+package io.nzbee.resources.bag;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.stereotype.Component;
+import io.nzbee.dto.bag.BagDTO;
+import io.nzbee.resources.controllers.BagController;
+
+@Component
+public class BagResourceAssembler extends RepresentationModelAssemblerSupport<BagDTO, BagResource> {
+
+	public BagResourceAssembler() {
+		super(BagController.class, BagResource.class);
+	}
+
+
+	@Override
+	public BagResource toModel(BagDTO bag) {
+		BagResource br = new BagResource(bag);
+		br.add(linkTo(methodOn(BagController.class).getCustomerBag(null, null, null)).withSelfRel());
+		return br;
+	}
+
+}
