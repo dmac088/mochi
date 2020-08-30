711c09b85dcffa6fd7c2191e13d6b7bedcc79f73
dmac088 <danielmackie82@gmail.com>
2020-08-30 14:07:28 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/bag/item/BagItemResourceAssembler.java
+++ be/src/main/java/io/nzbee/resources/bag/item/BagItemResourceAssembler.java
@@ -3,7 +3,6 @@ package io.nzbee.resources.bag.item;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
-import io.nzbee.dto.bag.BagDTO;
 import io.nzbee.dto.bag.item.BagItemDTOOut;
 import io.nzbee.resources.controllers.BagController;
 
@@ -18,7 +17,7 @@ public class BagItemResourceAssembler extends RepresentationModelAssemblerSuppor
 	@Override
 	public BagItemResource toModel(BagItemDTOOut bag) {
 		BagItemResource br = new BagItemResource(bag);
-		//br.add(linkTo(methodOn(BagController.class)(null, null, null)).withSelfRel());
+		br.add(linkTo(methodOn(BagController.class).getBagContents(null, null, null)).withSelfRel());
 		return br;
 	}
 
