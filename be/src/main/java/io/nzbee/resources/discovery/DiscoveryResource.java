ef43ce212c792813212683dcdca5e163ec854345
dmac088 <danielmackie82@gmail.com>
2020-08-31 10:05:00 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/discovery/DiscoveryResource.java
+++ be/src/main/java/io/nzbee/resources/discovery/DiscoveryResource.java
@@ -3,6 +3,8 @@ package io.nzbee.resources.discovery;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.RepresentationModel;
+
+import io.nzbee.resources.controllers.BagController;
 import io.nzbee.resources.controllers.BrandController;
 import io.nzbee.resources.controllers.CategoryController;
 import io.nzbee.resources.controllers.CustomerController;
@@ -12,10 +14,10 @@ import io.nzbee.resources.controllers.SearchController;
 public class DiscoveryResource extends RepresentationModel<DiscoveryResource>{
 
 	public DiscoveryResource(String rootURL) {
-		this.add(linkTo(methodOn(BrandController.class).getBrands(null)).withRel(					"getAllBrands"));
+		this.add(linkTo(methodOn(BrandController.class).getBrands(null)).withRel(						"getAllBrands"));
 		this.add(linkTo(methodOn(CategoryController.class).getCategories(null, null)).withRel(			"getAllCategories"));
-		this.add(linkTo(methodOn(CategoryController.class).getProductCategories(null)).withRel(	"getAllProductCategories"));
-		this.add(linkTo(methodOn(CategoryController.class).getBrandCategories(null)).withRel(		"getAllBrandCategories"));
+		this.add(linkTo(methodOn(CategoryController.class).getProductCategories(null)).withRel(			"getAllProductCategories"));
+		this.add(linkTo(methodOn(CategoryController.class).getBrandCategories(null)).withRel(			"getAllBrandCategories"));
 		this.add(linkTo(methodOn(CustomerController.class).getCustomer(null)).withRel(					"getCustomer"));
 		this.add(linkTo(methodOn(CustomerController.class).registerNewCustomer(null, null)).withRel(	"registerCustomer"));
 		this.add(linkTo(methodOn(ProductController.class).getProducts(null, 
@@ -31,6 +33,7 @@ public class DiscoveryResource extends RepresentationModel<DiscoveryResource>{
 																	  ).withRel(						"searchProduct"));
 		this.add(linkTo(methodOn(SearchController.class).getSuggestions(null, null, null)
 																	  ).withRel(						"searchSuggestion"));
+		this.add(linkTo(methodOn(BagController.class).getCustomerBag(null, null, null)).withRel(		"getBag"));
 		this.add(new Link(rootURL + "/oauth/token").withRel(											"accessTokens"));
 		
 	}
