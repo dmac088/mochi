ce8c90607146e507581f9a896dd8bcaffc2c53b1
dmac088 <danielmackie82@gmail.com>
2020-09-19 17:17:41 +0800

    asdf

--- fe/src/components/Layout/Bag/Bag.js
+++ fe/src/components/Layout/Bag/Bag.js
@@ -15,11 +15,11 @@ function Bag() {
         dispatch(bagService.removeItem(e.target.id));
     }
 
-    useEffect(() => {
-        if(!bag.loading && bag.isDone ) {
-            dispatch(bagService.getBagContents());
-        }
-    }, [bag.loading, bag.isDone]);
+    // useEffect(() => {
+    //     if(!bag.loading && bag.isDone ) {
+    //         dispatch(bagService.getBagContents());
+    //     }
+    // }, [bag.loading, bag.isDone]);
 
     const renderCartProducts = (items = []) => {
         return items.map((product, index) => {
