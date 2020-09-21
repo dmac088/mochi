995c601a94c6ad9cce6cacee653a577752a4eb87
dmac088 <danielmackie82@gmail.com>
2020-09-21 13:44:37 +0800

    routing to login page when user tries to add item to cart anonymously

--- fe/src/components/Layout/Account/Account.js
+++ fe/src/components/Layout/Account/Account.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
 import Dashboard from './Dashboard/Dashboard'
 import Auth from '../Login/Auth';
 
