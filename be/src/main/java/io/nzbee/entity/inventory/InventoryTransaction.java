318ee7359d352b2cdfe05eef969aa99e73342a97
dmac088 <danielmackie82@gmail.com>
2020-09-25 19:59:00 +0800

    commented out inventory table mappings in order for hte service to run

--- be/src/main/java/io/nzbee/entity/inventory/InventoryTransaction.java
+++ be/src/main/java/io/nzbee/entity/inventory/InventoryTransaction.java
@@ -3,8 +3,8 @@ package io.nzbee.entity.inventory;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Entity
-@Table(name = "inventory_transaction", schema = "mochi")
+//@Entity
+//@Table(name = "inventory_transaction", schema = "mochi")
 public class InventoryTransaction {
 
 	
