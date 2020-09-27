b0d3c6d2dd073e286b5d0dbce1be5ec462b62d02
dmac088 <danielmackie82@gmail.com>
2020-09-27 21:09:15 +0800

    added InventoryLocation and InventroyType

--- be/src/main/java/io/nzbee/entity/inventory/location/InventoryLocation.java
+++ be/src/main/java/io/nzbee/entity/inventory/location/InventoryLocation.java
@@ -1,10 +1,49 @@
 package io.nzbee.entity.inventory.location;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "inventory_location", schema = "mochi")
 public class InventoryLocation {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="inv_loc_id")
+	private Long locationId;
+	
+	@Column(name="inv_loc_cd")
+	private String locationCode;
+	
+	@Column(name="inv_loc_desc")
+	private String locationDesc;
+
+	public Long getLocationId() {
+		return locationId;
+	}
+
+	public void setLocationId(Long locationId) {
+		this.locationId = locationId;
+	}
+
+	public String getLocationCode() {
+		return locationCode;
+	}
+
+	public void setLocationCode(String locationCode) {
+		this.locationCode = locationCode;
+	}
+
+	public String getLocationDesc() {
+		return locationDesc;
+	}
+
+	public void setLocationDesc(String locationDesc) {
+		this.locationDesc = locationDesc;
+	}
+	
 }
--- be/src/main/java/io/nzbee/entity/inventory/type/InventoryType.java
+++ be/src/main/java/io/nzbee/entity/inventory/type/InventoryType.java
@@ -1,12 +1,38 @@
 package io.nzbee.entity.inventory.type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "inventory_type", schema = "mochi")
+
+@Entity
+@Table(name = "inventory_type", schema = "mochi")
 public class InventoryType {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="inv_trx_typ_id")
+	private Long inventoryTypeId;
 	
+	@Column(name="inv_trx_typ_desc")
+	private String inventoryTypeDesc;
+
+	public Long getInventoryTypeId() {
+		return inventoryTypeId;
+	}
+
+	public void setInventoryTypeId(Long inventoryTypeId) {
+		this.inventoryTypeId = inventoryTypeId;
+	}
+
+	public String getInventoryTypeDesc() {
+		return inventoryTypeDesc;
+	}
+
+	public void setInventoryTypeDesc(String inventoryTypeDesc) {
+		this.inventoryTypeDesc = inventoryTypeDesc;
+	}
 	
 }
