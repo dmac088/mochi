be12eb34c95cf2707f7f36491581b3be13a4e76d
dmac088 <danielmackie82@gmail.com>
2020-09-27 22:41:20 +0800

    added Supplier class for inventory supplier

--- be/src/main/java/io/nzbee/entity/inventory/InventoryTransaction.java
+++ be/src/main/java/io/nzbee/entity/inventory/InventoryTransaction.java
@@ -127,5 +127,4 @@ public class InventoryTransaction {
 		this.inventoryTransactionDate = inventoryTransactionDate;
 	}
 	
-	
 }
--- be/src/main/java/io/nzbee/entity/inventory/supplier/Supplier.java
+++ be/src/main/java/io/nzbee/entity/inventory/supplier/Supplier.java
@@ -1,5 +1,45 @@
 package io.nzbee.entity.inventory.supplier;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Supplier {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="sup_id")
+	private Long supplierId;
+	
+	@Column(name="sup_cd")
+	private String supplierCode;
+	
+	@Column(name="sup_desc")
+	private String supplierDesc;
+
+	public Long getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(Long supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public String getSupplierCode() {
+		return supplierCode;
+	}
+
+	public void setSupplierCode(String supplierCode) {
+		this.supplierCode = supplierCode;
+	}
+
+	public String getSupplierDesc() {
+		return supplierDesc;
+	}
+
+	public void setSupplierDesc(String supplierDesc) {
+		this.supplierDesc = supplierDesc;
+	}
+	
 }
