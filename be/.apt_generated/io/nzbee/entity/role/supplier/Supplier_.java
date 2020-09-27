8f493eeb1e743480d628b3fc48a2001bad2c8901
dmac088 <danielmackie82@gmail.com>
2020-09-27 22:46:39 +0800

    asdf

---
+++ be/.apt_generated/io/nzbee/entity/role/supplier/Supplier_.java
@@ -0,0 +1,13 @@
+package io.nzbee.entity.role.supplier;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Supplier.class)
+public abstract class Supplier_ extends io.nzbee.entity.role.Role_ {
+
+
+
+}
+
--- be/src/main/java/io/nzbee/entity/role/supplier/Supplier.java
+++ be/src/main/java/io/nzbee/entity/role/supplier/Supplier.java
@@ -1,9 +1,13 @@
 package io.nzbee.entity.role.supplier;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import io.nzbee.entity.role.Role;
 
+@Entity
+@Table(name = "supplier", schema = "mochi") 
 public class Supplier extends Role {
-	
 	
 	
 	
