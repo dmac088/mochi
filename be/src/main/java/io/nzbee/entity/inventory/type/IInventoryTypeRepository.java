2e375e8d8115adc1f940b93fb12a38a441a7687d
dmac088 <danielmackie82@gmail.com>
2020-09-28 12:36:18 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/inventory/type/IInventoryTypeRepository.java
+++ be/src/main/java/io/nzbee/entity/inventory/type/IInventoryTypeRepository.java
@@ -1,7 +1,11 @@
 package io.nzbee.entity.inventory.type;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface IInventoryTypeRepository extends CrudRepository<InventoryType, Long>  {
 
+	Optional<InventoryType> findByInventoryTypeCode(String code);
+	
 }
