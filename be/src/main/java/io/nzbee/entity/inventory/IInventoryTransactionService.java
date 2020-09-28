bb028574bd2e133861be19a11741727f5c318c99
dmac088 <danielmackie82@gmail.com>
2020-09-28 11:39:08 +0800

    added necessary boilerplate to enable uploading of inventory from a CSV / TSV file

---
+++ be/src/main/java/io/nzbee/entity/inventory/IInventoryTransactionService.java
@@ -0,0 +1,9 @@
+package io.nzbee.entity.inventory;
+
+import io.nzbee.entity.IService;
+
+public interface IInventoryTransactionService extends IService<InventoryTransaction> {
+
+	
+
+}
--- be/src/main/java/io/nzbee/util/inventory/InventoryMasterService.java
+++ be/src/main/java/io/nzbee/util/inventory/InventoryMasterService.java
@@ -1,5 +1,58 @@
 package io.nzbee.util.inventory;
 
-public class InventoryMasterService {
+import java.io.File;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import io.nzbee.entity.inventory.IInventoryTransactionService;
+import io.nzbee.entity.inventory.InventoryTransaction;
+import io.nzbee.util.FileStorageServiceUpload;
 
+public class InventoryMasterService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(InventoryMasterService.class);
+	
+	@Autowired
+	private IInventoryTransactionService inventoryTransactionService;
+	
+	@Autowired
+    private FileStorageServiceUpload fileStorageServiceUpload;
+	
+	public void writeInventoryTransaction(String fileName) {
+		logger.debug("called writeInventoryTransaction with parameter {} ", fileName);
+		try {
+			File file = fileStorageServiceUpload.loadFileAsResource(fileName).getFile();
+	    	CsvSchema bootstrapSchema = 
+	    			CsvSchema.emptySchema()
+	    				.withHeader()
+	    				.withColumnSeparator('\t')
+	    				.withQuoteChar('"');
+	    	
+	        CsvMapper mapper = new CsvMapper();
+	        MappingIterator<InventoryMasterSchema> readValues =
+	        	mapper.readerFor(InventoryMasterSchema.class).with(bootstrapSchema).readValues(file);
+	        
+	        readValues.readAll().stream().forEach(c -> {
+	        	this.persistInventoryTransactionMaster(c);
+	        });
+	        
+		} catch (IOException e) {
+			logger.error(e.toString());
+		}
+	}
+	
+	public void persistInventoryTransactionMaster(InventoryMasterSchema ims) {
+		logger.debug("called persistBrandMaster() ");
+		
+		InventoryTransaction i = new InventoryTransaction();
+				
+		inventoryTransactionService.save(i);
+		
+	}
+	
+	
 }
