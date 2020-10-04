afa0267dd881b75e88523083accb96d1d77d6f30
dmac088 <danielmackie82@gmail.com>
2020-10-04 12:32:24 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/inventory/location/InventoryLocationServiceImpl.java
+++ be/src/main/java/io/nzbee/entity/inventory/location/InventoryLocationServiceImpl.java
@@ -3,7 +3,9 @@ package io.nzbee.entity.inventory.location;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class InventoryLocationServiceImpl implements IInventoryLocationService {
 
 	@Autowired
