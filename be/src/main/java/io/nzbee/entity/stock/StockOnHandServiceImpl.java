754bfc7910d204a9b157fe7fad07753102607413
dmac088 <danielmackie82@gmail.com>
2020-10-09 14:18:00 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/stock/StockOnHandServiceImpl.java
@@ -0,0 +1,50 @@
+package io.nzbee.entity.stock;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockOnHandServiceImpl implements IStockOnHandService {
+
+	@Autowired
+	private IStockOnHandRepository stockOnHandRepository;
+	
+	@Override
+	public List<StockOnHand> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<StockOnHand> findById(long id) {
+		return stockOnHandRepository.findById(id);
+	}
+
+	@Override
+	public Optional<StockOnHand> findByCode(String code) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(StockOnHand t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(StockOnHand t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(StockOnHand t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
