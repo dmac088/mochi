03734ce2a2853958dc9e4a840e696040659bf47c
dmac088 <danielmackie82@gmail.com>
2020-10-13 00:45:07 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/BeanConfiguration.java
+++ be/src/main/java/io/nzbee/domain/BeanConfiguration.java
@@ -1,11 +1,5 @@
 package io.nzbee.domain;
 
-import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieModule;
-import org.kie.api.runtime.KieContainer;
-import org.kie.internal.io.ResourceFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.nzbee.domain.bag.BagItemServiceImpl;
