8a8d8d5e24d6968d06c97f9a933abdd2dae2df6d
dmac088 <danielmackie82@gmail.com>
2020-10-13 18:12:04 +0800

    removed unused imports

--- be/src/main/java/io/nzbee/resources/controllers/BagController.java
+++ be/src/main/java/io/nzbee/resources/controllers/BagController.java
@@ -1,8 +1,6 @@
 package io.nzbee.resources.controllers;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
