cc279643083b05e7fba16f18376f3b407f0b8b16
dmac088 <danielmackie82@gmail.com>
2020-10-19 19:17:38 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/IT_ProductUploadForUpdateIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_ProductUploadForUpdateIntegrationTest.java
@@ -66,7 +66,7 @@ public class IT_ProductUploadForUpdateIntegrationTest {
 		String path = "src/test/resources";
 		File file = new File(path);
 	
-		pms.writeProductMaster(file.getAbsolutePath() + "/data/product/product_master.tsv");
+		pms.writeProductMaster(file.getAbsolutePath() + "/data/product/update/product_master.tsv");
 	}
 	
 	@Test
