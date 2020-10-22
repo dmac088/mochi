f805dac839b287443c9a162cbc6950e2b0a9e82e
dmac088 <danielmackie82@gmail.com>
2020-10-22 20:01:45 +0800

    added integration test for stock on hand

--- be/src/main/java/io/nzbee/entity/inventory/IInventoryTransactionRepository.java
+++ be/src/main/java/io/nzbee/entity/inventory/IInventoryTransactionRepository.java
@@ -1,8 +1,12 @@
 package io.nzbee.entity.inventory;
 
+import java.util.Set;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface IInventoryTransactionRepository extends CrudRepository<InventoryTransaction, Long> {
+	
+	Set<InventoryTransaction> findByProductProductUPC(String productCode);
 	
 }
 
