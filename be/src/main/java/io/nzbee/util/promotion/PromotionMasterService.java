13b7303affe87824ccbfebd53aac3c38da879059
dmac088 <danielmackie82@gmail.com>
2020-10-25 10:12:45 +0800

    asdf

--- be/src/main/java/io/nzbee/util/promotion/PromotionMasterService.java
+++ be/src/main/java/io/nzbee/util/promotion/PromotionMasterService.java
@@ -21,11 +21,8 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import io.nzbee.Constants;
-import io.nzbee.entity.category.Category;
-import io.nzbee.entity.category.ICategoryService;
-import io.nzbee.entity.category.attribute.CategoryAttribute;
-import io.nzbee.entity.category.brand.CategoryBrand;
-import io.nzbee.entity.category.product.CategoryProduct;
+import io.nzbee.entity.promotion.Promotion;
+import io.nzbee.entity.promotion.IPromotionService;
 import io.nzbee.util.FileStorageServiceUpload;
 
 @Service
@@ -34,14 +31,14 @@ public class PromotionMasterService {
 	private static final Logger logger = LoggerFactory.getLogger(PromotionMasterService.class);
 	
 	@Autowired
-	private ICategoryService categoryService;
+	private IPromotionService promotionService;
 	
     @Autowired
     private FileStorageServiceUpload fileStorageServiceUpload;
 	
     @Transactional
-	public void writeCategoryMaster(String fileName) {
-		logger.debug("called writeCategoryMaster with parameter {} ", fileName);
+	public void writePromotionMaster(String fileName) {
+		logger.debug("called writePromotionMaster with parameter {} ", fileName);
 		try {
 			File file = fileStorageServiceUpload.loadFileAsResource(fileName).getFile();
 	    	CsvSchema bootstrapSchema = 
@@ -55,20 +52,9 @@ public class PromotionMasterService {
 	        	mapper.readerFor(PromotionMasterSchema.class).with(bootstrapSchema).readValues(file);
 	        
 	        readValues.readAll().stream().forEach(c -> {
-	        	this.persistCategoryMaster(c);
+	        	this.persistPromotionMaster(c);
 	        });
 	        
-	        
-	        Set<Category> lc = categoryService.findAll();
-	        lc.stream().forEach(c -> {
-	        	if(!(c.getCategoryParentCode() == null)) {
-	        		Optional<io.nzbee.entity.category.Category> opc = categoryService.findByCode(c.getCategoryParentCode());
-	        		if(opc.isPresent()) {
-	        			c.setCategoryParentId(opc.get().getCategoryId());
-	        			categoryService.save(c);
-	        		}
-	        	}
-	        });
 	        
 	        
 		} catch (IOException e) {
@@ -76,142 +62,39 @@ public class PromotionMasterService {
 		}
 	}
 	
-	public void persistCategoryMaster(PromotionMasterSchema c) {
-		logger.debug("called persistCategoryMaster() ");
+	public void persistPromotionMaster(PromotionMasterSchema pms) {
+		logger.debug("called persistPromotionMaster() ");
 		
-		Category cEN = c.get_CATEGORY_TYPE().equals("productcategory")
-						? mapToProductCategory(c.get_CATEGORY_CODE(),
-											   new Long(c.get_CATEGORY_LEVEL()),
-											   c.get_PARENT_CATEGORY_CODE(),
-											   c.get_CATEGORY_DESC_EN(),
-											   Constants.localeENGB)
-						: mapToBrandCategory(c.get_CATEGORY_CODE(),
-											 c.get_CATEGORY_DESC_EN(),
-											 Constants.localeENGB);
+		Promotion cEN = mapToPromotion(
+						pms.get
+						pms.get_CATEGORY_DESC_HK());
 
-		categoryService.save(cEN);
-		
-		Category cCN =	c.get_CATEGORY_TYPE().equals("productcategory")
-				?  mapToProductCategory(
-						c.get_CATEGORY_CODE(),
-						new Long(c.get_CATEGORY_LEVEL()),
-						c.get_PARENT_CATEGORY_CODE(),
-						c.get_CATEGORY_DESC_HK(),
-						Constants.localeZHHK)
-				:  mapToBrandCategory(
-						c.get_CATEGORY_CODE(),
-						c.get_CATEGORY_DESC_HK(),
-						Constants.localeZHHK);
-
-		categoryService.save(cCN);
+		promotionService.save(cCN);
 	}
 	
-	private CategoryProduct mapToProductCategory(String categoryCode,
-												 Long   categoryLevel,
-												 String parentCategoryCode,
-												 String categoryDesc,
-												 String locale
-												 ) {
-		logger.debug("called mapToProductCategory() ");
+	private Promotion mapToPromotion(String promotionCode,
+									 String promotionDesc) {
+		logger.debug("called mapToProductPromotion() ");
 		
-		Optional<Category> oc = categoryService.findByCode(categoryCode);
+		Optional<Promotion> oc = promotionService.findByCode(promotionCode);
 		
-		CategoryProduct cp = (oc.isPresent()) 
-							 ? (CategoryProduct) Hibernate.unproxy(oc.get())
-							 : new CategoryProduct();
+		PromotionProduct cp = (oc.isPresent()) 
+							 ? (PromotionProduct) Hibernate.unproxy(oc.get())
+							 : new PromotionProduct();
 							 
-		CategoryAttribute ca = new CategoryAttribute();
-		if (oc.isPresent()) {
-			Optional<CategoryAttribute> oca = cp.getAttributes().stream().filter(a -> a.getLclCd().equals(locale)).findFirst();
-			ca = (oca.isPresent()) 
-				? oca.get()
-				: new CategoryAttribute();
-		}
 		
-		cp.setCategoryCode(categoryCode);
-		cp.setCategoryLevel(categoryLevel);
-		cp.setCategoryParentCode(parentCategoryCode);
 		
-		ca.setCategoryDesc(categoryDesc);
+		cp.setPromotionCode(promotionCode);
+		cp.setPromotionLevel(promotionLevel);
+		cp.setPromotionParentCode(parentPromotionCode);
+		
+		ca.setPromotionDesc(promotionDesc);
 		ca.setLclCd(locale);
-		ca.setCategory(cp);
-		cp.addCategoryAttribute(ca);
+		ca.setPromotion(cp);
+		cp.addPromotionAttribute(ca);
 		
 		return cp;
 	}
 	
-	private CategoryBrand mapToBrandCategory(String categoryCode,
-											 String categoryDesc,
-											 String locale
-											) {
-		logger.debug("called mapToBrandCategory() ");
-		
-		Optional<Category> oc = 
-				categoryService.findByCode(locale, categoryCode);
-		
-		CategoryBrand cb = (oc.isPresent()) 
-				 ? (CategoryBrand) oc.get()
-				 : new CategoryBrand();
-
-		CategoryAttribute ca = (oc.isPresent()) 
-				 ? cb.getAttributes().stream().filter(a -> a.getLclCd().equals(locale)).findFirst().get()
-				 : new CategoryAttribute();
-		
-		ca.setCategoryDesc(categoryDesc);
-		ca.setLclCd(locale);
-		ca.setCategory(cb);
-		
-		cb.setCategoryCode(categoryCode);
-		cb.addCategoryAttribute(ca);
-		cb.setCategoryAttribute(ca);
-		
-		return cb;
-	}
-	
-	public void extractCategoryMaster(Resource resource) {
-		logger.debug("called extractCategoryMaster() ");
-		List<PromotionMasterSchema> lpms = new ArrayList<PromotionMasterSchema>();
-	    try {
-		    	List<Category> categoryList = new ArrayList<Category>(categoryService.findAll());
-		    	
-		    	//create a map of categories (full list)
-		    	Map<String, PromotionMasterSchema> map = categoryList
-		    												.stream().collect(Collectors.toMap(c -> c.getCategoryCode(), c -> new PromotionMasterSchema()));
-		 
-		    	lpms.addAll(categoryList.stream().map(c -> {
-		    		
-		    		PromotionMasterSchema cms = map.get(c.getCategoryCode());
-		    		
-				    cms.set_CATEGORY_CODE(c.getCategoryCode());
-				    cms.set_CATEGORY_TYPE(c.getCategoryType().getCategoryTypeCode());
-			    
-				    if(c.getCategoryType().getCategoryTypeDesc().equals(CategoryProduct.class.getSimpleName().toLowerCase())) {
-				    	cms.set_PARENT_CATEGORY_CODE(((CategoryProduct) c).getCategoryParentCode());
-				    	cms.set_CATEGORY_LEVEL(((CategoryProduct) c).getCategoryLevel().toString());
-				    }
-			    	
-				    cms.set_CATEGORY_DESC_EN(c.getCategoryDescENGB());
-				    cms.set_CATEGORY_DESC_HK(c.getCategoryDescZHHK());
-			    	
-				    return cms;
-		    	}).collect(Collectors.toSet()));
-	    	
-		    	CsvMapper mapper = new CsvMapper(); 
-		    	CsvSchema schema = mapper.schemaFor(PromotionMasterSchema.class)
-	        		.withHeader()
-	        		.withColumnSeparator('\t')
-	        		.withQuoteChar('"');
-	        
-		        ObjectWriter myObjectWriter = mapper.writer(schema);
-		        String ow = myObjectWriter.writeValueAsString(lpms);
-		        PrintWriter out = new PrintWriter(resource.getFile().getAbsolutePath());
-		        out.write(ow);
-		        out.flush();
-		        out.close();
-	        
-	    } catch (Exception e) {
-	        logger.error("Error occurred while loading object list from file " + resource.getFilename(), e);
-	    }
-	}
 	
 }
