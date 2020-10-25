c78260f947790251ba2efc4fdb4a6cd8b4233c71
dmac088 <danielmackie82@gmail.com>
2020-10-25 10:12:45 +0800

    asdf

--- be/src/main/java/io/nzbee/util/promotion/PromotionMasterSchema.java
+++ be/src/main/java/io/nzbee/util/promotion/PromotionMasterSchema.java
@@ -1,75 +1,86 @@
 package io.nzbee.util.promotion;
 
+import java.time.LocalDateTime;
 
 public class PromotionMasterSchema {
 	
-	private String CATEGORY_CODE;
+	private String PROMOTION_CODE;
 	
-	private String PARENT_CATEGORY_CODE;
+	private String PROMOTION_DESC;
 	
-	private String CATEGORY_DESC_EN;
+	private LocalDateTime PROMOTION_START_DATE;
 	
-	private String CATEGORY_DESC_HK;
+	private LocalDateTime PROMOTION_END_DATE;
 	
-	private String CATEGORY_TYPE;
+	private Boolean PROMOTION_ACTIVE;
 	
-	private String CATEGORY_LEVEL;
+	private String PROMOTION_MECHANIC_CODE;
 	
-	public String get_CATEGORY_CODE() {
-		return CATEGORY_CODE;
+	private String PROMOTION_TYPE_CODE;
+	
+	
+	public String get_PROMOTION_CODE() {
+		return PROMOTION_CODE;
 	}
 
-	public void set_CATEGORY_CODE(String CATEGORY_CODE) {
-		this.CATEGORY_CODE = CATEGORY_CODE;
+	public void set_PROMOTION_CODE(String PROMOTION_CODE) {
+		this.PROMOTION_CODE = PROMOTION_CODE;
 	}
 	
-	public String get_PARENT_CATEGORY_CODE() {
-		return PARENT_CATEGORY_CODE;
+	public String get_PROMOTION_DESC() {
+		return PROMOTION_DESC;
 	}
 
-	public void set_PARENT_CATEGORY_CODE(String pARENT_CATEGORY_CODE) {
-		PARENT_CATEGORY_CODE = pARENT_CATEGORY_CODE;
+	public void set_PROMOTION_DESC(String pROMOTION_DESC) {
+		PROMOTION_DESC = pROMOTION_DESC;
 	}
 
-	public String get_CATEGORY_DESC_EN() {
-		return CATEGORY_DESC_EN;
+	public LocalDateTime getPROMOTION_START_DATE() {
+		return PROMOTION_START_DATE;
 	}
 
-	public void set_CATEGORY_DESC_EN(String pRIMARY_CATEGORY_DESC_EN) {
-		CATEGORY_DESC_EN = pRIMARY_CATEGORY_DESC_EN;
+	public void setPROMOTION_START_DATE(LocalDateTime pROMOTION_START_DATE) {
+		PROMOTION_START_DATE = pROMOTION_START_DATE;
 	}
 
-	public String get_CATEGORY_DESC_HK() {
-		return CATEGORY_DESC_HK;
+	public LocalDateTime getPROMOTION_END_DATE() {
+		return PROMOTION_END_DATE;
 	}
 
-	public void set_CATEGORY_DESC_HK(String pRIMARY_CATEGORY_DESC_HK) {
-		CATEGORY_DESC_HK = pRIMARY_CATEGORY_DESC_HK;
+	public void setPROMOTION_END_DATE(LocalDateTime pROMOTION_END_DATE) {
+		PROMOTION_END_DATE = pROMOTION_END_DATE;
+	}
+
+	public String getPROMOTION_MECHANIC_CODE() {
+		return PROMOTION_MECHANIC_CODE;
+	}
+
+	public void setPROMOTION_MECHANIC_CODE(String pROMOTION_MECHANIC_CODE) {
+		PROMOTION_MECHANIC_CODE = pROMOTION_MECHANIC_CODE;
+	}
+
+	public String getPROMOTION_TYPE_CODE() {
+		return PROMOTION_TYPE_CODE;
+	}
+
+	public void setPROMOTION_TYPE_CODE(String pROMOTION_TYPE_CODE) {
+		PROMOTION_TYPE_CODE = pROMOTION_TYPE_CODE;
 	}
 	
-	public String get_CATEGORY_TYPE() {
-		return CATEGORY_TYPE;
+	public Boolean getPROMOTION_ACTIVE() {
+		return PROMOTION_ACTIVE;
 	}
 
-	public void set_CATEGORY_TYPE(String cATEGORY_TYPE) {
-		CATEGORY_TYPE = cATEGORY_TYPE;
-	}
-	
-	public String get_CATEGORY_LEVEL() {
-		return CATEGORY_LEVEL;
+	public void setPROMOTION_ACTIVE(Boolean pROMOTION_ACTIVE) {
+		PROMOTION_ACTIVE = pROMOTION_ACTIVE;
 	}
 
-	public void set_CATEGORY_LEVEL(String cATEGORY_LEVEL) {
-		CATEGORY_LEVEL = cATEGORY_LEVEL;
-	}
-	
 	@Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
-        builder.append(", CATEGORY_CODE=").append(CATEGORY_CODE)
+        builder.append(", PROMOTION_CODE=").append(PROMOTION_CODE)
                .append("]");
-        return builder.toString();
-        
+        return builder.toString();    
     }
-	
+
 }
