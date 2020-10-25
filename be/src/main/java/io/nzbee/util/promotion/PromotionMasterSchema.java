3038ca2d6f644b778e9568a006b7c6daf745d956
dmac088 <danielmackie82@gmail.com>
2020-10-25 12:18:34 +0800

    asdf

--- be/src/main/java/io/nzbee/util/promotion/PromotionMasterSchema.java
+++ be/src/main/java/io/nzbee/util/promotion/PromotionMasterSchema.java
@@ -1,6 +1,5 @@
 package io.nzbee.util.promotion;
 
-import java.time.LocalDateTime;
 
 public class PromotionMasterSchema {
 	
@@ -8,11 +7,11 @@ public class PromotionMasterSchema {
 	
 	private String PROMOTION_DESC;
 	
-	private LocalDateTime PROMOTION_START_DATE;
+	private String PROMOTION_START_DATE;
 	
-	private LocalDateTime PROMOTION_END_DATE;
+	private String PROMOTION_END_DATE;
 	
-	private Boolean PROMOTION_ACTIVE;
+	private String PROMOTION_ACTIVE;
 	
 	private String PROMOTION_MECHANIC_CODE;
 	
@@ -31,48 +30,48 @@ public class PromotionMasterSchema {
 		return PROMOTION_DESC;
 	}
 
-	public void set_PROMOTION_DESC(String pROMOTION_DESC) {
-		PROMOTION_DESC = pROMOTION_DESC;
+	public void set_PROMOTION_DESC(String PROMOTION_DESC) {
+		 this.PROMOTION_DESC = PROMOTION_DESC;
 	}
 
-	public LocalDateTime getPROMOTION_START_DATE() {
-		return PROMOTION_START_DATE;
+	public String get_PROMOTION_START_DATE() {
+		return this.PROMOTION_START_DATE;
 	}
 
-	public void setPROMOTION_START_DATE(LocalDateTime pROMOTION_START_DATE) {
-		PROMOTION_START_DATE = pROMOTION_START_DATE;
+	public void set_PROMOTION_START_DATE(String PROMOTION_START_DATE) {
+		 this.PROMOTION_START_DATE = PROMOTION_START_DATE;
 	}
 
-	public LocalDateTime getPROMOTION_END_DATE() {
-		return PROMOTION_END_DATE;
+	public String get_PROMOTION_END_DATE() {
+		return  this.PROMOTION_END_DATE;
 	}
 
-	public void setPROMOTION_END_DATE(LocalDateTime pROMOTION_END_DATE) {
-		PROMOTION_END_DATE = pROMOTION_END_DATE;
+	public void set_PROMOTION_END_DATE(String PROMOTION_END_DATE) {
+		 this.PROMOTION_END_DATE = PROMOTION_END_DATE;
 	}
 
-	public String getPROMOTION_MECHANIC_CODE() {
-		return PROMOTION_MECHANIC_CODE;
+	public String get_PROMOTION_MECHANIC_CODE() {
+		return  this.PROMOTION_MECHANIC_CODE;
 	}
 
-	public void setPROMOTION_MECHANIC_CODE(String pROMOTION_MECHANIC_CODE) {
-		PROMOTION_MECHANIC_CODE = pROMOTION_MECHANIC_CODE;
+	public void set_PROMOTION_MECHANIC_CODE(String PROMOTION_MECHANIC_CODE) {
+		 this.PROMOTION_MECHANIC_CODE = PROMOTION_MECHANIC_CODE;
 	}
 
-	public String getPROMOTION_TYPE_CODE() {
+	public String get_PROMOTION_TYPE_CODE() {
 		return PROMOTION_TYPE_CODE;
 	}
 
-	public void setPROMOTION_TYPE_CODE(String pROMOTION_TYPE_CODE) {
-		PROMOTION_TYPE_CODE = pROMOTION_TYPE_CODE;
+	public void set_PROMOTION_TYPE_CODE(String PROMOTION_TYPE_CODE) {
+		this.PROMOTION_TYPE_CODE = PROMOTION_TYPE_CODE;
 	}
 	
-	public Boolean getPROMOTION_ACTIVE() {
+	public String get_PROMOTION_ACTIVE() {
 		return PROMOTION_ACTIVE;
 	}
 
-	public void setPROMOTION_ACTIVE(Boolean pROMOTION_ACTIVE) {
-		PROMOTION_ACTIVE = pROMOTION_ACTIVE;
+	public void set_PROMOTION_ACTIVE(String PROMOTION_ACTIVE) {
+		this.PROMOTION_ACTIVE = PROMOTION_ACTIVE;
 	}
 
 	@Override
