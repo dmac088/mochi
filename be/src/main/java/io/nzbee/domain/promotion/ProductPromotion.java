e0c7583cef0840fd46af02aaeb00eddc365902a7
dmac088 <danielmackie82@gmail.com>
2020-10-26 20:17:27 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/domain/promotion/ProductPromotion.java
@@ -0,0 +1,7 @@
+package io.nzbee.domain.promotion;
+
+public class ProductPromotion extends Promotion  {
+
+	//the contract on this class includes a Product
+	
+}
---
+++ be/src/main/java/io/nzbee/domain/promotion/Promotion.java
@@ -0,0 +1,5 @@
+package io.nzbee.domain.promotion;
+
+public abstract class Promotion {
+
+}
