ebc50311ebd4e3051b0bcc8050141c7ecb1892a5
dmac088 <danielmackie82@gmail.com>
2020-10-31 21:08:28 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/brand/BrandMapperImpl.java
+++ be/src/main/java/io/nzbee/entity/brand/BrandMapperImpl.java
@@ -2,26 +2,21 @@ package io.nzbee.entity.brand;
 
 import org.springframework.stereotype.Component;
 
+import io.nzbee.domain.brand.Brand;
+
 @Component
 public class BrandMapperImpl implements IBrandMapper {
 
 	@Override
-	public io.nzbee.domain.brand.Brand entityToDo(BrandEntity e, String locale, String currency) {
-
+	public Brand DTOToDo(BrandDTO dto) {
 		io.nzbee.domain.brand.Brand bo = 
 				new io.nzbee.domain.brand.Brand(
-						 e.getBrandCode(),
-						 e.getBrandAttribute().getBrandDesc(),
-						 e.getCount(),
-						 locale);
+						dto.getBrandCode(),
+						dto.getBrandDesc(),
+						dto.getCount(),
+						dto.getLocale());
 	
 		return bo;
-	}
-
-	@Override
-	public io.nzbee.domain.brand.Brand entityToDo(BrandEntity e) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
@@ -29,5 +24,7 @@ public class BrandMapperImpl implements IBrandMapper {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
