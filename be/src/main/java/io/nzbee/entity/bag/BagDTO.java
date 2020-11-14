769d4d28d592abeec984bd17f7acf763ad660103
dmac088 <danielmackie82@gmail.com>
2020-11-14 20:38:36 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/bag/BagDTO.java
+++ be/src/main/java/io/nzbee/entity/bag/BagDTO.java
@@ -3,9 +3,7 @@ package io.nzbee.entity.bag;
 import java.math.BigInteger;
 import java.util.Map;
 import java.util.Set;
-
 import io.nzbee.entity.bag.item.BagItemDTO;
-import io.nzbee.entity.party.person.CustomerDTO;
 
 public class BagDTO {
 	
@@ -14,20 +12,14 @@ public class BagDTO {
 	private Long bagId;
 	
 	private Set<BagItemDTO> bagItems;
-	
-	private CustomerDTO customer;
 
 	public BagDTO(Object[] tuple, Map<String, Integer> aliasToIndexMap) {
 		this.bagId = ((BigInteger) tuple[aliasToIndexMap.get(ID_ALIAS)]).longValue();
-		this.customer = new CustomerDTO(tuple, aliasToIndexMap);
+		
 	}
 
 	public Set<BagItemDTO> getBagItems() {
 		return bagItems;
-	}
-
-	public CustomerDTO getCustomer() {
-		return customer;
 	}
 
 	public Long getBagId() {
