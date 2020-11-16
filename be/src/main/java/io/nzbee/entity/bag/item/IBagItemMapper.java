8a7c57c0dccc4ec8d865aa61f611b9e7d90bdee3
dmac088 <danielmackie82@gmail.com>
2020-11-16 14:34:05 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/bag/item/IBagItemMapper.java
+++ be/src/main/java/io/nzbee/entity/bag/item/IBagItemMapper.java
@@ -1,11 +1,12 @@
 package io.nzbee.entity.bag.item;
 
+import io.nzbee.domain.bag.Bag;
 import io.nzbee.domain.bag.BagItem;
 import io.nzbee.entity.IMapper;
 import io.nzbee.entity.bag.item.BagItemEntity;
 
 public interface IBagItemMapper extends IMapper<BagItem, BagItemEntity, BagItemDTO>{
 
-	//BagItem DTOToDo(String locale, String currency, BagItemDTO e);
+	BagItem DTOToDo(Bag bDo, BagItemDTO dto);
 	
 }
