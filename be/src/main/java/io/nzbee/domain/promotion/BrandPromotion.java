59644e580636d660e07764d577ccb16ee97d3822
dmac088 <danielmackie82@gmail.com>
2020-11-21 12:43:59 +0800

    sadf

--- be/src/main/java/io/nzbee/domain/promotion/BrandPromotion.java
+++
@@ -1,7 +0,0 @@
-package io.nzbee.domain.promotion;
-
-public class BrandPromotion extends Promotion {
-
-	//the contract on this class includes a Brand
-	
-}
--- be/src/main/java/io/nzbee/domain/promotion/ProductPromotion.java
+++
@@ -1,7 +0,0 @@
-package io.nzbee.domain.promotion;
-
-public class ProductPromotion extends Promotion  {
-
-	//the contract on this class includes a Product
-	
-}
