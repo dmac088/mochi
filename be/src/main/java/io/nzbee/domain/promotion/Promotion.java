479b47511b503379ed7749ab05877e425058fd39
dmac088 <danielmackie82@gmail.com>
2020-11-21 12:47:29 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/promotion/Promotion.java
+++ be/src/main/java/io/nzbee/domain/promotion/Promotion.java
@@ -1,5 +1,23 @@
 package io.nzbee.domain.promotion;
 
 public class Promotion {
+	
+	private String promotionCode;
+	
+	private String promotionDesc;
+
+	public Promotion(String promotionCode, String promotionDesc) {
+		super();
+		this.promotionCode = promotionCode;
+		this.promotionDesc = promotionDesc;
+	}
+
+	public String getPromotionCode() {
+		return promotionCode;
+	}
+
+	public String getPromotionDesc() {
+		return promotionDesc;
+	}
 
 }
