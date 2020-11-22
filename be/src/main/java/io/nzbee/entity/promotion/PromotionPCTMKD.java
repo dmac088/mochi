7ff4f488c775ab196a5177f91cb5c380d6a8b0f1
dmac088 <danielmackie82@gmail.com>
2020-11-22 14:09:43 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/promotion/PromotionPCTMKD.java
+++ be/src/main/java/io/nzbee/entity/promotion/PromotionPCTMKD.java
@@ -1,5 +1,26 @@
 package io.nzbee.entity.promotion;
 
-public class PromotionPCTMKD {
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "promotion_pctmkd", schema = "mochi")
+@PrimaryKeyJoinColumn(name = "prm_id")
+@DiscriminatorValue("2")
+public class PromotionPCTMKD extends Promotion {
+
+	@Column(name="mkd_pct")
+	private int mkdPct;
+
+	public int getMkdPct() {
+		return mkdPct;
+	}
+
+	public void setMkdPct(int mkdPct) {
+		this.mkdPct = mkdPct;
+	}
+	
 }
