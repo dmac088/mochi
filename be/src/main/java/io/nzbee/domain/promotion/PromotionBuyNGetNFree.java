5e7e342fe348c13b00942b744785e5878dc9112c
dmac088 <danielmackie82@gmail.com>
2020-11-22 12:29:18 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/promotion/PromotionBuyNGetNFree.java
+++ be/src/main/java/io/nzbee/domain/promotion/PromotionBuyNGetNFree.java
@@ -2,11 +2,11 @@ package io.nzbee.domain.promotion;
 
 import java.util.Date;
 
-public class PromotionBuyNGerNFree extends Promotion {
 
-	
-	
-	public PromotionBuyNGerNFree(String promotionCode, String promotionDesc, Date promotionStartDt, Date promotionEndDt,
+
+public class PromotionBuyNGetNFree extends Promotion {
+
+	public PromotionBuyNGetNFree(String promotionCode, String promotionDesc, Date promotionStartDt, Date promotionEndDt,
 			PromotionType promotionType) {
 		super(promotionCode, promotionDesc, promotionStartDt, promotionEndDt, promotionType);
 		
