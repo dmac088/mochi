2861d916d0e9075f095f72b1c5ab8392a431515e
dmac088 <danielmackie82@gmail.com>
2020-11-22 13:50:11 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/promotion/PromotionBNGNF.java
@@ -0,0 +1,40 @@
+package io.nzbee.entity.promotion;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "promotion_bngnf", schema = "mochi")
+@PrimaryKeyJoinColumn(name = "prm_id")
+@DiscriminatorValue("1")
+public class PromotionBNGNF extends Promotion {
+
+
+	@Column(name="buy_qty")
+	private int buyQty;
+	
+	@Column(name="free_qty")
+	private int freeQty;
+
+	public int getBuyQty() {
+		return buyQty;
+	}
+
+	public void setBuyQty(int buyQty) {
+		this.buyQty = buyQty;
+	}
+
+	public int getFreeQty() {
+		return freeQty;
+	}
+
+	public void setFreeQty(int freeQty) {
+		this.freeQty = freeQty;
+	}
+
+	
+	
+}
