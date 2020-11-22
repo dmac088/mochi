0d151fba44fbfa93a407cd1bb51543d63044988a
dmac088 <danielmackie82@gmail.com>
2020-11-22 10:31:54 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/tag/ITagService.java
+++ be/src/main/java/io/nzbee/domain/tag/ITagService.java
@@ -9,6 +9,4 @@ public interface ITagService extends ILocalizedService<Tag> {
 	Set<Tag> findAll(String locale, String currency, String categoryCode, Set<String> collect, Set<String> collect2,
 			Double maxPrice);
 
-
-
 }
