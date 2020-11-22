b110b59358f2a149c19cc5b3fb85713ef5babf72
dmac088 <danielmackie82@gmail.com>
2020-11-22 14:09:43 +0800

    asdf

---
+++ be/.apt_generated/io/nzbee/entity/promotion/PromotionPCTMKD_.java
@@ -0,0 +1,16 @@
+package io.nzbee.entity.promotion;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PromotionPCTMKD.class)
+public abstract class PromotionPCTMKD_ extends io.nzbee.entity.promotion.Promotion_ {
+
+	public static volatile SingularAttribute<PromotionPCTMKD, Integer> mkdPct;
+
+	public static final String MKD_PCT = "mkdPct";
+
+}
+
