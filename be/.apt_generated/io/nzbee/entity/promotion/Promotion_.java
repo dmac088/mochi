963849a00bc8854299338e3cb8b895fb3a589a80
dmac088 <danielmackie82@gmail.com>
2020-11-27 19:50:25 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/promotion/Promotion_.java
+++ be/.apt_generated/io/nzbee/entity/promotion/Promotion_.java
@@ -12,7 +12,6 @@ public abstract class Promotion_ {
 
 	public static volatile SingularAttribute<Promotion, String> promotionShortDesc;
 	public static volatile SingularAttribute<Promotion, String> promotionLongDesc;
-	public static volatile SingularAttribute<Promotion, PromotionMechanic> promotionType;
 	public static volatile SingularAttribute<Promotion, LocalDateTime> promotionEndDate;
 	public static volatile SingularAttribute<Promotion, String> promotionCode;
 	public static volatile SingularAttribute<Promotion, LocalDateTime> promotionStartDate;
@@ -21,7 +20,6 @@ public abstract class Promotion_ {
 
 	public static final String PROMOTION_SHORT_DESC = "promotionShortDesc";
 	public static final String PROMOTION_LONG_DESC = "promotionLongDesc";
-	public static final String PROMOTION_TYPE = "promotionType";
 	public static final String PROMOTION_END_DATE = "promotionEndDate";
 	public static final String PROMOTION_CODE = "promotionCode";
 	public static final String PROMOTION_START_DATE = "promotionStartDate";
