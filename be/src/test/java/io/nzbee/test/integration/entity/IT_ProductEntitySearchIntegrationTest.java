dbb2900e5f570c3e319d685bec9ce5ca9243951e
dmac088 <danielmackie82@gmail.com>
2020-11-29 22:06:50 +0800

    added unit test for seach result sort order

--- be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntitySearchIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_ProductEntitySearchIntegrationTest.java
@@ -599,10 +599,10 @@ public class IT_ProductEntitySearchIntegrationTest {
 		
 		// then
 		assertThat(pp.getTotalPages())
-		 .isEqualTo(1);
+		 .isEqualTo(7);
 		
 		assertThat(pp.getTotalElements())
-		.isEqualTo(new Long(1));
+		.isEqualTo(new Long(7));
 		
 		assertThat(pp.stream().findFirst().get().getProductUPC()).isEqualTo("30833047");
 		
