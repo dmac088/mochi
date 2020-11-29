5516c0c6621885865da11f9e8c25aa256eb0c23a
dmac088 <danielmackie82@gmail.com>
2020-11-29 13:57:35 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_ProductPromotionMappingUploadForCreateIntegrationTest.java
@@ -0,0 +1,88 @@
+package io.nzbee.test.integration.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.jdbc.SqlConfig.TransactionMode;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.entity.promotion.IPromotionService;
+import io.nzbee.entity.promotion.PromotionEntity;
+import io.nzbee.util.promotion.PromotionMasterService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles(profiles = "tst")
+@SqlGroup({
+		@Sql(scripts = "/database/mochi_schema.sql", config = @SqlConfig(dataSource = "mochiDataSourceOwner", transactionManager = "mochiTransactionManagerOwner", transactionMode = TransactionMode.ISOLATED)),
+		@Sql(scripts = "/database/mochi_data.sql", config = @SqlConfig(dataSource = "mochiDataSource", transactionManager = "mochiTransactionManager", transactionMode = TransactionMode.ISOLATED)) })
+public class IT_ProductPromotionMappingUploadForCreateIntegrationTest {
+
+	@MockBean
+	private JavaMailSender mailSender;
+
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager entityManager;
+
+	@Autowired
+	private PromotionMasterService pms;
+
+	@Autowired
+	private IPromotionService promotionService;
+
+	@Before
+	public void persistANewPromotion() {
+		String path = "src/test/resources";
+		File file = new File(path);
+
+		pms.writePromotionMaster(file.getAbsolutePath() + "/data/promotion/mapping/product_promotion_mapping.tsv");
+	}
+
+	@Test
+	public void whenPromotionUploadedForCreate_thenReturnCorrectlyCreatedPromotion() {
+		// when
+		Optional<PromotionEntity> found = promotionService.findByCode("B2G1F");
+
+		// then
+		assertFound(found);
+	}
+
+
+	private void assertFound(Optional<PromotionEntity> found) {
+		
+		assertNotNull(found);
+		
+		assertTrue(found.isPresent());
+		
+		PromotionEntity cp = (PromotionEntity) found.get();
+		
+		assertThat(cp.getProducts().size())
+		.isEqualTo(2);
+		
+	}
+
+
+	@After
+	public void closeConnection() {
+		entityManager.close();
+	}
+}
