dd4e65d9512c6b0cb92ee5fd42f45d919740b7f9
dmac088 <danielmackie82@gmail.com>
2020-12-05 11:14:17 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/tag/ITagService.java
+++ be/src/main/java/io/nzbee/entity/tag/ITagService.java
@@ -1,5 +1,6 @@
 package io.nzbee.entity.tag;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import io.nzbee.entity.ILocalizedService;
@@ -7,7 +8,7 @@ import io.nzbee.search.ISearchDimensionService;
 
 public interface ITagService  extends ILocalizedService<TagDTO, TagEntity>, ISearchDimensionService<TagDTO> {
 
-	Set<TagDTO> findAll(String locale, String currency, String categoryCode, Set<String> categoryCodes,
+	List<TagDTO> findAll(String locale, String currency, String categoryCode, Set<String> categoryCodes,
 			Set<String> brandCodes, Double maxPrice);
 
 	Optional<TagEntity> findByCode(String tagCode);
