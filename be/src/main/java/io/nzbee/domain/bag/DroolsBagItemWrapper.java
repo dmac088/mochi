9230a6cb67059d017bf4f8003c50b22c617728f4
dmac088 <danielmackie82@gmail.com>
2020-12-11 21:49:35 +0800

    asdf

--- be/src/main/java/io/nzbee/domain/bag/DroolsBagItemWrapper.java
+++ be/src/main/java/io/nzbee/domain/bag/DroolsBagItemWrapper.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 public class DroolsBagItemWrapper {
 	
+	private BagItem bagItem;
+	
 	private int bagItemQuantity;
 	
 	private String bagItemStatus;
@@ -17,13 +19,26 @@ public class DroolsBagItemWrapper {
 	
 	private List<String> promotionCodes;
 	
+	private Boolean inStock;
+	
+	private Boolean errors;
+	
+	private String error;
+	
+	private String customerId;
+	
 	public DroolsBagItemWrapper(BagItem bagItem) {
+		this.bagItem 			= bagItem;
 		this.bagItemQuantity 	= bagItem.getQuantity();
 		this.bagQuantity 		= bagItem.getBag().getTotalQuantity();
 		this.productDesc 		= bagItem.getProduct().getProductDesc();
 		this.markdownPrice 		= bagItem.getProduct().getProductMarkdown();
 		this.bagItemStatus 		= bagItem.getBagItemStatus();
 		this.promotionCodes 	= bagItem.getProduct().getPromotions().stream().map(p -> p.getPromotionCode()).collect(Collectors.toList());
+		this.inStock 			= bagItem.getProduct().isInStock();
+		this.customerId			= bagItem.getBag().getCustomer().getCustomerID();
+		this.errors				= bagItem.isErrors();
+		this.error 				= bagItem.getError();
 	}
 	
 	public int getBagItemQuantity() {
@@ -49,4 +64,29 @@ public class DroolsBagItemWrapper {
 	public List<String> getPromotionCodes() {
 		return this.promotionCodes;
 	}
+	
+	public Boolean isInStock() {
+		return this.inStock;
+	}
+	
+	public Boolean isErrors() {
+		return this.errors;
+	}
+	
+	public void setErrors(Boolean errors) {
+		this.errors = errors;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.bagItem.setError(error);
+	}
+	
+	public String getCustomerId() {
+		return this.customerId;
+	}
+	
 }
