e1f78088b3aeeb55b6f4418fd70f301be88adf01
dmac088 <danielmackie82@gmail.com>
2020-12-14 19:13:48 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/product/basic/PhysicalProductEntity_.java
+++ be/.apt_generated/io/nzbee/entity/product/basic/PhysicalProductEntity_.java
@@ -1,13 +1,22 @@
 package io.nzbee.entity.product.basic;
 
 import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
 @StaticMetamodel(PhysicalProductEntity.class)
 public abstract class PhysicalProductEntity_ extends io.nzbee.entity.product.ProductEntity_ {
 
+	public static volatile SingularAttribute<PhysicalProductEntity, Integer> heightDimension;
+	public static volatile SingularAttribute<PhysicalProductEntity, Integer> lengthDimension;
+	public static volatile SingularAttribute<PhysicalProductEntity, Integer> weightDimension;
+	public static volatile SingularAttribute<PhysicalProductEntity, Integer> widthDimension;
 
+	public static final String HEIGHT_DIMENSION = "heightDimension";
+	public static final String LENGTH_DIMENSION = "lengthDimension";
+	public static final String WEIGHT_DIMENSION = "weightDimension";
+	public static final String WIDTH_DIMENSION = "widthDimension";
 
 }
 
