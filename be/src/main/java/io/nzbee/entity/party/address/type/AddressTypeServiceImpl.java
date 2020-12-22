5aa7409341bfc0b5cf32c936c191546416e1c2e6
dmac088 <danielmackie82@gmail.com>
2020-12-22 15:48:54 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/party/address/type/AddressTypeServiceImpl.java
+++ be/src/main/java/io/nzbee/entity/party/address/type/AddressTypeServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AddressTypeServiceImpl implements IAddressTypeService {
 
 	@Autowired
