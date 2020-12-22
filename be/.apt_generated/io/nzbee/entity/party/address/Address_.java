2cd8c05a73de1f74ce2ca589e2fd17e1e8465f49
dmac088 <danielmackie82@gmail.com>
2020-12-22 11:45:29 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/party/address/Address_.java
+++ be/.apt_generated/io/nzbee/entity/party/address/Address_.java
@@ -10,21 +10,21 @@ import javax.persistence.metamodel.StaticMetamodel;
 public abstract class Address_ {
 
 	public static volatile SingularAttribute<Address, String> addressCountry;
-	public static volatile SingularAttribute<Address, Party> addressParty;
-	public static volatile SingularAttribute<Address, Party> addressType;
 	public static volatile SingularAttribute<Address, String> addressLine1;
 	public static volatile SingularAttribute<Address, String> addressPostCode;
 	public static volatile SingularAttribute<Address, String> addressLine2;
 	public static volatile SingularAttribute<Address, String> addressLine3;
+	public static volatile SingularAttribute<Address, Party> type;
+	public static volatile SingularAttribute<Address, Party> party;
 	public static volatile SingularAttribute<Address, Long> addressId;
 
 	public static final String ADDRESS_COUNTRY = "addressCountry";
-	public static final String ADDRESS_PARTY = "addressParty";
-	public static final String ADDRESS_TYPE = "addressType";
 	public static final String ADDRESS_LINE1 = "addressLine1";
 	public static final String ADDRESS_POST_CODE = "addressPostCode";
 	public static final String ADDRESS_LINE2 = "addressLine2";
 	public static final String ADDRESS_LINE3 = "addressLine3";
+	public static final String TYPE = "type";
+	public static final String PARTY = "party";
 	public static final String ADDRESS_ID = "addressId";
 
 }
