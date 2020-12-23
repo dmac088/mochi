7d4edd0b0adb0e97f27c2d24e1a373af7b81f941
dmac088 <danielmackie82@gmail.com>
2020-12-23 22:26:40 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/bag/IBagDoBeanFactory.java
@@ -0,0 +1,8 @@
+package io.nzbee.test.unit.domain.beans.bag;
+
+import io.nzbee.domain.bag.Bag;
+import io.nzbee.test.unit.domain.beans.IDoBeanFactory;
+
+public interface IBagDoBeanFactory extends IDoBeanFactory<Bag> {
+
+}
