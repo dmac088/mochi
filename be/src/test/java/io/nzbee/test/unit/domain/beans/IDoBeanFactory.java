dc601c42588c9a4238f9d3bef6e61ab508072ae7
dmac088 <danielmackie82@gmail.com>
2020-12-23 22:26:40 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/IDoBeanFactory.java
@@ -0,0 +1,7 @@
+package io.nzbee.test.unit.domain.beans;
+
+public interface IDoBeanFactory<T> {
+
+	T getBean();
+	
+}
--- be/src/test/java/io/nzbee/test/unit/domain/beans/bag/BagDoBeanFactory.java
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/bag/BagDoBeanFactory.java
@@ -1,23 +1,24 @@
-package io.nzbee.test.unit.domain.beans;
+package io.nzbee.test.unit.domain.beans.bag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import io.nzbee.domain.bag.Bag;
+import io.nzbee.test.unit.domain.beans.CustomerDoBeanFactory;
+import io.nzbee.test.unit.domain.beans.IDoBeanFactory;
 
 @Service("ut")
 @Profile(value = "tst")
-public class BagDoBeanFactory {
+public class BagDoBeanFactory implements IDoBeanFactory<Bag> {
 	
 	@Autowired
 	private CustomerDoBeanFactory customerDoBeanFactory;
 	
-	public final Bag getBagDoBean() {
-	
+	@Override
+	public Bag getBean() {
 		Bag bag = new Bag(customerDoBeanFactory.getCustomerDoBean());
 		
 		return bag;
-		
 	}
 	
 }
