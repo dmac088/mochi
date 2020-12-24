38e8610572afb2dec1bc21b835e4b0b3f0333c47
dmac088 <danielmackie82@gmail.com>
2020-12-25 00:51:00 +0800

    adsf

---
+++ be/src/test/java/io/nzbee/test/integration/domain/beans/customer/ICustomerDoBeanFactory.java
@@ -0,0 +1,8 @@
+package io.nzbee.test.integration.domain.beans.customer;
+
+import io.nzbee.domain.customer.Customer;
+import io.nzbee.test.integration.domain.beans.IDoBeanFactory;
+
+public interface ICustomerDoBeanFactory extends IDoBeanFactory<Customer> {
+
+}
