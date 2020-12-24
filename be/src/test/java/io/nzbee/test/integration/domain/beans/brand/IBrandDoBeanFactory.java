046a1d1af33a388fa517c4c5e9a98c5104c4cc96
dmac088 <danielmackie82@gmail.com>
2020-12-24 15:43:23 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/domain/beans/brand/IBrandDoBeanFactory.java
@@ -0,0 +1,8 @@
+package io.nzbee.test.integration.domain.beans.brand;
+
+import io.nzbee.domain.brand.Brand;
+import io.nzbee.test.integration.domain.beans.IDoBeanFactory;
+
+public interface IBrandDoBeanFactory extends IDoBeanFactory<Brand> {
+
+}
---
+++ be/src/test/java/io/nzbee/test/integration/domain/beans/department/IDepartmentDoBeanFactory.java
@@ -0,0 +1,8 @@
+package io.nzbee.test.integration.domain.beans.department;
+
+import io.nzbee.domain.department.Department;
+import io.nzbee.test.integration.domain.beans.IDoBeanFactory;
+
+public interface IDepartmentDoBeanFactory extends IDoBeanFactory<Department> {
+
+}
