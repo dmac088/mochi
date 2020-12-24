03713fabb91de5a32b787f196ef7e5cacb6eb5a2
dmac088 <danielmackie82@gmail.com>
2020-12-24 15:20:10 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/domain/beans/tag/ITagDoBeanFactory.java
@@ -0,0 +1,9 @@
+package io.nzbee.test.integration.domain.beans.tag;
+
+import io.nzbee.domain.tag.Tag;
+import io.nzbee.test.integration.domain.beans.IDoBeanFactory;
+
+public interface ITagDoBeanFactory extends IDoBeanFactory<Tag> {
+
+
+}
