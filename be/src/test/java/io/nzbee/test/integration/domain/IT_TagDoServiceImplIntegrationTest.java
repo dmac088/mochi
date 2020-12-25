a92a1222ffa4fbdcc6b8016c30dd50a590bed338
dmac088 <danielmackie82@gmail.com>
2020-12-25 17:21:25 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/domain/IT_TagDoServiceImplIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/domain/IT_TagDoServiceImplIntegrationTest.java
@@ -25,7 +25,7 @@ import io.nzbee.test.integration.domain.beans.tag.ITagDoBeanFactory;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
-@ActiveProfiles(profiles = "it")
+@ActiveProfiles("it")
 @SqlGroup({
 	@Sql(scripts = "/database/mochi_schema.sql",
 			config = @SqlConfig(dataSource = "mochiDataSourceOwner", 
