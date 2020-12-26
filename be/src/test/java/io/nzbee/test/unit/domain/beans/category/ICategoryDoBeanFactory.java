36e4b09305e88c00d5cbe87c7cfedcbfa756c7c2
dmac088 <danielmackie82@gmail.com>
2020-12-26 20:06:47 +0800

    asdf

--- be/src/test/java/io/nzbee/test/unit/domain/beans/category/ICategoryDoBeanFactory.java
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/category/ICategoryDoBeanFactory.java
@@ -2,11 +2,12 @@ package io.nzbee.test.unit.domain.beans.category;
 
 import io.nzbee.domain.category.BrandCategory;
 import io.nzbee.domain.category.Category;
+import io.nzbee.domain.category.ProductCategory;
 import io.nzbee.test.unit.domain.beans.IDoBeanFactory;
 
 public interface ICategoryDoBeanFactory extends IDoBeanFactory<Category> {
 
-	Category getProductCategoryDoBean();
+	ProductCategory getProductCategoryDoBean();
 
 	BrandCategory getBrandCategoryDoBean();
 
