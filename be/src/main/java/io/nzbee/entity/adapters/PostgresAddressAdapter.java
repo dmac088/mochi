c30883beb72b11bff7124665c7a3ccd9591f0dde
dmac088 <danielmackie82@gmail.com>
2021-01-02 16:57:09 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/adapters/PostgresAddressAdapter.java
+++ be/src/main/java/io/nzbee/entity/adapters/PostgresAddressAdapter.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import io.nzbee.Constants;
 import io.nzbee.domain.customer.address.Address;
@@ -12,6 +13,7 @@ import io.nzbee.entity.party.address.IAddressMapper;
 import io.nzbee.entity.party.address.IPartyAddressService;
 import io.nzbee.entity.party.address.PartyAddressDTO;
 
+@Service
 public class PostgresAddressAdapter implements IAddressPortService {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
