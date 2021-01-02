d19229a3e7eaed03cead3e5dc5066d401607d86a
dmac088 <danielmackie82@gmail.com>
2021-01-02 16:29:03 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/dto/customer/address/CustomerAddressDTOMapperImpl.java
@@ -0,0 +1,29 @@
+package io.nzbee.dto.customer.address;
+
+import org.springframework.stereotype.Component;
+import io.nzbee.domain.customer.address.Address;
+import io.nzbee.dto.customer.CustomerDTOIn;
+
+@Component
+public class CustomerAddressDTOMapperImpl implements ICustomerAddressDTOMapper {
+
+	@Override
+	public CustomerAddressDTOOut doToDto(Address d) {
+		CustomerAddressDTOOut cdo = new CustomerAddressDTOOut();
+		cdo.setAddressLine1(d.getAddressLine1());
+		cdo.setAddressLine2(d.getAddressLine2());
+		cdo.setAddressLine3(d.getAddressLine3());
+		cdo.setCountry(d.getCountry());
+		cdo.setPostCode(d.getPostCode());
+		cdo.setCustomerId(d.getCustomer().getCustomerID());
+		return cdo;
+	}
+
+	@Override
+	public Address dtoToDo(CustomerDTOIn dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ be/src/main/java/io/nzbee/dto/customer/address/CustomerAddressDTOOut.java
@@ -0,0 +1,69 @@
+package io.nzbee.dto.customer.address;
+
+public class CustomerAddressDTOOut {
+
+	private String customerId;
+	
+    private String addressLine1;
+	
+    private String addressLine2;
+    
+    private String addressLine3;
+    
+    private String country;
+
+    private String postCode;
+
+	public CustomerAddressDTOOut() {
+	
+	}
+    
+	public String getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getAddressLine3() {
+		return addressLine3;
+	}
+
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+}
