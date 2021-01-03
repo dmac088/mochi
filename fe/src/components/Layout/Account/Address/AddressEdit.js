eba829ea26926d76862e118a9345e9428d1057c9
dmac088 <danielmackie82@gmail.com>
2021-01-03 22:56:25 +0800

    asdf

--- fe/src/components/Layout/Account/Address/AddressEdit.js
+++ fe/src/components/Layout/Account/Address/AddressEdit.js
@@ -3,36 +3,31 @@ import { Spinner } from '../../../Layout/Helpers/Animation/Spinner';
 
 function AddressEdit(props) {
     const { addressState } = props;
-
-    if(!addressState.address) {
-        return null;
-    }
+    console.log(addressState);
     return (
-        (!addressState.address)
-        ? <Spinner />
-        : <React.Fragment>
+         <React.Fragment>
             <h3>Edit Billing Address</h3>
             <div className="account-details-form">
                 <form action="#">
                     <div className="row">
                         <div className="col-12 mb-30">
-                            <input id="address-line-1" placeholder={addressState.address.addressLine1} type="text" />
+                            <input id="address-line-1" placeholder="Address Line 1" type="text" />
                         </div>
 
                         <div className="col-12 mb-30">
-                            <input id="address-line-2" placeholder={addressState.address.addressLine2} type="text" />
+                            <input id="address-line-2" placeholder="Address Line 2" type="text" />
                         </div>
 
                         <div className="col-12 mb-30">
-                            <input id="address-line-3" placeholder={addressState.address.addressLine3} type="text" />
+                            <input id="address-line-3" placeholder="Address Line 3" type="text" />
                         </div>
 
                         <div className="col-12 mb-30">
-                            <input id="country" placeholder={addressState.address.country} type="text" />
+                            <input id="country" placeholder="Country" type="text" />
                         </div>
 
                         <div className="col-12 mb-30">
-                            <input id="post-code" placeholder={addressState.address.postCode} type="text" />
+                            <input id="post-code" placeholder="Post Code" type="text" />
                         </div>
 
                         <div className="col-12">
