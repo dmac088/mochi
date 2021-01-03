09882f5afe7fe2e7de5f1d8687822ec4ec889390
dmac088 <danielmackie82@gmail.com>
2021-01-03 18:08:07 +0800

    asdf

--- fe/src/components/Layout/Account/Address/Address.js
+++ fe/src/components/Layout/Account/Address/Address.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { instance as axios } from "../../../Layout/Helpers/api/axios";
+import { Spinner } from '../../../Layout/Helpers/Animation/Spinner';
 
 function Address(props) {
     const { customer } = props;
 
     const [stateObject, setObjectState] = useState({
         address: null,
-        loading: false,
+        loading: true,
         isDone: false,
     });
 
@@ -30,35 +31,38 @@ function Address(props) {
     }, [customer.loading, customer.isDone]);
 
     if(!stateObject.loading && !stateObject.isDone) {
+        //spinner
         return null;
     }
 
     return (
-        <React.Fragment>
-            <h3>Default Billing Address</h3>
+        (stateObject.loading)
+        ?   <Spinner />
+        :   <React.Fragment>
+                <h3>Default Billing Address</h3>
 
-            <address>
-                <p><strong>{customer.data.givenName}</strong></p>
-                <p>{stateObject.address.addressLine1} 
-                    <br />{stateObject.address.addressLine2}
-                    <br />{stateObject.address.addressLine3}
-                </p>
-                <p>{stateObject.address.country}</p>
-                <p>{stateObject.address.postCode}</p>
-            
-            <a href="#" className="btn d-inline-block edit-address-btn"><i className="fa fa-edit"></i>Edit Address</a>
-            </address>
+                <address>
+                    <p><strong>{customer.data.givenName}</strong></p>
+                    <p>{stateObject.address.addressLine1} 
+                        <br />{stateObject.address.addressLine2}
+                        <br />{stateObject.address.addressLine3}
+                    </p>
+                    <p>{stateObject.address.country}</p>
+                    <p>{stateObject.address.postCode}</p>
+                
+                <a href="#" className="btn d-inline-block edit-address-btn"><i className="fa fa-edit"></i>Edit Address</a>
+                </address>
 
-            <h3>Default Shipping Address</h3>
+                <h3>Default Shipping Address</h3>
 
-            <address>
-                <p><strong>Alex Tuntuni</strong></p>
-                <p>1355 Market St, Suite 900 <br />San Francisco, CA 94103</p>
-                <p>Mobile: (123) 456-7890</p>
-            
-            <a href="#" className="btn d-inline-block edit-address-btn"><i className="fa fa-edit"></i>Edit Address</a>
-            </address>
-        </React.Fragment>
+                <address>
+                    <p><strong>Alex Tuntuni</strong></p>
+                    <p>1355 Market St, Suite 900 <br />San Francisco, CA 94103</p>
+                    <p>Mobile: (123) 456-7890</p>
+                
+                <a href="#" className="btn d-inline-block edit-address-btn"><i className="fa fa-edit"></i>Edit Address</a>
+                </address>
+            </React.Fragment>
     );
 }
 
