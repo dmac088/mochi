0ffda28312aa79b31cf8e9f21b3b6d160179f3ea
dmac088 <danielmackie82@gmail.com>
2021-01-03 17:31:28 +0800

    asdf

--- fe/src/components/Layout/Account/Address/Address.js
+++ fe/src/components/Layout/Account/Address/Address.js
@@ -1,27 +1,45 @@
-
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { instance as axios } from "../../../Layout/Helpers/api/axios";
 
 function Address(props) {
     const { customer } = props;
 
-    useEffect(() => {
-        let isSubscribed = true;
-        console.log(customer);
-        // axios.get(
-
-        // );
-        return () => (isSubscribed = false);
+    const [stateObject, setObjectState] = useState({
+        address: null,
+        loading: false,
     });
 
+    useEffect(() => {
+        let isSubscribed = true;
+        if(!customer.loading && customer.isDone) {
+            axios.get(customer._links.address.href)
+            .then((response) => {
+                if(isSubscribed) {
+                    console.log(response.data);
+                    setObjectState((prevState) => ({
+                        ...prevState,
+                        address: response.data.data,
+                        loading: false,
+                    }));
+                }
+            });
+        }
+        return () => (isSubscribed = false);
+    }, [customer.loading, customer.isDone]);
 
+    if(!stateObject.address) {
+        return null;
+    }
+
+    console.log(customer);
     return (
         <React.Fragment>
             <h3>Default Billing Address</h3>
 
             <address>
-                <p><strong>Alex Tuntuni</strong></p>
-                <p>1355 Market St, Suite 900 <br />San Francisco, CA 94103</p>
-                <p>Mobile: (123) 456-7890</p>
+                <p><strong>{customer.data.givenName}</strong></p>
+                <p>{stateObject.address.addressLine1} <br />{stateObject.address.addressLine2}</p>
+                <p>{stateObject.address.addressLine3}</p>
             
             <a href="#" className="btn d-inline-block edit-address-btn"><i className="fa fa-edit"></i>Edit Address</a>
             </address>
