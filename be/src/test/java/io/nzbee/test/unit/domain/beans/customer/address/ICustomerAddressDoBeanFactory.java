7c1766f6bbf29bed64daffefdb86f49ce8945d58
dmac088 <danielmackie82@gmail.com>
2021-01-03 13:56:00 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/customer/address/ICustomerAddressDoBeanFactory.java
@@ -0,0 +1,8 @@
+package io.nzbee.test.unit.domain.beans.customer.address;
+
+import io.nzbee.domain.customer.address.Address;
+import io.nzbee.test.unit.domain.beans.IDoBeanFactory;
+
+public interface ICustomerAddressDoBeanFactory extends IDoBeanFactory<Address> {
+
+}
