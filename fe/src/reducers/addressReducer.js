440625f23e93ac661c5a7adfb41a1e7395cd6f6d
dmac088 <danielmackie82@gmail.com>
2021-01-04 10:43:42 +0800

    asdf

---
+++ fe/src/reducers/addressReducer.js
@@ -0,0 +1,41 @@
+import {
+    GET_ADDRESS_STARTED,
+    GET_ADDRESS_SUCCESS,
+    GET_ADDRESS_FAILURE,
+  } from "../actions/ActionTypes";
+  
+  const initialState = {
+      loading: false,
+      isDone: false,
+      error: null,
+  };
+  
+  export default function (state = initialState, action) {
+  
+    switch (action.type) {
+
+      case GET_ADDRESS_STARTED:
+        return {
+          ...state,
+          loading: true,
+      }
+  
+      case GET_ADDRESS_SUCCESS:
+        return {
+          ...state,
+          ...action.payload,
+          loading: false,
+      }
+  
+      case GET_ADDRESS_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload.error
+      }
+  
+      default:
+        return state;
+    }
+  }
+   
