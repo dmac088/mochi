9d49e1044b5f2ce5b914ca8ec3dc465534e7398d
dmac088 <danielmackie82@gmail.com>
2021-01-10 21:12:42 +0800

    asdf

--- be/src/main/java/io/nzbee/security/SimpleCORSFilter.java
+++ be/src/main/java/io/nzbee/security/SimpleCORSFilter.java
@@ -20,30 +20,31 @@ import org.springframework.stereotype.Component;
 
 public class SimpleCORSFilter implements Filter {
 
-@Override
-public void init(FilterConfig fc) throws ServletException {
-}
+	@Override
+	public void init(FilterConfig fc) throws ServletException {
+	}
 
-@Override
-public void doFilter(ServletRequest req, ServletResponse resp,
-    FilterChain chain) throws IOException, ServletException {
-		    HttpServletResponse response = (HttpServletResponse) resp;
-		    HttpServletRequest request = (HttpServletRequest) req;
-		    response.setHeader("Access-Control-Allow-Origin", "*");
-		    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-		    response.setHeader("Access-Control-Max-Age", "3600");
-		    response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Authorization, Content-Type, Cache-Control, X-XSRF-TOKEN");
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) resp;
+		HttpServletRequest request = (HttpServletRequest) req;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"x-requested-with, Authorization, Content-Type, Cache-Control, X-XSRF-TOKEN");
 
-    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-        response.setStatus(HttpServletResponse.SC_OK);
-    } else {
-        chain.doFilter(req, resp);
-    }
+		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
+			chain.doFilter(req, resp);
+		}
 
-}
+	}
 
-@Override
-public void destroy() {
-}
+	@Override
+	public void destroy() {
+	}
 
 }
