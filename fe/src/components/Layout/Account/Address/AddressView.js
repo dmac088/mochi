c203172701ad8105c4d81b9b0aef1d2f7bbd0468
dmac088 <danielmackie82@gmail.com>
2021-01-10 15:42:29 +0800

    asdf

--- fe/src/components/Layout/Account/Address/AddressView.js
+++ fe/src/components/Layout/Account/Address/AddressView.js
@@ -4,23 +4,22 @@ function AddressView(props) {
     const { address, customer, toggleEdit } = props;
 
     return (
-            <React.Fragment>
-                <h3>Default {address.data.addressTypeDesc}</h3>
+        <React.Fragment>
+            <h3>Default {address.data.addressTypeDesc}</h3>
 
-                <address>
-                    <p><strong>{customer.data.givenName} {customer.data.familyName}</strong></p>
-                    <p>{address.data.addressLine1}
-                        <br />{address.data.addressLine2}
-                        <br />{address.data.addressLine3}
-                    </p>
-                    <p>{address.data.country}</p>
-                    <p>{address.data.postCode}</p>
-                    <a href="#" onClick={toggleEdit}
-                        className="btn d-inline-block edit-address-btn">
-                        <i className="fa fa-edit"></i>Edit Address</a>
-                </address>
-
-            </React.Fragment>
+            <address>
+                <p><strong>{customer.data.givenName} {customer.data.familyName}</strong></p>
+                <p>{address.data.addressLine1}
+                    <br />{address.data.addressLine2}
+                    <br />{address.data.addressLine3}
+                </p>
+                <p>{address.data.country}</p>
+                <p>{address.data.postCode}</p>
+                <a onClick={toggleEdit}
+                    className="btn d-inline-block edit-address-btn">
+                    <i className="fa fa-edit"></i>Edit Address</a>
+            </address>
+        </React.Fragment>
     );
 }
 
