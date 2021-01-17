caa1d4224708a0c55af2c095db1f326705b6fa59
dmac088 <danielmackie82@gmail.com>
2021-01-17 12:56:12 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/product/IT_PhysicalProductUploadForCreateIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/product/IT_PhysicalProductUploadForCreateIntegrationTest.java
@@ -1,11 +1,15 @@
-package io.nzbee.test.integration.entity;
+package io.nzbee.test.integration.entity.product;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
 import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,12 +20,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.test.context.jdbc.SqlConfig.TransactionMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import io.nzbee.Constants;
 import io.nzbee.entity.product.IProductService;
@@ -32,16 +34,6 @@ import io.nzbee.util.product.physical.PhysicalProductMasterService;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(profiles = "it")
-@SqlGroup({
-	@Sql(scripts = "/database/mochi_schema.sql",
-			config = @SqlConfig(dataSource = "mochiDataSourceOwner", 
-			transactionManager = "mochiTransactionManagerOwner",
-			transactionMode = TransactionMode.ISOLATED)), 
-	@Sql(scripts = "/database/mochi_data.sql",
-			config = @SqlConfig(dataSource = "mochiDataSource", 
-			transactionManager = "mochiTransactionManager",
-			transactionMode = TransactionMode.ISOLATED))
-})
 public class IT_PhysicalProductUploadForCreateIntegrationTest {
 	
 	@MockBean
@@ -56,8 +48,30 @@ public class IT_PhysicalProductUploadForCreateIntegrationTest {
     
     @Autowired
     private IProductService productService;
+    
+	@Autowired
+	@Qualifier("mochiDataSourceOwner")
+	private DataSource database;
+	
+	private static boolean setUpIsDone = false;
 	
 	@Before
+	public void setUp() {
+		if (setUpIsDone) {
+			return;
+		}
+		try (Connection con = database.getConnection()) {
+			ScriptUtils.executeSqlScript(con, new ClassPathResource("/database/mochi_schema.sql"));
+			ScriptUtils.executeSqlScript(con, new ClassPathResource("/database/mochi_data.sql"));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.persistANewProduct();
+		setUpIsDone = true;
+	}
+	
+	
 	public void persistANewProduct() {
 		String path = "src/test/resources";
 		File file = new File(path);
