96fb96292153c9d7861c190287f1ebdbc5efc689
dmac088 <danielmackie82@gmail.com>
2021-01-18 09:20:36 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/controllers/ProductController.java
+++ be/src/main/java/io/nzbee/resources/controllers/ProductController.java
@@ -5,12 +5,10 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-
 import org.apache.commons.compress.utils.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.CollectionModel;
