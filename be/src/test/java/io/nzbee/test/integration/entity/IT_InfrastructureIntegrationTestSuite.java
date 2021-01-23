a057ef2ea87db368b5ab3d7b953aae0ae393da50
dmac088 <danielmackie82@gmail.com>
2021-01-23 15:37:35 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/entity/IT_InfrastructureIntegrationTestSuite.java
@@ -0,0 +1,11 @@
+package io.nzbee.test.integration.entity;
+
+import org.junit.runner.RunWith;
+import com.googlecode.junittoolbox.SuiteClasses;
+import com.googlecode.junittoolbox.WildcardPatternSuite;
+
+@RunWith(WildcardPatternSuite.class)
+@SuiteClasses({ "**/*Test.class" })
+public class IT_InfrastructureIntegrationTestSuite {
+
+}
