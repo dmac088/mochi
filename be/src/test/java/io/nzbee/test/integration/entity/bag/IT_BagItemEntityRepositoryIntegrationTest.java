61d2d8c199c27fb989d77a2c21ab123983752976
dmac088 <danielmackie82@gmail.com>
2021-01-23 18:33:59 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/bag/IT_BagItemEntityRepositoryIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/bag/IT_BagItemEntityRepositoryIntegrationTest.java
@@ -20,6 +20,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -41,6 +42,7 @@ import io.nzbee.test.integration.entity.beans.bag.IBagEntityBeanFactory;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(profiles = "it")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class IT_BagItemEntityRepositoryIntegrationTest {
 	
 	@Autowired
