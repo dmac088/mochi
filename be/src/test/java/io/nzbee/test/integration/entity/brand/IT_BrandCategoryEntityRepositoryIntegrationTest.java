ecfcad7bfc11bff80dc9adf4feefecd41a0550b3
dmac088 <danielmackie82@gmail.com>
2021-01-23 16:31:53 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/brand/IT_BrandCategoryEntityRepositoryIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/brand/IT_BrandCategoryEntityRepositoryIntegrationTest.java
@@ -3,13 +3,11 @@ package io.nzbee.test.integration.entity.brand;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,10 +26,6 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.jdbc.SqlGroup;
-import org.springframework.test.context.jdbc.SqlConfig.TransactionMode;
 import org.springframework.test.context.junit4.SpringRunner;
 import io.nzbee.domain.product.Product;
 import io.nzbee.entity.category.CategoryEntity;
@@ -44,16 +38,6 @@ import io.nzbee.test.integration.entity.beans.category.CategoryEntityBeanFactory
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(profiles = "it")
-@SqlGroup({
-	@Sql(scripts = "/database/mochi_schema.sql",
-			config = @SqlConfig(dataSource = "mochiDataSourceOwner", 
-			transactionManager = "mochiTransactionManagerOwner",
-			transactionMode = TransactionMode.ISOLATED)), 
-	@Sql(scripts = "/database/mochi_data.sql",
-			config = @SqlConfig(dataSource = "mochiDataSource", 
-			transactionManager = "mochiTransactionManager",
-			transactionMode = TransactionMode.ISOLATED))
-})
 public class IT_BrandCategoryEntityRepositoryIntegrationTest {
  
 	@TestConfiguration
