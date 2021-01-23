3d287913ae3db31c71601e2bd6b3509156ab6547
dmac088 <danielmackie82@gmail.com>
2021-01-23 15:19:17 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/entity/suites/IT_InfrastructureIntegrationTestSuite.java
@@ -0,0 +1,13 @@
+package io.nzbee.test.integration.entity.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import io.nzbee.test.integration.entity.product.IT_PhysicalProductUploadForCreateIntegrationTest;
+
+@RunWith(Suite.class)
+@SuiteClasses(IT_PhysicalProductUploadForCreateIntegrationTest.class)
+public class IT_InfrastructureIntegrationTestSuite {
+
+}
