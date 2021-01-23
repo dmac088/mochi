0bdbd53d588967fd9fcb4900a0de091cfef6ec91
dmac088 <danielmackie82@gmail.com>
2021-01-23 18:51:15 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/brand/IT_BrandCategoryEntityRepositoryIntegrationTest.java
+++ be/src/test/java/io/nzbee/test/integration/entity/brand/IT_BrandCategoryEntityRepositoryIntegrationTest.java
@@ -23,7 +23,6 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -38,7 +37,6 @@ import io.nzbee.test.integration.entity.beans.category.CategoryEntityBeanFactory
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles(profiles = "it")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class IT_BrandCategoryEntityRepositoryIntegrationTest {
  
 	@TestConfiguration
