8955f8dab0913369cc0576a1033300d06ab37d2c
dmac088 <danielmackie82@gmail.com>
2021-01-27 14:51:47 +0800

    asdf

---
+++ be/.apt_generated/io/nzbee/entity/postage/destination/PostageDestination_.java
@@ -0,0 +1,13 @@
+package io.nzbee.entity.postage.destination;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PostageDestination.class)
+public abstract class PostageDestination_ {
+
+
+
+}
+
---
+++ be/src/main/java/io/nzbee/entity/postage/destination/PostageDestination.java
@@ -0,0 +1,14 @@
+package io.nzbee.entity.postage.destination;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name="prostage_destination")
+//public class PostageDestination {
+//
+//	private Long postageDestinationId;
+//	
+//	
+//	
+//}
