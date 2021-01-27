5ffc10a6b5e928f33957c863d5034d6814aabd79
dmac088 <danielmackie82@gmail.com>
2021-01-27 18:42:39 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/product/shipping/ShippingProductEntity_.java
+++ be/.apt_generated/io/nzbee/entity/product/shipping/ShippingProductEntity_.java
@@ -1,5 +1,7 @@
 package io.nzbee.entity.product.shipping;
 
+import io.nzbee.entity.postage.type.PostageTypeEntity;
+import io.nzbee.entity.postage.zone.PostageZoneEntity;
 import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -11,12 +13,16 @@ public abstract class ShippingProductEntity_ extends io.nzbee.entity.product.Pro
 	public static volatile SingularAttribute<ShippingProductEntity, String> cityName;
 	public static volatile SingularAttribute<ShippingProductEntity, String> countryCode;
 	public static volatile SingularAttribute<ShippingProductEntity, String> postCode;
+	public static volatile SingularAttribute<ShippingProductEntity, PostageTypeEntity> postageType;
 	public static volatile SingularAttribute<ShippingProductEntity, String> countryName;
+	public static volatile SingularAttribute<ShippingProductEntity, PostageZoneEntity> postageZone;
 
 	public static final String CITY_NAME = "cityName";
 	public static final String COUNTRY_CODE = "countryCode";
 	public static final String POST_CODE = "postCode";
+	public static final String POSTAGE_TYPE = "postageType";
 	public static final String COUNTRY_NAME = "countryName";
+	public static final String POSTAGE_ZONE = "postageZone";
 
 }
 
