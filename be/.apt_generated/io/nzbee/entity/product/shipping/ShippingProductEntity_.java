131e51aa2bf86accd201b081076df0e6b33ae3ec
dmac088 <danielmackie82@gmail.com>
2021-01-27 18:46:12 +0800

    asdf

--- be/.apt_generated/io/nzbee/entity/product/shipping/ShippingProductEntity_.java
+++ be/.apt_generated/io/nzbee/entity/product/shipping/ShippingProductEntity_.java
@@ -1,5 +1,9 @@
 package io.nzbee.entity.product.shipping;
 
+import io.nzbee.entity.postage.customs.CustomsFormEntity;
+import io.nzbee.entity.postage.destination.PostageDestinationEntity;
+import io.nzbee.entity.postage.insurance.PostageInsuranceEntity;
+import io.nzbee.entity.postage.size.PostageSizeEntity;
 import io.nzbee.entity.postage.type.PostageTypeEntity;
 import io.nzbee.entity.postage.zone.PostageZoneEntity;
 import javax.annotation.Generated;
@@ -10,18 +14,26 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(ShippingProductEntity.class)
 public abstract class ShippingProductEntity_ extends io.nzbee.entity.product.ProductEntity_ {
 
+	public static volatile SingularAttribute<ShippingProductEntity, PostageSizeEntity> postageSize;
 	public static volatile SingularAttribute<ShippingProductEntity, String> cityName;
+	public static volatile SingularAttribute<ShippingProductEntity, CustomsFormEntity> customsForm;
+	public static volatile SingularAttribute<ShippingProductEntity, PostageInsuranceEntity> postageInsurance;
 	public static volatile SingularAttribute<ShippingProductEntity, String> countryCode;
 	public static volatile SingularAttribute<ShippingProductEntity, String> postCode;
 	public static volatile SingularAttribute<ShippingProductEntity, PostageTypeEntity> postageType;
 	public static volatile SingularAttribute<ShippingProductEntity, String> countryName;
+	public static volatile SingularAttribute<ShippingProductEntity, PostageDestinationEntity> postageDestination;
 	public static volatile SingularAttribute<ShippingProductEntity, PostageZoneEntity> postageZone;
 
+	public static final String POSTAGE_SIZE = "postageSize";
 	public static final String CITY_NAME = "cityName";
+	public static final String CUSTOMS_FORM = "customsForm";
+	public static final String POSTAGE_INSURANCE = "postageInsurance";
 	public static final String COUNTRY_CODE = "countryCode";
 	public static final String POST_CODE = "postCode";
 	public static final String POSTAGE_TYPE = "postageType";
 	public static final String COUNTRY_NAME = "countryName";
+	public static final String POSTAGE_DESTINATION = "postageDestination";
 	public static final String POSTAGE_ZONE = "postageZone";
 
 }
