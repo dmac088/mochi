008413324113e37656baa3d2ccc428af7d3f7049
dmac088 <danielmackie82@gmail.com>
2021-02-15 00:46:44 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/entity/product/shipping/destination/IShippingDestinationRepository.java
@@ -0,0 +1,8 @@
+package io.nzbee.entity.product.shipping.destination;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface IShippingDestinationRepository extends CrudRepository<ShippingDestinationEntity, Long> {
+		
+}
+
