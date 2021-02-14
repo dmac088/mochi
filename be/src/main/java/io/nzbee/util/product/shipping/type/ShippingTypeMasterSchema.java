9b40b642b5ad0c2445c4c5bc90767fed6f1ef8dc
dmac088 <danielmackie82@gmail.com>
2021-02-14 16:20:08 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/util/product/shipping/type/ShippingTypeMasterSchema.java
@@ -0,0 +1,45 @@
+package io.nzbee.util.product.shipping.type;
+
+public class ShippingTypeMasterSchema {
+
+	private String SHIPPING_TYPE_CODE;
+	
+	private String SHIPPING_TYPE_DESC_EN;
+	
+	private String SHIPPING_TYPE_DESC_HK;
+	
+	private String SHIPPING_TYPE_DESC_CN;
+
+	public String get_SHIPPING_TYPE_CODE() {
+		return SHIPPING_TYPE_CODE;
+	}
+
+	public void set_SHIPPING_TYPE_CODE(String sHIPPING_TYPE_CODE) {
+		SHIPPING_TYPE_CODE = sHIPPING_TYPE_CODE;
+	}
+
+	public String get_SHIPPING_TYPE_DESC_EN() {
+		return SHIPPING_TYPE_DESC_EN;
+	}
+
+	public void set_SHIPPING_TYPE_DESC_EN(String sHIPPING_TYPE_DESC_EN) {
+		SHIPPING_TYPE_DESC_EN = sHIPPING_TYPE_DESC_EN;
+	}
+
+	public String get_SHIPPING_TYPE_DESC_HK() {
+		return SHIPPING_TYPE_DESC_HK;
+	}
+
+	public void set_SHIPPING_TYPE_DESC_HK(String sHIPPING_TYPE_DESC_HK) {
+		SHIPPING_TYPE_DESC_HK = sHIPPING_TYPE_DESC_HK;
+	}
+
+	public String get_SHIPPING_TYPE_DESC_CN() {
+		return SHIPPING_TYPE_DESC_CN;
+	}
+
+	public void set_SHIPPING_TYPE_DESC_CN(String sHIPPING_TYPE_DESC_CN) {
+		SHIPPING_TYPE_DESC_CN = sHIPPING_TYPE_DESC_CN;
+	}
+	
+}
