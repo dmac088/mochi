5106c3de832f3460a6553ffbeacc0367c41092a7
dmac088 <danielmackie82@gmail.com>
2021-02-15 17:09:31 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/shipping/destination/IShippingDestinationRepository.java
+++ be/src/main/java/io/nzbee/entity/product/shipping/destination/IShippingDestinationRepository.java
@@ -1,8 +1,11 @@
 package io.nzbee.entity.product.shipping.destination;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface IShippingDestinationRepository extends CrudRepository<ShippingDestinationEntity, Long> {
 		
+	Optional<ShippingDestinationEntity> findByAttributesLclCdAndAttributesShippingDestinationDesc(String locale, String shippingDestinationDesc);
 }
 
+ 
