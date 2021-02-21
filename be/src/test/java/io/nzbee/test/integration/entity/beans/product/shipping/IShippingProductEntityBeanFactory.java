c3859a3a1c3897b57767e69e9ef61a76452a30c0
dmac088 <danielmackie82@gmail.com>
2021-02-21 15:35:36 +0800

    asdf

--- be/src/test/java/io/nzbee/test/integration/entity/beans/product/shipping/IShippingProductEntityBeanFactory.java
+++ be/src/test/java/io/nzbee/test/integration/entity/beans/product/shipping/IShippingProductEntityBeanFactory.java
@@ -1,8 +1,8 @@
 package io.nzbee.test.integration.entity.beans.product.shipping;
 
-import io.nzbee.entity.product.ProductEntity;
+import io.nzbee.entity.product.shipping.ShippingProductEntity;
 import io.nzbee.test.integration.entity.beans.IEntityBeanFactory;
 
-public interface IShippingProductEntityBeanFactory extends IEntityBeanFactory<ProductEntity> {
+public interface IShippingProductEntityBeanFactory extends IEntityBeanFactory<ShippingProductEntity> {
 
 }
