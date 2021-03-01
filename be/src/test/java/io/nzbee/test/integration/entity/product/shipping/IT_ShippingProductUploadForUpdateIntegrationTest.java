418a5811f26aa7ddf2335424be372c95c294745d
dmac088 <danielmackie82@gmail.com>
2021-03-01 19:48:06 +0800

    asdf

---
+++ be/src/test/java/io/nzbee/test/integration/entity/product/shipping/IT_ShippingProductUploadForUpdateIntegrationTest.java
@@ -0,0 +1,121 @@
+package io.nzbee.test.integration.entity.product.shipping;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import io.nzbee.Constants;
+import io.nzbee.entity.product.shipping.IShippingProductService;
+import io.nzbee.entity.product.shipping.ShippingProductDTO;
+import io.nzbee.util.product.shipping.ShippingProductMasterService;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles(profiles = "it")
+public class IT_ShippingProductUploadForUpdateIntegrationTest {
+
+	@MockBean
+	private JavaMailSender mailSender;
+
+	@Autowired
+	@Qualifier("mochiEntityManagerFactory")
+	private EntityManager entityManager;
+
+	@Autowired
+	private ShippingProductMasterService pms;
+
+	@Autowired
+	private IShippingProductService shippingProductService;
+	
+	@Autowired
+    @Qualifier("mochiDataSourceOwner")
+    private DataSource database;
+	
+	private static boolean setUpIsDone = false;
+
+    @Before
+	public void setUp() {
+    	if (setUpIsDone) {
+            return;
+        }
+    	try (Connection con = database.getConnection()) {
+            ScriptUtils.executeSqlScript(con, new ClassPathResource("/database/mochi_schema.sql"));
+            ScriptUtils.executeSqlScript(con, new ClassPathResource("/database/mochi_data.sql"));
+        } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	this.uploadShippingProducts();
+        setUpIsDone = true;
+	}
+
+	public void uploadShippingProducts() {
+		String path = "src/test/resources";
+		File file = new File(path);
+
+		pms.writeShippingProductMaster(file.getAbsolutePath() + "/data/product/shipping/Product/update/shipping_product_master.tsv");
+	}
+
+	@Test
+	@Rollback(false)
+	public void whenShippingProductUploadedForUpdate_thenReturnCorrectlyUpdatedShippingProduct_ENGB() {
+		// when
+		Optional<ShippingProductDTO> found = shippingProductService.findByCode(Constants.localeENGB, "TST01");
+
+		// then
+		assertFound_ENGB(found);
+	}
+
+	@Test
+	@Rollback(false)
+	public void whenShippingProductUploadedForUpdate_thenReturnCorrectlyUpdatedShippingProduct_ZHHK() {
+		// when
+		Optional<ShippingProductDTO> found = shippingProductService.findByCode(Constants.localeZHHK, "TST01");
+
+		// then
+		assertFound_ZHHK(found);
+	}
+
+	private void assertFound_ENGB(Optional<ShippingProductDTO> found) {
+		
+		assertNotNull(found);
+		
+		assertTrue(found.isPresent());
+		
+		assertThat(found.get().getProductDesc())
+		.isEqualTo("Test localized shipping Product create");
+		
+	}
+
+	private void assertFound_ZHHK(Optional<ShippingProductDTO> found) {
+		
+		assertNotNull(found);
+		
+		assertTrue(found.isPresent());
+		
+		assertThat(found.get().getProductDesc())
+		.isEqualTo("測試本地化的送貨目的地創建");
+	}
+
+}
