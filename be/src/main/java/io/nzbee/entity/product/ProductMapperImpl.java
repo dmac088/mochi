024ca9433cc78523784f4d57a9cccc04878d12db
dmac088 <danielmackie82@gmail.com>
2021-03-09 20:46:50 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/ProductMapperImpl.java
+++ be/src/main/java/io/nzbee/entity/product/ProductMapperImpl.java
@@ -4,11 +4,13 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import io.nzbee.domain.product.PhysicalProduct;
+import io.nzbee.domain.product.ShippingProduct;
 import io.nzbee.domain.product.Product;
 import io.nzbee.entity.brand.IBrandMapper;
 import io.nzbee.entity.category.product.ICategoryProductMapper;
 import io.nzbee.entity.product.department.IDepartmentMapper;
 import io.nzbee.entity.product.physical.PhysicalProductDTO;
+import io.nzbee.entity.product.shipping.ShippingProductDTO;
 import io.nzbee.entity.promotion.IPromotionMapper;
 
 @Component(value="productMapper")
@@ -47,6 +49,33 @@ public class ProductMapperImpl implements IProductMapper {
 				   	dto.getCategories().stream().map(c -> categoryProductMapper.DTOToDo(c)).collect(Collectors.toList()),
 				   	dto.getPromotions().stream().map(promo -> promotionMapper.DTOToDo(promo)).collect(Collectors.toList()));
 				   
+			return pO;
+		}
+		if(dto instanceof ShippingProductDTO) {
+			Product pO = new ShippingProduct(
+					dto.getProductUPC(),
+				   	dto.getProductCreateDt(),
+				   	dto.getProductStatusCode(),
+				   	dto.getProductDesc(),
+				   	dto.getProductLongDesc(),
+				   	dto.getRetailPrice(),
+				   	dto.getMarkdownPrice(),
+				   	dto.getProductImage(),
+				   	dto.getLocale(),
+				   	dto.getCurrency(),
+				   	dto.isInStock(),
+				   	brandMapper.DTOToDo(dto.getBrand()),
+				   	departmentMapper.DTOToDo(dto.getDepartment()),
+				   	dto.getCategories().stream().map(c -> categoryProductMapper.DTOToDo(c)).collect(Collectors.toList()),
+				   	dto.getPromotions().stream().map(promo -> promotionMapper.DTOToDo(promo)).collect(Collectors.toList()),
+				   	new Double(((ShippingProductDTO) dto).getWeightLimit()),
+				   	new Double(((ShippingProductDTO) dto).getWeightFrom()),
+				   	new Double(((ShippingProductDTO) dto).getWeightTo()),
+				   	((ShippingProductDTO) dto).getShippingDestination().getShippingDestinationCode(),
+				   	((ShippingProductDTO) dto).getShippingDestination().getShippingDestinationDesc(),
+				   	((ShippingProductDTO) dto).getShippingtype().getShippingTypeCode(),
+				   	((ShippingProductDTO) dto).getShippingtype().getShippingTypeDesc()
+			);
 			return pO;
 		}
 		return null;
