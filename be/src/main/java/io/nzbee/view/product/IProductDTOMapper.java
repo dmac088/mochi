a9783ee09adff73556158c46447bde4c162ce9ec
dmac088 <danielmackie82@gmail.com>
2021-03-17 00:10:04 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/view/product/IProductDTOMapper.java
@@ -0,0 +1,8 @@
+package io.nzbee.view.product;
+
+import io.nzbee.domain.product.Product;
+import io.nzbee.view.IMapper;
+
+public interface IProductDTOMapper extends IMapper<ProductDTO, ProductDTO, Product> {
+
+}
---
+++ be/src/main/java/io/nzbee/view/product/ProductDTOMapperImpl.java
@@ -0,0 +1,19 @@
+package io.nzbee.view.product;
+
+import io.nzbee.domain.product.Product;
+
+public class ProductDTOMapperImpl implements IProductDTOMapper {
+
+	@Override
+	public ProductDTO doToDto(Product d) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product dtoToDo(ProductDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
