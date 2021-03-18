7e8e6ab0fe8167b09d6e5a37b7451516eb732b0e
dmac088 <danielmackie82@gmail.com>
2021-03-18 20:47:19 +0800

    asdf

--- be/src/main/java/io/nzbee/view/product/shipping/destination/IShippingDestinationDTOMapper.java
+++ be/src/main/java/io/nzbee/view/product/shipping/destination/IShippingDestinationDTOMapper.java
@@ -1,8 +1,7 @@
 package io.nzbee.view.product.shipping.destination;
 
-import io.nzbee.entity.product.shipping.destination.ShippingDestinationDTO;
 import io.nzbee.view.IMapper;
 
-public interface IShippingDestinationDTOMapper extends IMapper<ShippingDestinationDTO, ShippingDestinationDTO, ShippingDestinationDTO> {
+public interface IShippingDestinationDTOMapper extends IMapper<ShippingDestinationDTO, ShippingDestinationDTO, io.nzbee.entity.product.shipping.destination.ShippingDestinationDTO> {
 
 }
--- be/src/main/java/io/nzbee/view/product/shipping/destination/ShippingDestinationDTOMapperImpl.java
+++ be/src/main/java/io/nzbee/view/product/shipping/destination/ShippingDestinationDTOMapperImpl.java
@@ -7,15 +7,19 @@ import io.nzbee.entity.product.shipping.destination.ShippingDestinationDTO;
 public class ShippingDestinationDTOMapperImpl implements IShippingDestinationDTOMapper {
 
 	@Override
-	public ShippingDestinationDTO doToDto(ShippingDestinationDTO d) {
+	public io.nzbee.view.product.shipping.destination.ShippingDestinationDTO doToDto(ShippingDestinationDTO d) {
+		io.nzbee.view.product.shipping.destination.ShippingDestinationDTO sp = new io.nzbee.view.product.shipping.destination.ShippingDestinationDTO();
+		sp.setProductDestinationCode(d.getShippingDestinationCode());
+		sp.setProductDestinationDesc(d.getShippingDestinationDesc());
+		return sp;
+	}
+
+	@Override
+	public ShippingDestinationDTO dtoToDo(io.nzbee.view.product.shipping.destination.ShippingDestinationDTO dto) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public ShippingDestinationDTO dtoToDo(ShippingDestinationDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 
 }
