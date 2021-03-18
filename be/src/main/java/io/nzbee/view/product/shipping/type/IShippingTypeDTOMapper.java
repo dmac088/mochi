c68bc82a681d72594a9d7a7c91d3aa73fa3c13dd
dmac088 <danielmackie82@gmail.com>
2021-03-18 20:55:00 +0800

    asdf

---
+++ be/src/main/java/io/nzbee/view/product/shipping/type/IShippingTypeDTOMapper.java
@@ -0,0 +1,7 @@
+package io.nzbee.view.product.shipping.type;
+
+import io.nzbee.view.IMapper;
+
+public interface IShippingTypeDTOMapper extends IMapper<ShippingTypeDTO, ShippingTypeDTO, io.nzbee.entity.product.shipping.type.ShippingTypeDTO> {
+
+}
---
+++ be/src/main/java/io/nzbee/view/product/shipping/type/ShippingTypeDTOMapperImpl.java
@@ -0,0 +1,25 @@
+package io.nzbee.view.product.shipping.type;
+
+import org.springframework.stereotype.Component;
+import io.nzbee.entity.product.shipping.type.ShippingTypeDTO;
+
+@Component
+public class ShippingTypeDTOMapperImpl implements IShippingTypeDTOMapper {
+
+	@Override
+	public io.nzbee.view.product.shipping.type.ShippingTypeDTO doToDto(ShippingTypeDTO d) {
+		io.nzbee.view.product.shipping.type.ShippingTypeDTO sp = new io.nzbee.view.product.shipping.type.ShippingTypeDTO();
+		sp.setShippingTypeCode(d.getShippingTypeCode());
+		sp.setShippingTypeDesc(d.getShippingTypeDesc());
+		return sp;
+	}
+
+	@Override
+	public ShippingTypeDTO dtoToDo(io.nzbee.view.product.shipping.type.ShippingTypeDTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
