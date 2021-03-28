c59eae3f2153f358241a73ef5100488a19d44c33
dmac088 <danielmackie82@gmail.com>
2021-03-28 10:45:31 +0800

    asdf

---
+++ be/.apt_generated/io/nzbee/entity/promotion/type/PromotionTypeEntity_.java
@@ -0,0 +1,13 @@
+package io.nzbee.entity.promotion.type;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(PromotionTypeEntity.class)
+public abstract class PromotionTypeEntity_ {
+
+
+
+}
+
--- be/src/main/java/io/nzbee/entity/promotion/type/PromotionTypeEntity.java
+++ be/src/main/java/io/nzbee/entity/promotion/type/PromotionTypeEntity.java
@@ -1,5 +1,10 @@
 package io.nzbee.entity.promotion.type;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "promotion_type", schema = "mochi")
 public class PromotionTypeEntity {
 
 }
