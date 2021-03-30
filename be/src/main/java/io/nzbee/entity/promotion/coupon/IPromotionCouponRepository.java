115ef680affb5be476c2bfaeaed455b8c7ab89e1
dmac088 <danielmackie82@gmail.com>
2021-03-30 12:08:58 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/promotion/coupon/IPromotionCouponRepository.java
+++ be/src/main/java/io/nzbee/entity/promotion/coupon/IPromotionCouponRepository.java
@@ -1,8 +1,6 @@
 package io.nzbee.entity.promotion.coupon;
 
-
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import io.nzbee.entity.promotion.PromotionDTO;
@@ -11,14 +9,20 @@ public interface IPromotionCouponRepository extends CrudRepository<PromotionCoup
 
 	@Query(
 			"SELECT new io.nzbee.entity.promotion.coupon.PromotionCouponDTO (" +
-			"" +
-			"" +
-			"" +
+			" pce.promotionId," +
+			" pce.promotionCode," +
+			" pce.promotionDesc," +
+			" pce.promotionStartDate," +
+			" pce.promotionEndDate," +
+			" pce.locale, " +
+			" pce.couponCode" +
+			") " +
 			"FROM PromotionCouponEntity pce " +
-			"WHERE pce.promotionCouponCode = :couponCode "
+			"WHERE pce.promotionCouponCode = :couponCode " + 
+			"AND pce.locale = :locale "
 			
 	)
-	Optional<PromotionDTO> findByPromotionCouponCode(String couponCode);
+	Optional<PromotionDTO> findByPromotionCouponCode(String locale, String couponCode);
 	
 }
 
