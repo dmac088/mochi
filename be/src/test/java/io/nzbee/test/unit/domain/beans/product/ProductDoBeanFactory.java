9a7a6ed1cee87e9f9667671c4c617c828eee8d5b
dmac088 <danielmackie82@gmail.com>
2021-04-11 23:00:27 +0800

    asdf

--- be/src/test/java/io/nzbee/test/unit/domain/beans/product/ProductDoBeanFactory.java
+++ be/src/test/java/io/nzbee/test/unit/domain/beans/product/ProductDoBeanFactory.java
@@ -11,7 +11,6 @@ import io.nzbee.domain.category.ProductCategory;
 import io.nzbee.domain.product.Product;
 import io.nzbee.domain.product.physical.PhysicalProduct;
 import io.nzbee.domain.promotion.Promotion;
-import io.nzbee.test.unit.domain.beans.brand.IBrandDoBeanFactory;
 import io.nzbee.test.unit.domain.beans.category.ICategoryDoBeanFactory;
 import io.nzbee.test.unit.domain.beans.department.IDepartmentDoBeanFactory;
 import io.nzbee.test.unit.domain.beans.promotion.IPromotionDoBeanFactory;
@@ -20,9 +19,6 @@ import io.nzbee.test.unit.domain.beans.promotion.IPromotionDoBeanFactory;
 @Profile(value = "ut")
 public class ProductDoBeanFactory implements IProductDoBeanFactory {
 
-	@Autowired
-	private IBrandDoBeanFactory brandDoBeanFactory;
-	
 	@Autowired 
 	private IDepartmentDoBeanFactory departmentDoBeanFactory;
 	
@@ -47,7 +43,6 @@ public class ProductDoBeanFactory implements IProductDoBeanFactory {
 								   Constants.currencyHKD,
 								   true,
 								   new Double(0.72),
-								   brandDoBeanFactory.getBean(),
 								   departmentDoBeanFactory.getBean(),
 								   new ArrayList<ProductCategory>(Arrays.asList(categoryDoBeanFactory.getProductCategoryDoBean())),
 								   new ArrayList<Promotion>(Arrays.asList(promotionDoBeanFactory.getBean())));
@@ -68,7 +63,6 @@ public class ProductDoBeanFactory implements IProductDoBeanFactory {
 								   Constants.currencyHKD,
 								   false,
 								   new Double(0.52),
-								   brandDoBeanFactory.getBean(),
 								   departmentDoBeanFactory.getBean(),
 								   new ArrayList<ProductCategory>(Arrays.asList(categoryDoBeanFactory.getProductCategoryDoBean())),
 								   new ArrayList<Promotion>(Arrays.asList(promotionDoBeanFactory.getBean())));
