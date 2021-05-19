adae0e103a94697cf0ba26daf3a85538e216466b
dannymacknz <danielmackie82@gmail.com>
2021-05-19 18:20:32 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/category/CategoryServiceImpl.java
+++ be/src/main/java/io/nzbee/entity/category/CategoryServiceImpl.java
@@ -2,8 +2,6 @@ package io.nzbee.entity.category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import io.nzbee.search.IFacetService;
 
@@ -15,16 +13,6 @@ public class CategoryServiceImpl implements ICategoryService, IFacetService {
 	@Autowired
 	@Qualifier(value = "categoryEntityPostgresDao")
 	private ICategoryDao categoryDAO;
-	
-	@Override
-	@Caching(evict = {
-			@CacheEvict(cacheNames = CACHE_NAME + "Other", 	allEntries = true),
-			@CacheEvict(cacheNames = CACHE_NAME, key="#category.categoryId.toString()"),
-			@CacheEvict(cacheNames = CACHE_NAME, key="#category.categoryCode"),
-			@CacheEvict(cacheNames = CACHE_NAME, key="#category.locale + \", \" + #category.categoryId.toString()"),
-			@CacheEvict(cacheNames = CACHE_NAME, key="#category.locale + \", \" + #category.categoryCode")
-	})
-	
 
 
 	public String getFacetField() {
