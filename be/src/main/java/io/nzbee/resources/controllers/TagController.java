c2b33d60a97cd52da3b0f71a12152b355667e87f
dannymacknz <danielmackie82@gmail.com>
2021-05-23 16:38:20 +0800

    asdf

--- be/src/main/java/io/nzbee/resources/controllers/TagController.java
+++ be/src/main/java/io/nzbee/resources/controllers/TagController.java
@@ -14,14 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import io.nzbee.resources.tag.browse.facet.TagBrowseFacetResource;
 import io.nzbee.resources.tag.browse.facet.TagBrowseFacetResourceAssembler;
-import io.nzbee.resources.tag.search.facet.TagSearchFacetResource;
-import io.nzbee.resources.tag.search.facet.TagSearchFacetResourceAssembler;
-import io.nzbee.search.facet.EntityFacet;
 import io.nzbee.search.facet.IFacet;
-import io.nzbee.search.facet.IFacetMapper;
 import io.nzbee.view.product.tag.facet.ITagFacetViewService;
 import io.nzbee.view.product.tag.facet.TagFacetView;
 
@@ -35,16 +30,10 @@ public class TagController {
     @Autowired
     private ITagFacetViewService tagService;
     
-	@Autowired
-	private IFacetMapper<TagFacetView> facetMapper;
-    
     @Autowired
     private TagBrowseFacetResourceAssembler tagResourceAssembler;
-    
-	@Autowired
-	private TagSearchFacetResourceAssembler tagFacetResourceAssembler;
 	
-	@PostMapping("/Tag/{locale}/{currency}/category/{categoryCode}")
+	@PostMapping("/Tag/Facet/{locale}/{currency}/Category/Code/{categoryCode}")
     public ResponseEntity<CollectionModel<TagBrowseFacetResource>> getTags(@PathVariable String locale, 
     																@PathVariable String currency, 
     																@PathVariable String categoryCode,
@@ -70,31 +59,6 @@ public class TagController {
     	return ResponseEntity.ok(tagResourceAssembler.toCollectionModel(collection));
     }
 	
-	@PostMapping("/Tag/Facet/{locale}/{currency}/Category/Code/{categoryCode}")
-    public ResponseEntity<CollectionModel<TagSearchFacetResource>> getTagFacets(@PathVariable String locale, 
-    																 @PathVariable String currency, 
-    																 @PathVariable String categoryCode,
-    																 @RequestBody Set<IFacet> selectedFacets) {
-    	
-    	LOGGER.debug("call TagController.getTagFacets with parameters: {}, {}, {}", locale, currency, categoryCode);
-    	 
-    	Optional<String> oMaxPrice = selectedFacets.stream().filter(p -> p.getFacetingName().equals("price")).map(p -> p.getValue()).findFirst();
-    	Double maxPrice = null;
-    	if(oMaxPrice.isPresent()) {
-    		maxPrice = Double.valueOf(oMaxPrice.get());
-    	}
-    	
-    	final Set<EntityFacet> collection =
-    			tagService.findAll(	 locale, 
-    								 currency, 
-    	 							 categoryCode,
-    								 selectedFacets.stream().filter(f -> f.getFacetingName().equals("category")).map(f -> f.getValue()).collect(Collectors.toSet()),
-    								 selectedFacets.stream().filter(f -> f.getFacetingName().equals("brand")).map(f -> f.getValue()).collect(Collectors.toSet()),
-    								 maxPrice    							  
-    			).stream().map(c -> facetMapper.toEntityFacet(c)).collect(Collectors.toSet());;
-    	
-    	return ResponseEntity.ok(tagFacetResourceAssembler.toCollectionModel(collection));
-    }
-	
+
 	
 }
--- be/src/main/java/io/nzbee/resources/tag/browse/facet/TagBrowseFacetResource.java
+++ be/src/main/java/io/nzbee/resources/tag/browse/facet/TagBrowseFacetResource.java
@@ -3,9 +3,11 @@ package io.nzbee.resources.tag.browse.facet;
 import org.springframework.hateoas.RepresentationModel;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 
 import io.nzbee.view.product.tag.facet.TagFacetView;
 
+@JsonRootName("tagFacets")
 public class TagBrowseFacetResource  extends RepresentationModel<TagBrowseFacetResource> {
 
 	private final TagFacetView data;
