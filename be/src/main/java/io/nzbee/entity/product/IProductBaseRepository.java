b2f4034b9f574a8e7ea70fa88ebed67641ce0bf9
dannymacknz <danielmackie82@gmail.com>
2021-06-20 13:30:32 +0800

    asdf

--- be/src/main/java/io/nzbee/entity/product/IProductBaseRepository.java
+++ be/src/main/java/io/nzbee/entity/product/IProductBaseRepository.java
@@ -1,12 +1,22 @@
 package io.nzbee.entity.product;
 
 import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
 public interface IProductBaseRepository<P extends ProductEntity>  extends CrudRepository<P, Long> {
 
+	@Query(
+	"SELECT p,"
+	+ "		pa"
+	+ "FROM ProductEntity p "
+	+ " JOIN FETCH ProductAttributeEntity pa "		
+	)
+	Optional<P> find(String productCode);
+	
 	List<P> findAll();
 	
 }
